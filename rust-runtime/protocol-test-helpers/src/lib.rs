5be189a707634240ada480e416b3a759cb197bd9
Russell Cohen <rcoh@amazon.com>
2020-11-02 15:23:17 -0500

    Fix Rust code formatting

--- rust-runtime/protocol-test-helpers/src/lib.rs
+++ rust-runtime/protocol-test-helpers/src/lib.rs
@@ -9,7 +9,10 @@ pub enum ProtocolTestFailure {
     },
 }
 
-pub fn validate_query_string<B>(request: &Request<B>, params: &[&str]) -> Result<(), ProtocolTestFailure> {
+pub fn validate_query_string<B>(
+    request: &Request<B>,
+    params: &[&str],
+) -> Result<(), ProtocolTestFailure> {
     let query_str = request.uri().query().unwrap_or_default();
     let request_params: HashSet<&str> = query_str.split('&').collect();
     let expected: HashSet<&str> = params.iter().copied().collect();
@@ -35,12 +38,11 @@ mod tests {
 
     #[test]
     fn test_validate_empty_query_string() {
-        let request = Request::builder()
-            .uri("/foo")
-            .body(())
-            .unwrap();
+        let request = Request::builder().uri("/foo").body(()).unwrap();
         validate_query_string(&request, &vec![]).expect("no required params should pass");
-        validate_query_string(&request, &vec!["a"]).err().expect("no params provided");
+        validate_query_string(&request, &vec!["a"])
+            .err()
+            .expect("no params provided");
     }
 
     #[test]
@@ -50,15 +52,23 @@ mod tests {
             .body(())
             .unwrap();
         validate_query_string(&request, &vec!["a=b"]).expect("a=b is in the query string");
-        validate_query_string(&request, &vec!["c", "a=b"]).expect("both params are in the query string");
+        validate_query_string(&request, &vec!["c", "a=b"])
+            .expect("both params are in the query string");
         validate_query_string(&request, &vec!["a=b", "c", "d=efg", "hello=a%20b"])
             .expect("all params are in the query string");
         validate_query_string(&request, &vec![]).expect("no required params should pass");
 
-        validate_query_string(&request, &vec!["a"]).err().expect("no parameter should match");
-        validate_query_string(&request, &vec!["a=bc"]).err().expect("no parameter should match");
-        validate_query_string(&request, &vec!["a=bc"]).err().expect("no parameter should match");
-        validate_query_string(&request, &vec!["hell=a%20"]).err().expect("no parameter should match");
-
+        validate_query_string(&request, &vec!["a"])
+            .err()
+            .expect("no parameter should match");
+        validate_query_string(&request, &vec!["a=bc"])
+            .err()
+            .expect("no parameter should match");
+        validate_query_string(&request, &vec!["a=bc"])
+            .err()
+            .expect("no parameter should match");
+        validate_query_string(&request, &vec!["hell=a%20"])
+            .err()
+            .expect("no parameter should match");
     }
 }
