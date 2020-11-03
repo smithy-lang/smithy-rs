53b0e398ef7eebcf3f2561863340af5b1255a0c6
Russell Cohen <rcoh@amazon.com>
2020-11-03 18:37:52 -0500

    We don't need REPO_ROOT anymore

--- test.sh
+++ test.sh
@@ -5,10 +5,6 @@
 #
 
 set -e
-# Used by codegen-test/smithy-build.json
-declare REPO_ROOT
-REPO_ROOT="$(git rev-parse --show-toplevel)"
-export REPO_ROOT
 ./gradlew test
 ./gradlew ktlintFormat
 ./gradlew ktlint
