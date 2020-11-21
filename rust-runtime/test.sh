8fedc263f007db0209b6411f5193d3ebc92d6d22
Russell Cohen <rcoh@amazon.com>
2020-11-20 21:35:52 -0500

    Fixup the test script now that it's also a gradle project

--- rust-runtime/test.sh
+++ rust-runtime/test.sh
@@ -3,8 +3,10 @@
 set -e
 for crate in "$(dirname "$0")"/*/
 do
-  echo "Testing $crate"
-  (cd "$crate" && cargo fmt -- --check)
-  (cd "$crate" && cargo clippy -- -D warnings)
-  (cd "$crate" && cargo test)
+  if [ -d "$crate" ] && [ -f "$crate/Cargo.toml" ]; then
+    echo "Testing $crate"
+    (cd "$crate" && cargo fmt -- --check)
+    (cd "$crate" && cargo clippy -- -D warnings)
+    (cd "$crate" && cargo test)
+  fi
 done
