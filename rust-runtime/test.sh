efdca4484c25d7c0c107d3e170dfaf899e36de33
Russell Cohen <russell.r.cohen@gmail.com>
2020-11-25 18:49:38 -0500

    Add uuidv4 implementation & rust-runtime as JAR (#55)
    * Add uuidv4 implementation & rust-runtime as JAR
    
    * Add inlineable crate
    
    * Flesh out dependency test

--- rust-runtime/test.sh
+++ rust-runtime/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Currently avoiding creating a workspace of the runtime packages
+
 set -e
 for crate in "$(dirname "$0")"/*/
 do
-  echo "Testing $crate"
-  (cd "$crate" && cargo fmt -- --check)
-  (cd "$crate" && cargo clippy -- -D warnings)
-  (cd "$crate" && cargo test)
+  if [ -d "$crate" ] && [ -f "$crate/Cargo.toml" ]; then
+    echo "Testing $crate"
+    (cd "$crate" && cargo fmt -- --check)
+    (cd "$crate" && cargo clippy -- -D warnings)
+    (cd "$crate" && cargo test)
+  fi
 done
