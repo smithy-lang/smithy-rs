02d0db5d292cf08f9a3f31297e4665a9bf3a855a
Russell Cohen <russell.r.cohen@gmail.com>
2020-12-30 10:07:49 -0500

    Generate deserializers dynamically for nested types (#95)
    This code replaces the hand-written deserializers that were used to serialize types like Instant & Blob with autogenerated code. A follow-on diff will do the same thing for serializers.
    
    Once sufficiently developed (and if desired) this would allow us to completely remove our use of Serde-derived serializers with serializers & deserializers generated at codegen time.

--- codegen/src/test/kotlin/software/amazon/smithy/rust/codegen/smithy/protocols/SerializerBuilderTest.kt
+++ codegen/src/test/kotlin/software/amazon/smithy/rust/codegen/smithy/protocols/SerializerBuilderTest.kt
@@ -8,8 +8,11 @@ package software.amazon.smithy.rust.codegen.smithy.protocols
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import software.amazon.smithy.model.traits.TimestampFormatTrait
+import software.amazon.smithy.rust.codegen.lang.rust
 import software.amazon.smithy.rust.codegen.util.lookup
+import software.amazon.smithy.rust.testutil.TestWorkspace
 import software.amazon.smithy.rust.testutil.asSmithyModel
+import software.amazon.smithy.rust.testutil.compileAndTest
 import software.amazon.smithy.rust.testutil.testSymbolProvider
 
 internal class SerializerBuilderTest {
@@ -30,5 +33,28 @@ internal class SerializerBuilderTest {
         serializerBuilder.serializerFor(model.lookup("test#S\$b"))!!.name shouldBe "stdoptionoptionblob_ser"
         serializerBuilder.deserializerFor(model.lookup("test#S\$b"))!!.name shouldBe "stdoptionoptionblob_deser"
         serializerBuilder.deserializerFor(model.lookup("test#S\$s")) shouldBe null
+    }
+
+    @Test
+    fun `generate deserializers that compile`() {
+        val serializerBuilder = SerializerBuilder(provider, model, TimestampFormatTrait.Format.EPOCH_SECONDS)
+        val timestamp = serializerBuilder.deserializerFor(model.lookup("test#S\$ts"))!!
+        val blob = serializerBuilder.deserializerFor(model.lookup("test#S\$b"))!!
+        val writer = TestWorkspace.testProject(provider)
+        writer.useFileWriter("src/lib.rs", "crate::lib") {
+            it.rust(
+                """
+                fn foo() {
+                    // commented out so that we generate the import & inject the serializer
+                    // but I don't want to deal with getting the argument to compile
+                    // let _ = #T();
+                    // let _ = #T();
+                }
+            """,
+                timestamp, blob
+            )
+        }
+        println("file:///${writer.baseDir}/src/serde_util.rs")
+        writer.compileAndTest()
     }
 }
