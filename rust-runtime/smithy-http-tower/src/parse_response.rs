19d35b58700c61264275106a2663711c05bb7d8f
Russell Cohen <rcoh@amazon.com>
2021-03-10 17:28:37 -0500

    Remove unused From<Bytes> bound

--- rust-runtime/smithy-http-tower/src/parse_response.rs
+++ rust-runtime/smithy-http-tower/src/parse_response.rs
@@ -4,7 +4,6 @@
  */
 
 use crate::SendOperationError;
-use bytes::Bytes;
 use smithy_http::middleware::load_response;
 use smithy_http::operation;
 use smithy_http::operation::Operation;
@@ -73,7 +72,7 @@ impl<S, O, T, E, B, R> tower::Service<operation::Operation<O, R>> for ParseRespo
 where
     S: Service<operation::Request, Response = http::Response<B>, Error = SendOperationError>,
     S::Future: 'static,
-    B: http_body::Body + Unpin + From<Bytes> + 'static,
+    B: http_body::Body + Unpin + 'static,
     B::Error: Into<BoxError>,
     O: ParseHttpResponse<B, Output = Result<T, E>> + 'static,
     E: Error,
