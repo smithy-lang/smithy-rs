f3975130e254d8bc826a7bc14dd4c9fb1a37ae4d
Russell Cohen <russell.r.cohen@gmail.com>
2021-04-01 11:35:33 -0400

    Some cleanups

--- aws/sdk/examples/polly-helloworld/src/main.rs
+++ aws/sdk/examples/polly-helloworld/src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>{
+async fn main() -> Result<(), Box<dyn Error>> {
     let client = polly::fluent::Client::from_env();
     let mut tok = None;
     // manually paginate
@@ -12,10 +12,14 @@ async fn main() -> Result<(), Box<dyn Error>>{
         }
         let voices = req.send().await?;
         for voice in voices.voices.unwrap_or_default() {
-            println!("I can speak as: {} ({:?}) in these languages: {:?}", voice.name.unwrap(), voice.gender.unwrap(), voice.language_name.unwrap());
+            println!(
+                "I can speak as: {} in {:?}",
+                voice.name.unwrap(),
+                voice.language_name.unwrap()
+            );
         }
         if voices.next_token == None {
-            break
+            break;
         } else {
             tok = voices.next_token;
         }
