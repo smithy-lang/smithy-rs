cae7eeb5bd86adb6c65e476f4735d40167b4dec8
Russell Cohen <russell.r.cohen@gmail.com>
2021-04-01 12:13:26 -0400

    CR Feedback

--- aws/sdk/examples/polly-generate-speech/src/main.rs
+++ aws/sdk/examples/polly-generate-speech/src/main.rs
@@ -1,18 +1,25 @@
+use polly::model::{Engine, OutputFormat, VoiceId};
 use std::error::Error;
-use polly::model::{VoiceId, OutputFormat};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let client = polly::fluent::Client::from_env();
-    let resp = client.synthesize_speech()
-        .voice_id(VoiceId::Carla)
+    let resp = client
+        .synthesize_speech()
+        .voice_id(VoiceId::Emma)
+        .engine(Engine::Neural)
         .output_format(OutputFormat::Mp3)
-        .text("hello, I am polly!").send().await?;
+        .text("Hello, I am polly!")
+        .send()
+        .await?;
     let audio = resp.audio_stream.expect("data should be included");
     let mut file = File::create("audio.mp3").await?;
     file.write_all(audio.as_ref()).await?;
-    println!("Audio written to audio.mp3");
+    println!(
+        "Audio written to audio.mp3 ({} bytes)",
+        audio.as_ref().len()
+    );
     Ok(())
 }
--- aws/sdk/examples/polly-helloworld/src/main.rs
+++ aws/sdk/examples/polly-helloworld/src/main.rs
@@ -1,27 +1,44 @@
+use polly::model::{Engine, Voice};
 use std::error::Error;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     let client = polly::fluent::Client::from_env();
     let mut tok = None;
+    let mut voices: Vec<Voice> = vec![];
     // Below is an an example of how pagination can be implemented manually.
     loop {
         let mut req = client.describe_voices();
         if let Some(tok) = tok {
             req = req.next_token(tok);
         }
-        let voices = req.send().await?;
-        for voice in voices.voices.unwrap_or_default() {
+        let resp = req.send().await?;
+        for voice in resp.voices.unwrap_or_default() {
             println!(
                 "I can speak as: {} in {:?}",
-                voice.name.unwrap(),
-                voice.language_name.unwrap()
+                voice.name.as_ref().unwrap(),
+                voice.language_name.as_ref().unwrap()
             );
+            voices.push(voice);
         }
-        tok = match voices.next_token {
+        tok = match resp.next_token {
             Some(next) => Some(next),
             None => break,
         };
     }
+
+    println!(
+        "Voices supporting a neural engine: {:?}",
+        voices
+            .iter()
+            .filter(|voice| voice
+                .supported_engines
+                .as_ref()
+                .map(|v| v.as_slice())
+                .unwrap_or_default()
+                .contains(&Engine::Neural))
+            .map(|voice| voice.id.as_ref().unwrap())
+            .collect::<Vec<_>>()
+    );
     Ok(())
 }
