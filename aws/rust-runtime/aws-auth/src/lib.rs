60350162e41144b5310d16ce056edbe881f9535f
Russell Cohen <russell.r.cohen@gmail.com>
2021-04-07 11:52:09 -0400

    Zero credentials when dropped

--- aws/rust-runtime/aws-auth/src/lib.rs
+++ aws/rust-runtime/aws-auth/src/lib.rs
@@ -6,15 +6,19 @@ use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
 use std::sync::Arc;
 use std::time::SystemTime;
+use zeroize::Zeroize;
 
 /// AWS SDK Credentials
 ///
 /// An opaque struct representing credentials that may be used in an AWS SDK, modeled on
 /// the [CRT credentials implementation](https://github.com/awslabs/aws-c-auth/blob/main/source/credentials.c).
 ///
-/// Future design note: It may be desirable to make Credentials cheap to clone because they are cloned frequently.
+/// When `Credentials` is dropped, its contents are zeroed in memory. Credentials uses an interior Arc to ensure
+/// that even when cloned, credentials don't exist in multiple memory locations.
 #[derive(Clone)]
-pub struct Credentials {
+pub struct Credentials(Arc<Inner>);
+
+struct Inner {
     access_key_id: String,
     secret_access_key: String,
     session_token: Option<String>,
@@ -31,41 +35,70 @@ pub struct Credentials {
     provider_name: &'static str,
 }
 
+impl Drop for Inner {
+    fn drop(&mut self) {
+        self.access_key_id.zeroize();
+        self.secret_access_key.zeroize();
+        if let Some(tok) = &mut self.session_token {
+            tok.zeroize();
+        }
+    }
+}
+
 impl Debug for Credentials {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let mut creds = f.debug_struct("Credentials");
-        creds.field("provider_name", &self.provider_name);
+        creds.field("provider_name", &self.0.provider_name);
         creds.finish()
     }
 }
 
 const STATIC_CREDENTIALS: &str = "Static";
 impl Credentials {
+    pub fn new(
+        access_key_id: impl Into<String>,
+        secret_access_key: impl Into<String>,
+        session_token: Option<String>,
+        expires_after: Option<SystemTime>,
+        provider_name: &'static str,
+    ) -> Self {
+        Credentials(Arc::new(Inner {
+            access_key_id: access_key_id.into(),
+            secret_access_key: secret_access_key.into(),
+            session_token,
+            expires_after,
+            provider_name,
+        }))
+    }
+
     pub fn from_keys(
         access_key_id: impl Into<String>,
         secret_access_key: impl Into<String>,
         session_token: Option<String>,
     ) -> Self {
-        Credentials {
-            access_key_id: access_key_id.into(),
-            secret_access_key: secret_access_key.into(),
+        Self::new(
+            access_key_id,
+            secret_access_key,
             session_token,
-            expires_after: None,
-
-            provider_name: STATIC_CREDENTIALS,
-        }
+            None,
+            STATIC_CREDENTIALS,
+        )
     }
 
     pub fn access_key_id(&self) -> &str {
-        &self.access_key_id
+        &self.0.access_key_id
     }
 
     pub fn secret_access_key(&self) -> &str {
-        &self.secret_access_key
+        &self.0.secret_access_key
+    }
+
+    pub fn expiry(&self) -> Option<SystemTime> {
+        self.0.expires_after
     }
 
     pub fn session_token(&self) -> Option<&str> {
-        self.session_token.as_deref()
+        self.0.session_token.as_deref()
     }
 }
 
@@ -119,4 +152,16 @@ impl ProvideCredentials for Credentials {
 
 pub fn set_provider(config: &mut PropertyBag, provider: Arc<dyn ProvideCredentials>) {
     config.insert(provider);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Credentials;
+
+    fn assert_send_sync<T: Send + Sync>() {}
+
+    #[test]
+    fn creds_are_send_sync() {
+        assert_send_sync::<Credentials>()
+    }
 }
