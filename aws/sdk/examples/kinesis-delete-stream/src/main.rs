fb2ec6de8236f5fd34199e10aa0054a57ac0d448
Doug Schwartz <dougsch@amazon.com>
2021-04-19 10:29:30 -0700

    Updated Kinesis DeleteStream code example

--- aws/sdk/examples/kinesis-delete-stream/src/main.rs
+++ aws/sdk/examples/kinesis-delete-stream/src/main.rs
@@ -3,9 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use std::env;
+use std::process;
 
 use kinesis::{Client, Config, Region};
+
+use aws_types::region::{EnvironmentProvider, ProvideRegion};
 
 use structopt::StructOpt;
 use tracing_subscriber::fmt::format::FmtSpan;
@@ -13,37 +15,36 @@ use tracing_subscriber::fmt::SubscriberBuilder;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    #[structopt(default_value = "", short, long)]
-    region: String,
+    /// The region
+    #[structopt(short, long)]
+    region: Option<String>,
 
+    /// The name of the stream to delete
     #[structopt(short, long)]
     name: String,
 
+    /// Whether to display additional information
     #[structopt(short, long)]
     verbose: bool,
 }
 
 #[tokio::main]
 async fn main() {
-    let mut opt = Opt::from_args();
+    let Opt {
+        name,
+        region,
+        verbose,
+    } = Opt::from_args();
 
-    let mut default_region = match env::var("AWS_DEFAULT_REGION") {
-        Ok(val) => val,
-        Err(_e) => String::from(""),
-    };
+    let region = EnvironmentProvider::new()
+        .region()
+        .or_else(|| region.as_ref().map(|region| Region::new(region.clone())))
+        .unwrap_or_else(|| Region::new("us-west-2"));
 
-    if default_region == "" {
-        default_region = String::from("us-west-2");
-    }
-
-    if opt.region == "" {
-        opt.region = default_region;
-    }
-
-    if opt.verbose {
-        println!("kinesis client version: {}\n", kinesis::PKG_VERSION);
-        println!("Region:      {}", opt.region);
-        println!("Stream name: {}", opt.name);
+    if verbose {
+        println!("Kinesis client version: {}\n", kinesis::PKG_VERSION);
+        println!("Region:      {:?}", &region);
+        println!("Stream name: {}", name);
 
         SubscriberBuilder::default()
             .with_env_filter("info")
@@ -51,20 +52,16 @@ async fn main() {
             .init();
     }
 
-    let r = &opt.region;
-    let n = &opt.name;
-
-    let config = Config::builder()
-        .region(Region::new(String::from(r)))
-        .build();
+    let config = Config::builder().region(region).build();
 
     let client = Client::from_conf_conn(config, aws_hyper::conn::Standard::https());
 
-    match client.delete_stream().stream_name(n).send().await {
-        Ok(_) => println!("\nDeleted stream {} from {} region.\n", n, opt.region),
+    match client.delete_stream().stream_name(name).send().await {
+        Ok(_) => println!("Deleted stream."),
         Err(e) => {
-            println!("Got an error deleting the stream {}:", n);
+            println!("Got an error deleting the stream:");
             println!("{:?}", e);
+            process::exit(1);
         }
     };
 }
