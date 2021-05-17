baccf2a680fc89594ea987c25cc98a65b34617a0
Russell Cohen <rcoh@amazon.com>
2021-05-17 11:23:11 -0400

    Write XML Encoding Abstractions
    This commit creates an XML encode & escaping abstraction that allows "fearless code generation"
    (invalid XML is a compile error).

---
+++ rust-runtime/smithy-xml/tests/handwritten_serializers.rs
@@ -0,0 +1,100 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+use protocol_test_helpers::{validate_body, MediaType};
+use smithy_xml::encode;
+use smithy_xml::encode::TagWriter;
+
+// @namespace http://www.example.com
+struct WithNamespace {
+    foo: String,
+    bar: String,
+}
+
+struct Nested {
+    // @xmlAttribute("a")
+    a: String,
+    inner: WithNamespace,
+}
+
+fn serialize_nested(nested: &Nested) -> Result<String, smithy_xml::encode::Error> {
+    let mut out = String::new();
+    {
+        let mut writer = encode::XmlWriter::new(&mut out);
+        let mut start_el = writer.start_el("Nested");
+        start_el.write_attribute("a", &nested.a);
+        let mut tag = start_el.finish();
+        let mut inner = tag.start_el("inner").finish();
+        with_namespace_inner(&mut inner, &nested.inner);
+    }
+    Ok(out)
+}
+
+fn serialize_with_namespace(
+    with_namespace: &WithNamespace,
+) -> Result<String, smithy_xml::encode::Error> {
+    let mut out = String::new();
+    {
+        let mut writer = encode::XmlWriter::new(&mut out);
+        let mut root = writer.start_el("MyStructure");
+        root.write_ns("http://foo.com");
+        let mut tag = root.finish();
+        with_namespace_inner(&mut tag, with_namespace);
+        tag.finish();
+    }
+
+    Ok(out)
+}
+
+fn with_namespace_inner(tag: &mut TagWriter, with_namespace: &WithNamespace) {
+    let mut foo_tag = tag.start_el("foo").finish();
+    foo_tag.data(&with_namespace.foo);
+    foo_tag.finish();
+
+    let mut bar_tag = tag.start_el("bar").finish();
+    bar_tag.data(&with_namespace.bar);
+    bar_tag.finish();
+}
+
+#[test]
+fn test_serialize_with_namespace() {
+    let inp = WithNamespace {
+        foo: "FooFoo".to_string(),
+        bar: "BarBar".to_string(),
+    };
+
+    validate_body(
+        serialize_with_namespace(&inp).unwrap(),
+        r#"<MyStructure xmlns="http://foo.com">
+            <foo>FooFoo</foo>
+            <bar>BarBar</bar>
+        </MyStructure>"#,
+        MediaType::Xml,
+    )
+    .expect("correct XML should be generated");
+}
+
+#[test]
+fn test_serialize_nested() {
+    let inp = Nested {
+        a: "avalue".to_string(),
+        inner: WithNamespace {
+            foo: "foovalue".to_string(),
+            bar: "barvalue".to_string(),
+        },
+    };
+
+    validate_body(
+        serialize_nested(&inp).unwrap(),
+        r#"<Nested a="avalue">
+            <inner>
+                <foo>foovalue</foo>
+                <bar>barvalue</bar>
+            </inner>
+        </Nested>"#,
+        MediaType::Xml,
+    )
+    .expect("correct XML should be generated");
+}
