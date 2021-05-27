21660455a357ef2b3853d89131bbaf225e69868b
John DiSanti <jdisanti@amazon.com>
2021-05-26 17:56:07 -0700

    Escape control characters in JSON strings

--- rust-runtime/smithy-json/src/escape.rs
+++ rust-runtime/smithy-json/src/escape.rs
@@ -5,35 +5,38 @@
 
 use std::borrow::Cow;
 
-const ESCAPES: &[char] = &['"', '\\', '\u{08}', '\u{0C}', '\n', '\r', '\t'];
-
 /// Escapes a string for embedding in a JSON string value.
 pub fn escape_string(value: &str) -> Cow<str> {
-    if !value.contains(ESCAPES) {
-        return Cow::Borrowed(value);
+    let bytes = value.as_bytes();
+    for (index, byte) in bytes.iter().enumerate() {
+        match byte {
+            0..=0x1F | b'"' | b'\\' => {
+                return Cow::Owned(escape_string_inner(&bytes[0..index], &bytes[index..]))
+            }
+            _ => {}
+        }
     }
+    Cow::Borrowed(value)
+}
 
-    let mut escaped = String::new();
-    let (mut last, end) = (0, value.len());
-    for (index, chr) in value
-        .char_indices()
-        .filter(|(_index, chr)| ESCAPES.contains(chr))
-    {
-        escaped.push_str(&value[last..index]);
-        escaped.push_str(match chr {
-            '"' => "\\\"",
-            '\\' => "\\\\",
-            '\u{08}' => "\\b",
-            '\u{0C}' => "\\f",
-            '\n' => "\\n",
-            '\r' => "\\r",
-            '\t' => "\\t",
-            _ => unreachable!(),
-        });
-        last = index + 1;
+fn escape_string_inner(start: &[u8], rest: &[u8]) -> String {
+    let mut escaped = start.to_vec();
+    for byte in rest {
+        match byte {
+            b'"' => escaped.extend("\\\"".bytes()),
+            b'\\' => escaped.extend("\\\\".bytes()),
+            0x08 => escaped.extend("\\b".bytes()),
+            0x0C => escaped.extend("\\f".bytes()),
+            b'\n' => escaped.extend("\\n".bytes()),
+            b'\r' => escaped.extend("\\r".bytes()),
+            b'\t' => escaped.extend("\\t".bytes()),
+            0..=0x1F => escaped.extend(format!("\\u{:04x}", byte).bytes()),
+            _ => escaped.push(*byte),
+        }
     }
-    escaped.push_str(&value[last..end]);
-    Cow::Owned(escaped)
+    // Our input was originally valid UTF-8, and we didn't do anything to invalidate it
+    debug_assert!(String::from_utf8(escaped.clone()).is_ok());
+    unsafe { String::from_utf8_unchecked(escaped) }
 }
 
 #[cfg(test)]
@@ -53,12 +56,14 @@ mod test {
             escape_string("\u{08}f\u{0C}o\to\r\n").as_ref()
         );
         assert_eq!("\\\"test\\\"", escape_string("\"test\"").as_ref());
+        assert_eq!("\\u0000", escape_string("\u{0}").as_ref());
+        assert_eq!("\\u001f", escape_string("\u{1f}").as_ref());
     }
 
     use proptest::proptest;
     proptest! {
         #[test]
-        fn matches_serde_json(s: String) {
+        fn matches_serde_json(s in ".*") {
             assert_eq!(
                 serde_json::to_string(&s).unwrap(),
                 format!(r#""{}""#, escape_string(&s))
