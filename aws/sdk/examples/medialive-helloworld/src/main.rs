22a37b27afdf0321b3f9ee1d1bc08c38ed5642a0
Alistair McLean <mclean@amazon.com>
2021-06-01 21:18:04 -0400

    Fixing clippy error

--- aws/sdk/examples/medialive-helloworld/src/main.rs
+++ aws/sdk/examples/medialive-helloworld/src/main.rs
@@ -3,14 +3,14 @@ async fn main() -> Result<(), medialive::Error> {
     let client = medialive::Client::from_env();
     let input_list = client.list_inputs().send().await?;
 
-    match input_list.inputs {
-        Some(inputs) => inputs.iter().for_each(|i| {
+    if let Some(inputs) = input_list.inputs {
+        inputs.iter().for_each(|i| {
             let input_arn = i.arn.to_owned().unwrap_or_default();
             let input_name = i.name.to_owned().unwrap_or_default();
 
             println!("Input Name : {}, Input ARN : {}", input_name, input_arn);
-        }),
-        _ => (),
+        })
     }
+
     Ok(())
 }
