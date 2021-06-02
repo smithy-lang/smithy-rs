51d8badd6f6781243de55cea53a80e6fd4949391
Russell Cohen <rcoh@amazon.com>
2021-06-02 16:45:47 -0400

    Add protocol test

--- aws/sdk/examples/batch-helloworld/src/main.rs
+++ aws/sdk/examples/batch-helloworld/src/main.rs
@@ -1,25 +1,26 @@
+use batch::Region;
+
 #[tokio::main]
 async fn main() -> Result<(), batch::Error> {
     tracing_subscriber::fmt::init();
 
-    let client = batch::Client::from_env();
-    let compute_envs = client
-        .describe_compute_environments()
-        .max_results(25)
-        .send()
-        .await?;
+    let conf = batch::Config::builder()
+        .region(Region::new("us-east-2"))
+        .build();
+    let client = batch::Client::from_conf(conf);
+    let rsp = client.describe_compute_environments().send().await?;
 
-    for env in compute_envs.compute_environments.unwrap_or_default() {
+    let compute_envs = rsp.compute_environments.unwrap_or_default();
+    println!("Compute environments ({}):", compute_envs.len());
+    for env in compute_envs {
         let arn = env.compute_environment_arn.as_deref().unwrap_or_default();
         let name = env.compute_environment_name.as_deref().unwrap_or_default();
 
         println!(
-            "Compute Environment Name : {}, Compute Environment ARN : {}",
+            "  Compute Environment Name : {}, Compute Environment ARN : {}",
             name, arn
         );
     }
-
-    println!("Hello, world!");
 
     Ok(())
 }
