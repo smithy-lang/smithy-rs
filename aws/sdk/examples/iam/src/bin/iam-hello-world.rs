c86cc1233584c978c7ca859bca68780742e3112e
Russell Cohen <rcoh@amazon.com>
2021-06-03 12:52:15 -0400

    Override with static endpoint

--- aws/sdk/examples/iam/src/bin/iam-hello-world.rs
+++ aws/sdk/examples/iam/src/bin/iam-hello-world.rs
@@ -3,16 +3,21 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+use http::Uri;
+use iam::Endpoint;
+
 #[tokio::main]
 async fn main() -> Result<(), iam::Error> {
     tracing_subscriber::fmt::init();
-    let client = iam::Client::from_env();
-    let rsp = client.list_access_keys().send().await?;
-    for akid in rsp.access_key_metadata.unwrap_or_default() {
+    let ep = Endpoint::immutable(Uri::from_static("https://iam.amazonaws.com"));
+    let conf = iam::Config::builder().endpoint_resolver(ep).build();
+    let client = iam::Client::from_conf(conf);
+    let rsp = client.list_policies().send().await?;
+    for policy in rsp.policies.unwrap_or_default() {
         println!(
-            "akid: {}; status: {:?}",
-            akid.access_key_id.unwrap(),
-            akid.status
+            "arn: {}; description: {}",
+            policy.arn.unwrap(),
+            policy.description.unwrap_or_default()
         );
     }
     Ok(())
