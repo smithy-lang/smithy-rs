36bdc85989c1632a155f18b868d7450cde2d26f5
Russell Cohen <rcoh@amazon.com>
2021-06-07 17:43:09 -0400

    IAM works now

--- aws/sdk/examples/iam/src/bin/iam-hello-world.rs
+++ aws/sdk/examples/iam/src/bin/iam-hello-world.rs
@@ -9,9 +9,7 @@ use iam::Endpoint;
 #[tokio::main]
 async fn main() -> Result<(), iam::Error> {
     tracing_subscriber::fmt::init();
-    let ep = Endpoint::immutable(Uri::from_static("https://iam.amazonaws.com"));
-    let conf = iam::Config::builder().endpoint_resolver(ep).build();
-    let client = iam::Client::from_conf(conf);
+    let client = iam::Client::from_env();
     let rsp = client.list_policies().send().await?;
     for policy in rsp.policies.unwrap_or_default() {
         println!(
