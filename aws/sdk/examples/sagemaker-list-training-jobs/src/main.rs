79880e13d5fba5e3f70b03f0e82973053819c847
Alistair McLean <mclean@amazon.com>
2021-06-08 16:59:40 -0400

    Using to_chrono now

--- aws/sdk/examples/sagemaker-list-training-jobs/src/main.rs
+++ aws/sdk/examples/sagemaker-list-training-jobs/src/main.rs
@@ -1,5 +1,3 @@
-use chrono::{DateTime, Utc};
-
 #[tokio::main]
 async fn main() -> Result<(), sagemaker::Error> {
     let client = sagemaker::Client::from_env();
@@ -8,23 +6,17 @@ async fn main() -> Result<(), sagemaker::Error> {
     println!("Job Name\tCreation DateTime\tDuration\tStatus");
     for j in job_details.training_job_summaries.unwrap_or_default() {
         let name = j.training_job_name.as_deref().unwrap_or_default();
-        let creation_time_secs = j.creation_time.unwrap().epoch_seconds();
-        let training_end_time_secs = j.training_end_time.unwrap().epoch_seconds();
-
-        let native_creation_time_string =
-            chrono::NaiveDateTime::from_timestamp(creation_time_secs, 0);
-
-        let utc_creation_time: DateTime<Utc> =
-            chrono::DateTime::from_utc(native_creation_time_string, chrono::Utc);
+        let creation_time = j.creation_time.unwrap().to_chrono();
+        let training_end_time = j.training_end_time.unwrap().to_chrono();
 
         let status = j.training_job_status.unwrap();
-        let duration = training_end_time_secs - creation_time_secs;
+        let duration = training_end_time - creation_time;
 
         let deets = format!(
             "{}\t{}\t{}\t{:#?}",
             name,
-            utc_creation_time.format("%Y-%m-%d@%H:%M:%S"),
-            duration,
+            creation_time.format("%Y-%m-%d@%H:%M:%S"),
+            duration.num_seconds(),
             status
         );
         println!("{}", deets);
