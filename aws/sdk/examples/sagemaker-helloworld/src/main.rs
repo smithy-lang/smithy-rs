141db7ee610b30f7a334f59d27fa4f65e1f3955c
Alistair McLean <mclean@amazon.com>
2021-06-08 12:58:14 -0400

    Tidied up instance type and notebook status enums

--- aws/sdk/examples/sagemaker-helloworld/src/main.rs
+++ aws/sdk/examples/sagemaker-helloworld/src/main.rs
@@ -3,23 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use sagemaker::model::{InstanceType, NotebookInstanceStatus};
-use std::fmt::format;
-
 #[tokio::main]
 async fn main() -> Result<(), sagemaker::Error> {
     let client = sagemaker::Client::from_env();
     let notebooks = client.list_notebook_instances().send().await?;
 
     for n in notebooks.notebook_instances.unwrap_or_default() {
-        let n_instance_type = n
-            .instance_type
-            .unwrap_or(InstanceType::Unknown("Unknown".to_string()));
-
-        let n_status = n
-            .notebook_instance_status
-            .unwrap_or(NotebookInstanceStatus::Unknown("Unknown".to_string()));
-
+        let n_instance_type = n.instance_type.unwrap();
+        let n_status = n.notebook_instance_status.unwrap();
         let n_name = n.notebook_instance_name.as_deref().unwrap_or_default();
 
         let details = format!(
