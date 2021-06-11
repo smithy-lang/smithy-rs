a47ada58c67c2adeb1847c4a948c46a96d742109
Russell Cohen <rcoh@amazon.com>
2021-06-11 14:52:58 -0400

    Name collision refactor (#492)
    * Centralize colliding renames
    
    * Add integration test
    
    * Cleanup exception
    
    * Add tests & fixes for more invalid Rust identifiers

--- codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/rustlang/RustReservedWords.kt
+++ codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/rustlang/RustReservedWords.kt
@@ -10,17 +10,44 @@ import software.amazon.smithy.codegen.core.ReservedWords
 import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.model.shapes.MemberShape
 import software.amazon.smithy.model.shapes.Shape
+import software.amazon.smithy.model.traits.EnumDefinition
+import software.amazon.smithy.rust.codegen.smithy.MaybeRenamed
 import software.amazon.smithy.rust.codegen.smithy.RustSymbolProvider
 import software.amazon.smithy.rust.codegen.smithy.WrappingSymbolProvider
+import software.amazon.smithy.rust.codegen.util.toPascalCase
 
-class RustReservedWordSymbolProvider(base: RustSymbolProvider) : WrappingSymbolProvider(base) {
-    private val internal = ReservedWordSymbolProvider.builder().symbolProvider(base).memberReservedWords(RustReservedWords).build()
+class RustReservedWordSymbolProvider(private val base: RustSymbolProvider) : WrappingSymbolProvider(base) {
+    private val internal =
+        ReservedWordSymbolProvider.builder().symbolProvider(base).memberReservedWords(RustReservedWords).build()
+
     override fun toMemberName(shape: MemberShape): String {
         return internal.toMemberName(shape)
     }
 
     override fun toSymbol(shape: Shape): Symbol {
         return internal.toSymbol(shape)
+    }
+
+    override fun toEnumVariantName(definition: EnumDefinition): MaybeRenamed? {
+        val baseName = base.toEnumVariantName(definition) ?: return null
+        check(baseName.name.toPascalCase() == baseName.name) {
+            "Enum variants must already be in pascal case"
+        }
+        check(baseName.renamedFrom == null) {
+            "definitions should only pass through the renamer once"
+        }
+        return when (baseName.name) {
+            // Self cannot be used as a raw identifier, so we can't use the normal escaping strategy
+            // https://internals.rust-lang.org/t/raw-identifiers-dont-work-for-all-identifiers/9094/4
+            "Self" -> MaybeRenamed("SelfValue", "Self")
+            // Real models won't end in `_` so it's safe to stop here
+            "SelfValue" -> MaybeRenamed("SelfValue_", "SelfValue")
+            // Unknown is used as the name of the variant containing unexpected values
+            "Unknown" -> MaybeRenamed("UnknownValue", "Unknown")
+            // Real models won't end in `_` so it's safe to stop here
+            "UnknownValue" -> MaybeRenamed("UnknownValue_", "UnknownValue")
+            else -> baseName
+        }
     }
 }
 
@@ -81,7 +108,12 @@ object RustReservedWords : ReservedWords {
         "try"
     )
 
-    override fun escape(word: String): String = "r##$word"
+    private val cantBeRaw = setOf("self", "crate", "super")
+
+    override fun escape(word: String): String = when {
+        cantBeRaw.contains(word) -> "${word}_"
+        else -> "r##$word"
+    }
 
     override fun isReserved(word: String): Boolean = RustKeywords.contains(word)
 }
