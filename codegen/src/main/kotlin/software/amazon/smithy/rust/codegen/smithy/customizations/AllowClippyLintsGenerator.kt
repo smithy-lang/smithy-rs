dc3fd82e3ee3e9e56c0231b0c41181988e50110d
Russell Cohen <rcoh@amazon.com>
2021-06-23 14:10:30 -0700

    Ignore clippy another clippy lint

--- codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/customizations/AllowClippyLintsGenerator.kt
+++ codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/customizations/AllowClippyLintsGenerator.kt
@@ -11,19 +11,26 @@ import software.amazon.smithy.rust.codegen.smithy.generators.LibRsCustomization
 import software.amazon.smithy.rust.codegen.smithy.generators.LibRsSection
 
 val ClippyAllowLints = listOf(
-    // Sometimes a operation be named the same as our module eg. output leading to `output::output`
+    // Sometimes operations are named the same as our module eg. output leading to `output::output`
     "module_inception",
+
     // Currently, we don't recase acronyms in models, eg. SSEVersion
     "upper_case_acronyms",
+
     // Large errors trigger this warning, we are unlikely to optimize this case currently
     "large_enum_variant",
+
     // Some models have members with `is` in the name, which leads to builder functions with the wrong self convention
     "wrong_self_convention",
+
     // models like ecs use method names like "add()" which confuses clippy
-    "should_implement_trait"
+    "should_implement_trait",
+
+    // protocol tests use silly names like `baz`, don't flag that
+    "blacklisted-name"
 )
 
-class AllowClippyLints() : LibRsCustomization() {
+class AllowClippyLints : LibRsCustomization() {
     override fun section(section: LibRsSection) = when (section) {
         is LibRsSection.Attributes -> writable {
             ClippyAllowLints.forEach {
