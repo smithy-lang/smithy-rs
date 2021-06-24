f8b2fc58744e607856a512b639e5c1685fa30419
Russell Cohen <rcoh@amazon.com>
2021-06-23 19:03:09 -0700

    Suppress noisy logging from the renamer

--- codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/RustCodegenPlugin.kt
+++ codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/RustCodegenPlugin.kt
@@ -7,16 +7,21 @@ package software.amazon.smithy.rust.codegen.smithy
 
 import software.amazon.smithy.build.PluginContext
 import software.amazon.smithy.build.SmithyBuildPlugin
+import software.amazon.smithy.codegen.core.ReservedWordSymbolProvider
 import software.amazon.smithy.model.Model
 import software.amazon.smithy.model.shapes.ServiceShape
 import software.amazon.smithy.rust.codegen.rustlang.RustReservedWordSymbolProvider
 import software.amazon.smithy.rust.codegen.smithy.customize.CombinedCodegenDecorator
+import java.util.logging.Level
+import java.util.logging.Logger
 
 class RustCodegenPlugin : SmithyBuildPlugin {
     override fun getName(): String = "rust-codegen"
 
     override fun execute(context: PluginContext) {
         val codegenDecorator = CombinedCodegenDecorator.fromClasspath(context)
+        // Suppress extremely noisy logs about reserved words
+        Logger.getLogger(ReservedWordSymbolProvider::class.java.name).level = Level.OFF
         CodegenVisitor(context, codegenDecorator).execute()
     }
 
