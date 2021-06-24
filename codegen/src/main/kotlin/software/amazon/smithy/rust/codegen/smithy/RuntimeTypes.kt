b39fa03369ed14fa90ecddb13b33bebad85ace46
Russell Cohen <rcoh@amazon.com>
2021-06-24 09:12:26 -0700

    Remove double unescape

--- codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/RuntimeTypes.kt
+++ codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/RuntimeTypes.kt
@@ -72,7 +72,7 @@ data class RuntimeType(val name: String?, val dependency: RustDependency?, val n
 
     fun fullyQualifiedName(): String {
         val postFix = name?.let { "::$name" } ?: ""
-        return "$namespace$postFix".replace("##", "#")
+        return "$namespace$postFix"
     }
 
     // TODO: refactor to be RuntimeTypeProvider a la Symbol provider that packages the `RuntimeConfig` state.
