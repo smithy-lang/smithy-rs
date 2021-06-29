3a7f0d299d23a789b4a89be238016d23323f696a
Doug <dougsch@amazon.com>
2021-06-29 06:40:16 -0700

    Reordered crates in Cargo.toml; refactored code examples to use common example pattern

--- aws/sdk/examples/kinesis/src/bin/list-streams.rs
+++ aws/sdk/examples/kinesis/src/bin/list-streams.rs
@@ -3,21 +3,15 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use std::process;
-
-use kinesis::{Client, Config, Region};
-
-use aws_types::region::{EnvironmentProvider, ProvideRegion};
-
+use aws_types::region::ProvideRegion;
+use kinesis::{Client, Config, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
-use tracing_subscriber::fmt::format::FmtSpan;
-use tracing_subscriber::fmt::SubscriberBuilder;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The region
+    /// The default AWS Region.
     #[structopt(short, long)]
-    region: Option<String>,
+    default_region: Option<String>,
 
     /// Whether to display additional information
     #[structopt(short, long)]
@@ -25,43 +19,44 @@ struct Opt {
 }
 
 #[tokio::main]
-async fn main() {
-    let Opt { region, verbose } = Opt::from_args();
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+    let Opt {
+        default_region,
+        verbose,
+    } = Opt::from_args();
 
-    let region = EnvironmentProvider::new()
-        .region()
-        .or_else(|| region.as_ref().map(|region| Region::new(region.clone())))
+    let region = default_region
+        .as_ref()
+        .map(|region| Region::new(region.clone()))
+        .or_else(|| aws_types::region::default_provider().region())
         .unwrap_or_else(|| Region::new("us-west-2"));
 
-    if verbose {
-        println!("Kinesis client version: {}\n", kinesis::PKG_VERSION);
-        println!("Region:      {:?}", &region);
+    println!();
 
-        SubscriberBuilder::default()
-            .with_env_filter("info")
-            .with_span_events(FmtSpan::CLOSE)
-            .init();
+    if verbose {
+        println!("Kinesis version: {}", PKG_VERSION);
+        println!("Region:          {:?}", &default_region);
+        println!();
     }
 
     let config = Config::builder().region(region).build();
-
     let client = Client::from_conf(config);
 
-    match client.list_streams().send().await {
-        Ok(resp) => {
-            println!("Stream names:");
+    let resp = client
+        .list_streams()
+        .send()
+        .await
+        .expect("Could not get list of streame");
 
-            let streams = resp.stream_names.unwrap_or_default();
-            for stream in &streams {
-                println!("  {}", stream);
-            }
+    println!("Stream names:");
 
-            println!("Found {} stream(s)", streams.len());
-        }
-        Err(e) => {
-            println!("Got an error listing stream names:");
-            println!("{}", e);
-            process::exit(1);
-        }
-    };
+    let streams = resp.stream_names.unwrap_or_default();
+    for stream in &streams {
+        println!("  {}", stream);
+    }
+
+    println!("Found {} stream(s)", streams.len());
+
+    Ok(())
 }
