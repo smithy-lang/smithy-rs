287af2ff891ea8e85f165a935a8f16fd807e93e2
Doug <dougsch@amazon.com>
2021-06-30 13:11:43 -0700

    Updated SNS code examples to use common pattern; cleaned up Cargo.toml

--- aws/sdk/examples/sns/src/bin/sns-hello-world.rs
+++ aws/sdk/examples/sns/src/bin/sns-hello-world.rs
@@ -3,24 +3,21 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
-use sns::{Client, Config, Region};
-use std::process::exit;
-
 use aws_types::region::ProvideRegion;
-
+use sns::{Client, Config, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The region. Overrides environment variable AWS_DEFAULT_REGION.
+    /// The default AWS Region.
     #[structopt(short, long)]
     default_region: Option<String>,
 
-    /// Specifies the email address to subscribe to the topic
+    /// Specifies the email address to subscribe to the topic.
     #[structopt(short, long)]
     email_address: String,
 
-    /// Whether to display additional runtime information
+    /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
@@ -31,12 +28,12 @@ struct Opt {
 /// # Arguments
 ///
 /// * `-e EMAIL_ADDRESS` - The email address of a user subscribing to the topic.
-/// * `[-d DEFAULT-REGION]` - The region in which the client is created.
-///    If not supplied, uses the value of the **AWS_DEFAULT_REGION** environment variable.
+/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
 #[tokio::main]
-async fn main() -> Result<(), sns::Error> {
+async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
     let Opt {
@@ -51,10 +48,13 @@ async fn main() -> Result<(), sns::Error> {
         .or_else(|| aws_types::region::default_provider().region())
         .unwrap_or_else(|| Region::new("us-west-2"));
 
+    println!();
+
     if verbose {
-        println!("SNS client version: {}", sns::PKG_VERSION);
-        println!("Region:             {:?}", &region);
-        println!("Email address:      {}", &email_address);
+        println!("SNS version:   {}", PKG_VERSION);
+        println!("Region:        {:?}", &region);
+        println!("Email address: {}", &email_address);
+        println!();
     }
 
     let conf = Config::builder().region(region).build();
@@ -62,19 +62,14 @@ async fn main() -> Result<(), sns::Error> {
 
     let topics = client.list_topics().send().await?;
     let mut topics = topics.topics.unwrap_or_default();
-    let topic_arn = match topics.pop() {
-        Some(topic) => topic.topic_arn.expect("topics have ARNs"),
-        None => {
-            eprintln!("No topics in this account. Please create a topic to proceed");
-            exit(1);
-        }
-    };
+    let topic_arn = &topics.pop().unwrap().topic_arn;
+    let topic_string = topic_arn.as_deref().unwrap_or_default();
 
-    println!("Receiving on topic with ARN: `{}`", topic_arn);
+    println!("Receiving on topic with ARN: `{}`", topic_string);
 
     let rsp = client
         .subscribe()
-        .topic_arn(&topic_arn)
+        .topic_arn(topic_string)
         .protocol("email")
         .endpoint(email_address)
         .send()
@@ -84,7 +79,7 @@ async fn main() -> Result<(), sns::Error> {
 
     let rsp = client
         .publish()
-        .topic_arn(&topic_arn)
+        .topic_arn(topic_string)
         .message("hello sns!")
         .send()
         .await?;
