534f788cc1cf5cdf03c736adccec14ff2129aea2
Doug <dougsch@amazon.com>
2021-07-02 06:23:14 -0700

    Updated Lambda invoke-function code example to use InvokeErrorKind::ResourceNotFoundException

--- aws/sdk/examples/lambda/src/bin/invoke-function.rs
+++ aws/sdk/examples/lambda/src/bin/invoke-function.rs
@@ -138,7 +138,7 @@ async fn main() {
         // For our example, we will simply print that the function doesn't
         // exist and return a non-zero exit code to indicate the failure.
         Err(SdkError::ServiceError { err, .. })
-            if matches!(err.kind, InvokeErrorKind::ResourceNotFoundError(_)) =>
+            if matches!(err.kind, InvokeErrorKind::ResourceNotFoundException(_)) =>
         {
             println!("This lambda function does not exist: {}", err);
             process::exit(1);
