dffbacf8e080de6bc741fb615bf12c1e95beccd6
Landon James <lnj@amazon.com>
2021-07-02 18:43:41 -0700

    updating copyright notices

--- aws/sdk/examples/snowball/src/bin/create-address.rs
+++ aws/sdk/examples/snowball/src/bin/create-address.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
 use aws_sdk_snowball::model::Address;
 use aws_sdk_snowball::{Config, Region};
 use aws_types::region::{EnvironmentProvider, ProvideRegion};
--- aws/sdk/examples/snowball/src/bin/list-jobs.rs
+++ aws/sdk/examples/snowball/src/bin/list-jobs.rs
@@ -1,9 +1,9 @@
-use aws_sdk_snowball::{Config, Region};
-
 /*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0.
  */
+
+use aws_sdk_snowball::{Config, Region};
 
 #[tokio::main]
 async fn main() -> Result<(), aws_sdk_snowball::Error> {
