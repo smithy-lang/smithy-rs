8e02a34665786ff47dad742a2cf5b8e2f3505e16
Doug <dougsch@amazon.com>
2021-07-21 13:51:32 -0700

    Renamed config-helloword as show-resource-history; added delete-configuration-recorder, delete-delivery-channel, enable-config, list-configuration-recorders, list-delivery-channels, and list-resources code examples; all for AWS Config

--- aws/sdk/examples/config/src/bin/show-resource-history.rs
+++ aws/sdk/examples/config/src/bin/show-resource-history.rs
@@ -1,34 +1,36 @@
 use aws_types::region;
 use aws_types::region::ProvideRegion;
 use config::model::ResourceType;
-use config::{Client, Config, Error, Region};
+use config::{Client, Config, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The default AWS Region.
+    /// The AWS Region.
     #[structopt(short, long)]
-    default_region: Option<String>,
+    region: Option<String>,
 
-    /// The resource id.
-    #[structopt(long)]
-    resource_id: String,
+    /// The ID of the resource.
+    #[structopt(short, long)]
+    id: String,
 
-    /// The resource type, eg. "AWS::EC2::SecurityGroup"
-    #[structopt(long)]
-    resource_type: String,
+    /// The resource type.
+    #[structopt(short, long)]
+    type_: String,
 
     /// Whether to display additional information.
     #[structopt(short, long)]
     verbose: bool,
 }
 
-/// Lists the configuration history for a resource
+/// Displays the configuration history for a resource.
 ///
-/// NOTE: AWS Config must be enabled to discover resources
+/// NOTE: AWS Config must be enabled to discover resources.
 /// # Arguments
 ///
-/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+/// * `-i ID` - The ID of the resource.
+/// * `-t TYPE` - The resource type, such as 'AWS::EC2::SecurityGroup'.
+/// * `[-r REGION]` - The Region in which the client is created.
 ///   If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///   If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display information.
@@ -36,42 +38,50 @@ struct Opt {
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
     let Opt {
-        default_region,
-        resource_id,
-        resource_type,
+        region,
+        id,
+        type_,
         verbose,
     } = Opt::from_args();
 
-    let region = region::ChainProvider::first_try(default_region.map(Region::new))
+    let region = region::ChainProvider::first_try(region.map(Region::new))
         .or_default_provider()
         .or_else(Region::new("us-west-2"));
 
     println!();
 
     if verbose {
-        println!("Config client version: {}", config::PKG_VERSION);
-        println!("Region:               {:?}", region.region());
+        println!("Config client version: {}", PKG_VERSION);
+        println!(
+            "Region:                {}",
+            region.region().unwrap().as_ref()
+        );
+        println!("Resource ID:           {}", &id);
+        println!("Resource type:         {}", &type_);
         println!();
     }
 
-    let conf = Config::builder().region(region).build();
-    // parse resource type from user input
-    let parsed = ResourceType::from(resource_type.as_str());
+    // Parse resource type from user input.
+    let parsed = ResourceType::from(type_.as_str());
+
+    // Make sure it's a known type.
     if matches!(parsed, ResourceType::Unknown(_)) {
         panic!(
             "unknown resource type: `{}`. Valid resource types: {:#?}",
-            &resource_type,
+            &type_,
             ResourceType::values()
         )
     }
+    let conf = Config::builder().region(region).build();
     let client = Client::from_conf(conf);
+
     let rsp = client
         .get_resource_config_history()
-        .resource_id(&resource_id)
+        .resource_id(&id)
         .resource_type(parsed)
         .send()
         .await?;
-    println!("configuration history for {}:", resource_id);
+    println!("configuration history for {}:", id);
     for item in rsp.configuration_items.unwrap_or_default() {
         println!("item: {:?}", item);
     }
