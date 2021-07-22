e7df14fe4dd5dc5e8d1b034780038e594a203ede
Doug <dougsch@amazon.com>
2021-07-22 09:47:00 -0700

    Updated CloudFormation code examples to follow latest code guidelines and proper service naming

--- aws/sdk/examples/cloudformation/src/bin/describe-stack.rs
+++ aws/sdk/examples/cloudformation/src/bin/describe-stack.rs
@@ -3,17 +3,18 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+use aws_sdk_cloudformation::{Client, Config, Error, Region, PKG_VERSION};
+use aws_types::region;
 use aws_types::region::ProvideRegion;
-use cloudformation::{Client, Config, Error, Region, PKG_VERSION};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    /// The default AWS Region.
+    /// The AWS Region.
     #[structopt(short, long)]
-    default_region: Option<String>,
+    region: Option<String>,
 
-    /// The name of the stack.
+    /// The name of the AWS CloudFormation stack.
     #[structopt(short, long)]
     stack_name: String,
 
@@ -26,7 +27,7 @@ struct Opt {
 /// # Arguments
 ///
 /// * `-s STACK-NAME` - The name of the stack.
-/// * `[-d DEFAULT-REGION]` - The Region in which the client is created.
+/// * `[-r REGION]` - The Region in which the client is created.
 ///    If not supplied, uses the value of the **AWS_REGION** environment variable.
 ///    If the environment variable is not set, defaults to **us-west-2**.
 /// * `[-v]` - Whether to display additional information.
@@ -35,22 +36,23 @@ async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt::init();
 
     let Opt {
-        default_region,
+        region,
         stack_name,
         verbose,
     } = Opt::from_args();
 
-    let region = default_region
-        .as_ref()
-        .map(|region| Region::new(region.clone()))
-        .or_else(|| aws_types::region::default_provider().region())
-        .unwrap_or_else(|| Region::new("us-west-2"));
+    let region = region::ChainProvider::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
 
     println!();
 
     if verbose {
         println!("CloudFormation version: {}", PKG_VERSION);
-        println!("Region:                 {:?}", &region);
+        println!(
+            "Region:                 {}",
+            region.region().unwrap().as_ref()
+        );
         println!("Stack:                  {}", &stack_name);
         println!();
     }
