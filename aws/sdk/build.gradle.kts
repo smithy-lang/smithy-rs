dba6ec4093cb87b92be3fd4e45f5606b0d44f3d4
John DiSanti <jdisanti@amazon.com>
2021-08-03 10:50:06 -0700

    Prepare August 3rd release (#628)
    * Version bump generated SDK
    
    * Update the CHANGELOG
    
    * Make the changelog readable in a normal text editor

--- aws/sdk/build.gradle.kts
+++ aws/sdk/build.gradle.kts
@@ -201,7 +201,7 @@ fun generateSmithyBuild(tests: List<AwsService>): String {
                       },
                       "service": "${it.service}",
                       "module": "aws-sdk-${it.module}",
-                      "moduleVersion": "0.0.13-alpha",
+                      "moduleVersion": "${getProperty("aws.sdk.version")}",
                       "moduleAuthors": ["AWS Rust SDK Team <aws-sdk-rust@amazon.com>", "Russell Cohen <rcoh@amazon.com>"],
                       "license": "Apache-2.0"
                       ${it.extraConfig ?: ""}
