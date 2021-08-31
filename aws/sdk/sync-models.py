011c46f5c9c6f078b5205718191fb017e1574c4b
John DiSanti <jdisanti@amazon.com>
2021-08-31 11:58:10 -0700

    Clean up `sync-models.py`

--- aws/sdk/sync-models.py
+++ aws/sdk/sync-models.py
@@ -21,44 +21,58 @@ def discover_aws_models():
     else:
         return None
 
-# Acquire model location
-aws_models_repo = discover_aws_models()
-if aws_models_repo == None:
-    if len(sys.argv) != 2:
-        print("Please provide the location of the aws-models repository as the first argument")
-        sys.exit(1)
-    else:
-        aws_models_repo = sys.argv[1]
+def discover_new_models(aws_models_repo, known_models):
+    new_models = []
+    for model in os.listdir(aws_models_repo):
+        if model not in known_models and path.exists(Path(aws_models_repo) / model / "smithy" / "model.json"):
+            new_models.append(model)
+    return new_models
 
-print("Copying over known models...")
-known_models = set()
-for model in os.listdir("aws-models"):
-    if not model.endswith('.json'):
-        continue
-    model_name = model[:-len('.json')]
-    known_models.add(model_name)
-    source_path = Path(aws_models_repo) / model_name / "smithy" / "model.json"
-    if not source_path.exists():
-        print(f"  Warning: cannot find model for '{model_name}' in aws-models, but it exists in smithy-rs")
-        continue
+def copy_model(source_path, model_path, model_name):
     # Add a newline at the end when copying the model over
     source = source_path.read_text()
-    with open(Path("aws-models") / model, "w") as file:
+    with open(Path("aws-models") / model_path, "w") as file:
         file.write(source)
         if not source.endswith("\n"):
             file.write("\n")
 
-print("Looking for new models...")
-new_models = []
-for model in os.listdir(aws_models_repo):
-    if model not in known_models and path.exists(Path(aws_models_repo) / model / "smithy" / "model.json"):
-        new_models.append(model)
+def copy_known_models(aws_models_repo):
+    known_models = set()
+    for model in os.listdir("aws-models"):
+        if not model.endswith('.json'):
+            continue
+        model_name = model[:-len('.json')]
+        known_models.add(model_name)
+        source_path = Path(aws_models_repo) / model_name / "smithy" / "model.json"
+        if not source_path.exists():
+            print(f"  Warning: cannot find model for '{model_name}' in aws-models, but it exists in smithy-rs")
+            continue
+        copy_model(source_path, model, model_name)
+    return known_models
 
-if len(new_models) > 0:
-    print(f"  Warning: found models for {new_models} in aws-models that aren't in smithy-rs")
-    print(f"  Run the following commands to bring these in:\n")
-    for model in new_models:
-        print(f"  touch aws-models/{model}.json")
-    print(f"  ./sync-models.py\n")
+def main():
+    # Acquire model location
+    aws_models_repo = discover_aws_models()
+    if aws_models_repo == None:
+        if len(sys.argv) != 2:
+            print("Please provide the location of the aws-models repository as the first argument")
+            sys.exit(1)
+        else:
+            aws_models_repo = sys.argv[1]
 
-print("Models synced.")
+    print("Copying over known models...")
+    known_models = copy_known_models(aws_models_repo)
+
+    print("Looking for new models...")
+    new_models = discover_new_models(aws_models_repo, known_models)
+    if len(new_models) > 0:
+        print(f"  Warning: found models for {new_models} in aws-models that aren't in smithy-rs")
+        print(f"  Run the following commands to bring these in:\n")
+        for model in new_models:
+            print(f"  touch aws-models/{model}.json")
+        print(f"  ./sync-models.py\n")
+
+    print("Models synced.")
+
+if __name__ == "__main__":
+    main()
