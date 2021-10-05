7d715603488eb7d1fa6393713bde9f32ab1c41a6
Doug <dougsch@amazon.com>
2021-10-05 09:51:15 -0700

    Fixed typos in AWS Config code examples

--- aws/sdk/examples/config/src/bin/list-resources.rs
+++ aws/sdk/examples/config/src/bin/list-resources.rs
@@ -66,7 +66,7 @@ async fn main() -> Result<(), Error> {
         let resources = resp.resource_identifiers.unwrap_or_default();
         let length = resources.len();
 
-        if !length.is_emtpy() || verbose {
+        if !length.is_empty() || verbose {
             println!();
             println!("Resources of type {}:", value);
         }
