622a5b374157889e1f3027f00ee9ea94a8ae22b1
Russell Cohen <rcoh@amazon.com>
2021-10-07 13:06:10 -0400

    add retry logic and tests

--- aws/rust-runtime/aws-config/src/http_provider.rs
+++ aws/rust-runtime/aws-config/src/http_provider.rs
@@ -19,12 +19,17 @@ use smithy_http::result::SdkError;
 use smithy_http::retry::ClassifyResponse;
 use smithy_types::retry::{ErrorKind, RetryKind};
 
+use crate::provider_config::ProviderConfig;
 use tower::layer::util::Identity;
 
 pub(crate) struct HttpCredentialProvider {
     uri: Uri,
     client: smithy_client::Client<DynConnector, Identity>,
     provider_name: &'static str,
+}
+
+pub(crate) struct Builder {
+    provider_config: Option<ProviderConfig>,
 }
 
 impl HttpCredentialProvider {
@@ -60,11 +65,6 @@ impl ParseStrictResponse for CredentialsResponseParser {
     type Output = credentials::Result;
 
     fn parse(&self, response: &Response<Bytes>) -> Self::Output {
-        if !response.status().is_success() {
-            return Err(CredentialsError::ProviderError(
-                "non-200 status from HTTP prpvider".into(),
-            ));
-        }
         let str_resp = std::str::from_utf8(response.body().as_ref())
             .map_err(|err| CredentialsError::Unhandled(err.into()))?;
         let json_creds = parse_json_credentials(str_resp)
@@ -83,7 +83,7 @@ impl ParseStrictResponse for CredentialsResponseParser {
                 self.provider_name,
             )),
             JsonCredentials::Error { code, message } => Err(CredentialsError::ProviderError(
-                format!("failed to load credentials[{}]: {}", code, message).into(),
+                format!("failed to load credentials [{}]: {}", code, message).into(),
             )),
         }
     }
@@ -126,5 +126,91 @@ impl ClassifyResponse<SdkSuccess<Credentials>, SdkError<CredentialsError>>
             }
             Err(_) => RetryKind::NotRetryable,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::http_provider::{CredentialsResponseParser, HttpCredentialRetryPolicy};
+    use aws_hyper::SdkSuccess;
+    use aws_types::credentials::CredentialsError;
+    use aws_types::Credentials;
+    use bytes::Bytes;
+    use smithy_http::body::SdkBody;
+    use smithy_http::operation;
+    use smithy_http::response::ParseStrictResponse;
+    use smithy_http::result::SdkError;
+    use smithy_http::retry::ClassifyResponse;
+    use smithy_types::retry::{ErrorKind, RetryKind};
+
+    fn sdk_resp(
+        resp: http::Response<&'static str>,
+    ) -> Result<SdkSuccess<Credentials>, SdkError<CredentialsError>> {
+        let resp = resp.map(|data| Bytes::from_static(data.as_bytes()));
+        match (CredentialsResponseParser {
+            provider_name: "test",
+        })
+        .parse(&resp)
+        {
+            Ok(creds) => Ok(SdkSuccess {
+                raw: operation::Response::new(resp.map(SdkBody::from)),
+                parsed: creds,
+            }),
+            Err(err) => Err(SdkError::ServiceError {
+                err,
+                raw: operation::Response::new(resp.map(SdkBody::from)),
+            }),
+        }
+    }
+
+    #[test]
+    fn non_parseable_is_retriable() {
+        let bad_response = http::Response::builder()
+            .status(200)
+            .body("notjson")
+            .unwrap();
+
+        assert_eq!(
+            HttpCredentialRetryPolicy.classify(sdk_resp(bad_response).as_ref()),
+            RetryKind::Error(ErrorKind::ServerError)
+        );
+    }
+
+    #[test]
+    fn ok_response_not_retriable() {
+        let ok_response = http::Response::builder()
+            .status(200)
+            .body(
+                r#" {
+   "AccessKeyId" : "MUA...",
+   "SecretAccessKey" : "/7PC5om....",
+   "Token" : "AQoDY....=",
+   "Expiration" : "2016-02-25T06:03:31Z"
+ }"#,
+            )
+            .unwrap();
+        let sdk_result = sdk_resp(ok_response);
+
+        assert_eq!(
+            HttpCredentialRetryPolicy.classify(sdk_result.as_ref()),
+            RetryKind::NotRetryable
+        );
+
+        assert!(sdk_result.is_ok(), "should be ok: {:?}", sdk_result)
+    }
+
+    #[test]
+    fn explicit_error_not_retriable() {
+        let error_response = http::Response::builder()
+            .status(400)
+            .body(r#"{ "Code": "Error", "Message": "There was a problem, it was your fault" }"#)
+            .unwrap();
+        let sdk_result = sdk_resp(error_response);
+        assert_eq!(
+            HttpCredentialRetryPolicy.classify(sdk_result.as_ref()),
+            RetryKind::NotRetryable
+        );
+
+        assert!(sdk_result.is_ok(), "should be ok: {:?}", sdk_result)
     }
 }
