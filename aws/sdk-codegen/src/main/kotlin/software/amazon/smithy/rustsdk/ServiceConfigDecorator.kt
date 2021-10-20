ba88b040d223aabdec0f5cba1d507762f32da01f
John DiSanti <jdisanti@amazon.com>
2021-10-20 11:08:48 -0700

    Use `emptySection`

--- aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/ServiceConfigDecorator.kt
+++ aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/ServiceConfigDecorator.kt
@@ -40,7 +40,7 @@ class SharedConfigDocsCustomization : ConfigCustomization() {
                 )
             }
         } else {
-            writable {}
+            emptySection
         }
     }
 }
