ada03d4ca104f88d16d6732f91ee1d420f1f6d32
Russell Cohen <rcoh@amazon.com>
2021-11-12 12:47:48 -0500

    Generate a Docs Landing Page (#853)
    * Generate a Docs Landing Page
    
    This commit refactors a portion of the build into `buildSrc` and adds a job to generate a docs landing page. The generated SDK now contains a `docs.md` that contains top level links to all services.
    
    * Add copyright headers
    
    * Order by human name

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,36 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+import java.util.Properties
+
+plugins {
+    `kotlin-dsl`
+}
+repositories {
+    maven("https://plugins.gradle.org/m2")
+}
+
+// Load properties manually to avoid hard coding smithy version
+val props = Properties().apply {
+    file("../gradle.properties").inputStream().use { load(it) }
+}
+
+val smithyVersion = props["smithyVersion"]
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
+dependencies {
+    api("software.amazon.smithy:smithy-codegen-core:$smithyVersion")
+    implementation("software.amazon.smithy:smithy-utils:$smithyVersion")
+    implementation("software.amazon.smithy:smithy-protocol-test-traits:$smithyVersion")
+    implementation("software.amazon.smithy:smithy-aws-traits:$smithyVersion")
+    implementation("software.amazon.smithy:smithy-aws-iam-traits:$smithyVersion")
+    implementation("software.amazon.smithy:smithy-aws-cloudformation-traits:$smithyVersion")
+    implementation(gradleApi())
+}
