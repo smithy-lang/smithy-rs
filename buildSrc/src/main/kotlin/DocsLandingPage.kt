e87fc7768846ffb85c86d83fe99801cdf686576c
Russell Cohen <rcoh@amazon.com>
2021-11-12 12:30:32 -0500

    Add copyright headers

--- buildSrc/src/main/kotlin/DocsLandingPage.kt
+++ buildSrc/src/main/kotlin/DocsLandingPage.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
 import org.gradle.api.Project
 import software.amazon.smithy.utils.CodeWriter
 import java.io.File
--- buildSrc/src/main/kotlin/ServiceLoader.kt
+++ buildSrc/src/main/kotlin/ServiceLoader.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
 import org.gradle.api.Project
 import software.amazon.smithy.aws.traits.ServiceTrait
 import software.amazon.smithy.model.Model
