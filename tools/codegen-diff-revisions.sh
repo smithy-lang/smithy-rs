70fe207329ff1a896a92ffd127a622479f53262e
John DiSanti <jdisanti@amazon.com>
2021-12-01 19:06:17 -0800

    Disable Gradle build cache when generating code

--- tools/codegen-diff-revisions.sh
+++ tools/codegen-diff-revisions.sh
@@ -29,8 +29,8 @@
 set -e
 
 generate_and_commit_generated_code() {
-    ./gradlew :aws:sdk:assemble
-    ./gradlew :codegen-server-test:assemble
+    ./gradlew --no-build-cache :aws:sdk:assemble
+    ./gradlew --no-build-cache :codegen-server-test:assemble
 
     # Move generated code into codegen-diff/ directory
     rm -rf codegen-diff/
