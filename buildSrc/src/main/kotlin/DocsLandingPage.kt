cb5b1f07ad52decfe42863d7371508006e0f5887
Russell Cohen <rcoh@amazon.com>
2021-12-13 16:07:58 -0500

    Update buildSrc/src/main/kotlin/DocsLandingPage.kt
    Co-authored-by: John DiSanti <jdisanti@amazon.com>

--- buildSrc/src/main/kotlin/DocsLandingPage.kt
+++ buildSrc/src/main/kotlin/DocsLandingPage.kt
@@ -20,7 +20,7 @@ fun Project.docsLandingPage(awsServices: List<AwsService>, outputDir: File) {
         write("# AWS SDK for Rust")
         write(
             "The AWS SDK for Rust contains one crate for each AWS service, as well as ${cratesIo("aws-config")} " +
-                "${docsRs("aws-config")},a crate implementing configuration loading such as credential providers. " +
+                "${docsRs("aws-config")}, a crate implementing configuration loading such as credential providers. " +
                 "For usage documentation see the [Developer Guide](https://docs.aws.amazon.com/sdk-for-rust/latest/dg/welcome.html)."
         )
 
