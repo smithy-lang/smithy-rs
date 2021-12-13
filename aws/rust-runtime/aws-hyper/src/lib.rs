45abced6802c1e8a6d15217f3e275d228943dcc0
Russell Cohen <rcoh@amazon.com>
2021-12-13 10:39:02 -0500

    Add deprecation note to aws-hyper

--- aws/rust-runtime/aws-hyper/src/lib.rs
+++ aws/rust-runtime/aws-hyper/src/lib.rs
@@ -3,4 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+#![deprecated(
+    since = "0.3.0",
+    note = "The functionality of this crate is included in individual AWS services."
+)]
 //! This crate has been removed. Its functionality has be merged into aws-smithy-client and individual AWS services.
