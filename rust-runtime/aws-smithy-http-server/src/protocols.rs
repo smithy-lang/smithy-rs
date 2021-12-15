21feed7075ea79448737e0195d36d0a1f3de0212
Guy Margalit <guymguym@gmail.com>
2021-12-15 22:35:26 +0200

    [Server] codegen-server support for restXml (S3)
    Signed-off-by: Guy Margalit <guymguym@gmail.com>

--- rust-runtime/aws-smithy-http-server/src/protocols.rs
+++ rust-runtime/aws-smithy-http-server/src/protocols.rs
@@ -4,7 +4,7 @@
  */
 
 //! Protocol helpers.
-use crate::rejection::{ContentTypeRejection, MimeParsingFailed, MissingJsonContentType};
+use crate::rejection::{ContentTypeRejection, MimeParsingFailed, MissingJsonContentType, MissingXmlContentType};
 use axum_core::extract::RequestParts;
 
 /// Validate that the request had the standard JSON content-type header.
@@ -25,5 +25,26 @@ pub fn check_json_content_type<B>(req: &RequestParts<B>) -> Result<(), ContentTy
         Ok(())
     } else {
         Err(ContentTypeRejection::MissingJsonContentType(MissingJsonContentType))
+    }
+}
+
+/// Validate that the request had the standard XML content-type header.
+pub fn check_xml_content_type<B>(req: &RequestParts<B>) -> Result<(), ContentTypeRejection> {
+    let mime = req
+        .headers()
+        .ok_or(MissingXmlContentType)?
+        .get(http::header::CONTENT_TYPE)
+        .ok_or(MissingXmlContentType)?
+        .to_str()
+        .map_err(|_| MissingXmlContentType)?
+        .parse::<mime::Mime>()
+        .map_err(|_| MimeParsingFailed)?;
+
+    if mime.type_() == "application"
+        && (mime.subtype() == "xml" || mime.suffix().filter(|name| *name == "xml").is_some())
+    {
+        Ok(())
+    } else {
+        Err(ContentTypeRejection::MissingXmlContentType(MissingXmlContentType))
     }
 }
