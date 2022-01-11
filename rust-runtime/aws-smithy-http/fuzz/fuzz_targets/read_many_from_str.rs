d5b30de1959fe13cbac4269120ffe0be0fd4f574
John DiSanti <jdisanti@amazon.com>
2022-01-11 22:25:50 +0000

    Add fuzz test for `read_many_from_str`

---
+++ rust-runtime/aws-smithy-http/fuzz/fuzz_targets/read_many_from_str.rs
@@ -0,0 +1,14 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use aws_smithy_http::header::read_many_from_str;
+use http;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(s) = std::str::from_utf8(data) {
+        if let Ok(req) = http::Request::builder().header("test", s).body(()) {
+            // Shoudln't panic
+            let _ = read_many_from_str::<String>(req.headers().get_all("test").iter());
+        }
+    }
+});
