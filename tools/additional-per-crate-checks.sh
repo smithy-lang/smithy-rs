c45628a2a1d3f0a0dffc32e2d1a5a0e5afd0459f
John DiSanti <jdisanti@amazon.com>
2022-01-18 22:55:23 +0000

    Simplify Canary Lambda CI

--- tools/additional-per-crate-checks.sh
+++ tools/additional-per-crate-checks.sh
@@ -4,22 +4,36 @@
 # SPDX-License-Identifier: Apache-2.0.
 #
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <runtime crate workspace path>"
+set -e
+
+# TTY colors
+C_CYAN='\033[1;36m'
+C_YELLOW='\033[1;33m'
+C_RESET='\033[0m'
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 [crate workspace path(s)...]"
+    echo "Visits all immediate sub-directories of the given workspace path,"
+    echo "looks for Cargo.toml files and additional-ci scripts. If both are"
+    echo "found, it executes the additional-ci script in that directory."
     exit 1
 fi
 
-cd $1
+for workspace in "$@"; do
+    pushd "${workspace}" &>/dev/null
+    echo -e "${C_CYAN}Scanning ${workspace}...${C_RESET}"
 
-set -e
+    for path in *; do
+        if [[ -d "${path}" && -f "${path}/Cargo.toml" && -f "${path}/additional-ci" ]]; then
+            echo
+            echo -e "${C_YELLOW}Running additional checks for ${path}...${C_RESET}"
+            echo
+            pushd "${path}" &>/dev/null
+            ./additional-ci
+            popd &>/dev/null
+        fi
+    done
 
-for path in *; do
-    if [[ -d "${path}" && -f "${path}/Cargo.toml" && -f "${path}/additional-ci" ]]; then
-        echo
-        echo "# Running additional checks for ${path}..."
-        echo
-        pushd "${path}" &>/dev/null
-        ./additional-ci
-        popd &>/dev/null
-    fi
+    echo
+    popd &>/dev/null
 done
