fd6788ff781505c8521637241ad6910cf0fcac08
John DiSanti <jdisanti@amazon.com>
2022-01-19 09:42:23 -0800

    Simplify Canary Lambda CI (#1084)
    * Simplify Canary Lambda CI
    
    * Remove `Cargo.toml` check and make the script recursive

--- tools/additional-per-crate-checks.sh
+++ tools/additional-per-crate-checks.sh
@@ -4,22 +4,32 @@
 # SPDX-License-Identifier: Apache-2.0.
 #
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <runtime crate workspace path>"
+set -e
+
+# TTY colors
+C_CYAN='\033[1;36m'
+C_YELLOW='\033[1;33m'
+C_RESET='\033[0m'
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 [crate workspace path(s)...]"
+    echo "Recursively visits 'additional-ci' scripts in the given directories and executes them."
     exit 1
 fi
 
-cd $1
+for area in "$@"; do
+    pushd "${area}" &>/dev/null
+    echo -e "${C_CYAN}Scanning '${area}'...${C_RESET}"
 
-set -e
-
-for path in *; do
-    if [[ -d "${path}" && -f "${path}/Cargo.toml" && -f "${path}/additional-ci" ]]; then
+    find . -name 'additional-ci' -print0 | while read -d $'\0' file; do
         echo
-        echo "# Running additional checks for ${path}..."
+        echo -e "${C_YELLOW}Running additional checks script '${file}'...${C_RESET}"
         echo
-        pushd "${path}" &>/dev/null
+        pushd "$(dirname ${file})" &>/dev/null
         ./additional-ci
         popd &>/dev/null
-    fi
+    done
+
+    echo
+    popd &>/dev/null
 done
