1e3135794e551ed10839610b2438a98a673e3cc3
John DiSanti <jdisanti@amazon.com>
2022-01-19 02:13:21 +0000

    Move re-exports into `types` and re-export `AggregatedBytes`

--- codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/customizations/SmithyTypesPubUseGenerator.kt
+++ codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/customizations/SmithyTypesPubUseGenerator.kt
@@ -7,7 +7,9 @@ package software.amazon.smithy.rust.codegen.smithy.customizations
 
 import software.amazon.smithy.rust.codegen.rustlang.CargoDependency
 import software.amazon.smithy.rust.codegen.rustlang.asType
+import software.amazon.smithy.rust.codegen.rustlang.docs
 import software.amazon.smithy.rust.codegen.rustlang.rust
+import software.amazon.smithy.rust.codegen.rustlang.rustBlock
 import software.amazon.smithy.rust.codegen.rustlang.writable
 import software.amazon.smithy.rust.codegen.smithy.RuntimeConfig
 import software.amazon.smithy.rust.codegen.smithy.RuntimeType
@@ -17,17 +19,29 @@ import software.amazon.smithy.rust.codegen.smithy.generators.LibRsSection
 fun pubUseTypes(runtimeConfig: RuntimeConfig) = listOf(
     RuntimeType.Blob(runtimeConfig),
     RuntimeType.DateTime(runtimeConfig),
-    CargoDependency.SmithyHttp(runtimeConfig).asType().member("result::SdkError"),
-    CargoDependency.SmithyHttp(runtimeConfig).asType().member("byte_stream::ByteStream"),
-)
+) + CargoDependency.SmithyHttp(runtimeConfig).asType().let { http ->
+    listOf(
+        http.member("result::SdkError"),
+        http.member("byte_stream::ByteStream"),
+        http.member("byte_stream::AggregatedBytes")
+    )
+}
 
 class SmithyTypesPubUseGenerator(private val runtimeConfig: RuntimeConfig) : LibRsCustomization() {
     override fun section(section: LibRsSection) = writable {
         when (section) {
-            LibRsSection.Body -> pubUseTypes(runtimeConfig).forEach {
-                rust("pub use #T;", it)
+            LibRsSection.Body -> {
+                val types = pubUseTypes(runtimeConfig)
+                if (types.isNotEmpty()) {
+                    docs("Re-exported types from supporting crates.")
+                    rustBlock("pub mod types") {
+                        pubUseTypes(runtimeConfig).forEach {
+                            rust("pub use #T;", it)
+                        }
+                    }
+                }
             }
-            else -> { }
+            else -> {}
         }
     }
 }
