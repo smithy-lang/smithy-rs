ef54ef09bc0bb82274bf2fb2e834aca829c1d77b
John DiSanti <jdisanti@amazon.com>
2022-02-08 09:34:56 -0800

    Fix git repo root detection in tools

--- tools/smithy-rs-tool-common/src/git.rs
+++ tools/smithy-rs-tool-common/src/git.rs
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0.
  */
 
+use crate::shell::ShellOperation;
 use anyhow::{bail, Result};
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
@@ -16,26 +17,15 @@ pub use get_current_tag::GetCurrentTag;
 mod get_last_commit;
 pub use get_last_commit::GetLastCommit;
 
+mod get_repo_root;
+pub use get_repo_root::GetRepoRoot;
+
 /// Attempts to find git repository root from the given location.
 pub fn find_git_repository_root(repo_name: &str, location: impl AsRef<Path>) -> Result<PathBuf> {
-    let mut current_dir = location.as_ref().canonicalize()?;
-    let os_name = OsStr::new(repo_name);
-    loop {
-        if is_git_root(&current_dir) {
-            if let Some(file_name) = current_dir.file_name() {
-                if os_name == file_name {
-                    return Ok(current_dir);
-                }
-            }
-            break;
-        } else if !current_dir.pop() {
-            break;
-        }
+    let path = GetRepoRoot::new(location.as_ref()).run()?;
+    if path.file_name() == Some(OsStr::new(repo_name)) {
+        Ok(path)
+    } else {
+        bail!("failed to find {} repository root", repo_name);
     }
-    bail!("failed to find {0} repository root", repo_name)
-}
-
-fn is_git_root(path: &Path) -> bool {
-    let path = path.join(".git");
-    path.exists() && path.is_dir()
 }
