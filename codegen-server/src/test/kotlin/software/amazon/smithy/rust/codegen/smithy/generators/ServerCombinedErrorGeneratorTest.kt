smithy-lang/smithy-rs

commit 116f81bf4882d7847d2732d69558109261750d5d

Add converters into server error enums (#1175)
Closes #1160.

codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerCombinedErrorGenerator.kt
@@ -83,6 +83,15 @@ class ServerCombinedErrorGenerator(
                 }
             }
         }
+
+        for (error in errors) {
+            val errorSymbol = symbolProvider.toSymbol(error)
+            writer.rustBlock("impl From<#T> for #T", errorSymbol, symbol) {
+                rustBlock("fn from(variant: #T) -> #T", errorSymbol, symbol) {
+                    rust("Self::${errorSymbol.name}(variant)")
+                }
+            }
+        }
     }
 
     /**

commit 5ed6936316c72ac9ed19f8aa023659f563fab629

Add converter test to `ServerCombinedErrorGeneratorTest.kt`
So as to not regress on the functionality introduced in 116f81bf.

codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/smithy/generators/ServerCombinedErrorGeneratorTest.kt
@@ -78,6 +78,14 @@ class ServerCombinedErrorGeneratorTest {
                 """
             )
 
+            writer.unitTest(
+                name = "generates_converters_into_combined_error_enums",
+                test = """
+                    let variant = InvalidGreeting { message: String::from("an error") };
+                    let error: GreetingError = variant.into();
+                """
+            )
+
             println("file:///${project.baseDir}/src/lib.rs")
             println("file:///${project.baseDir}/src/error.rs")
             project.compileAndTest()