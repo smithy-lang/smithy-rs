9c685a78df3ca18f985e76f08f7ae65d5c7775b1
Bigo <1781140+crisidev@users.noreply.github.com>
2022-03-07 11:10:59 +0000

    Calculate benchmark against origin / main

--- rust-runtime/aws-smithy-http-server/examples/pokemon_service/tests/benchmark.rs
+++ rust-runtime/aws-smithy-http-server/examples/pokemon_service/tests/benchmark.rs
@@ -44,14 +44,15 @@ async fn benchmark() -> Result<(), Box<dyn std::error::Error>> {
         wrk.bench(&benches)?;
 
         // Calculate variance from last run and write it to disk.
-        let variance = wrk.variance(HistoryPeriod::Last)?;
-        let mut variance_file = OpenOptions::new()
-            .create(true)
-            .write(true)
-            .truncate(true)
-            .open("/tmp/smithy_rs_benchmark_variance.txt")
-            .unwrap();
-        variance_file.write_all(variance.to_github_markdown().as_bytes())?;
+        if let Ok(variance) = wrk.variance(HistoryPeriod::Last) {
+            let mut variance_file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .truncate(true)
+                .open("/tmp/smithy_rs_benchmark_variance.txt")
+                .unwrap();
+            variance_file.write_all(variance.to_github_markdown().as_bytes())?;
+        }
     }
     Ok(())
 }
