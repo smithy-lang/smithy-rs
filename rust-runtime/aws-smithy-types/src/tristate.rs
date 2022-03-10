4f07045a0c43f9e579c502ba214d9f8bb4f7c617
Zelda Hessler <zhessler@amazon.com>
2022-03-10 11:23:58 -0600

    add: missing copyright header

--- rust-runtime/aws-smithy-types/src/tristate.rs
+++ rust-runtime/aws-smithy-types/src/tristate.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
 //! Contains [`TriState`](TriState) definition and impls
 
 /// Utility for tracking set vs. unset vs explicitly disabled
