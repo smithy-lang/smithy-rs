74d6f92e0b59acd3a7dce109ec35bb0cf976f756
Zelda Hessler <zhessler@amazon.com>
2022-03-10 12:58:33 -0600

    Revert "fix: bad doc link"
    This reverts commit eaa1b68e3b5e7c15f655cd294135c40f9b52e178.

--- aws/rust-runtime/aws-types/src/config.rs
+++ aws/rust-runtime/aws-types/src/config.rs
@@ -5,10 +5,10 @@
 
 #![deny(missing_docs)]
 
-//! AWS Shared Config (deprecated, replaced with [`aws_smithy_types::SdkConfig`](aws_smithy_types::SdkConfig))
+//! AWS Shared Config (deprecated, replaced with [`aws_types::SdkConfig`](aws_types::SdkConfig))
 //!
 //! This module contains an shared configuration representation that is agnostic from a specific service.
 
-#[deprecated(since = "0.39.0", note = "renamed to aws_smithy_types::SdkConfig")]
+#[deprecated(since = "0.9.0", note = "renamed to aws_types::SdkConfig")]
 /// AWS Shared Configuration
 pub type Config = super::SdkConfig;
