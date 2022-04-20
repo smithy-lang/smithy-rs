1c451085ec96ecaaffa36689327fb901d6de5a37
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2022-04-20 13:58:47 +0200

    chore: implement with opaque future

--- rust-runtime/aws-smithy-http-server/src/routing/into_make_lambda_service.rs
+++ rust-runtime/aws-smithy-http-server/src/routing/into_make_lambda_service.rs
@@ -5,19 +5,17 @@
 
 // This code was copied and then modified from https://github.com/hanabu/lambda-web
 
-use crate::{runtime_error::RuntimeErrorKind};
+use futures_util::future::BoxFuture;
 use hyper::{
     service::Service as HyperService,
     body::HttpBody as HyperHttpBody,
 };
-use lambda_http::{Body, Error as LambdaError, Request, Response, Service};
+use lambda_http::{Body, Error as LambdaError, IntoResponse, Request, Response, Service};
 use std::{
     convert::Infallible,
     error::Error as StdError,
     fmt::Debug,
-    future::Future,
     marker::PhantomData,
-    pin::Pin,
     task::{Context, Poll},
 };
 
@@ -28,14 +26,14 @@ type HyperResponse<B> = hyper::Response<B>;
 #[derive(Debug, Clone)]
 pub struct IntoMakeLambdaService<'a, S> {
     service: S,
-    _phantom: PhantomData<&'a ()>,
+    _marker: PhantomData<&'a ()>,
 }
 
-impl<'a, S> IntoMakeLambdaService<'a, S>{
+impl<'a, S> IntoMakeLambdaService<'a, S> {
     pub(super) fn new(service: S) -> Self {
         Self {
             service,
-            _phantom: PhantomData,
+            _marker: PhantomData,
         }
     }
 }
@@ -43,14 +41,15 @@ impl<'a, S> IntoMakeLambdaService<'a, S>{
 impl<'a, S, B> Service<Request> for IntoMakeLambdaService<'a, S>
 where
     S: HyperService<HyperRequest, Response = HyperResponse<B>, Error = Infallible>
-        + Send + 'static,
+        + Send + Clone + 'static,
     S::Future: Send + 'a,
-    B: HyperHttpBody + Debug,
+    B: HyperHttpBody + Send + Debug,
     <B as HyperHttpBody>::Error: StdError + Send + Sync + 'static,
+    <B as HyperHttpBody>::Data: Send,
 {
     type Error = LambdaError;
     type Response = Response<Body>;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+    type Future = MakeRouteLambdaServiceFuture;
 
     fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
@@ -64,39 +63,29 @@ where
         let hyper_request = lambda_to_hyper_request(event);
 
         // Call hyper service when request parsing succeeded
-        let svc_call = hyper_request.map(|req| self.service.call(req));
+        let svc_call = hyper_request.map(|req| Service::call(&mut self.service.clone(), req));
 
         let fut = async move {
-            // let svc_fut = svc_call.expect("Request parsing error");
-            // let response = svc_fut.await.expect("Some hyper error");
-            // hyper_to_lambda_response(response).await
             match svc_call {
                 Ok(svc_fut) => {
                     // Request parsing succeeded
                     let response = svc_fut.await.expect("It should not fail");
                     // Returns as Lambda response
                     hyper_to_lambda_response(response).await
-                    // match svc_fut.await {
-                    //     Ok(response) => {
-                    //         // Returns as Lambda response
-                    //         hyper_to_lambda_response(response).await
-                    //     }
-                    //     Err(response_err) => {
-                    //         // Some hyper error -> 500 Internal Server Error
-                    //         Err(response_err)
-                    //         // Err(RuntimeErrorKind::InternalFailure(crate::Error::new(response_err)))
-                    //     }
-                    // }
                 }
                 Err(request_err) => {
                     // Request parsing error
                     Err(request_err.into())
-                    // Err(RuntimeErrorKind::Serialization(crate::Error::new(request_err)))
                 }
             }
         };
-        Box::pin(fut)
+        MakeRouteLambdaServiceFuture::new(Box::pin(fut))
     }
+}
+
+opaque_future! {
+    /// Response future for [`IntoMakeLambdaService`] services.
+    pub type MakeRouteLambdaServiceFuture = BoxFuture<'static, Result<Response<Body>, LambdaError>>;
 }
 
 fn lambda_to_hyper_request(event: Request) -> Result<HyperRequest, hyper::Error> {
@@ -123,7 +112,7 @@ where
     let body = hyper::body::to_bytes(body).await?;
     let res = Response::from_parts(parts, Body::from(body.as_ref()));
     println!("Lambda response {:?}", res);
-    Ok(res)
+    Ok(res.into_response())
 }
 
 #[cfg(test)]
