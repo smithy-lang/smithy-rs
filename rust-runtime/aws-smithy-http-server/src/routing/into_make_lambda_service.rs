0637951a064a79f863ea1528523b5ec1f133abc5
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2022-05-03 00:22:23 +0200

    chore(http-server): add unit tests

--- rust-runtime/aws-smithy-http-server/src/routing/into_make_lambda_service.rs
+++ rust-runtime/aws-smithy-http-server/src/routing/into_make_lambda_service.rs
@@ -137,6 +137,15 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::{body::to_boxed, test_helpers::get_body_as_string};
+    use http::header::CONTENT_TYPE;
+    use hyper::Response;
+    use lambda_http::request::LambdaRequest;
+    use serde_json::error::Error as JsonError;
+
+    fn from_str(s: &str) -> Result<Request, JsonError> {
+        serde_json::from_str(s).map(LambdaRequest::into)
+    }
 
     #[test]
     fn traits() {
@@ -144,5 +153,50 @@ mod tests {
 
         assert_send::<IntoMakeLambdaService<()>>();
         assert_sync::<IntoMakeLambdaService<()>>();
+    }
+
+    #[test]
+    fn converts_apigw_event_to_hyper_request() {
+        let input = include_str!("../../test_data/apigw_request.json");
+        let req = from_str(input).expect("failed to parse request");
+        let result = lambda_to_hyper_request(req).expect("failed to convert to hyper request");
+        assert_eq!(result.method(), "GET");
+        assert_eq!(
+            result.uri(),
+            "https://wt6mne2s9k.execute-api.us-west-2.amazonaws.com/hello?name=me"
+        );
+    }
+
+    #[tokio::test]
+    async fn converts_apigw_v2_event_to_hyper_request() {
+        let input = include_str!("../../test_data/apigw_v2_request.json");
+        let req = from_str(input).expect("failed to parse request");
+        let mut result = lambda_to_hyper_request(req).expect("failed to convert to hyper request");
+        assert_eq!(result.method(), "POST");
+        assert_eq!(result.uri(), "https://id.execute-api.us-east-1.amazonaws.com/my/path?parameter1=value1&parameter1=value2&parameter2=value");
+        let actual_body = get_body_as_string(result.body_mut()).await;
+        assert_eq!(actual_body, r##"{"message":"Hello from Lambda"}"##);
+    }
+
+    #[tokio::test]
+    async fn converts_hyper_to_lambda_response() {
+        let builder = Response::builder()
+            .status(200)
+            .header(CONTENT_TYPE, "application/json")
+            .body(to_boxed("{\"hello\":\"lambda\"}"));
+        let res = builder.expect("failed to parse response");
+        let mut result = hyper_to_lambda_response(res)
+            .await
+            .expect("failed to convert to lambda request");
+        assert_eq!(result.status(), 200);
+        assert_eq!(
+            result
+                .headers()
+                .get(CONTENT_TYPE)
+                .map(|h| h.to_str().expect("invalid header")),
+            Some("application/json")
+        );
+        let actual_body = get_body_as_string(result.body_mut()).await;
+        assert_eq!(actual_body, r##"{"hello":"lambda"}"##);
     }
 }
