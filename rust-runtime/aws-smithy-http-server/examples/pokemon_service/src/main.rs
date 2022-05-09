a0539e20b069a7de021c84521d8f3c7ba098ad6d
John DiSanti <jdisanti@amazon.com>
2022-05-09 07:50:46 -0700

    Fix `SPDX-License-Identifier` in header comments (#1377)
    * Fix misc whitespace with pre-commit
    
    * Revise copyright check in `sdk-lints`
    
    * Fix `SPDX-License-Identifier` in header comments

--- rust-runtime/aws-smithy-http-server/examples/pokemon_service/src/main.rs
+++ rust-runtime/aws-smithy-http-server/examples/pokemon_service/src/main.rs
@@ -1,6 +1,6 @@
 /*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 // This program is exported as a binary named `pokemon_service`.
--- tools/generate-doc-preview-index.sh
+++ tools/generate-doc-preview-index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 if [[ $# -ne 1 ]]; then
