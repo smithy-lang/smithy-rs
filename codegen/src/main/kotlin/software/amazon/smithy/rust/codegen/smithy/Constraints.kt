80ec70539220783fe65cd89877a63994ac969ddd
david-perez <d@vidp.dev>
2022-05-10 17:45:10 +0200

    replace DFS in Constraints.kt in favor of Walker implementation

--- codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/Constraints.kt
+++ codegen/src/main/kotlin/software/amazon/smithy/rust/codegen/smithy/Constraints.kt
@@ -2,6 +2,7 @@ package software.amazon.smithy.rust.codegen.smithy
 
 import software.amazon.smithy.codegen.core.SymbolProvider
 import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.neighbor.Walker
 import software.amazon.smithy.model.shapes.CollectionShape
 import software.amazon.smithy.model.shapes.ListShape
 import software.amazon.smithy.model.shapes.MapShape
@@ -14,8 +15,6 @@ import software.amazon.smithy.model.traits.PatternTrait
 import software.amazon.smithy.model.traits.RangeTrait
 import software.amazon.smithy.model.traits.RequiredTrait
 import software.amazon.smithy.rust.codegen.util.hasTrait
-
-// TODO Unit test these functions and then refactor to use a `Walker` instead of hand-rolling our own DFS.
 
 /**
  * A shape has a constraint trait if it has one of these traits attached.
@@ -52,39 +51,29 @@ fun Shape.isConstrained(symbolProvider: SymbolProvider) = when (this) {
     }
 }
 
-fun StructureShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider, visited: Set<Shape> = emptySet()): Boolean {
-    if (this.isConstrained(symbolProvider)) {
-        return true
-    }
+fun StructureShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider) =
+    Walker(model).walkShapes(this).toSet().any { it.isConstrained(symbolProvider) }
 
-    return this.members().map { model.expectShape(it.target) }.any {
-        it.isConstrained(symbolProvider) || unconstrainedShapeCanReachConstrainedShape(it, model, symbolProvider, visited)
-    }
-}
-
-fun CollectionShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider, visited: Set<Shape> = emptySet()): Boolean {
-    if (this.isConstrained(symbolProvider)) {
-        return true
-    }
-
-    val member = model.expectShape(this.member.target)
-
-    return member.isConstrained(symbolProvider) || unconstrainedShapeCanReachConstrainedShape(member, model, symbolProvider, visited)
-}
+fun CollectionShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider) =
+    Walker(model).walkShapes(this).toSet().any { it.isConstrained(symbolProvider) }
 
 fun ListShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider) = (this as CollectionShape).canReachConstrainedShape(model, symbolProvider)
 fun SetShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider) = (this as CollectionShape).canReachConstrainedShape(model, symbolProvider)
 
-fun MapShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider, visited: Set<Shape> = emptySet()): Boolean {
-    if (this.isConstrained(symbolProvider)) {
-        return true
+fun MapShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider) =
+    Walker(model).walkShapes(this).toSet().any { it.isConstrained(symbolProvider) }
+
+fun MemberShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider): Boolean =
+    this.isConstrained(symbolProvider) || this.targetCanReachConstrainedShape(model, symbolProvider)
+
+// TODO Callers should use `MemberShape.canReachConstrainedShape`, and this function should be inlined.
+fun MemberShape.targetCanReachConstrainedShape(model: Model, symbolProvider: SymbolProvider): Boolean =
+    when (val targetShape = model.expectShape(this.target)) {
+        is CollectionShape -> targetShape.canReachConstrainedShape(model, symbolProvider)
+        is MapShape -> targetShape.asMapShape().get().canReachConstrainedShape(model, symbolProvider)
+        is StructureShape -> targetShape.asStructureShape().get().canReachConstrainedShape(model, symbolProvider)
+        else -> false
     }
-
-    val key = model.expectShape(this.key.target)
-    val value = model.expectShape(this.value.target)
-
-    return key.isConstrained(symbolProvider) || value.isConstrained(symbolProvider) || unconstrainedShapeCanReachConstrainedShape(value, model, symbolProvider, visited)
-}
 
 fun MemberShape.requiresNewtype() =
     // Note that member shapes whose only constraint trait is `required` do not require a newtype.
@@ -93,35 +82,3 @@ fun MemberShape.requiresNewtype() =
             // `uniqueItems` is deprecated, so we ignore it.
             // this.hasTrait<UniqueItemsTrait>() ||
             this.hasTrait<PatternTrait>()
-
-fun MemberShape.canReachConstrainedShape(model: Model, symbolProvider: SymbolProvider): Boolean =
-    this.isConstrained(symbolProvider) || this.targetCanReachConstrainedShape(model, symbolProvider)
-
-// TODO Callers should use `MemberShape.canReachConstrainedShape`, and this function should be inlined.
-fun MemberShape.targetCanReachConstrainedShape(model: Model, symbolProvider: SymbolProvider): Boolean =
-    when (val targetShape = model.expectShape(this.target)) {
-        // TODO Use is CollectionShape
-        is ListShape -> targetShape.asListShape().get().canReachConstrainedShape(model, symbolProvider)
-        is SetShape -> targetShape.asSetShape().get().canReachConstrainedShape(model, symbolProvider)
-        is MapShape -> targetShape.asMapShape().get().canReachConstrainedShape(model, symbolProvider)
-        is StructureShape -> targetShape.asStructureShape().get().canReachConstrainedShape(model, symbolProvider)
-        else -> false
-    }
-
-private fun unconstrainedShapeCanReachConstrainedShape(shape: Shape, model: Model, symbolProvider: SymbolProvider, visited: Set<Shape>): Boolean {
-    check(!shape.isConstrained(symbolProvider)) { "This function can only be called with unconstrained shapes" }
-
-    if (visited.contains(shape)) {
-        return false
-    }
-
-    val newVisited = setOf(shape).plus(visited)
-
-    return when (shape) {
-        is StructureShape -> shape.canReachConstrainedShape(model, symbolProvider, newVisited)
-        is CollectionShape -> shape.canReachConstrainedShape(model, symbolProvider, newVisited)
-        is MapShape -> shape.canReachConstrainedShape(model, symbolProvider, newVisited)
-        // TODO(https://github.com/awslabs/smithy-rs/pull/1199) Constraint traits on simple shapes.
-        else -> false
-    }
-}
