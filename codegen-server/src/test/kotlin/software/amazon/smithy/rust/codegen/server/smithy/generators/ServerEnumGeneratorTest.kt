1ca293992d3a04f4513e0c3c4938fa3fdc2a24dd
Daniele Ahmed <ahmeddan@amazon.de>
2022-05-20 11:13:39 +0000

    update tests
    Signed-off-by: Daniele Ahmed <ahmeddan@amazon.de>

--- codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerEnumGeneratorTest.kt
+++ codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerEnumGeneratorTest.kt
@@ -5,217 +5,80 @@
 
 package software.amazon.smithy.rust.codegen.server.smithy.generators
 
-import io.kotest.matchers.string.shouldContain
+import io.kotest.matchers.string.shouldNotContain
 import org.junit.jupiter.api.Test
 import software.amazon.smithy.model.shapes.StringShape
-import software.amazon.smithy.model.traits.EnumTrait
 import software.amazon.smithy.rust.codegen.rustlang.RustWriter
+import software.amazon.smithy.rust.codegen.server.smithy.testutil.serverTestSymbolProvider
 import software.amazon.smithy.rust.codegen.testutil.TestRuntimeConfig
 import software.amazon.smithy.rust.codegen.testutil.asSmithyModel
 import software.amazon.smithy.rust.codegen.testutil.compileAndTest
-import software.amazon.smithy.rust.codegen.testutil.testSymbolProvider
 import software.amazon.smithy.rust.codegen.util.expectTrait
 import software.amazon.smithy.rust.codegen.util.lookup
 
 class ServerEnumGeneratorTest {
+    private val model = """
+        namespace test
+        @enum([
+            {
+                value: "t2.nano",
+                name: "T2_NANO",
+                documentation: "T2 instances are Burstable Performance Instances.",
+                tags: ["ebsOnly"]
+            },
+            {
+                value: "t2.micro",
+                name: "T2_MICRO",
+                documentation: "T2 instances are Burstable Performance Instances.",
+                tags: ["ebsOnly"]
+            },
+        ])
+        string InstanceType
+    """.asSmithyModel()
+
     @Test
-    fun `it generates named enums`() {
-        val model = """
-            namespace test
-            @enum([
-                {
-                    value: "t2.nano",
-                    name: "T2_NANO",
-                    documentation: "T2 instances are Burstable Performance Instances.",
-                    tags: ["ebsOnly"]
-                },
-                {
-                    value: "t2.micro",
-                    name: "T2_MICRO",
-                    documentation: "T2 instances are Burstable Performance Instances.",
-                    tags: ["ebsOnly"]
-                },
-            ])
-            string InstanceType
-        """.asSmithyModel()
-        val provider = testSymbolProvider(model)
+    fun `it generates TryFrom, FromStr and errors for enums`() {
+        val provider = serverTestSymbolProvider(model)
         val writer = RustWriter.forModule("model")
         val shape = model.lookup<StringShape>("test#InstanceType")
         val generator = ServerEnumGenerator(model, provider, writer, shape, shape.expectTrait(), TestRuntimeConfig)
         generator.render()
         writer.compileAndTest(
             """
-            let instance = InstanceType::T2Micro;
-            assert_eq!(instance.as_str(), "t2.micro");
+            use std::str::FromStr;
             assert_eq!(InstanceType::try_from("t2.nano").unwrap(), InstanceType::T2Nano);
+            assert_eq!(InstanceType::from_str("t2.nano").unwrap(), InstanceType::T2Nano);
+            assert_eq!(InstanceType::try_from("unknown").unwrap_err(), InstanceTypeUnknownVariantError("unknown".to_string()));
+            """
+        )
+    }
+
+    @Test
+    fun `it generates named enums without the unknown variant`() {
+        val provider = serverTestSymbolProvider(model)
+        val writer = RustWriter.forModule("model")
+        val shape = model.lookup<StringShape>("test#InstanceType")
+        val generator = ServerEnumGenerator(model, provider, writer, shape, shape.expectTrait(), TestRuntimeConfig)
+        generator.render()
+        writer.compileAndTest(
+            """
             // check no unknown
+            let instance = InstanceType::T2Micro;
             match instance {
             InstanceType::T2Micro => (),
             InstanceType::T2Nano => (),
             }
             """
         )
-
-        writer.toString() shouldContain "#[non_exhaustive]"
     }
 
     @Test
-    fun `named enums implement eq and hash`() {
-        val model = """
-            namespace test
-            @enum([
-            {
-                value: "Foo",
-                name: "Foo",
-            },
-            {
-                value: "Bar",
-                name: "Bar"
-            }])
-            string FooEnum
-        """.asSmithyModel()
-        val shape = model.lookup<StringShape>("test#FooEnum")
-        val trait = shape.expectTrait<EnumTrait>()
+    fun `it generates named enums without non_exhaustive`() {
+        val provider = serverTestSymbolProvider(model)
         val writer = RustWriter.forModule("model")
-        val generator = ServerEnumGenerator(model, testSymbolProvider(model), writer, shape, trait, TestRuntimeConfig)
+        val shape = model.lookup<StringShape>("test#InstanceType")
+        val generator = ServerEnumGenerator(model, provider, writer, shape, shape.expectTrait(), TestRuntimeConfig)
         generator.render()
-        writer.compileAndTest(
-            """
-            assert_eq!(FooEnum::Foo, FooEnum::Foo);
-            assert_ne!(FooEnum::Bar, FooEnum::Foo);
-            let mut hash_of_enums = std::collections::HashSet::new();
-            hash_of_enums.insert(FooEnum::Foo);
-            """.trimIndent()
-        )
-    }
-
-    @Test
-    fun `unnamed enums implement eq and hash`() {
-        val model = """
-            namespace test
-            @enum([
-            {
-                value: "Foo",
-            },
-            {
-                value: "Bar",
-            }])
-            string FooEnum
-        """.asSmithyModel()
-        val shape = model.lookup<StringShape>("test#FooEnum")
-        val trait = shape.expectTrait<EnumTrait>()
-        val writer = RustWriter.forModule("model")
-        val generator = ServerEnumGenerator(model, testSymbolProvider(model), writer, shape, trait, TestRuntimeConfig)
-        generator.render()
-        writer.compileAndTest(
-            """
-            assert_eq!(FooEnum::try_from("Foo").unwrap(), FooEnum::try_from("Foo").unwrap());
-            assert_ne!(FooEnum::try_from("Bar").unwrap(), FooEnum::try_from("Foo").unwrap());
-            let mut hash_set_of_enums = std::collections::HashSet::new();
-            hash_set_of_enums.insert(FooEnum::try_from("Foo").unwrap());
-            """
-        )
-    }
-
-    @Test
-    fun `it escapes the Unknown variant if the enum has an unknown value in the model`() {
-        val model = """
-            namespace test
-            @enum([
-                { name: "Known", value: "Known" },
-                { name: "Unknown", value: "Unknown" },
-                { name: "UnknownValue", value: "UnknownValue" },
-            ])
-            string SomeEnum
-        """.asSmithyModel()
-
-        val shape: StringShape = model.lookup("test#SomeEnum")
-        val trait = shape.expectTrait<EnumTrait>()
-        val provider = testSymbolProvider(model)
-        val writer = RustWriter.forModule("model")
-        ServerEnumGenerator(model, provider, writer, shape, trait, TestRuntimeConfig).render()
-
-        writer.compileAndTest(
-            """
-            assert_eq!(SomeEnum::try_from("Unknown").unwrap(), SomeEnum::UnknownValue);
-            assert_eq!(SomeEnum::try_from("UnknownValue").unwrap(), SomeEnum::UnknownValue_);
-            assert_eq!(SomeEnum::try_from("SomethingNew").is_err(), true);
-            """
-        )
-    }
-
-    @Test
-    fun `it should generate documentation for enums`() {
-        val model = """
-            namespace test
-
-            /// Some top-level documentation.
-            @enum([
-                { name: "Known", value: "Known" },
-                { name: "Unknown", value: "Unknown" },
-            ])
-            string SomeEnum
-        """.asSmithyModel()
-
-        val shape: StringShape = model.lookup("test#SomeEnum")
-        val trait = shape.expectTrait<EnumTrait>()
-        val provider = testSymbolProvider(model)
-        val rendered = RustWriter.forModule("model").also { ServerEnumGenerator(model, provider, it, shape, trait, TestRuntimeConfig).render() }.toString()
-
-        rendered shouldContain
-            """
-            /// Some top-level documentation.
-            ///
-            /// _Note: `SomeEnum::Unknown` has been renamed to `::UnknownValue`._
-            """.trimIndent()
-    }
-
-    @Test
-    fun `it should generate documentation for unnamed enums`() {
-        val model = """
-            namespace test
-
-            /// Some top-level documentation.
-            @enum([
-                { value: "One" },
-                { value: "Two" },
-            ])
-            string SomeEnum
-        """.asSmithyModel()
-
-        val shape: StringShape = model.lookup("test#SomeEnum")
-        val trait = shape.expectTrait<EnumTrait>()
-        val provider = testSymbolProvider(model)
-        val rendered = RustWriter.forModule("model").also { ServerEnumGenerator(model, provider, it, shape, trait, TestRuntimeConfig).render() }.toString()
-
-        rendered shouldContain
-            """
-            /// Some top-level documentation.
-            """.trimIndent()
-    }
-
-    @Test
-    fun `it handles variants that clash with Rust reserved words`() {
-        val model = """
-            namespace test
-            @enum([
-                { name: "Known", value: "Known" },
-                { name: "Self", value: "other" },
-            ])
-            string SomeEnum
-        """.asSmithyModel()
-
-        val shape: StringShape = model.lookup("test#SomeEnum")
-        val trait = shape.expectTrait<EnumTrait>()
-        val provider = testSymbolProvider(model)
-        val writer = RustWriter.forModule("model")
-        ServerEnumGenerator(model, provider, writer, shape, trait, TestRuntimeConfig).render()
-
-        writer.compileAndTest(
-            """
-            assert_eq!(SomeEnum::try_from("other").unwrap(), SomeEnum::SelfValue);
-            assert_eq!(SomeEnum::try_from("SomethingNew").is_err(), true);
-            """
-        )
+        writer.toString() shouldNotContain "#[non_exhaustive]"
     }
 }
