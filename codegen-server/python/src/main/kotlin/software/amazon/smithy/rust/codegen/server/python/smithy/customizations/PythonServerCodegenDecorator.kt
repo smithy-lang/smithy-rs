8cca266dec519c18fd6099109d2d7921995bc562
Bigo <1781140+crisidev@users.noreply.github.com>
2022-05-21 23:05:53 +0100

    Add Python server decorator

---
+++ codegen-server/python/src/main/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/customizations/PythonServerCodegenDecorator.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.rust.codegen.server.python.smithy.customizations
+
+import software.amazon.smithy.rust.codegen.server.smithy.customizations.AddInternalServerErrorToAllOpsDecorator
+import software.amazon.smithy.rust.codegen.smithy.CodegenContext
+import software.amazon.smithy.rust.codegen.smithy.customize.CombinedCodegenDecorator
+import software.amazon.smithy.rust.codegen.smithy.customize.RustCodegenDecorator
+import software.amazon.smithy.rust.codegen.smithy.generators.ManifestCustomizations
+import java.util.logging.Logger
+
+val DECORATORS = listOf(AddInternalServerErrorToAllOpsDecorator(), CdylibDecorator())
+
+class CdylibDecorator : RustCodegenDecorator {
+    override val name: String = "CdylibDecorator"
+    override val order: Byte = 0
+
+    private val logger: Logger = Logger.getLogger(javaClass.name)
+
+    override fun crateManifestCustomizations(
+        codegenContext: CodegenContext
+    ): ManifestCustomizations =
+        mapOf("lib" to mapOf("name" to codegenContext.settings.moduleName, "crate-type" to listOf("cdylib")))
+}
+
+class PythonServerCodegenDecorator : CombinedCodegenDecorator(DECORATORS) {
+    override val name: String = "PythonServerCodegenDecorator"
+    override val order: Byte = -1
+}
