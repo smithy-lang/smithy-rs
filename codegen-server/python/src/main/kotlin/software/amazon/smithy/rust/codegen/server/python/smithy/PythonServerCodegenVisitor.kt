95d9db2d624be6dad7c8003bc9dd7677005a48b9
Bigo <1781140+crisidev@users.noreply.github.com>
2022-05-23 13:26:30 +0100

    Remove leftovers

--- codegen-server/python/src/main/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/PythonServerCodegenVisitor.kt
+++ codegen-server/python/src/main/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/PythonServerCodegenVisitor.kt
@@ -8,10 +8,7 @@ package software.amazon.smithy.rust.codegen.server.python.smithy
 
 import software.amazon.smithy.build.PluginContext
 import software.amazon.smithy.model.shapes.ServiceShape
-import software.amazon.smithy.model.shapes.StringShape
 import software.amazon.smithy.model.shapes.StructureShape
-import software.amazon.smithy.model.shapes.UnionShape
-import software.amazon.smithy.model.traits.EnumTrait
 import software.amazon.smithy.rust.codegen.server.python.smithy.generators.PythonServerServiceGenerator
 import software.amazon.smithy.rust.codegen.server.python.smithy.generators.PythonServerStructureGenerator
 import software.amazon.smithy.rust.codegen.server.smithy.ServerCodegenVisitor
@@ -24,10 +21,7 @@ import software.amazon.smithy.rust.codegen.smithy.SymbolVisitorConfig
 import software.amazon.smithy.rust.codegen.smithy.customize.RustCodegenDecorator
 import software.amazon.smithy.rust.codegen.smithy.generators.BuilderGenerator
 import software.amazon.smithy.rust.codegen.smithy.generators.CodegenTarget
-import software.amazon.smithy.rust.codegen.smithy.generators.EnumGenerator
-import software.amazon.smithy.rust.codegen.smithy.generators.UnionGenerator
 import software.amazon.smithy.rust.codegen.smithy.generators.implBlock
-import software.amazon.smithy.rust.codegen.util.getTrait
 
 /**
  * Entrypoint for server-side code generation. This class will walk the in-memory model and
@@ -75,34 +69,6 @@ class PythonServerCodegenVisitor(context: PluginContext, private val codegenDeco
             writer.implBlock(shape, symbolProvider) {
                 builderGenerator.renderConvenienceMethod(this)
             }
-        }
-    }
-
-    /**
-     * String Shape Visitor
-     *
-     * Although raw strings require no code generation, enums are actually [EnumTrait] applied to string shapes.
-     */
-    override fun stringShape(shape: StringShape) {
-        logger.info("[python-server-codegen] Generating an enum $shape")
-        shape.getTrait<EnumTrait>()?.also { enum ->
-            rustCrate.useShapeWriter(shape) { writer ->
-                EnumGenerator(model, symbolProvider, writer, shape, enum).render()
-            }
-        }
-    }
-
-    /**
-     * Union Shape Visitor
-     *
-     * Generate an `enum` for union shapes.
-     *
-     * This function _does not_ generate any serializers.
-     */
-    override fun unionShape(shape: UnionShape) {
-        logger.info("[python-server-codegen] Generating an union $shape")
-        rustCrate.useShapeWriter(shape) {
-            UnionGenerator(model, symbolProvider, it, shape, renderUnknownVariant = false).render()
         }
     }
 
