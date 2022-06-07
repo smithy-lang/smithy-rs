eaf918b8fff12f08e4c2ac7d9232493facb84ab6
david-perez <d@vidp.dev>
2022-06-07 14:46:54 +0200

    Revert "Fail at trying to rewrite unit tests spanning multiple modules using project and unitTest() to writer.compileAndTest()"
    This reverts commit ad86d7d3c0b257850f07d9517c7ecb0fba179d63.

--- codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/UnconstrainedCollectionGeneratorTest.kt
+++ codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/UnconstrainedCollectionGeneratorTest.kt
@@ -9,160 +9,148 @@ import org.junit.jupiter.api.Test
 import software.amazon.smithy.model.shapes.ListShape
 import software.amazon.smithy.model.shapes.ServiceShape
 import software.amazon.smithy.model.shapes.StructureShape
-import software.amazon.smithy.rust.codegen.rustlang.RustWriter
+import software.amazon.smithy.rust.codegen.rustlang.RustModule
 import software.amazon.smithy.rust.codegen.server.smithy.ConstraintViolationSymbolProvider
 import software.amazon.smithy.rust.codegen.server.smithy.testutil.serverRenderWithModelBuilder
 import software.amazon.smithy.rust.codegen.server.smithy.testutil.serverTestSymbolProvider
 import software.amazon.smithy.rust.codegen.smithy.ModelsModule
 import software.amazon.smithy.rust.codegen.smithy.PubCrateConstrainedShapeSymbolProvider
 import software.amazon.smithy.rust.codegen.smithy.UnconstrainedShapeSymbolProvider
+import software.amazon.smithy.rust.codegen.testutil.TestWorkspace
 import software.amazon.smithy.rust.codegen.testutil.asSmithyModel
 import software.amazon.smithy.rust.codegen.testutil.compileAndTest
+import software.amazon.smithy.rust.codegen.testutil.unitTest
 import software.amazon.smithy.rust.codegen.util.lookup
 
 class UnconstrainedCollectionGeneratorTest {
-    private val model =
-        """
-        namespace test
+    @Test
+    fun `it should generate unconstrained lists`() {
+        val model =
+            """
+            namespace test
 
-        service TestService {
-            version: "123",
-            operations: [TestOperation]
-        }
-        
-        operation TestOperation {
-            input: TestInputOutput,
-            output: TestInputOutput,
-        }
-        
-        structure TestInputOutput {
-            list: ListA
-        }
-        
-        list ListA {
-            member: ListB
-        }
-        
-        list ListB {
-            member: StructureC
-        }
-        
-        structure StructureC {
-            @required
-            int: Integer,
+            service TestService {
+                version: "123",
+                operations: [TestOperation]
+            }
             
-            @required
-            string: String
+            operation TestOperation {
+                input: TestInputOutput,
+                output: TestInputOutput,
+            }
+            
+            structure TestInputOutput {
+                list: ListA
+            }
+            
+            list ListA {
+                member: ListB
+            }
+            
+            list ListB {
+                member: StructureC
+            }
+            
+            structure StructureC {
+                @required
+                int: Integer,
+                
+                @required
+                string: String
+            }
+            """.asSmithyModel()
+        val symbolProvider = serverTestSymbolProvider(model)
+
+        val serviceShape = model.lookup<ServiceShape>("test#TestService")
+        val listA = model.lookup<ListShape>("test#ListA")
+        val listB = model.lookup<ListShape>("test#ListB")
+
+        val project = TestWorkspace.testProject(symbolProvider)
+
+        project.withModule(RustModule.public("model")) { writer ->
+            model.lookup<StructureShape>("test#StructureC").serverRenderWithModelBuilder(model, symbolProvider, writer)
         }
-        """.asSmithyModel()
-    private val symbolProvider = serverTestSymbolProvider(model)
 
-    private val serviceShape = model.lookup<ServiceShape>("test#TestService")
-    private val listA = model.lookup<ListShape>("test#ListA")
-    private val listB = model.lookup<ListShape>("test#ListB")
-
-    private val unconstrainedShapeSymbolProvider = UnconstrainedShapeSymbolProvider(symbolProvider, model, serviceShape)
-    private val pubCrateConstrainedShapeSymbolProvider =
-        PubCrateConstrainedShapeSymbolProvider(symbolProvider, model, serviceShape)
-
-    private fun renderModel(): RustWriter {
-        val writer = RustWriter.root()
-        val modelsModuleWriter = writer.withModule(ModelsModule.name) {
-            model.lookup<StructureShape>("test#StructureC").serverRenderWithModelBuilder(model, symbolProvider, this)
-        }
-
-        writer.withModule("constrained") {
-            listOf(listA, listB).forEach { shape ->
+        val unconstrainedShapeSymbolProvider = UnconstrainedShapeSymbolProvider(symbolProvider, model, serviceShape)
+        val pubCrateConstrainedShapeSymbolProvider = PubCrateConstrainedShapeSymbolProvider(symbolProvider, model, serviceShape)
+        project.withModule(RustModule.private("constrained")) { writer ->
+            listOf(listA, listB).forEach {
                 PubCrateConstrainedCollectionGenerator(
                     model,
                     symbolProvider,
                     unconstrainedShapeSymbolProvider,
                     pubCrateConstrainedShapeSymbolProvider,
-                    this,
-                    shape
-                ).render()
-            }
-        }
-        writer.withModule("unconstrained") {
-            val unconstrainedModuleWriter = this
-
-            val constraintViolationSymbolProvider =
-                ConstraintViolationSymbolProvider(symbolProvider, model, serviceShape)
-            listOf(listA, listB).forEach {
-                UnconstrainedCollectionGenerator(
-                    model,
-                    symbolProvider,
-                    unconstrainedShapeSymbolProvider,
-                    pubCrateConstrainedShapeSymbolProvider,
-                    constraintViolationSymbolProvider,
-                    unconstrainedModuleWriter,
-                    modelsModuleWriter,
+                    writer,
                     it
                 ).render()
             }
         }
+        project.withModule(RustModule.private("unconstrained")) { unconstrainedModuleWriter ->
+            project.withModule(ModelsModule) { modelsModuleWriter ->
+                val constraintViolationSymbolProvider = ConstraintViolationSymbolProvider(symbolProvider, model, serviceShape)
+                listOf(listA, listB).forEach {
+                    UnconstrainedCollectionGenerator(
+                        model,
+                        symbolProvider,
+                        unconstrainedShapeSymbolProvider,
+                        pubCrateConstrainedShapeSymbolProvider,
+                        constraintViolationSymbolProvider,
+                        unconstrainedModuleWriter,
+                        modelsModuleWriter,
+                        it
+                    ).render()
+                }
 
-        return writer
-    }
+                unconstrainedModuleWriter.unitTest(
+                    name = "list_a_unconstrained_fail_to_constrain_with_first_error",
+                    test = """
+                    let c_builder1 = crate::model::StructureC::builder().int(69);
+                    let c_builder2 = crate::model::StructureC::builder().string(String::from("david"));
+                    let list_b_unconstrained = list_b_unconstrained::ListBUnconstrained(vec![c_builder1, c_builder2]);
+                    let list_a_unconstrained = list_a_unconstrained::ListAUnconstrained(vec![list_b_unconstrained]);
 
-    @Test
-    fun `it should fail to constrain with the first error`() {
-        val writer = renderModel()
+                    let expected_err =
+                        crate::model::list_a::ConstraintViolation(crate::model::list_b::ConstraintViolation(
+                            crate::model::structure_c::ConstraintViolation::MissingString,
+                        ));
+                        
+                    assert_eq!(
+                        expected_err,
+                        crate::constrained::list_a_constrained::ListAConstrained::try_from(list_a_unconstrained).unwrap_err()
+                    );
+                """
+                )
 
-        writer.compileAndTest(
-            """
-            let c_builder1 = crate::model::StructureC::builder().int(69);
-            let c_builder2 = crate::model::StructureC::builder().string(String::from("david"));
-            let list_b_unconstrained = list_b_unconstrained::ListBUnconstrained(vec![c_builder1, c_builder2]);
-            let list_a_unconstrained = list_a_unconstrained::ListAUnconstrained(vec![list_b_unconstrained]);
+                unconstrainedModuleWriter.unitTest(
+                    name = "list_a_unconstrained_succeed_to_constrain",
+                    test = """
+                    let c_builder = crate::model::StructureC::builder().int(69).string(String::from("david"));
+                    let list_b_unconstrained = list_b_unconstrained::ListBUnconstrained(vec![c_builder]);
+                    let list_a_unconstrained = list_a_unconstrained::ListAUnconstrained(vec![list_b_unconstrained]);
 
-            let expected_err =
-                crate::model::list_a::ConstraintViolation(crate::model::list_b::ConstraintViolation(
-                    crate::model::structure_c::ConstraintViolation::MissingString,
-                ));
-                
-            assert_eq!(
-                expected_err,
-                crate::constrained::list_a_constrained::ListAConstrained::try_from(list_a_unconstrained).unwrap_err()
-            );
-            """
-        )
-    }
+                    let expected: Vec<Vec<crate::model::StructureC>> = vec![vec![crate::model::StructureC {
+                        string: String::from("david"),
+                        int: 69
+                    }]];
+                    let actual: Vec<Vec<crate::model::StructureC>> = 
+                        crate::constrained::list_a_constrained::ListAConstrained::try_from(list_a_unconstrained).unwrap().into();
+                        
+                    assert_eq!(expected, actual);
+                """
+                )
 
-    @Test
-    fun `it should succeed to constrain`() {
-        val writer = renderModel()
+                unconstrainedModuleWriter.unitTest(
+                    name = "list_a_unconstrained_converts_into_constrained",
+                    test = """
+                    let c_builder = crate::model::StructureC::builder();
+                    let list_b_unconstrained = list_b_unconstrained::ListBUnconstrained(vec![c_builder]);
+                    let list_a_unconstrained = list_a_unconstrained::ListAUnconstrained(vec![list_b_unconstrained]);
 
-        writer.compileAndTest(
-            """
-            let c_builder = crate::model::StructureC::builder().int(69).string(String::from("david"));
-            let list_b_unconstrained = list_b_unconstrained::ListBUnconstrained(vec![c_builder]);
-            let list_a_unconstrained = list_a_unconstrained::ListAUnconstrained(vec![list_b_unconstrained]);
-
-            let expected: Vec<Vec<crate::model::StructureC>> = vec![vec![crate::model::StructureC {
-                string: String::from("david"),
-                int: 69
-            }]];
-            let actual: Vec<Vec<crate::model::StructureC>> = 
-                crate::constrained::list_a_constrained::ListAConstrained::try_from(list_a_unconstrained).unwrap().into();
-                
-            assert_eq!(expected, actual);
-            """
-        )
-    }
-
-    @Test
-    fun `it should convert into MaybeConstrained`() {
-        val writer = renderModel()
-
-        writer.compileAndTest(
-            """
-            let c_builder = crate::model::StructureC::builder();
-            let list_b_unconstrained = list_b_unconstrained::ListBUnconstrained(vec![c_builder]);
-            let list_a_unconstrained = list_a_unconstrained::ListAUnconstrained(vec![list_b_unconstrained]);
-
-            let _list_a: crate::constrained::MaybeConstrained<crate::constrained::list_a_constrained::ListAConstrained> = list_a_unconstrained.into();
-            """
-        )
+                    let _list_a: crate::constrained::MaybeConstrained<crate::constrained::list_a_constrained::ListAConstrained> = list_a_unconstrained.into();
+                """
+                )
+                project.compileAndTest()
+            }
+        }
     }
 }
