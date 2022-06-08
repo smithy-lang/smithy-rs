60b910fbff41652b4c4a36eeef4a35d31cf0866e
Harry Barber <hlbarber@amazon.co.uk>
2022-06-08 12:26:12 +0000

    Modernize RustWriterTest tests

--- codegen/src/test/kotlin/software/amazon/smithy/rust/lang/RustWriterTest.kt
+++ codegen/src/test/kotlin/software/amazon/smithy/rust/lang/RustWriterTest.kt
@@ -16,81 +16,91 @@ import software.amazon.smithy.model.shapes.SetShape
 import software.amazon.smithy.model.shapes.StringShape
 import software.amazon.smithy.model.shapes.StructureShape
 import software.amazon.smithy.rust.codegen.rustlang.CargoDependency
+import software.amazon.smithy.rust.codegen.rustlang.RustModule
 import software.amazon.smithy.rust.codegen.rustlang.RustType
-import software.amazon.smithy.rust.codegen.rustlang.RustWriter
 import software.amazon.smithy.rust.codegen.rustlang.docs
 import software.amazon.smithy.rust.codegen.rustlang.isEmpty
 import software.amazon.smithy.rust.codegen.rustlang.rustBlock
 import software.amazon.smithy.rust.codegen.rustlang.writable
 import software.amazon.smithy.rust.codegen.smithy.RuntimeType
+import software.amazon.smithy.rust.codegen.testutil.TestRuntimeConfig
+import software.amazon.smithy.rust.codegen.testutil.TestWorkspace
 import software.amazon.smithy.rust.codegen.testutil.asSmithyModel
-import software.amazon.smithy.rust.codegen.testutil.compileAndRun
 import software.amazon.smithy.rust.codegen.testutil.compileAndTest
-import software.amazon.smithy.rust.codegen.testutil.shouldCompile
 import software.amazon.smithy.rust.codegen.testutil.testSymbolProvider
+import software.amazon.smithy.rust.codegen.testutil.unitTest
 import software.amazon.smithy.rust.codegen.util.lookup
 
 class RustWriterTest {
     @Test
     fun `inner modules correctly handle dependencies`() {
-        val sut = RustWriter.forModule("parent")
-        val requestBuilder = RuntimeType.HttpRequestBuilder
-        sut.withModule("inner") {
-            rustBlock("fn build(builder: #T)", requestBuilder) {
+        val project = TestWorkspace.testProject()
+        project.withModule(RustModule.public("parent")) {
+            val requestBuilder = RuntimeType.HttpRequestBuilder
+
+            it.withModule("inner") {
+                rustBlock("fn build(builder: #T)", requestBuilder) {}
             }
+            val httpDep = CargoDependency.Http.dependencies[0]
+            it.dependencies shouldContain httpDep
+            it.toString() shouldContainOnlyOnce "DO NOT EDIT"
         }
-        val httpDep = CargoDependency.Http.dependencies[0]
-        sut.dependencies shouldContain httpDep
-        sut.toString() shouldContainOnlyOnce "DO NOT EDIT"
+        project.compileAndTest()
     }
 
     @Test
     fun `manually created struct`() {
-        val sut = RustWriter.forModule("lib")
-        val stringShape = StringShape.builder().id("test#Hello").build()
-        val set = SetShape.builder()
-            .id("foo.bar#Records")
-            .member(stringShape.id)
-            .build()
-        val model = Model.assembler()
-            .addShapes(set, stringShape)
-            .assemble()
-            .unwrap()
+        val project = TestWorkspace.testProject()
+        project.lib {
+            val stringShape = StringShape.builder().id("test#Hello").build()
+            val set = SetShape.builder()
+                .id("foo.bar#Records")
+                .member(stringShape.id)
+                .build()
+            val model = Model.assembler()
+                .addShapes(set, stringShape)
+                .assemble()
+                .unwrap()
 
-        val provider: SymbolProvider = testSymbolProvider(model)
-        val setSymbol = provider.toSymbol(set)
-        val stringSymbol = provider.toSymbol(stringShape)
-        sut.rustBlock("struct Test") {
-            write("member: #T,", setSymbol)
-            write("otherMember: #T,", stringSymbol)
+            val provider: SymbolProvider = testSymbolProvider(model)
+            val setSymbol = provider.toSymbol(set)
+            val stringSymbol = provider.toSymbol(stringShape)
+            it.rustBlock("struct Test") {
+                write("member: #T,", setSymbol)
+                write("otherMember: #T,", stringSymbol)
+            }
+            it.addDependency(CargoDependency.SmithyTypes(TestRuntimeConfig))
+            it.unitTest(
+                "manually_created_struct",
+                """
+                let test = Test { member: aws_smithy_types::Set::default(), otherMember: "hello".to_string() };
+                assert_eq!(test.otherMember, "hello");
+                assert_eq!(test.member.is_empty(), true);
+                """
+            )
+            val output = it.toString()
+            output shouldContain RustType.HashSet.Type
+            output shouldContain "struct Test"
         }
-        val output = sut.toString()
-        output.shouldCompile()
-        output shouldContain RustType.HashSet.Type
-        output shouldContain "struct Test"
-        output.compileAndRun(
-            """
-            let test = Test { member: ${RustType.HashSet.Namespace}::${RustType.HashSet.Type}::default(), otherMember: "hello".to_string() };
-            assert_eq!(test.otherMember, "hello");
-            assert_eq!(test.member.is_empty(), true);
-            """
-        )
+        project.compileAndTest()
     }
 
     @Test
     fun `generate docs`() {
-        val sut = RustWriter.forModule("lib")
-        sut.docs(
-            """Top level module documentation
-            |More docs
-            |/* handle weird characters */
-            |`a backtick`
-            |[a link](asdf)
-            """.trimMargin()
-        )
-        sut.rustBlock("pub fn foo()") { }
-        sut.compileAndTest()
-        sut.toString() shouldContain "Top level module"
+        val project = TestWorkspace.testProject()
+        project.lib {
+            it.docs(
+                """Top level module documentation
+                |More docs
+                |/* handle weird characters */
+                |`a backtick`
+                |[a link](asdf)
+                """.trimMargin()
+            )
+            it.rustBlock("pub fn foo()") { }
+            it.toString() shouldContain "Top level module"
+        }
+        project.compileAndTest()
     }
 
     @Test
@@ -99,11 +109,17 @@ class RustWriterTest {
             namespace test
             structure Foo {}
         """.asSmithyModel()
-        val shape = model.lookup<StructureShape>("test#Foo")
-        val symbol = testSymbolProvider(model).toSymbol(shape)
-        val sut = RustWriter.forModule("lib")
-        sut.docs("A link! #D", symbol)
-        sut.toString() shouldContain "/// A link! [`Foo`](crate::model::Foo)"
+        val symbolProvider = testSymbolProvider(model)
+        val project = TestWorkspace.testProject(symbolProvider)
+
+        project.lib {
+            val shape = model.lookup<StructureShape>("test#Foo")
+            val symbol = symbolProvider.toSymbol(shape)
+            it.docs("A link! #D", symbol)
+            it.toString() shouldContain "/// A link! [`Foo`](crate::model::Foo)"
+            it.rustBlock("pub fn foo()") { }
+        }
+        project.compileAndTest()
     }
 
     @Test
