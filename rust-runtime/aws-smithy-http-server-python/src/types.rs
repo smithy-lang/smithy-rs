f69c211a208c33295d6368f11f388eddcb5fe783
Bigo <1781140+crisidev@users.noreply.github.com>
2022-06-10 13:21:57 +0100

    Refactor Blob wrapper to have the same Rust API as its inner from aws_smithy_types

--- rust-runtime/aws-smithy-http-server-python/src/types.rs
+++ rust-runtime/aws-smithy-http-server-python/src/types.rs
@@ -12,24 +12,42 @@ use pyo3::prelude::*;
 #[derive(Debug, Clone, PartialEq)]
 pub struct Blob(aws_smithy_types::Blob);
 
+impl Blob {
+    /// Creates a new blob from the given `input`.
+    pub fn new<T: Into<Vec<u8>>>(input: T) -> Self {
+        Self(aws_smithy_types::Blob::new(input))
+    }
+
+    /// Consumes the `Blob` and returns a `Vec<u8>` with its contents.
+    pub fn into_inner(self) -> Vec<u8> {
+        self.0.into_inner()
+    }
+}
+
+impl AsRef<[u8]> for Blob {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
+
 #[pymethods]
 impl Blob {
     /// Create a new Python instance of `Blob`.
     #[new]
-    pub fn new(input: &[u8]) -> Self {
-        Self(aws_smithy_types::Blob::new::<Vec<u8>>(input.into()))
+    pub fn pynew(input: Vec<u8>) -> Self {
+        Self(aws_smithy_types::Blob::new(input))
     }
 
     /// Python getter for the `Blob` byte array.
     #[getter(data)]
     pub fn get_data(&self) -> &[u8] {
-        self.0.as_ref()
+        self.as_ref()
     }
 
     /// Python setter for the `Blob` byte array.
     #[setter(data)]
-    pub fn set_data(&mut self, data: &[u8]) {
-        *self = Self::new(data);
+    pub fn set_data(&mut self, data: Vec<u8>) {
+        *self = Self::pynew(data);
     }
 }
 
@@ -43,7 +61,7 @@ mod tests {
     fn blob_can_be_used_in_python_when_initialized_in_rust() {
         crate::tests::initialize();
         Python::with_gil(|py| {
-            let blob = Blob(aws_smithy_types::Blob::new("some data".as_bytes().to_vec()));
+            let blob = Blob::new("some data".as_bytes().to_vec());
             let blob = PyCell::new(py, blob).unwrap();
             py_run!(
                 py,
