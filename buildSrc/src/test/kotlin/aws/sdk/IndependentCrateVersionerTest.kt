9657464ed5c85e2a229391265b6b419fb287cbd5
John DiSanti <jdisanti@amazon.com>
2022-06-13 17:08:12 -0700

    Add missing copyright headers

--- buildSrc/src/test/kotlin/aws/sdk/IndependentCrateVersionerTest.kt
+++ buildSrc/src/test/kotlin/aws/sdk/IndependentCrateVersionerTest.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package aws.sdk
 
 import org.junit.jupiter.api.Assertions.assertEquals
