6337d56c477fd5fe2ff1a97fce20024ea6f52976
Bigo <1781140+crisidev@users.noreply.github.com>
2022-06-16 12:43:02 +0100

    Move the python shared library module export to a Decorator

--- codegen-server/python/src/main/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/customizations/PythonServerCodegenDecorator.kt
+++ codegen-server/python/src/main/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/customizations/PythonServerCodegenDecorator.kt
@@ -5,14 +5,17 @@
 
 package software.amazon.smithy.rust.codegen.server.python.smithy.customizations
 
+import software.amazon.smithy.model.neighbor.Walker
 import software.amazon.smithy.rust.codegen.rustlang.Writable
 import software.amazon.smithy.rust.codegen.rustlang.docs
 import software.amazon.smithy.rust.codegen.rustlang.rust
 import software.amazon.smithy.rust.codegen.rustlang.rustBlock
 import software.amazon.smithy.rust.codegen.rustlang.writable
 import software.amazon.smithy.rust.codegen.server.python.smithy.PythonServerRuntimeType
+import software.amazon.smithy.rust.codegen.server.python.smithy.generators.PythonServerModuleGenerator
 import software.amazon.smithy.rust.codegen.server.smithy.customizations.AddInternalServerErrorToAllOperationsDecorator
 import software.amazon.smithy.rust.codegen.smithy.CodegenContext
+import software.amazon.smithy.rust.codegen.smithy.RustCrate
 import software.amazon.smithy.rust.codegen.smithy.customize.CombinedCodegenDecorator
 import software.amazon.smithy.rust.codegen.smithy.customize.RustCodegenDecorator
 import software.amazon.smithy.rust.codegen.smithy.generators.LibRsCustomization
@@ -56,6 +59,20 @@ class PubUsePythonTypes(private val codegenContext: CodegenContext) : LibRsCusto
 }
 
 /**
+ * Render the Python shared library module export.
+ */
+class PythonExportModuleDecorator : RustCodegenDecorator {
+    override val name: String = "PythonExportModuleDecorator"
+    override val order: Byte = 0
+
+    override fun extras(codegenContext: CodegenContext, rustCrate: RustCrate) {
+        val service = codegenContext.settings.getService(codegenContext.model)
+        val serviceShapes = Walker(codegenContext.model).walkShapes(service)
+        PythonServerModuleGenerator(codegenContext, rustCrate, serviceShapes).render()
+    }
+}
+
+/**
  * Decorator applying the customization from [PubUsePythonTypes] class.
  */
 class PubUsePythonTypesDecorator : RustCodegenDecorator {
@@ -79,7 +96,9 @@ val DECORATORS = listOf(
     // Add the [lib] section to Cargo.toml to configure the generation of the shared library.
     CdylibManifestDecorator(),
     // Add `pub use` of `aws_smithy_http_server_python::types`.
-    PubUsePythonTypesDecorator()
+    PubUsePythonTypesDecorator(),
+    // Render the Python shared library export.
+    PythonExportModuleDecorator()
 )
 
 // Combined codegen decorator for Python services.
