1c8cd578682de650a55a2f3e58108879360b3326
Harry Barber <hlbarber@amazon.co.uk>
2022-07-08 13:35:56 +0000

    Add Sensitive wrapper

--- rust-runtime/aws-smithy-logging/src/lib.rs
+++ rust-runtime/aws-smithy-logging/src/lib.rs
@@ -2,3 +2,10 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
+mod sensitive;
+
+pub use sensitive::*;
+
+/// The string placeholder for redacted data.
+pub const REDACTED: &str = "{redacted}";
---
+++ rust-runtime/aws-smithy-logging/src/sensitive.rs
@@ -0,0 +1,88 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+use std::fmt::{Debug, Display, Error, Formatter};
+
+use crate::REDACTED;
+
+/// A wrapper used to modify the [`Display`] and [`Debug`] implementation of the inner structure
+/// based on the feature flag `debug-logging`. When the `debug-logging` feature is enabled, the
+/// implementations will defer to those on `T`, when disabled they will defer to [`REDACTED`].
+///
+/// Note that there are [`Display`] and [`Debug`] implementations for `&T` where `T: Display`
+/// and `T: Debug` respectively - wrapping references is allowed for the cases when consuming the
+/// inner struct is not desired.
+///
+/// # Example
+///
+/// ```
+/// # use aws_smithy_logging::Sensitive;
+/// # let address = "";
+/// tracing::debug!(
+///     name = %Sensitive("Alice"),
+///     friends = ?Sensitive(["Bob"]),
+///     address = ?Sensitive(&address)
+/// );
+/// ```
+pub struct Sensitive<T>(pub T);
+
+impl<T> Debug for Sensitive<T>
+where
+    T: Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        if cfg!(feature = "debug-logging") {
+            self.0.fmt(f)
+        } else {
+            Debug::fmt(&REDACTED, f)
+        }
+    }
+}
+
+impl<T> Display for Sensitive<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        if cfg!(feature = "debug-logging") {
+            self.0.fmt(f)
+        } else {
+            Display::fmt(&REDACTED, f)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::REDACTED;
+
+    use super::Sensitive;
+
+    #[test]
+    fn debug() {
+        let inner = "hello world";
+        let sensitive = Sensitive(inner);
+        let actual = format!("{:?}", sensitive);
+        let expected = if cfg!(feature = "debug-logging") {
+            format!("{:?}", inner)
+        } else {
+            format!("{:?}", REDACTED)
+        };
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn display() {
+        let inner = "hello world";
+        let sensitive = Sensitive(inner);
+        let actual = format!("{}", sensitive);
+        let expected = if cfg!(feature = "debug-logging") {
+            format!("{}", inner)
+        } else {
+            format!("{}", REDACTED)
+        };
+        assert_eq!(actual, expected)
+    }
+}
