058290878f3768850a59ce1965acf29adbea77f9
Weihang Lo <whlo@amazon.co.uk>
2022-07-25 17:49:38 +0100

    Support `@deprecated` trait for ServerCombinedErrorGenerator

--- codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerCombinedErrorGeneratorTest.kt
+++ codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerCombinedErrorGeneratorTest.kt
@@ -24,7 +24,7 @@ class ServerCombinedErrorGeneratorTest {
         namespace error
 
         operation Greeting {
-            errors: [InvalidGreeting, ComplexError, FooException]
+            errors: [InvalidGreeting, ComplexError, FooException, Deprecated]
         }
 
         @error("client")
@@ -42,6 +42,10 @@ class ServerCombinedErrorGeneratorTest {
             abc: String,
             other: Integer
         }
+
+        @error("server")
+        @deprecated
+        structure Deprecated { }
     """.asSmithyModel()
     private val model = OperationNormalizer.transform(baseModel)
     private val symbolProvider = serverTestSymbolProvider(model)
@@ -50,7 +54,7 @@ class ServerCombinedErrorGeneratorTest {
     fun `generates combined error enums`() {
         val project = TestWorkspace.testProject(symbolProvider)
         project.withModule(RustModule.public("error")) { writer ->
-            listOf("FooException", "ComplexError", "InvalidGreeting").forEach {
+            listOf("FooException", "ComplexError", "InvalidGreeting", "Deprecated").forEach {
                 model.lookup<StructureShape>("error#$it").renderWithModelBuilder(model, symbolProvider, writer, CodegenTarget.SERVER)
             }
             val errors = listOf("FooException", "ComplexError", "InvalidGreeting").map { model.lookup<StructureShape>("error#$it") }
@@ -76,7 +80,10 @@ class ServerCombinedErrorGeneratorTest {
 
                     // Indicate the original name in the display output.
                     let error = FooException::builder().build();
-                    assert_eq!(format!("{}", error), "FooException")
+                    assert_eq!(format!("{}", error), "FooException");
+
+                    let error = Deprecated::builder().build();
+                    assert_eq!(error.to_string(), "Deprecated");
                 """
             )
 
@@ -88,6 +95,8 @@ class ServerCombinedErrorGeneratorTest {
                 """
             )
 
+            println("file:///${project.baseDir}/src/lib.rs")
+            println("file:///${project.baseDir}/src/error.rs")
             project.compileAndTest()
         }
     }
