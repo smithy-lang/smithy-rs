5d79679fa8ccb7967059767b729eed5ef59f74e3
Hugo Bastien <hugo@stedi.com>
2022-08-16 21:34:39 -0400

    feat: lambda_http run example to pokemon service

--- rust-runtime/aws-smithy-http-server/examples/pokemon-service/src/main.rs
+++ rust-runtime/aws-smithy-http-server/examples/pokemon-service/src/main.rs
@@ -16,6 +16,11 @@ use pokemon_service_server_sdk::operation_registry::OperationRegistryBuilder;
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 
+/// Returns true if it is running on AWS Lambda
+fn is_lambda_environment() -> bool {
+    std::env::var("AWS_LAMBDA_RUNTIME_API").is_ok()
+}
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -27,10 +32,40 @@ struct Args {
     port: u16,
 }
 
-#[tokio::main]
-pub async fn main() {
+pub async fn run_lambda() {
+    let app: Router<lambda_http::Body> = OperationRegistryBuilder::default()
+        // Build a registry containing implementations to all the operations in the service. These
+        // are async functions or async closures that take as input the operation's input and
+        // return the operation's output.
+        .get_pokemon_species(get_pokemon_species)
+        .get_storage(get_storage)
+        .get_server_statistics(get_server_statistics)
+        .capture_pokemon_operation(capture_pokemon)
+        .empty_operation(empty_operation)
+        .health_check_operation(health_check_operation)
+        .build()
+        .expect("Unable to build operation registry")
+        // Convert it into a router that will route requests to the matching operation
+        // implementation.
+        .into();
+
+    // Setup shared state and middlewares.
+    let shared_state = Arc::new(State::default());
+    let app = app.layer(
+        ServiceBuilder::new()
+            .layer(TraceLayer::new_for_http())
+            .layer(AddExtensionLayer::new(shared_state)),
+    );
+
+    let lambda = lambda_http::run(app);
+
+    if let Err(err) = lambda.await {
+        eprintln!("lambda error: {}", err);
+    }
+}
+
+pub async fn run_hyper_server() {
     let args = Args::parse();
-    setup_tracing();
     let app: Router = OperationRegistryBuilder::default()
         // Build a registry containing implementations to all the operations in the service. These
         // are async functions or async closures that take as input the operation's input and
@@ -64,5 +99,16 @@ pub async fn main() {
     // Run forever-ish...
     if let Err(err) = server.await {
         eprintln!("server error: {}", err);
+    }
+}
+
+#[tokio::main]
+pub async fn main() {
+    setup_tracing();
+
+    if is_lambda_environment() {
+        run_lambda().await;
+    } else {
+        run_hyper_server().await;
     }
 }
