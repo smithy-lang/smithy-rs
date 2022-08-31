1c9511441c8096dc64814dab35f3143cb8c9eabd
Harry Barber <hlbarber@amazon.co.uk>
2022-08-31 23:26:26 +0000

    Add ServerOperationGenerator

---
+++ codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerOperationGenerator.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.rust.codegen.server.smithy.generators
+
+import software.amazon.smithy.model.shapes.OperationShape
+import software.amazon.smithy.model.shapes.ShapeId
+import software.amazon.smithy.model.traits.DocumentationTrait
+import software.amazon.smithy.rust.codegen.rustlang.RustWriter
+import software.amazon.smithy.rust.codegen.rustlang.Writable
+import software.amazon.smithy.rust.codegen.rustlang.asType
+import software.amazon.smithy.rust.codegen.rustlang.rust
+import software.amazon.smithy.rust.codegen.rustlang.rustTemplate
+import software.amazon.smithy.rust.codegen.rustlang.writable
+import software.amazon.smithy.rust.codegen.server.smithy.ServerCargoDependency
+import software.amazon.smithy.rust.codegen.smithy.CoreCodegenContext
+import software.amazon.smithy.rust.codegen.util.getTrait
+import software.amazon.smithy.rust.codegen.util.toPascalCase
+
+private fun shapeToStructName(shapeId: ShapeId): String {
+    return shapeId.name.toPascalCase()
+}
+
+class ServerOperationGenerator(
+    coreCodegenContext: CoreCodegenContext,
+    private val operation: OperationShape,
+) {
+    private val runtimeConfig = coreCodegenContext.runtimeConfig
+    private val codegenScope =
+        arrayOf(
+            "SmithyHttpServer" to
+                ServerCargoDependency.SmithyHttpServer(runtimeConfig).asType(),
+        )
+
+    private val operationId = operation.id
+
+    private fun renderStructDef(): Writable = writable {
+        val documentationLines = operation.getTrait<DocumentationTrait>()?.value?.lines()
+        if (documentationLines != null) {
+            for (documentation in documentationLines) {
+                rust("/// $documentation")
+            }
+        }
+
+        rust("pub struct ${shapeToStructName(operationId)};")
+    }
+
+    private fun operationError(): Writable = writable {
+        if (operation.errors.isEmpty()) {
+            rust("std::convert::Infallible")
+        } else {
+            val operationStructName = shapeToStructName(operationId)
+            rust("crate::error::${operationStructName}Error")
+        }
+    }
+
+    private fun renderImpl(): Writable = writable {
+        val operationStructName = shapeToStructName(operationId)
+        rustTemplate(
+            """
+            impl #{SmithyHttpServer}::operation::OperationShape for $operationStructName {
+                const NAME: &'static str = "${operationId.toString().replace("#", "##")}";
+
+                type Input = crate::input::${operationStructName}Input;
+                type Output = crate::output::${operationStructName}Output;
+                type Error = #{Error:W};
+            }
+            """,
+            "Error" to operationError(),
+            *codegenScope,
+        )
+    }
+
+    fun render(writer: RustWriter) {
+        writer.rustTemplate(
+            """
+            #{Struct:W}
+
+            #{Impl:W}
+            """,
+            "Struct" to renderStructDef(),
+            "Impl" to renderImpl(),
+        )
+        // Adds newline to end of render
+        writer.rust("")
+    }
+}
