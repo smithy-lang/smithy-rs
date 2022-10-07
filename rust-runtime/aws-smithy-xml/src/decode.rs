3a99ce4300a5ecc365e14d23799e1e6d8a79124c
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2022-10-07 11:43:11 +0100

    Use more idiomatic assert

--- rust-runtime/aws-smithy-xml/src/decode.rs
+++ rust-runtime/aws-smithy-xml/src/decode.rs
@@ -463,7 +463,7 @@ mod test {
         let xml = r#"<Response/>"#;
         let mut doc = Document::new(xml);
         let mut scoped = doc.root_element().expect("valid doc");
-        assert_eq!(scoped.start_el.closed, true);
+        assert!(scoped.start_el.closed);
         assert!(scoped.next_tag().is_none())
     }
 
