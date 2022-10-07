238cf8b434107e82c86d9d4d4c3b51d8cb00872f
Julian Antonielli <julianantonielli@gmail.com>
2022-10-07 14:44:11 +0100

    Fix @sensitive handling in Display implementations on error shapes (#1802)
    * Use `Sensitive` wrapper in Debug impl for structures
    
    * Fix using the wrong import path for `Sensitive`
    
    * Use redactMemberIfNecessary
    
    * Fix display implementation on errors to respect @sensitive trait
    
    * Don't use Sensitive type just yet
    
    * Add entry in changelog
    
    * Improve redaction of sensitive error message
    
    * Use correct flags in changelog
    
    * Run ktlint

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/util/Smithy.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/util/Smithy.kt
@@ -84,14 +84,19 @@ fun ServiceShape.hasEventStreamOperations(model: Model): Boolean = operations.an
     model.expectShape(id, OperationShape::class.java).isEventStream(model)
 }
 
-fun Shape.redactIfNecessary(model: Model, safeToPrint: String): String =
+fun Shape.shouldRedact(model: Model): Boolean =
     when (this) {
-        is MemberShape -> model.expectShape(this.target).redactIfNecessary(model, safeToPrint)
-        else -> if (this.hasTrait<SensitiveTrait>()) {
-            "*** Sensitive Data Redacted ***".dq()
-        } else {
-            safeToPrint
-        }
+        is MemberShape -> model.expectShape(this.target).shouldRedact(model)
+        else -> this.hasTrait<SensitiveTrait>()
+    }
+
+const val REDACTION = "\"*** Sensitive Data Redacted ***\""
+
+fun Shape.redactIfNecessary(model: Model, safeToPrint: String): String =
+    if (this.shouldRedact(model)) {
+        REDACTION
+    } else {
+        safeToPrint
     }
 
 /*
