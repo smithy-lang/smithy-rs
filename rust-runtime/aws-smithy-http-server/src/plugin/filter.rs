d7e241f94c3a771cfcf168378d84efa0216c81a8
Harry Barber <hlbarber@amazon.co.uk>
2022-10-11 10:43:30 +0000

    Add copyright header

--- rust-runtime/aws-smithy-http-server/src/plugin/filter.rs
+++ rust-runtime/aws-smithy-http-server/src/plugin/filter.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 use tower::util::Either;
 
 use crate::operation::{Operation, OperationShape};
