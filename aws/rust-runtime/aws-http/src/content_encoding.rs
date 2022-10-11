70785a1927b4c6e8a86b49754c239fec7a9593fe
Julian Antonielli <julianantonielli@gmail.com>
2022-10-11 09:46:22 +0100

    Update aws/rust-runtime/aws-http/src/content_encoding.rs
    Co-authored-by: John DiSanti <jdisanti@amazon.com>

--- aws/rust-runtime/aws-http/src/content_encoding.rs
+++ aws/rust-runtime/aws-http/src/content_encoding.rs
@@ -513,7 +513,7 @@ mod tests {
         };
 
         let timeout_duration = Duration::from_secs(3);
-        if (tokio::time::timeout(timeout_duration, test_fut).await).is_err() {
+        if tokio::time::timeout(timeout_duration, test_fut).await.is_err() {
             panic!(
                 "test_aws_chunked_encoding_sputtering_body timed out after {timeout_duration:?}"
             );
