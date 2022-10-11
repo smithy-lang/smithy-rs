a04c7c4a5bdd73e7fcdc7ce6fcc229145def4695
Harry Barber <hlbarber@amazon.co.uk>
2022-10-11 11:26:30 +0000

    Fix spelling

--- rust-runtime/aws-smithy-http-server/src/plugin/identity.rs
+++ rust-runtime/aws-smithy-http-server/src/plugin/identity.rs
@@ -7,7 +7,7 @@ use crate::operation::Operation;
 
 use super::Plugin;
 
-/// An [`Plugin`] that maps an `input` [`Operation`] to itself.
+/// A [`Plugin`] that maps an `input` [`Operation`] to itself.
 pub struct IdentityPlugin;
 
 impl<P, Op, S, L> Plugin<P, Op, S, L> for IdentityPlugin {
