7b7bf6ce024bf313b11bfc20c9101614c667eb04
david-perez <d@vidp.dev>
2022-10-19 15:49:54 +0200

    Add comment to ServerInstantiator's enumFromStringFn

--- codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerInstantiator.kt
+++ codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerInstantiator.kt
@@ -12,6 +12,11 @@ import software.amazon.smithy.rust.codegen.core.rustlang.writable
 import software.amazon.smithy.rust.codegen.core.smithy.CodegenContext
 import software.amazon.smithy.rust.codegen.core.smithy.generators.Instantiator
 
+/**
+ * Server enums do not have an `Unknown` variant like client enums do, so constructing an enum from
+ * a string is a fallible operation (hence `try_from`). It's ok to panic here if construction fails,
+ * since this is only used in protocol tests.
+ */
 private fun enumFromStringFn(enumSymbol: Symbol, data: String): Writable = writable {
     rust(
         """#T::try_from($data).expect("This is used in tests ONLY")""",
