9207c4921a5a6ee29e0579fe00b8147723dbd95d
John DiSanti <jdisanti@amazon.com>
2022-11-02 18:12:57 -0700

    Fix clippy lints for the canary Lambda

--- tools/ci-cdk/canary-lambda/src/s3_canary.rs
+++ tools/ci-cdk/canary-lambda/src/s3_canary.rs
@@ -8,7 +8,7 @@ use crate::{mk_canary, CanaryEnv};
 use anyhow::Context;
 use aws_sdk_s3 as s3;
 use s3::error::{GetObjectError, GetObjectErrorKind};
-use s3::types::{ByteStream, SdkError};
+use s3::types::ByteStream;
 use uuid::Uuid;
 
 const METADATA_TEST_VALUE: &str = "some   value";
@@ -44,7 +44,7 @@ pub async fn s3_canary(client: s3::Client, s3_bucket_name: String) -> anyhow::Re
             } => {
                 // good
             }
-            err @ _ => Err(err).context("unexpected s3::GetObject failure")?,
+            err => Err(err).context("unexpected s3::GetObject failure")?,
         },
     }
 
