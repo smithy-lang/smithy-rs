11933b3cb3054e74923c8efc4077f3121f30582e
Julian Antonielli <julianantonielli@gmail.com>
2022-11-03 11:16:43 +0000

    Don't use external `base64_simd::Error` type in public interface

--- rust-runtime/aws-smithy-types/src/base64.rs
+++ rust-runtime/aws-smithy-types/src/base64.rs
@@ -5,15 +5,30 @@
 
 //! A thin wrapper over `base64-simd`
 
+use std::error::Error;
+
 use base64_simd::Base64;
+
+/// Failure to decode a base64 value.
+#[derive(Debug)]
+pub struct DecodeError(base64_simd::Error);
+
+impl Error for DecodeError {}
+
+impl std::fmt::Display for DecodeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
 
 /// Decode `input` from base64 using the standard base64 alphabet
 ///
 /// If input is not a valid base64 encoded string, this function will return `DecodeError`.
-pub fn decode(input: impl AsRef<str>) -> Result<Vec<u8>, base64_simd::Error> {
+pub fn decode(input: impl AsRef<str>) -> Result<Vec<u8>, DecodeError> {
     Base64::STANDARD
         .decode_to_boxed_bytes(input.as_ref().as_bytes())
         .map(|bytes| bytes.into_vec())
+        .map_err(DecodeError)
 }
 
 /// Encode `input` into base64 using the standard base64 alphabet
@@ -24,6 +39,7 @@ pub fn encode(input: impl AsRef<[u8]>) -> String {
 }
 
 /// Given the length of some data in bytes, return how many bytes it would take to base64 encode
+///
 /// that data.
 pub fn encoded_length(length: usize) -> usize {
     Base64::STANDARD.encoded_length(length)
