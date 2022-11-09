f8de9061df39f784de8d7b36ed7bce715a5b94b4
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2022-11-09 16:27:14 +0000

    Remove Pluggable. Rename `composer` module to `pipeline`.

--- rust-runtime/aws-smithy-http-server/src/plugin/mod.rs
+++ rust-runtime/aws-smithy-http-server/src/plugin/mod.rs
@@ -3,43 +3,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-mod composer;
 mod filter;
 mod identity;
+mod pipeline;
 mod stack;
 
 use crate::operation::Operation;
 
-pub use composer::PluginPipeline;
 pub use filter::FilterByOperationName;
 pub use identity::IdentityPlugin;
+pub use pipeline::PluginPipeline;
 pub use stack::PluginStack;
-
-/// Provides a standard interface for applying [`Plugin`]s to a service builder. This is implemented automatically for
-/// all builders.
-///
-/// As [`Plugin`]s modify the way in which [`Operation`]s are [`upgraded`](crate::operation::Upgradable) we can use
-/// [`Pluggable`] as a foundation to write extension traits which are implemented for all service builders.
-///
-/// # Example
-///
-/// ```
-/// # struct PrintPlugin;
-/// # use aws_smithy_http_server::plugin::Pluggable;
-/// trait PrintExt: Pluggable<PrintPlugin> {
-///     fn print(self) -> Self::Output where Self: Sized {
-///         self.apply(PrintPlugin)
-///     }
-/// }
-///
-/// impl<Builder> PrintExt for Builder where Builder: Pluggable<PrintPlugin> {}
-/// ```
-pub trait Pluggable<NewPlugin> {
-    type Output;
-
-    /// Applies a [`Plugin`] to the service builder.
-    fn apply(self, plugin: NewPlugin) -> Self::Output;
-}
 
 /// A mapping from one [`Operation`] to another. Used to modify the behavior of
 /// [`Upgradable`](crate::operation::Upgradable) and therefore the resulting service builder,
