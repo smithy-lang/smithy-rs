5972c11fc243bdcccd3d6a54b4466540f50231ab
Yuki Saito <awsaito@amazon.com>
2022-11-14 11:01:47 -0600

    Update the module documentation for types
    This commit updates rustdoc for the types module to reflect that fact
    that the module now contains not only re-exports but also an auxiliary
    struct `UnknownVariantValue`.

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/rustlang/RustModule.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/rustlang/RustModule.kt
@@ -29,10 +29,7 @@ data class RustModule(val name: String, val rustMetadata: RustMetadata, val docu
         val Model = public("model", documentation = "Data structures used by operation inputs/outputs.")
         val Input = public("input", documentation = "Input structures for operations.")
         val Output = public("output", documentation = "Output structures for operations.")
-        val Types = public(
-            "types",
-            documentation = "Re-exported types from supporting crates and synthesized types from the service.",
-        )
+        val Types = public("types", documentation = "Data primitives referenced by other data types.")
 
         /**
          * Helper method to generate the `operation` Rust module.
