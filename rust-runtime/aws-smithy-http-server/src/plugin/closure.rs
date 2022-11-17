91ddc2d49e3d0cde9cee2086363bf464a8cf71bd
Harry Barber <hlbarber@amazon.co.uk>
2022-11-17 20:19:15 +0000

    Add utility plugins

---
+++ rust-runtime/aws-smithy-http-server/src/plugin/closure.rs
@@ -0,0 +1,29 @@
+use tower::layer::util::Stack;
+
+use crate::operation::{Operation, OperationShape};
+
+use super::Plugin;
+
+/// A [`Plugin`] implemented by a closure over the operation name. See [`plugin_from_operation_name_fn`] for more details.
+pub struct OperationNameFn<F> {
+    f: F,
+}
+
+impl<P, Op, S, ExistingLayer, NewLayer, F> Plugin<P, Op, S, ExistingLayer> for OperationNameFn<F>
+where
+    F: Fn(&'static str) -> NewLayer,
+    Op: OperationShape,
+{
+    type Service = S;
+    type Layer = Stack<ExistingLayer, NewLayer>;
+
+    fn map(&self, input: Operation<S, ExistingLayer>) -> Operation<Self::Service, Self::Layer> {
+        input.layer((self.f)(Op::NAME))
+    }
+}
+
+/// Constructs a [`Plugin`] using a closure over the operation name `F: Fn(&'static str) -> L` where `L` is a HTTP
+/// [`Layer`](tower::Layer).
+pub fn plugin_from_operation_name_fn<F>(f: F) -> OperationNameFn<F> {
+    OperationNameFn { f }
+}
---
+++ rust-runtime/aws-smithy-http-server/src/plugin/layer.rs
@@ -0,0 +1,20 @@
+use tower::layer::util::Stack;
+
+use crate::operation::Operation;
+
+use super::Plugin;
+
+/// A [`Plugin`] which appends a HTTP [`Layer`](tower::Layer) to the existing `L` in [`Operation<S, L>`](Operation).
+pub struct HttpLayer<L>(pub L);
+
+impl<P, Op, S, ExistingLayer, NewLayer> Plugin<P, Op, S, ExistingLayer> for HttpLayer<NewLayer>
+where
+    NewLayer: Clone,
+{
+    type Service = S;
+    type Layer = Stack<ExistingLayer, NewLayer>;
+
+    fn map(&self, input: Operation<S, ExistingLayer>) -> Operation<Self::Service, Self::Layer> {
+        input.layer(self.0.clone())
+    }
+}
