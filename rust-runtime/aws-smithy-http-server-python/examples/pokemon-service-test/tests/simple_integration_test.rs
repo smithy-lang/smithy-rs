4b403d96d8a814018c8f92af4371474c941b80e5
Burak Varlı <burakvar@amazon.co.uk>
2022-11-17 16:08:55 +0000

    Add TLS support to Pokemon service

--- rust-runtime/aws-smithy-http-server-python/examples/pokemon-service-test/tests/simple_integration_test.rs
+++ rust-runtime/aws-smithy-http-server-python/examples/pokemon-service-test/tests/simple_integration_test.rs
@@ -7,24 +7,31 @@
 // These tests only have access to your crate's public API.
 // See: https://doc.rust-lang.org/book/ch11-03-test-organization.html#integration-tests
 
-use std::time::Duration;
+use serial_test::serial;
 
-use crate::helpers::{client, PokemonService};
-use aws_smithy_types::error::display::DisplayErrorContext;
-use tokio::time;
+use crate::helpers::{client, http2_client, PokemonClient, PokemonService};
 
 mod helpers;
 
 #[tokio::test]
+#[serial]
 async fn simple_integration_test() {
-    let _program = PokemonService::run();
-    // Give PokemonSérvice some time to start up.
-    time::sleep(Duration::from_millis(50)).await;
+    let _program = PokemonService::run().await;
+    simple_integration_test_with_client(client()).await;
+}
 
-    let service_statistics_out = client().get_server_statistics().send().await.unwrap();
+#[tokio::test]
+#[serial]
+async fn simple_integration_test_http2() {
+    let _program = PokemonService::run_http2().await;
+    simple_integration_test_with_client(http2_client()).await;
+}
+
+async fn simple_integration_test_with_client(client: PokemonClient) {
+    let service_statistics_out = client.get_server_statistics().send().await.unwrap();
     assert_eq!(0, service_statistics_out.calls_count.unwrap());
 
-    let pokemon_species_output = client()
+    let pokemon_species_output = client
         .get_pokemon_species()
         .name("pikachu")
         .send()
@@ -32,10 +39,10 @@ async fn simple_integration_test() {
         .unwrap();
     assert_eq!("pikachu", pokemon_species_output.name().unwrap());
 
-    let service_statistics_out = client().get_server_statistics().send().await.unwrap();
+    let service_statistics_out = client.get_server_statistics().send().await.unwrap();
     assert_eq!(1, service_statistics_out.calls_count.unwrap());
 
-    let pokemon_species_error = client()
+    let pokemon_species_error = client
         .get_pokemon_species()
         .name("some_pokémon")
         .send()
@@ -49,8 +56,8 @@ async fn simple_integration_test() {
         "expected '{message}' to contain '{expected}'"
     );
 
-    let service_statistics_out = client().get_server_statistics().send().await.unwrap();
+    let service_statistics_out = client.get_server_statistics().send().await.unwrap();
     assert_eq!(2, service_statistics_out.calls_count.unwrap());
 
-    let _health_check = client().check_health().send().await.unwrap();
+    let _health_check = client.check_health().send().await.unwrap();
 }
