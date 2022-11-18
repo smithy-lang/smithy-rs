9d9fdfe722bf11b31c568da35d69eca4cc282c56
Harry Barber <hlbarber@amazon.co.uk>
2022-11-18 09:32:55 +0000

    Add copyright licenses

--- rust-runtime/aws-smithy-http-server/src/plugin/closure.rs
+++ rust-runtime/aws-smithy-http-server/src/plugin/closure.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 use tower::layer::util::Stack;
 
 use crate::operation::{Operation, OperationShape};
