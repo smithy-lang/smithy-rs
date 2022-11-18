58c156fd0e1d95caeb4fc7f1b7efd6c98889003f
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2022-11-18 15:35:28 +0000

    Fix conversion.

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/protocols/serialize/JsonSerializerGenerator.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/protocols/serialize/JsonSerializerGenerator.kt
@@ -368,7 +368,7 @@ class JsonSerializerGenerator(
                 val rustType = symbolProvider.toSymbol(target).rustType()
                 val conversion = writable {
                     if (rustType is RustType.Opaque) {
-                        rust("(*${value.asRef()}.inner()).into()")
+                        rust("(*((${value.asRef()}).inner())).into()")
                     } else {
                         rust("(${value.asValue()}).into()")
                     }
