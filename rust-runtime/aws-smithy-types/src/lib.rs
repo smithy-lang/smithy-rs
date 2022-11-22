cec0c1b899e123e91f60759fac9cd8766702024b
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2022-11-22 23:34:01 +0000

    - RFC update - paritally implements serialization and deserialization for `DateTime`, `Document`, `Blob` and `Number` implemented in `aws_smithy_types` crate. - new features in Cargo.toml - serde to dependencies from dev.dependencies - add json in test_data

--- rust-runtime/aws-smithy-types/src/lib.rs
+++ rust-runtime/aws-smithy-types/src/lib.rs
@@ -14,7 +14,7 @@
 )]
 
 use std::collections::HashMap;
-
+use serde::{Deserialize, Serialize, de::Visitor};
 pub mod base64;
 pub mod date_time;
 pub mod endpoint;
@@ -30,6 +30,51 @@ pub use crate::date_time::DateTime;
 #[derive(Debug, PartialEq, Clone)]
 pub struct Blob {
     inner: Vec<u8>,
+}
+
+#[cfg(feature = "unstable-serde-serialize")]
+impl Serialize for Blob {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer 
+    {
+        serializer.serialize_str(&crate::base64::encode(&self.inner))
+    }
+}
+
+#[cfg(feature = "unstable-serde-deserialize")]
+pub use deserialize_blob::*;
+#[cfg(feature = "unstable-serde-deserialize")]
+mod deserialize_blob {
+    use super::*;
+    struct BlobVisitor;
+
+    impl<'de> Visitor<'de> for BlobVisitor {
+        type Value = Blob;
+        fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            formatter.write_str("expecting base64 encoded string")
+        }
+    
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error, 
+        {
+            match base64::decode(v) {
+                Ok(inner) => Ok(Blob { inner }),
+                Err(e) => Err(serde::de::Error::custom(e)),
+            }
+            
+        }
+    }
+    
+    impl<'de> Deserialize<'de> for Blob {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de> 
+        {
+            deserializer.deserialize_str(BlobVisitor)
+        }
+    }
 }
 
 impl Blob {
@@ -60,7 +105,8 @@ impl AsRef<[u8]> for Blob {
 /// Open content is useful for modeling unstructured data that has no schema, data that can't be
 /// modeled using rigid types, or data that has a schema that evolves outside of the purview of a model.
 /// The serialization format of a document is an implementation detail of a protocol.
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
 pub enum Document {
     /// JSON object
     Object(HashMap<String, Document>),
@@ -74,6 +120,40 @@ pub enum Document {
     Bool(bool),
     /// JSON null
     Null,
+}
+
+
+/// checks if a) serialization of json suceeds and b) it is compatible with serde_json
+#[test]
+fn serialize_json() {
+    let mut map: HashMap<String, Document> = HashMap::new();
+    // string
+    map.insert("hello".into(), "world".to_string().into());
+    // numbers 
+    map.insert("pos_int".into(), Document::Number(Number::PosInt(1).into()));
+    map.insert(
+        "neg_int".into(),
+        Document::Number(Number::NegInt(-1).into()),
+    );
+    map.insert(
+        "float".into(),
+        Document::Number(Number::Float(0.1 + 0.2).into()),
+    );
+    // booleans
+    map.insert("true".into(), true.into());
+    map.insert("false".into(), false.into());
+    // check if array with different datatypes would succeed
+    map.insert("array".into(), vec![map.clone().into(), "hello-world".to_string().into(), true.into(), false.into()].into());
+    // map
+    map.insert("map".into(), map.clone().into());
+    let obj = Document::Object(map);
+    // comparing string isnt going to work since there is no gurantee for the ordering of the keys
+    let target_file = include_str!("../test_data/serialize_document.json");
+    let json: Result<serde_json::Value, _> = serde_json::from_str(target_file);
+    // serializer
+    assert_eq!(serde_json::to_value(&obj).unwrap(), json.unwrap());
+    let doc: Result<Document, _> = serde_json::from_str(target_file);
+    assert_eq!(obj, doc.unwrap());
 }
 
 impl From<bool> for Document {
@@ -102,7 +182,8 @@ impl From<HashMap<String, Document>> for Document {
 
 /// A number type that implements Javascript / JSON semantics, modeled on serde_json:
 /// <https://docs.serde.rs/src/serde_json/number.rs.html#20-22>
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq, Deserialize)]
+#[serde(untagged)]
 pub enum Number {
     /// Unsigned 64-bit integer value.
     PosInt(u64),
@@ -110,6 +191,35 @@ pub enum Number {
     NegInt(i64),
     /// 64-bit floating-point value.
     Float(f64),
+}
+
+#[test]
+/// ensures that numbers are deserialized as expected
+/// 0 <= PosInt  
+/// 0 > NegInt  
+/// non integer values == Float
+fn number_deserialization_works() {
+    let n: Number = serde_json::from_str("1.1").unwrap();
+    assert_eq!(n, Number::Float(1.1));
+    let n: Number = serde_json::from_str("1").unwrap();
+    assert_eq!(n, Number::PosInt(1));
+    let n: Number = serde_json::from_str("0").unwrap();
+    assert_eq!(n, Number::PosInt(0));
+    let n: Number = serde_json::from_str("-1").unwrap();
+    assert_eq!(n, Number::NegInt(-1));
+}
+
+impl Serialize for Number {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer {
+        // serialization for smithy's number type
+        match self {
+            crate::Number::Float(f) => serializer.serialize_f64(*f),
+            crate::Number::NegInt(n) => serializer.serialize_i64(*n),
+            crate::Number::PosInt(n) => serializer.serialize_u64(*n),
+        }
+    }
 }
 
 /* ANCHOR_END: document */
