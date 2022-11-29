a8b672eec56a656796e75f440edc7f8947780aaf
Harry Barber <hlbarber@amazon.co.uk>
2022-11-29 17:26:34 +0000

    Add extractors

---
+++ rust-runtime/aws-smithy-http-server/src/request/lambda.rs
@@ -0,0 +1,49 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+use lambda_http::request::RequestContext;
+pub use lambda_http::{
+    aws_lambda_events::apigw::{ApiGatewayProxyRequestContext, ApiGatewayV2httpRequestContext},
+    Context,
+};
+
+use crate::Extension;
+
+use super::{extension::MissingExtension, FromParts};
+
+impl<P> FromParts<P> for Context {
+    type Rejection = MissingExtension;
+
+    fn from_parts(parts: &mut http::request::Parts) -> Result<Self, Self::Rejection> {
+        let Extension(context) = <Extension<Self> as FromParts<P>>::from_parts(parts)?;
+        Ok(context)
+    }
+}
+
+impl<P> FromParts<P> for ApiGatewayProxyRequestContext {
+    type Rejection = MissingExtension;
+
+    fn from_parts(parts: &mut http::request::Parts) -> Result<Self, Self::Rejection> {
+        let Extension(context) = <Extension<RequestContext> as FromParts<P>>::from_parts(parts)?;
+        if let RequestContext::ApiGatewayV1(context) = context {
+            Ok(context)
+        } else {
+            Err(MissingExtension)
+        }
+    }
+}
+
+impl<P> FromParts<P> for ApiGatewayV2httpRequestContext {
+    type Rejection = MissingExtension;
+
+    fn from_parts(parts: &mut http::request::Parts) -> Result<Self, Self::Rejection> {
+        let Extension(context) = <Extension<RequestContext> as FromParts<P>>::from_parts(parts)?;
+        if let RequestContext::ApiGatewayV2(context) = context {
+            Ok(context)
+        } else {
+            Err(MissingExtension)
+        }
+    }
+}
