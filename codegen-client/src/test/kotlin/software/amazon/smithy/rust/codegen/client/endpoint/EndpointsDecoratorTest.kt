84ed110fcbac6eff91c948d264e5c30154a7b24e
Russell Cohen <rcoh@amazon.com>
2022-12-01 14:39:19 -0500

    Add Endpoint Resolver Implementation (#2030)
    * Add Endpoint Resolver Implementation
    
    This commit adds `EndpointDecorator`, standard libary + tests that can be used to add endpoints 2.0 to the AWS SDK.
    
    It _does not_ actually wire these things up. We'll follow up with a PR that actually integrates everything.
    
    * CR Feedback
    
    * CR feedback II

--- codegen-client/src/test/kotlin/software/amazon/smithy/rust/codegen/client/endpoint/EndpointsDecoratorTest.kt
+++ codegen-client/src/test/kotlin/software/amazon/smithy/rust/codegen/client/endpoint/EndpointsDecoratorTest.kt
@@ -5,6 +5,8 @@
 
 package software.amazon.smithy.rust.codegen.client.endpoint
 
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.string.shouldContain
 import org.junit.jupiter.api.Test
 import software.amazon.smithy.rust.codegen.client.smithy.endpoint.EndpointsDecorator
 import software.amazon.smithy.rust.codegen.client.testutil.clientIntegrationTest
@@ -12,9 +14,13 @@ import software.amazon.smithy.rust.codegen.core.rustlang.rust
 import software.amazon.smithy.rust.codegen.core.testutil.TokioTest
 import software.amazon.smithy.rust.codegen.core.testutil.asSmithyModel
 import software.amazon.smithy.rust.codegen.core.testutil.integrationTest
+import software.amazon.smithy.rust.codegen.core.testutil.runWithWarnings
+import software.amazon.smithy.rust.codegen.core.util.CommandFailed
 
+/**
+ * End-to-end test of endpoint resolvers, attaching a real resolver to a fully generated service
+ */
 class EndpointsDecoratorTest {
-
     val model = """
         namespace test
 
@@ -29,7 +35,13 @@ class EndpointsDecoratorTest {
         @awsJson1_1
         @endpointRuleSet({
             "version": "1.0",
-            "rules": [],
+            "rules": [{
+                "conditions": [{"fn": "isSet", "argv": [{"ref":"Region"}]}],
+                "type": "endpoint",
+                "endpoint": {
+                    "url": "https://www.{Region}.example.com"
+                }
+            }],
             "parameters": {
                 "Bucket": { "required": false, "type": "String" },
                 "Region": { "required": false, "type": "String", "builtIn": "AWS::Region" },
@@ -37,13 +49,31 @@ class EndpointsDecoratorTest {
                 "ABoolParam": { "required": false, "type": "Boolean" }
             }
         })
-        @clientContextParams(AStringParam: {
-            documentation: "string docs",
-            type: "string"
-        },
-        aBoolParam: {
-            documentation: "bool docs",
-            type: "boolean"
+        @clientContextParams(
+            AStringParam: {
+                documentation: "string docs",
+                type: "string"
+            },
+            aBoolParam: {
+                documentation: "bool docs",
+                type: "boolean"
+            }
+        )
+        @endpointTests({
+          "version": "1.0",
+          "testCases": [
+            {
+              "documentation": "uriEncode when the string has nothing to encode returns the input",
+              "params": {
+                "Region": "test-region"
+              },
+              "expect": {
+                "endpoint": {
+                    "url": "https://failingtest.com"
+                }
+              }
+            }
+         ]
         })
         service TestService {
             operations: [TestOperation]
@@ -60,11 +90,13 @@ class EndpointsDecoratorTest {
         }
     """.asSmithyModel()
 
-    // NOTE: this test will fail once the endpoint starts being added directly (unless we preserve endpoint params in the
-    // property bag.
     @Test
-    fun `add endpoint params to the property bag`() {
-        clientIntegrationTest(model, addtionalDecorators = listOf(EndpointsDecorator())) { clientCodegenContext, rustCrate ->
+    fun `set an endpoint in the property bag`() {
+        val testDir = clientIntegrationTest(
+            model,
+            addtionalDecorators = listOf(EndpointsDecorator()),
+            command = { "cargo check".runWithWarnings(it) },
+        ) { clientCodegenContext, rustCrate ->
             rustCrate.integrationTest("endpoint_params_test") {
                 val moduleName = clientCodegenContext.moduleUseName()
                 TokioTest.render(this)
@@ -75,11 +107,14 @@ class EndpointsDecoratorTest {
                             let operation = $moduleName::operation::TestOperation::builder()
                                 .bucket("bucket-name").build().expect("input is valid")
                                 .make_operation(&conf).await.expect("valid operation");
-                            use $moduleName::endpoint::{Params, InvalidParams};
+                            use $moduleName::endpoint::{Params};
+                            use aws_smithy_http::endpoint::Result;
                             let props = operation.properties();
-                            let endpoint_params = props.get::<Result<Params, InvalidParams>>().unwrap();
+                            let endpoint_params = props.get::<Params>().unwrap();
+                            let endpoint_result = props.get::<Result>().unwrap();
+                            let endpoint = endpoint_result.as_ref().expect("endpoint resolved properly");
                             assert_eq!(
-                                endpoint_params.as_ref().expect("ok"),
+                                endpoint_params,
                                 &Params::builder()
                                     .bucket("bucket-name".to_string())
                                     .a_bool_param(false)
@@ -87,11 +122,17 @@ class EndpointsDecoratorTest {
                                     .region("us-east-2".to_string())
                                     .build().unwrap()
                             );
-                    }
 
+                            assert_eq!(endpoint.url(), "https://www.us-east-2.example.com");
+                    }
                     """,
                 )
             }
         }
+        // the model has an intentionally failing test—ensure it fails
+        val failure = shouldThrow<CommandFailed> { "cargo test".runWithWarnings(testDir) }
+        failure.output shouldContain "endpoint::test::test_1"
+        failure.output shouldContain "https://failingtest.com"
+        "cargo clippy".runWithWarnings(testDir)
     }
 }
