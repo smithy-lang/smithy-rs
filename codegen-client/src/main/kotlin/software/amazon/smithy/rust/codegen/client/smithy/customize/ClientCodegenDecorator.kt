71d842c8d6887cb5dc49698adf49335340974772
John DiSanti <jdisanti@amazon.com>
2022-12-14 10:44:32 -0800

    Incorporate feedback

--- codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customize/ClientCodegenDecorator.kt
+++ codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customize/ClientCodegenDecorator.kt
@@ -6,20 +6,16 @@
 package software.amazon.smithy.rust.codegen.client.smithy.customize
 
 import software.amazon.smithy.build.PluginContext
-import software.amazon.smithy.model.Model
 import software.amazon.smithy.model.shapes.OperationShape
-import software.amazon.smithy.model.shapes.ServiceShape
 import software.amazon.smithy.model.shapes.ShapeId
 import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
 import software.amazon.smithy.rust.codegen.client.smithy.endpoint.EndpointCustomization
 import software.amazon.smithy.rust.codegen.client.smithy.generators.config.ConfigCustomization
 import software.amazon.smithy.rust.codegen.client.smithy.generators.protocol.ClientProtocolGenerator
-import software.amazon.smithy.rust.codegen.core.smithy.RustCrate
+import software.amazon.smithy.rust.codegen.core.smithy.customize.CombinedCoreCodegenDecorator
+import software.amazon.smithy.rust.codegen.core.smithy.customize.CoreCodegenDecorator
 import software.amazon.smithy.rust.codegen.core.smithy.customize.OperationCustomization
-import software.amazon.smithy.rust.codegen.core.smithy.generators.LibRsCustomization
-import software.amazon.smithy.rust.codegen.core.smithy.generators.ManifestCustomizations
 import software.amazon.smithy.rust.codegen.core.smithy.protocols.ProtocolMap
-import software.amazon.smithy.rust.codegen.core.util.deepMergeWith
 import java.util.ServiceLoader
 import java.util.logging.Logger
 
@@ -32,23 +28,7 @@ typealias ClientProtocolMap = ProtocolMap<ClientProtocolGenerator, ClientCodegen
  * AWS services. A different downstream customer may wish to add a different set of derive
  * attributes to the generated classes.
  */
-interface ClientCodegenDecorator {
-    /**
-     * The name of this [ClientCodegenDecorator], used for logging and debug information
-     */
-    val name: String
-
-    /**
-     * Enable a deterministic ordering to be applied, with the lowest numbered integrations being applied first
-     */
-    val order: Byte
-
-    /**
-     * Whether this decorator can be discovered on the classpath (defaults to true).
-     * This is intended to only be overridden for decorators written specifically for tests.
-     */
-    fun classpathDiscoverable(): Boolean = true
-
+interface ClientCodegenDecorator : CoreCodegenDecorator<ClientCodegenContext> {
     fun configCustomizations(
         codegenContext: ClientCodegenContext,
         baseCustomizations: List<ConfigCustomization>,
@@ -60,23 +40,7 @@ interface ClientCodegenDecorator {
         baseCustomizations: List<OperationCustomization>,
     ): List<OperationCustomization> = baseCustomizations
 
-    fun libRsCustomizations(
-        codegenContext: ClientCodegenContext,
-        baseCustomizations: List<LibRsCustomization>,
-    ): List<LibRsCustomization> = baseCustomizations
-
-    /**
-     * Returns a map of Cargo.toml properties to change. For example, if a `homepage` needs to be
-     * added to the Cargo.toml `[package]` section, a `mapOf("package" to mapOf("homepage", "https://example.com"))`
-     * could be returned. Properties here overwrite the default properties.
-     */
-    fun crateManifestCustomizations(codegenContext: ClientCodegenContext): ManifestCustomizations = emptyMap()
-
-    fun extras(codegenContext: ClientCodegenContext, rustCrate: RustCrate) {}
-
     fun protocols(serviceId: ShapeId, currentProtocols: ClientProtocolMap): ClientProtocolMap = currentProtocols
-
-    fun transformModel(service: ServiceShape, model: Model): Model = model
 
     fun endpointCustomizations(codegenContext: ClientCodegenContext): List<EndpointCustomization> = listOf()
 }
@@ -86,8 +50,8 @@ interface ClientCodegenDecorator {
  *
  * This makes the actual concrete codegen simpler by not needing to deal with multiple separate decorators.
  */
-open class CombinedClientCodegenDecorator(decorators: List<ClientCodegenDecorator>) : ClientCodegenDecorator {
-    private val orderedDecorators = decorators.sortedBy { it.order }
+open class CombinedClientCodegenDecorator(decorators: List<ClientCodegenDecorator>) :
+    CombinedCoreCodegenDecorator<ClientCodegenContext, ClientCodegenDecorator>(decorators), ClientCodegenDecorator {
     override val name: String
         get() = "CombinedClientCodegenDecorator"
     override val order: Byte
@@ -96,59 +60,25 @@ open class CombinedClientCodegenDecorator(decorators: List<ClientCodegenDecorato
     override fun configCustomizations(
         codegenContext: ClientCodegenContext,
         baseCustomizations: List<ConfigCustomization>,
-    ): List<ConfigCustomization> {
-        return orderedDecorators.foldRight(baseCustomizations) { decorator: ClientCodegenDecorator, customizations ->
-            decorator.configCustomizations(codegenContext, customizations)
-        }
+    ): List<ConfigCustomization> = combineCustomizations(baseCustomizations) { decorator, customizations ->
+        decorator.configCustomizations(codegenContext, customizations)
     }
 
     override fun operationCustomizations(
         codegenContext: ClientCodegenContext,
         operation: OperationShape,
         baseCustomizations: List<OperationCustomization>,
-    ): List<OperationCustomization> {
-        return orderedDecorators.foldRight(baseCustomizations) { decorator: ClientCodegenDecorator, customizations ->
-            decorator.operationCustomizations(codegenContext, operation, customizations)
-        }
+    ): List<OperationCustomization> = combineCustomizations(baseCustomizations) { decorator, customizations ->
+        decorator.operationCustomizations(codegenContext, operation, customizations)
     }
 
-    override fun libRsCustomizations(
-        codegenContext: ClientCodegenContext,
-        baseCustomizations: List<LibRsCustomization>,
-    ): List<LibRsCustomization> {
-        return orderedDecorators.foldRight(baseCustomizations) { decorator, customizations ->
-            decorator.libRsCustomizations(
-                codegenContext,
-                customizations,
-            )
-        }
-    }
-
-    override fun protocols(serviceId: ShapeId, currentProtocols: ClientProtocolMap): ClientProtocolMap {
-        return orderedDecorators.foldRight(currentProtocols) { decorator, protocolMap ->
+    override fun protocols(serviceId: ShapeId, currentProtocols: ClientProtocolMap): ClientProtocolMap =
+        combineCustomizations(currentProtocols) { decorator, protocolMap ->
             decorator.protocols(serviceId, protocolMap)
         }
-    }
 
-    override fun crateManifestCustomizations(codegenContext: ClientCodegenContext): ManifestCustomizations {
-        return orderedDecorators.foldRight(emptyMap()) { decorator, customizations ->
-            customizations.deepMergeWith(decorator.crateManifestCustomizations(codegenContext))
-        }
-    }
-
-    override fun extras(codegenContext: ClientCodegenContext, rustCrate: RustCrate) {
-        return orderedDecorators.forEach { it.extras(codegenContext, rustCrate) }
-    }
-
-    override fun transformModel(service: ServiceShape, model: Model): Model {
-        return orderedDecorators.foldRight(model) { decorator, otherModel ->
-            decorator.transformModel(service, otherModel)
-        }
-    }
-
-    override fun endpointCustomizations(codegenContext: ClientCodegenContext): List<EndpointCustomization> {
-        return orderedDecorators.flatMap { it.endpointCustomizations(codegenContext) }
-    }
+    override fun endpointCustomizations(codegenContext: ClientCodegenContext): List<EndpointCustomization> =
+        addCustomizations { decorator -> decorator.endpointCustomizations(codegenContext) }
 
     companion object {
         fun fromClasspath(
--- codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/customize/ServerCodegenDecorator.kt
+++ codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/customize/ServerCodegenDecorator.kt
@@ -6,62 +6,21 @@
 package software.amazon.smithy.rust.codegen.server.smithy.customize
 
 import software.amazon.smithy.build.PluginContext
-import software.amazon.smithy.model.Model
-import software.amazon.smithy.model.shapes.ServiceShape
 import software.amazon.smithy.model.shapes.ShapeId
-import software.amazon.smithy.rust.codegen.core.smithy.RustCrate
-import software.amazon.smithy.rust.codegen.core.smithy.generators.LibRsCustomization
-import software.amazon.smithy.rust.codegen.core.smithy.generators.ManifestCustomizations
+import software.amazon.smithy.rust.codegen.core.smithy.customize.CombinedCoreCodegenDecorator
+import software.amazon.smithy.rust.codegen.core.smithy.customize.CoreCodegenDecorator
 import software.amazon.smithy.rust.codegen.core.smithy.protocols.ProtocolMap
-import software.amazon.smithy.rust.codegen.core.util.deepMergeWith
 import software.amazon.smithy.rust.codegen.server.smithy.ServerCodegenContext
 import software.amazon.smithy.rust.codegen.server.smithy.generators.protocol.ServerProtocolGenerator
-import java.util.ServiceLoader
 import java.util.logging.Logger
 
 typealias ServerProtocolMap = ProtocolMap<ServerProtocolGenerator, ServerCodegenContext>
 
 /**
  * [ServerCodegenDecorator] allows downstream users to customize code generation.
- *
- * For example, AWS-specific code generation generates customizations required to support
- * AWS services. A different downstream customer may wish to add a different set of derive
- * attributes to the generated classes.
  */
-interface ServerCodegenDecorator {
-    /**
-     * The name of this [ServerCodegenDecorator], used for logging and debug information
-     */
-    val name: String
-
-    /**
-     * Enable a deterministic ordering to be applied, with the lowest numbered integrations being applied first
-     */
-    val order: Byte
-
-    /**
-     * Whether this decorator can be discovered on the classpath (defaults to true).
-     * This is intended to only be overridden for decorators written specifically for tests.
-     */
-    fun classpathDiscoverable(): Boolean = true
-
-    fun libRsCustomizations(
-        codegenContext: ServerCodegenContext,
-        baseCustomizations: List<LibRsCustomization>,
-    ): List<LibRsCustomization> = baseCustomizations
-
-    /**
-     * Returns a map of Cargo.toml properties to change. For example, if a `homepage` needs to be
-     * added to the Cargo.toml `[package]` section, a `mapOf("package" to mapOf("homepage", "https://example.com"))`
-     * could be returned. Properties here overwrite the default properties.
-     */
-    fun crateManifestCustomizations(codegenContext: ServerCodegenContext): ManifestCustomizations = emptyMap()
-
-    fun extras(codegenContext: ServerCodegenContext, rustCrate: RustCrate) {}
-
+interface ServerCodegenDecorator : CoreCodegenDecorator<ServerCodegenContext> {
     fun protocols(serviceId: ShapeId, currentProtocols: ServerProtocolMap): ServerProtocolMap = currentProtocols
-
-    fun transformModel(service: ServiceShape, model: Model): Model = model
 }
 
 /**
@@ -69,46 +28,18 @@ interface ServerCodegenDecorator {
  *
  * This makes the actual concrete codegen simpler by not needing to deal with multiple separate decorators.
  */
-class CombinedServerCodegenDecorator(decorators: List<ServerCodegenDecorator>) : ServerCodegenDecorator {
-    private val orderedDecorators = decorators.sortedBy { it.order }
+class CombinedServerCodegenDecorator(decorators: List<ServerCodegenDecorator>) :
+    CombinedCoreCodegenDecorator<ServerCodegenContext, ServerCodegenDecorator>(decorators),
+    ServerCodegenDecorator {
     override val name: String
         get() = "CombinedServerCodegenDecorator"
     override val order: Byte
         get() = 0
 
-    override fun libRsCustomizations(
-        codegenContext: ServerCodegenContext,
-        baseCustomizations: List<LibRsCustomization>,
-    ): List<LibRsCustomization> {
-        return orderedDecorators.foldRight(baseCustomizations) { decorator, customizations ->
-            decorator.libRsCustomizations(
-                codegenContext,
-                customizations,
-            )
-        }
-    }
-
-    override fun protocols(serviceId: ShapeId, currentProtocols: ServerProtocolMap): ServerProtocolMap {
-        return orderedDecorators.foldRight(currentProtocols) { decorator, protocolMap ->
+    override fun protocols(serviceId: ShapeId, currentProtocols: ServerProtocolMap): ServerProtocolMap =
+        combineCustomizations(currentProtocols) { decorator, protocolMap ->
             decorator.protocols(serviceId, protocolMap)
         }
-    }
-
-    override fun crateManifestCustomizations(codegenContext: ServerCodegenContext): ManifestCustomizations {
-        return orderedDecorators.foldRight(emptyMap()) { decorator, customizations ->
-            customizations.deepMergeWith(decorator.crateManifestCustomizations(codegenContext))
-        }
-    }
-
-    override fun extras(codegenContext: ServerCodegenContext, rustCrate: RustCrate) {
-        return orderedDecorators.forEach { it.extras(codegenContext, rustCrate) }
-    }
-
-    override fun transformModel(service: ServiceShape, model: Model): Model {
-        return orderedDecorators.foldRight(model) { decorator, otherModel ->
-            decorator.transformModel(service, otherModel)
-        }
-    }
 
     companion object {
         fun fromClasspath(
@@ -116,17 +47,8 @@ class CombinedServerCodegenDecorator(decorators: List<ServerCodegenDecorator>) :
             vararg extras: ServerCodegenDecorator,
             logger: Logger = Logger.getLogger("RustServerCodegenSPILoader"),
         ): CombinedServerCodegenDecorator {
-            val decorators = ServiceLoader.load(
-                ServerCodegenDecorator::class.java,
-                context.pluginClassLoader.orElse(ServerCodegenDecorator::class.java.classLoader),
-            )
-
-            val filteredDecorators = decorators.asSequence()
-                .onEach { logger.info("Discovered Codegen Decorator: ${it.javaClass.name}") }
-                .filter { it.classpathDiscoverable() }
-                .onEach { logger.info("Adding Codegen Decorator: ${it.javaClass.name}") }
-                .toList()
-            return CombinedServerCodegenDecorator(filteredDecorators + extras)
+            val decorators = decoratorsFromClasspath(context, ServerCodegenDecorator::class.java, logger, *extras)
+            return CombinedServerCodegenDecorator(decorators)
         }
     }
 }
