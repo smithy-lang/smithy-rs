1953e531c381e9159073db06bc171a17af9e05b2
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2022-12-20 07:08:32 +0000

    fix feature gate

--- rust-runtime/aws-smithy-http/src/event_stream.rs
+++ rust-runtime/aws-smithy-http/src/event_stream.rs
@@ -5,63 +5,68 @@
 
 //! Provides Sender/Receiver implementations for Event Stream codegen.
 
-use std::{error::Error as StdError, fmt::Debug, marker::PhantomData};
+use std::{error::Error as StdError, fmt::Debug};
 
 mod receiver;
 mod sender;
 
 pub type BoxError = Box<dyn StdError + Send + Sync + 'static>;
-#[cfg(all(feature = "unstable", feature = "deserialize"))]
-use aws_smithy_eventstream::frame::UnmarshallMessage;
 #[doc(inline)]
 pub use sender::{EventStreamSender, MessageStreamAdapter, MessageStreamError};
 
 #[doc(inline)]
 pub use receiver::{RawMessage, Receiver};
 
-/// This data is used to fill a field when the users try to deserialize data that has a Receiver in one of the field.
-pub struct DeserializedReceiverStream<T, E>(PhantomData<(T, E)>);
-impl<T, E> Debug for DeserializedReceiverStream<T, E> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("DeserializedStream")
+pub use deserialized_stream::*;
+#[cfg(all(feature = "unstable", feature = "deserialize"))]
+pub mod deserialized_stream {
+    use super::*;
+    use aws_smithy_eventstream::frame::UnmarshallMessage;
+    use std::marker::PhantomData;
+    /// This data is used to fill a field when the users try to deserialize data that has a Receiver in one of the field.
+    pub struct DeserializedReceiverStream<T, E>(PhantomData<(T, E)>);
+    impl<T, E> Debug for DeserializedReceiverStream<T, E> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_str("DeserializedStream")
+        }
     }
-}
-impl<T, E> DeserializedReceiverStream<T, E>
-where
-    T: std::fmt::Debug,
-    E: std::fmt::Debug,
-{
-    /// default value
-    pub fn create() -> impl UnmarshallMessage<Output = T, Error = E> {
-        Self(PhantomData::<(T, E)>)
+    impl<T, E> DeserializedReceiverStream<T, E>
+    where
+        T: std::fmt::Debug,
+        E: std::fmt::Debug,
+    {
+        /// default value
+        pub fn create() -> impl UnmarshallMessage<Output = T, Error = E> {
+            Self(PhantomData::<(T, E)>)
+        }
     }
-}
 
-impl<T, E> UnmarshallMessage for DeserializedReceiverStream<T, E>
-where
-    T: Debug,
-    E: Debug,
-{
-    type Error = E;
-    type Output = T;
-    fn unmarshall(
-        &self,
-        _: &aws_smithy_eventstream::frame::Message,
-    ) -> Result<
-        aws_smithy_eventstream::frame::UnmarshalledMessage<Self::Output, Self::Error>,
-        aws_smithy_eventstream::error::Error,
-    > {
-        Err(aws_smithy_eventstream::error::Error::deserialized_stream())
+    impl<T, E> UnmarshallMessage for DeserializedReceiverStream<T, E>
+    where
+        T: Debug,
+        E: Debug,
+    {
+        type Error = E;
+        type Output = T;
+        fn unmarshall(
+            &self,
+            _: &aws_smithy_eventstream::frame::Message,
+        ) -> Result<
+            aws_smithy_eventstream::frame::UnmarshalledMessage<Self::Output, Self::Error>,
+            aws_smithy_eventstream::error::Error,
+        > {
+            Err(aws_smithy_eventstream::error::Error::deserialized_stream())
+        }
     }
-}
 
-/// Error returned from Deserialized Stream.
-#[derive(Debug)]
-struct DeserializedStreamError;
+    /// Error returned from Deserialized Stream.
+    #[derive(Debug)]
+    pub struct DeserializedStreamError;
 
-impl StdError for DeserializedStreamError {}
-impl std::fmt::Display for DeserializedStreamError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("Stream was deserialized")
+    impl StdError for DeserializedStreamError {}
+    impl std::fmt::Display for DeserializedStreamError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_str("Stream was deserialized")
+        }
     }
 }
