07bae40ede10db9322d1b8511b20a19bb3a58686
Thomas Cameron <68596478+thomas-k-cameron@users.noreply.github.com>
2023-01-11 15:11:37 +0900

    Update error.rs

--- rust-runtime/aws-smithy-eventstream/src/error.rs
+++ rust-runtime/aws-smithy-eventstream/src/error.rs
@@ -1,7 +1,7 @@
 /*
-* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-* SPDX-License-Identifier: Apache-2.0
-*/
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 use aws_smithy_types::DateTime;
 use std::error::Error as StdError;
