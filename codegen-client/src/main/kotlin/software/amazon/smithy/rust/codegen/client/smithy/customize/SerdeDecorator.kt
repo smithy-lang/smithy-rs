cd602bf1458e309a3ac2c5929556a54d539776fc
Thomas Cameron <68596478+thomas-k-cameron@users.noreply.github.com>
2023-01-12 11:56:25 +0000

    fixed?

--- codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customize/SerdeDecorator.kt
+++ codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customize/SerdeDecorator.kt
@@ -1,27 +1,41 @@
 /*
-    * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-    * SPDX-License-Identifier: Apache-2.0
-    */
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+* SPDX-License-Identifier: Apache-2.0
+*/
 
-package software.amazon.smithy.rust.codegen.client.smithy.customizations
+package software.amazon.smithy.rust.codegen.client.smithy.customize
 
-import software.amazon.smithy.rust.codegen.core.rustlang.Writable
-import software.amazon.smithy.rust.codegen.core.rustlang.containerDocs
-import software.amazon.smithy.rust.codegen.core.rustlang.writable
-import software.amazon.smithy.rust.codegen.core.smithy.generators.LibRsCustomization
-import software.amazon.smithy.rust.codegen.core.smithy.generators.LibRsSection
+import software.amazon.smithy.model.shapes.OperationShape
+import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
+import software.amazon.smithy.rust.codegen.client.smithy.customizations.EndpointPrefixGenerator
+import software.amazon.smithy.rust.codegen.client.smithy.customizations.HttpChecksumRequiredGenerator
+import software.amazon.smithy.rust.codegen.client.smithy.customizations.HttpVersionListCustomization
+import software.amazon.smithy.rust.codegen.client.smithy.customizations.IdempotencyTokenGenerator
+import software.amazon.smithy.rust.codegen.client.smithy.customizations.ResiliencyConfigCustomization
+import software.amazon.smithy.rust.codegen.client.smithy.customizations.ResiliencyReExportCustomization
 import software.amazon.smithy.rust.codegen.client.smithy.generators.config.ConfigCustomization
 import software.amazon.smithy.rust.codegen.client.smithy.customize.ClientCodegenDecorator
-import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
+import software.amazon.smithy.rust.codegen.core.rustlang.Feature
+import software.amazon.smithy.rust.codegen.core.smithy.RustCrate
+import software.amazon.smithy.rust.codegen.core.smithy.customizations.AllowLintsCustomization
+import software.amazon.smithy.rust.codegen.core.smithy.customizations.CrateVersionCustomization
+import software.amazon.smithy.rust.codegen.core.smithy.customizations.pubUseSmithyTypes
+import software.amazon.smithy.rust.codegen.core.smithy.customize.OperationCustomization
+import software.amazon.smithy.rust.codegen.core.smithy.generators.LibRsCustomization
+import software.amazon.smithy.rust.codegen.core.smithy.CodegenContext
 
-class SerdeDecorator: ClientCodegenDecorator {
-    override val name: String = "SerdeDecorator"
-    override val order: Byte = 0
+/**
+ * This class,
+ * - Adds serde as a dependency
+ *
+ */
+class SerdeDecorator : ClientCodegenDecorator {
+    override val name: String = "Required"
+    override val order: Byte = -1
 
-    override fun configCustomizations(
-        codegenContext: ClientCodegenContext,
-        baseCustomizations: List<ConfigCustomization>,
-    ): List<ConfigCustomization> {
-        codegenContext.settings.
+    override fun extras(codegenContext: ClientCodegenContext, rustCrate: RustCrate) {
+        rustCrate.mergeFeature(Feature("serde-serialize", false, listOf("aws-sdk-types/serde-serialize")))
+        rustCrate.mergeFeature(Feature("serde-deserialize", false, listOf("aws-sdk-types/serde-deserialize")))
     }
+
 }
