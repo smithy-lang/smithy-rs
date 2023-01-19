88482299900fb51071c9dbef73862fd6e99d399a
Zelda Hessler <zelda.hessler@pm.me>
2023-01-19 14:40:34 -0600

    add/license header

--- rust-runtime/aws-smithy-types/src/date_time/de.rs
+++ rust-runtime/aws-smithy-types/src/date_time/de.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 use super::*;
 use serde::de::Visitor;
 use serde::Deserialize;
