cddb822fb2d487f46bc411bbe8577ec831b1ecb5
John DiSanti <jdisanti@amazon.com>
2023-01-26 11:32:11 -0800

    Copy non-service integration tests into SDK root tests directory
    For tests in `aws/sdk/integration-tests` that are not named after a
    service module, copy them into the SDK's root `tests/` directory so that
    they are run as part of CI on the entire SDK.

--- aws/sdk/build.gradle.kts
+++ aws/sdk/build.gradle.kts
@@ -198,6 +198,28 @@ tasks.register("relocateExamples") {
     outputs.dir(outputDir)
 }
 
+tasks.register("relocateTests") {
+    description = "relocate the root integration tests and rewrite path dependencies"
+    doLast {
+        if (awsServices.rootTests.isNotEmpty()) {
+            copy {
+                val testDir = projectDir.resolve("integration-tests")
+                from(testDir)
+                awsServices.rootTests.forEach { test ->
+                    include(test.path.toRelativeString(testDir) + "/**")
+                }
+                into(outputDir.resolve("tests"))
+                exclude("**/target")
+                filter { line -> line.replace("build/aws-sdk/sdk/", "sdk/") }
+            }
+        }
+    }
+    for (test in awsServices.rootTests) {
+        inputs.dir(test.path)
+    }
+    outputs.dir(outputDir)
+}
+
 tasks.register<ExecRustBuildTool>("fixExampleManifests") {
     description = "Adds dependency path and corrects version number of examples after relocation"
     enabled = awsServices.examples.isNotEmpty()
@@ -287,6 +309,9 @@ tasks.register("generateCargoWorkspace") {
     if (awsServices.examples.isNotEmpty()) {
         inputs.dir(projectDir.resolve("examples"))
     }
+    for (test in awsServices.rootTests) {
+        inputs.dir(test.path)
+    }
     outputs.file(outputDir.resolve("Cargo.toml"))
     outputs.upToDateWhen { false }
 }
@@ -310,6 +335,7 @@ tasks.register<ExecRustBuildTool>("fixManifests") {
     dependsOn("relocateRuntime")
     dependsOn("relocateAwsRuntime")
     dependsOn("relocateExamples")
+    dependsOn("relocateTests")
 }
 
 tasks.register<ExecRustBuildTool>("hydrateReadme") {
@@ -371,6 +397,7 @@ tasks.register("finalizeSdk") {
         "relocateRuntime",
         "relocateAwsRuntime",
         "relocateExamples",
+        "relocateTests",
         "generateIndexMd",
         "fixManifests",
         "generateVersionManifest",
