1687bbc4dd01e0006f09a307d21f9e766f513d49
Eduardo de Moura Rodrigues <16357187+eduardomourar@users.noreply.github.com>
2023-01-27 21:09:56 +0100

    Update aws/rust-runtime/aws-config/src/lib.rs
    Co-authored-by: John DiSanti <johndisanti@gmail.com>

--- aws/rust-runtime/aws-config/src/lib.rs
+++ aws/rust-runtime/aws-config/src/lib.rs
@@ -121,7 +121,7 @@ pub mod meta;
 pub mod profile;
 pub mod provider_config;
 pub mod retry;
-#[cfg(feature = "credentials-sso ")]
+#[cfg(feature = "credentials-sso")]
 pub mod sso;
 pub(crate) mod standard_property;
 pub mod sts;
