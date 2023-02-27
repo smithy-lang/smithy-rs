8f702375e1f76df3e42282115635899e2f17135c
Julian Antonielli <julianantonielli@gmail.com>
2023-02-27 10:49:50 +0000

    Remove clippy allowance

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/protocols/serialize/EventStreamErrorMarshallerGenerator.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/protocols/serialize/EventStreamErrorMarshallerGenerator.kt
@@ -12,7 +12,6 @@ import software.amazon.smithy.model.shapes.StructureShape
 import software.amazon.smithy.model.shapes.UnionShape
 import software.amazon.smithy.model.traits.EventHeaderTrait
 import software.amazon.smithy.model.traits.EventPayloadTrait
-import software.amazon.smithy.rust.codegen.core.rustlang.Attribute
 import software.amazon.smithy.rust.codegen.core.rustlang.RustModule
 import software.amazon.smithy.rust.codegen.core.rustlang.RustWriter
 import software.amazon.smithy.rust.codegen.core.rustlang.render
@@ -20,7 +19,6 @@ import software.amazon.smithy.rust.codegen.core.rustlang.rust
 import software.amazon.smithy.rust.codegen.core.rustlang.rustBlock
 import software.amazon.smithy.rust.codegen.core.rustlang.rustBlockTemplate
 import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
-import software.amazon.smithy.rust.codegen.core.rustlang.writable
 import software.amazon.smithy.rust.codegen.core.smithy.CodegenTarget
 import software.amazon.smithy.rust.codegen.core.smithy.RuntimeConfig
 import software.amazon.smithy.rust.codegen.core.smithy.RuntimeType
@@ -72,22 +70,19 @@ class EventStreamErrorMarshallerGenerator(
     }
 
     private fun RustWriter.renderMarshaller(marshallerType: RuntimeType, unionSymbol: Symbol) {
-        rustTemplate(
+        rust(
             """
             /// Marshaller for event stream error.
             ##[non_exhaustive]
             ##[derive(Debug)]
             pub struct ${marshallerType.name};
 
-            #{AllowClippyNewWithoutDefault:W}
             impl ${marshallerType.name} {
-                /// Create a new `${marshallerType.name}`.
                 pub fn new() -> Self {
                     ${marshallerType.name}
                 }
             }
             """,
-            "AllowClippyNewWithoutDefault" to writable { Attribute.AllowClippyNewWithoutDefault.render(this) },
         )
 
         rustBlockTemplate(
