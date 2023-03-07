fdf1f2d20a90724a672a1e08510ce1067747deba
82marbag <69267416+82marbag@users.noreply.github.com>
2023-03-07 11:48:29 +0100

    Add request ID to response headers
    Signed-off-by: Daniele Ahmed <ahmeddan@amazon.de>

--- rust-runtime/aws-smithy-http-server/src/request/request_id.rs
+++ rust-runtime/aws-smithy-http-server/src/request/request_id.rs
@@ -17,6 +17,9 @@
 //!
 //! The [`ServerRequestId`] is not meant to be propagated to downstream dependencies of the service. You should rely on a distributed tracing implementation for correlation purposes (e.g. OpenTelemetry).
 //!
+//! To optionally add the [`ServerRequestId`] to the response headers, use [`ServerRequestIdResponseProviderLayer`].
+//! [`ServerRequestIdResponseProviderLayer`] must be added _before_ [`ServerRequestIdProviderLayer`]. Otherwise, it is a no-op layer.
+//!
 //! ## Examples
 //!
 //! Your handler can now optionally take as input a [`ServerRequestId`].
@@ -34,7 +37,9 @@
 //!     .operation(handler)
 //!     .build().unwrap();
 //!
-//! let app = app.layer(&ServerRequestIdProviderLayer::new()); /* Generate a server request ID */
+//! let app = app
+//!     .layer(&ServerRequestIdResponseProviderLayer::new("X-Request-Id".into())) /* Add the server generated ID to the response */
+//!     .layer(&ServerRequestIdProviderLayer::new()); /* Generate a server request ID */
 //!
 //! let bind: std::net::SocketAddr = format!("{}:{}", args.address, args.port)
 //!     .parse()
@@ -47,7 +52,9 @@ use std::{
     task::{Context, Poll},
 };
 
+use futures_util::{FutureExt, TryFutureExt};
 use http::request::Parts;
+use http::{HeaderName, HeaderValue, Response};
 use thiserror::Error;
 use tower::{Layer, Service};
 use uuid::Uuid;
@@ -148,5 +155,65 @@ where
 impl<Protocol> IntoResponse<Protocol> for MissingServerRequestId {
     fn into_response(self) -> http::Response<BoxBody> {
         internal_server_error()
+    }
+}
+
+#[derive(Clone)]
+pub struct ServerRequestIdResponseProvider<S> {
+    inner: S,
+    header_key: HeaderName,
+}
+
+/// A layer that inserts the generated [`ServerRequestId`] to the response headers
+#[derive(Debug)]
+#[non_exhaustive]
+pub struct ServerRequestIdResponseProviderLayer {
+    header_key: Box<str>,
+}
+
+impl ServerRequestIdResponseProviderLayer {
+    /// Add the request ID to the response header `header_key`
+    pub fn new(header_key: Box<str>) -> Self {
+        Self { header_key }
+    }
+}
+
+impl<S> Layer<S> for ServerRequestIdResponseProviderLayer {
+    type Service = ServerRequestIdResponseProvider<S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        let header_key = HeaderName::from_bytes(self.header_key.as_bytes()).unwrap();
+        ServerRequestIdResponseProvider { inner, header_key }
+    }
+}
+
+impl<Body, S> Service<http::Request<Body>> for ServerRequestIdResponseProvider<S>
+    where
+        S: Service<http::Request<Body>, Response = Response<crate::body::BoxBody>>,
+        S::Future: std::marker::Send + 'static
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = std::pin::Pin<Box<dyn Send + std::future::Future<Output=Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: http::Request<Body>) -> Self::Future {
+        let request_id = req.extensions().get::<ServerRequestId>();
+        if request_id.is_none() {
+            return Box::pin(self.inner.call(req))
+        }
+        let request_id = request_id.unwrap().to_owned();
+        let header_key = self.header_key.clone();
+        self.inner.call(req)
+            .map_ok(move |mut res| -> Self::Response {
+                if let Ok(value) = HeaderValue::from_str(&request_id.id.to_string()) {
+                    res.headers_mut().insert(header_key, value);
+                }
+                res
+            })
+            .boxed()
     }
 }
