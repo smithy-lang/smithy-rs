83b98fc793042d6d8d7c61e252de56fd26b48de5
Russell Cohen <rcoh@amazon.com>
2023-03-20 11:09:54 -0400

    Allow endpoint resolver to be omitted

--- aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/endpoints/RequireEndpointRules.kt
+++ aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/endpoints/RequireEndpointRules.kt
@@ -10,16 +10,19 @@ import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
 import software.amazon.smithy.rust.codegen.client.smithy.customize.ClientCodegenDecorator
 import software.amazon.smithy.rust.codegen.client.smithy.endpoint.EndpointTypesGenerator
 import software.amazon.smithy.rust.codegen.core.smithy.RustCrate
+import software.amazon.smithy.rustsdk.sdkSettings
 
 class RequireEndpointRules : ClientCodegenDecorator {
     override val name: String = "RequireEndpointRules"
     override val order: Byte = 100
     override fun extras(codegenContext: ClientCodegenContext, rustCrate: RustCrate) {
+        if (!codegenContext.sdkSettings().requireEndpointResolver) {
+            return
+        }
         val epTypes = EndpointTypesGenerator.fromContext(codegenContext)
         if (epTypes.defaultResolver() == null) {
             throw CodegenException(
-                "${codegenContext.serviceShape} did not provide endpoint rules. " +
-                    "This is a bug and the generated client will not work. All AWS services MUST define endpoint rules.",
+                "${codegenContext.serviceShape} did not provide endpoint rules. To explicitly allow this, set `awsSdk.requireEndpointResolver: false` in smithy-build.json.",
             )
         }
     }
