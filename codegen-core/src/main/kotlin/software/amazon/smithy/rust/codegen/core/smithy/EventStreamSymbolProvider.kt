3d4aa5927acd2763b3406c33d14f04875bb72012
Burak Varlı <burakvar@amazon.co.uk>
2023-03-20 10:10:04 +0000

    Use `RustType.Application` for event streaming symbols

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/EventStreamSymbolProvider.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/EventStreamSymbolProvider.kt
@@ -48,15 +48,15 @@ class EventStreamSymbolProvider(
                 } else {
                     symbolForEventStreamError(unionShape)
                 }
-                val errorFmt = error.rustType().render(fullyQualified = true)
-                val innerFmt = initial.rustType().stripOuter<RustType.Option>().render(fullyQualified = true)
+                val errorT = error.rustType()
+                val innerT = initial.rustType().stripOuter<RustType.Option>()
                 val isSender = (shape.isInputEventStream(model) && target == CodegenTarget.CLIENT) ||
                     (shape.isOutputEventStream(model) && target == CodegenTarget.SERVER)
                 val outer = when (isSender) {
-                    true -> "EventStreamSender<$innerFmt, $errorFmt>"
-                    else -> "Receiver<$innerFmt, $errorFmt>"
+                    true -> RuntimeType.eventStreamSender(runtimeConfig).toSymbol().rustType()
+                    else -> RuntimeType.eventStreamReceiver(runtimeConfig).toSymbol().rustType()
                 }
-                val rustType = RustType.Opaque(outer, "aws_smithy_http::event_stream")
+                val rustType = RustType.Application(outer, listOf(innerT, errorT))
                 return initial.toBuilder()
                     .name(rustType.name)
                     .rustType(rustType)
