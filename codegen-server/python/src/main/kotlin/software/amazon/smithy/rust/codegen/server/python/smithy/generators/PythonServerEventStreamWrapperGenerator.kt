0195c934482eb7c51365d79835e83fbfc4976ecb
Burak Varlı <burakvar@amazon.co.uk>
2023-03-28 18:39:55 +0100

    Try to extract error type or inner type from incoming streaming value and ignore the value otherwise for now

--- codegen-server/python/src/main/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/generators/PythonServerEventStreamWrapperGenerator.kt
+++ codegen-server/python/src/main/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/generators/PythonServerEventStreamWrapperGenerator.kt
@@ -64,11 +64,13 @@ class PythonServerEventStreamWrapperGenerator(
     private val memberName = shape.memberName.toPascalCase()
 
     private val smithyHttp = RuntimeType.smithyHttp(runtimeConfig)
+    private val smithyPython = PythonServerCargoDependency.smithyHttpServerPython(runtimeConfig).toType()
     private val pyO3 = PythonServerCargoDependency.PyO3.toType()
     private val pyO3Asyncio = PythonServerCargoDependency.PyO3Asyncio.toType()
     private val tokio = PythonServerCargoDependency.Tokio.toType()
     private val futures = PythonServerCargoDependency.Futures.toType()
     private val parkingLot = PythonServerCargoDependency.ParkingLot.toType()
+    private val tracing = PythonServerCargoDependency.Tracing.toType()
 
     fun render(writer: RustWriter) {
         if (shape.isOutputEventStream(model)) {
@@ -123,15 +125,20 @@ class PythonServerEventStreamWrapperGenerator(
                 fn extract(obj: &'source #{PyO3}::PyAny) -> #{PyO3}::PyResult<Self> {
                     use #{Futures}::StreamExt;
                     let stream = #{PyO3Asyncio}::tokio::into_stream_v2(obj)?;
-                    let stream = stream.map(|obj| {
-                        #{PyO3}::Python::with_gil(|py| {
-                            obj.extract(py).map_err(|_err| {
-                                // TODO: Extract real error.
-                                #{Error}::ThrottlingError(
-                                    crate::error::ThrottlingError {},
-                                )
-                            })
-                        })
+                    let stream = stream.filter_map(|obj| {
+                        #{Futures}::future::ready(#{PyO3}::Python::with_gil(|py| {
+                            if let Ok(err) = obj.extract::<#{Error}>(py) {
+                                return Some(Err(err));
+                            }
+
+                            if let Ok(res) = obj.extract::<#{Inner}>(py) {
+                                return Some(Ok(res));
+                            }
+
+                            // TODO: Add `InternalServerError` variant to all event streaming errors and return that variant here? 
+                            #{Tracing}::error!(value = ?obj, "could not extract '#{Inner}' or '#{Error}' from streamed value");
+                            None
+                        }))
                     });
             
                     Ok($name { inner: std::sync::Arc::new(#{Mutex}::new(Some(stream.into()))) })
@@ -145,9 +152,12 @@ class PythonServerEventStreamWrapperGenerator(
             }
             """,
             "Mutex" to parkingLot.resolve("Mutex"),
+            "SmithyPython" to smithyPython,
+            "Tracing" to tracing,
             "PyO3" to pyO3,
             "PyO3Asyncio" to pyO3Asyncio,
             "Error" to errorT,
+            "Inner" to innerT,
             "Futures" to futures,
         )
     }
