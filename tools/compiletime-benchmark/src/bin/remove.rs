7805451442410cd86457f21e57a9fb60edc17246
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-03-30 08:04:39 +0000

    update

--- tools/compiletime-benchmark/src/bin/remove.rs
+++ tools/compiletime-benchmark/src/bin/remove.rs
@@ -1,7 +1,51 @@
-fn main() {
-    // setup ec2
-}
+use aws_sdk_resourceexplorer2::output::SearchOutput;
+use compiletime_benchmark::common_tag;
+use tokio;
 
-fn ec2() {
+#[tokio::main]
+async fn main() {
+    let conf = aws_config::load_from_env().await;
+    let cc_client = aws_sdk_cloudcontrol::Client::new(&conf);
+    let re2_client = aws_sdk_resourceexplorer2::Client::new(&conf);
+    let arn = { 
+        let res = re2_client.create_view().view_name("smithy-rs-compiletime-benchmark").set_tags(common_tag()).send().await.unwrap();
+        res.view().unwrap().view_arn().unwrap().to_string()
+    };
+    let target_arns = {
+        let mut res: Option<SearchOutput> = None;
+        let mut tasks = vec![];
+        let mut target_arns = vec![];
+        loop {
+            let next_token = res.and_then(|i| i.next_token()).and_then(|i|i.to_string().into());
+            let out = re2_client.search().view_arn(arn).set_next_token(next_token).send().await.unwrap();
+            for i in out.resources().unwrap() {
+                target_arns.push(i.arn().unwrap().to_string());
+                let task = tokio::spawn(async move {
+                    println!("deleting {}", i.arn().unwrap());
+                    let res = cc_client.delete_resource().identifier(i.arn().unwrap()).send().await;
+                    let token = res.unwrap().progress_event().unwrap();
+                    let status  =cc_client.get_resource_request_status().request_token(token.identifier().unwrap()).send().await;
+                    res.unwrap().progress_event().unwrap().operation_status().unwrap();
+                    res.unwrap_err().into_service_error()
+                });
+                tasks.push(task);
+            }
+            if out.next_token().is_none() {
+                break
+            };
+            res.replace(out);
+        };
+        for i in tasks { 
+            i.await;
+        }
+        target_arns
+    };
 
+    {
+        for arn in target_arns {
+            
+        }
+    };
+    re2_client.delete_view().view_arn(arn).send().await;
+    {}
 }
--- tools/compiletime-benchmark/src/lib.rs
+++ tools/compiletime-benchmark/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn force_dry_run() -> bool {
     option_env!("DRY_RUN") != Some("FALSE")
 }
+
+
+pub fn common_tag<T: DeserializeOwned>() -> T {
+    toml::from_str(include_str!("../config/common/tag.toml")).unwrap()
+}
