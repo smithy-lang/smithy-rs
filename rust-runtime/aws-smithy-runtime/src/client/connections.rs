69815d1c6398127a90611d8e3d126c20de93120b
Zelda Hessler <zhessler@amazon.com>
2023-03-30 17:40:02 -0500

    Add stubs to make SRA test fully runnable (#2519)
    * add: stubs to make sra test fully runnable (but not useful 😅)
    
    * fix: disable build.gradle enableNewSmithyRuntime option
    
    * fix: clippy lints
    add: feature gate for test connection
    
    * fix: doc comment
    
    * move: connections module from API to runtime
    
    * update: TODO message
    add: exceptions for external types
    
    * Fix external types lint
    
    ---------
    
    Co-authored-by: John DiSanti <jdisanti@amazon.com>

--- rust-runtime/aws-smithy-runtime/src/client/connections.rs
+++ rust-runtime/aws-smithy-runtime/src/client/connections.rs
@@ -3,7 +3,5 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-pub mod orchestrator;
-
-/// Smithy connector runtime plugins
-pub mod connections;
+#[cfg(feature = "test-util")]
+pub mod test_connection;
