dce3336406bc1288b908a47fb4b1f95fa5314741
david-perez <d@vidp.dev>
2023-04-03 16:43:58 +0200

    Use `aws_smithy_types::primitive::Parse` in the server (#2528)
    This further reduces the number of request rejection variants.
    https://docs.rs/aws-smithy-types/latest/aws_smithy_types/primitive/trait.Parse.html
    
    Closes #1232.

--- rust-runtime/aws-smithy-http-server/src/proto/rest_xml/rejection.rs
+++ rust-runtime/aws-smithy-http-server/src/proto/rest_xml/rejection.rs
@@ -44,10 +44,6 @@ pub enum RequestRejection {
 
     PrimitiveParse(crate::Error),
 
-    IntParse(crate::Error),
-    FloatParse(crate::Error),
-    BoolParse(crate::Error),
-
     ConstraintViolation(String),
 }
 
@@ -81,9 +77,6 @@ convert_to_request_rejection!(aws_smithy_xml::decode::XmlDecodeError, XmlDeseria
 convert_to_request_rejection!(aws_smithy_http::header::ParseError, HeaderParse);
 convert_to_request_rejection!(aws_smithy_types::date_time::DateTimeParseError, DateTimeParse);
 convert_to_request_rejection!(aws_smithy_types::primitive::PrimitiveParseError, PrimitiveParse);
-convert_to_request_rejection!(std::str::ParseBoolError, BoolParse);
-convert_to_request_rejection!(std::num::ParseFloatError, FloatParse);
-convert_to_request_rejection!(std::num::ParseIntError, IntParse);
 convert_to_request_rejection!(serde_urlencoded::de::Error, InvalidUtf8);
 
 impl From<nom::Err<nom::error::Error<&str>>> for RequestRejection {
