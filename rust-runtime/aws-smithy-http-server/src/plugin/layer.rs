687c8976489119ea74ec0d601593f42ec5a75908
Julian Antonielli <julianantonielli@gmail.com>
2023-04-13 10:14:53 +0100

    Add `PluginPipeline::http_layer` method

--- rust-runtime/aws-smithy-http-server/src/plugin/layer.rs
+++ rust-runtime/aws-smithy-http-server/src/plugin/layer.rs
@@ -10,16 +10,16 @@ use crate::operation::Operation;
 use super::Plugin;
 
 /// A [`Plugin`] which appends a HTTP [`Layer`](tower::Layer) `L` to the existing `Layer` in [`Operation<S, Layer>`](Operation).
-pub struct HttpLayer<L>(pub L);
+pub struct HttpLayer<'a, L>(pub &'a L);
 
-impl<P, Op, S, ExistingLayer, NewLayer> Plugin<P, Op, S, ExistingLayer> for HttpLayer<NewLayer>
+impl<'a, P, Op, S, ExistingLayer, NewLayer> Plugin<P, Op, S, ExistingLayer> for HttpLayer<'a, NewLayer>
 where
     NewLayer: Clone,
 {
     type Service = S;
-    type Layer = Stack<ExistingLayer, NewLayer>;
+    type Layer = Stack<ExistingLayer, &'a NewLayer>;
 
     fn map(&self, input: Operation<S, ExistingLayer>) -> Operation<Self::Service, Self::Layer> {
-        input.layer(self.0.clone())
+        input.layer(self.0)
     }
 }
