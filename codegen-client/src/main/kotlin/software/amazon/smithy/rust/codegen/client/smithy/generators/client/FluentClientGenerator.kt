dee9fd5f896d9ca707037d7312f48085a3500390
Thomas Cameron <68596478+thomas-k-cameron@users.noreply.github.com>
2023-04-13 16:58:19 +0000

    FIX doctest

--- codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/generators/client/FluentClientGenerator.kt
+++ codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/generators/client/FluentClientGenerator.kt
@@ -312,7 +312,7 @@ class FluentClientGenerator(
 
                 /// This function replaces the parameter with new one.
                 /// It is useful when you want to replace the existing data with de-serialized data.
-                /// ```rust
+                /// ```compile_fail
                 /// let result_future = async {
                 ///     let deserialized_parameters: $inputBuilderType  = serde_json::from_str("some json data").unwrap();
                 ///     client.$fnName().set_fields(&deserialized_parameters).send().await
