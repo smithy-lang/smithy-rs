1d9cd835febea8577556d18e68aafe3eeb4b0da6
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-04-21 11:55:28 +0000

    remove polars

--- tools/compiletime-benchmark/format.py
+++ tools/compiletime-benchmark/format.py
@@ -2,35 +2,45 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
-
-import polars as pl
-
 def main():
     file = open("/tmp/compiletime-benchmark.txt", "r").read()
     stack = []
     idx = 0
     hashmap = {}
     for i in file.split("\n"):
-        if idx > 4:
-            idx = 0
+        idx += 1
+        if idx > 5:
+            idx = 1
 
-        if idx == 0:
+        if idx == 1:
             hashmap["sdk"] = i
-        elif idx == 1:
+            continue
+
+        i = float(i)
+        if idx == 2:
             hashmap["dev"] = i
-        elif idx == 2:
-            hashmap["release"] = i
         elif idx == 3:
-            hashmap["dev-w-all-features"] = i
+            hashmap["release"] = i
         elif idx == 4:
+            hashmap["dev-w-all-features"] = i
+        elif idx == 5:
             hashmap["release-w-all-features"] = i
+            stack.append(hashmap)
+            hashmap = {}
 
-        idx+=1
+    header = "|".join(stack[0].keys())
+    header = f"|{header}|"
+    table = [header]
+    for hashmap in stack:
+        row = []
+        for i in hashmap.keys():
+            row.append(str(hashmap[i]))
+        inner = "|".join(row)
+        inner = f"|{inner}|"
+        table.append(inner)
 
-    df = pl.DataFrame(stack).sort("sdk").select(pl.all().exclude("sdk").cast(pl.Float64))
-    dev = df.filter(pl.col("sdk") == "s3").to_dicts()[0]["dev"]
-    df = df.with_columns(pl.col(pl.Float64)/dev)
-    print(df)
-    df.to_pandas().to_markdown("./benchmark.md")
+    markdown = "\n".join(table)
+    print(markdown)
+    # send markdown to somewhere
 
 main()
