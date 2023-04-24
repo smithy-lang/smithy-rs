d06895b542962be5bde535a96ea9264e78be7e84
Zelda Hessler <zhessler@amazon.com>
2023-04-24 14:43:25 -0500

    feature: Invocation ID interceptor

---
+++ aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/InvocationIdDecorator.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.rustsdk
+
+import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
+import software.amazon.smithy.rust.codegen.client.smithy.customize.ClientCodegenDecorator
+import software.amazon.smithy.rust.codegen.client.smithy.generators.ServiceRuntimePluginCustomization
+import software.amazon.smithy.rust.codegen.client.smithy.generators.ServiceRuntimePluginSection
+import software.amazon.smithy.rust.codegen.core.rustlang.Writable
+import software.amazon.smithy.rust.codegen.core.rustlang.rust
+import software.amazon.smithy.rust.codegen.core.rustlang.writable
+import software.amazon.smithy.rust.codegen.core.util.letIf
+
+class InvocationIdDecorator : ClientCodegenDecorator {
+    override val name: String get() = "RecursionDetectionDecorator"
+    override val order: Byte get() = 0
+    override fun serviceRuntimePluginCustomizations(
+        codegenContext: ClientCodegenContext,
+        baseCustomizations: List<ServiceRuntimePluginCustomization>,
+    ): List<ServiceRuntimePluginCustomization> =
+        baseCustomizations.letIf(codegenContext.settings.codegenConfig.enableNewSmithyRuntime) {
+            it + listOf(InvocationIdRuntimePluginCustomization(codegenContext))
+        }
+}
+
+private class InvocationIdRuntimePluginCustomization(
+    private val codegenContext: ClientCodegenContext,
+) : ServiceRuntimePluginCustomization() {
+    override fun section(section: ServiceRuntimePluginSection): Writable = writable {
+        if (section is ServiceRuntimePluginSection.AdditionalConfig) {
+            section.registerInterceptor(codegenContext.runtimeConfig, this) {
+                rust(
+                    "#T::new()",
+                    AwsRuntimeType.awsRuntime(codegenContext.runtimeConfig)
+                        .resolve("invocation_id::InvocationIdInterceptor"),
+                )
+            }
+        }
+    }
+}
