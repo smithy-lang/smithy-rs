e866bb3074994fa550271cb7da55ace195827b1f
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-04-27 07:54:06 +0000

    Fix line break

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/generators/StructureGenerator.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/generators/StructureGenerator.kt
@@ -4,6 +4,7 @@
  */
 
 package software.amazon.smithy.rust.codegen.core.smithy.generators
+
 import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.model.Model
 import software.amazon.smithy.model.shapes.MemberShape
--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/generators/UnionGenerator.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/generators/UnionGenerator.kt
@@ -4,6 +4,7 @@
  */
 
 package software.amazon.smithy.rust.codegen.core.smithy.generators
+
 import software.amazon.smithy.codegen.core.Symbol
 import software.amazon.smithy.codegen.core.SymbolProvider
 import software.amazon.smithy.model.Model
