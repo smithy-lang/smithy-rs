55983b2ad684a7c4154c157022ea163e5d2f73a9
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-04-28 01:19:27 +0000

    update

--- rust-runtime/aws-smithy-types/src/serde_impl_check.rs
+++ rust-runtime/aws-smithy-types/src/serde_impl_check.rs
@@ -1,20 +1,42 @@
-use std::process::Command;
+use std::{path::PathBuf, process::Command, str::FromStr};
 
 /// ensures serde features is not enabled when features are not enabled
+/// Checks whether features are properly gated behind aws_sdk_unstable
 fn base(dt: &str) {
+    // data type
     let data_type = format!("aws-smithy-types::{dt}");
+    // commands 2 run
     let array = [
-        "cargo build --all-features",
-        "cargo build --features serde-serialize",
-        "cargo build --features serde-deserialize",
+        ("cargo build --all-features", [true; 2]),
+        ("cargo build --features serde-serialize", [true; 2]),
+        ("cargo build --features serde-deserialize", [true; 2]),
+        // checks if features are properly gated behind serde-serialize/deserialize
+        ("cargo build --cfg aws_sdk_unstable", [true; 2]),
+        (
+            "cargo build --features serde-serialize --cfg aws_sdk_unstable",
+            [true; 2],
+        ),
+        (
+            "cargo build --features serde-deserialize --cfg aws_sdk_unstable",
+            [true; 2],
+        ),
     ];
-    let cmdpath = "/tmp/cmd.sh";
+
+    // const
+    let replace_data_type = "ReplaceDataType";
+
+    // templates
     let deser = include_str!("../test_data/template/ser.rs");
     let ser = include_str!("../test_data/template/deser.rs");
     let cargo = include_str!("../test_data/template/Cargo.toml");
-    let replace_data_type = "ReplaceDataType";
-    
-    for cmd in array {
+
+    // paths
+    let cmdpath = "/tmp/cmd.sh";
+    let base_path = PathBuf::from_str("/tmp/").unwrap().join(dt);
+    let src_path = base_path.join("src");
+    let main_path = src_path.join("main.rs");
+
+    for (cmd, b) in array {
         std::fs::write(cmdpath, cmd).unwrap();
         let func = || {
             let check = Command::new("bash")
@@ -26,28 +48,21 @@ fn base(dt: &str) {
                 .success();
             assert!(!check);
         };
-        std::fs::create_dir("/tmp/test").unwrap();
-        std::fs::create_dir("/tmp/test/src").unwrap();
-        std::fs::write(
-            "/tmp/test/Cargo.toml",
-            cargo,
-        )
-        .unwrap();
-        std::fs::write(
-            "/tmp/test/src/main.rs",
-            ser.replace(replace_data_type, &data_type),
-        )
-        .unwrap();
-        func();
-        std::fs::write(
-            "/tmp/test/src/main.rs",
-            deser.replace(replace_data_type, &data_type),
-        )
-        .unwrap();
-        func();
+
+        std::fs::create_dir_all(&base_path).unwrap();
+        std::fs::create_dir_all(&src_path).unwrap();
+        std::fs::write(&base_path.join("Cargo.toml"), cargo).unwrap();
+
+        if b[0] {
+            std::fs::write(&main_path, ser.replace(replace_data_type, &data_type)).unwrap();
+            func();
+        }
+        if b[1] {
+            std::fs::write(&main_path, deser.replace(replace_data_type, &data_type)).unwrap();
+            func();
+        }
     }
 }
-
 
 #[test]
 fn number() {
@@ -58,7 +73,6 @@ fn number() {
 fn blob() {
     base("Blob");
 }
-
 
 #[test]
 fn document() {
