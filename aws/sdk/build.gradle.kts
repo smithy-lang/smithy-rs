7d548766ab1cc0b5f42ad69925eade9f57cb1296
Yuki Saito <awsaito@amazon.com>
2023-05-01 19:23:10 -0500

    Specify the right input and output directories

--- aws/sdk/build.gradle.kts
+++ aws/sdk/build.gradle.kts
@@ -360,7 +360,7 @@ tasks.register<ExecRustBuildTool>("hydrateReadme") {
     binaryName = "publisher"
     arguments = listOf(
         "hydrate-readme",
-        "--versions-manifest", sdkOutputDir.resolve("versions.toml").toString(),
+        "--versions-manifest", outputDir.resolve("versions.toml").toString(),
         "--msrv", getRustMSRV(),
         "--input", rootProject.projectDir.resolve("aws/SDK_README.md.hb").toString(),
         "--output", outputDir.resolve("README.md").absolutePath,
@@ -384,8 +384,10 @@ tasks.register<ExecRustBuildTool>("generateVersionManifest") {
     binaryName = "publisher"
     arguments = mutableListOf(
         "generate-version-manifest",
-        "--location",
+        "--input-location",
         sdkOutputDir.absolutePath,
+        "--output-location",
+        outputDir.absolutePath,
         "--smithy-build",
         buildDir.resolve("smithy-build.json").normalize().absolutePath,
         "--examples-revision",
