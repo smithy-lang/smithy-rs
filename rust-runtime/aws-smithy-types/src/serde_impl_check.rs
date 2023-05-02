0c533ff57c944ccb5f6a6ebb0b9cc8a663f0a211
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-05-02 06:29:54 +0000

    refactoring2

--- rust-runtime/aws-smithy-types/src/serde_impl_check.rs
+++ rust-runtime/aws-smithy-types/src/serde_impl_check.rs
@@ -5,62 +5,71 @@
 
 use std::{env::current_dir, path::PathBuf, process::Command, str::FromStr};
 
+/// Tests whether de-serialization feature for number is properly feature gated
 #[test]
 fn feature_gate_test_for_number_deserialization() {
     // create files
-    let cargo_project_path = create_cargo_dir(number);
+    let cargo_project_path = create_cargo_dir("Number");
     de_test(&cargo_project_path);
 }
 
+/// Tests whether serialization feature for number is properly feature gated
 #[test]
 fn feature_gate_test_for_number_serialization() {
     // create files
-    let cargo_project_path = create_cargo_dir(number);
+    let cargo_project_path = create_cargo_dir("Number");
     ser_test(&cargo_project_path);
 }
 
+/// Tests whether de-serialization feature for document is properly feature gated
 #[test]
 fn feature_gate_test_for_document_deserialization() {
     // create files
-    let cargo_project_path = create_cargo_dir(document);
+    let cargo_project_path = create_cargo_dir("Document");
     de_test(&cargo_project_path);
 }
 
+/// Tests whether serialization feature for document is properly feature gated
 #[test]
 fn feature_gate_test_for_document_serialization() {
     // create files
-    let cargo_project_path = create_cargo_dir(document);
+    let cargo_project_path = create_cargo_dir("Document");
     ser_test(&cargo_project_path);
 }
 
+/// Tests whether de-serialization feature for datetime is properly feature gated
 #[test]
 fn feature_gate_test_for_datetime_deserialization() {
     // create files
-    let cargo_project_path = create_cargo_dir(datetime);
+    let cargo_project_path = create_cargo_dir("DateTime");
     de_test(&cargo_project_path);
 }
 
+/// Tests whether serialization feature for datetime is properly feature gated
 #[test]
 fn feature_gate_test_for_datetime_serialization() {
     // create files
-    let cargo_project_path = create_cargo_dir(datetime);
+    let cargo_project_path = create_cargo_dir("DateTime");
     ser_test(&cargo_project_path);
 }
 
+/// Tests whether de-serialization feature for blob is properly feature gated
 #[test]
-fn feature_gate_test_for_number_deserialization() {
+fn feature_gate_test_for_blob_deserialization() {
     // create files
-    let cargo_project_path = create_cargo_dir(number);
+    let cargo_project_path = create_cargo_dir("Blob");
     de_test(&cargo_project_path);
 }
 
+/// Tests whether serialization feature for blob is properly feature gated
 #[test]
-fn feature_gate_test_for_number_serialization() {
+fn feature_gate_test_for_blob_serialization() {
     // create files
-    let cargo_project_path = create_cargo_dir(number);
+    let cargo_project_path = create_cargo_dir("Blob");
     ser_test(&cargo_project_path);
 }
 
+/// tests whether serialization features are proplery feature
 fn ser_test(cargo_project_path: &PathBuf) {
     // runs cargo check --all-features to compile a code that requries serialization feature is needed.
     // it is expected to successfully compile.
@@ -116,6 +125,7 @@ fn ser_test(cargo_project_path: &PathBuf) {
     let is_success = cmd.spawn().unwrap().wait().unwrap().success();
     assert_eq!(is_success, true);
 }
+
 fn de_test(cargo_project_path: &PathBuf) {
     // runs cargo check --all-features to compile a code that requries de-serialization feature is needed.
     // it is expected to successfully compile.
