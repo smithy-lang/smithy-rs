729a19faf7538b602e94c683904edab7f85d71d7
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-05-02 06:18:08 +0000

    refactoring

--- rust-runtime/aws-smithy-types/src/serde_impl_check.rs
+++ rust-runtime/aws-smithy-types/src/serde_impl_check.rs
@@ -5,102 +5,169 @@
 
 use std::{env::current_dir, path::PathBuf, process::Command, str::FromStr};
 
-/// ensures serde features is not enabled when features are not enabled
-/// Checks whether features are properly gated behind aws_sdk_unstable
-fn base(dt: &str) {
-    // data type
-    let data_type = format!("aws_smithy_types::{dt}");
-    // commands 2 run
-    let array = [
-        ("cargo check --all-features", [true; 2], false),
-        ("cargo check --features serde-serialize", [true; 2], false),
-        ("cargo check --features serde-deserialize", [true; 2], false),
-        // checks if features are properly gated behind serde-serialize/deserialize
-        ("cargo check", [true; 2], true),
-        (
-            "cargo check --features serde-serialize",
-            [false, true],
-            true,
-        ),
-        (
-            "cargo check --features serde-deserialize",
-            [true, false],
-            true,
-        ),
-    ];
-
-    // const
-    let replace_data_type = "ReplaceDataType";
-
-    // templates
-    let deser = include_str!("../test_data/template/ser.rs");
-    let ser = include_str!("../test_data/template/deser.rs");
-    let cargo = include_str!("../test_data/template/Cargo.toml").replace(
-        r#"aws-smithy-types = { path = "./" }"#,
-        &format!(
-            "aws-smithy-types = {{ path = {:#?} }}",
-            current_dir().unwrap().to_str().unwrap().to_string()
-        ),
-    );
-
-    // paths
-
-    let base_path = PathBuf::from_str("/tmp/smithy-rust-test").unwrap().join(dt);
-    let cmdpath = base_path.join("cmd.sh");
-    let src_path = base_path.join("src");
-    let main_path = src_path.join("main.rs");
-
-    for (cmd_txt, [check_deser, check_ser], env) in array {
-        std::fs::create_dir_all(&base_path).unwrap();
-        std::fs::create_dir_all(&src_path).unwrap();
-
-        std::fs::write(&cmdpath, cmd_txt).unwrap();
-        let func = || {
-            let mut cmd = Command::new("bash");
-            cmd.current_dir(&base_path);
-            cmd.arg(&cmdpath.to_str().unwrap().to_string());
-            if env {
-                cmd.env("RUSTFLAGS", "--cfg aws_sdk_unstable");
-            }
-
-            let check = cmd.spawn().unwrap().wait_with_output().unwrap();
-
-            assert!(
-                !check.status.success(),
-                "{:#?}",
-                (cmd, cmd_txt, check_ser, check_deser, env, dt)
-            );
-        };
-
-        std::fs::write(&base_path.join("Cargo.toml"), &cargo).unwrap();
-
-        if check_ser {
-            std::fs::write(&main_path, ser.replace(replace_data_type, &data_type)).unwrap();
-            func();
-        }
-        if check_deser {
-            std::fs::write(&main_path, deser.replace(replace_data_type, &data_type)).unwrap();
-            func();
-        }
-    }
+#[test]
+fn feature_gate_test_for_number_deserialization() {
+    // create files
+    let cargo_project_path = create_cargo_dir(number);
+    de_test(&cargo_project_path);
 }
 
 #[test]
-fn number() {
-    base("Number");
+fn feature_gate_test_for_number_serialization() {
+    // create files
+    let cargo_project_path = create_cargo_dir(number);
+    ser_test(&cargo_project_path);
 }
 
 #[test]
-fn blob() {
-    base("Blob");
+fn feature_gate_test_for_document_deserialization() {
+    // create files
+    let cargo_project_path = create_cargo_dir(document);
+    de_test(&cargo_project_path);
 }
 
 #[test]
-fn document() {
-    base("Document");
+fn feature_gate_test_for_document_serialization() {
+    // create files
+    let cargo_project_path = create_cargo_dir(document);
+    ser_test(&cargo_project_path);
 }
 
 #[test]
-fn date_time() {
-    base("DateTime");
+fn feature_gate_test_for_datetime_deserialization() {
+    // create files
+    let cargo_project_path = create_cargo_dir(datetime);
+    de_test(&cargo_project_path);
+}
+
+#[test]
+fn feature_gate_test_for_datetime_serialization() {
+    // create files
+    let cargo_project_path = create_cargo_dir(datetime);
+    ser_test(&cargo_project_path);
+}
+
+#[test]
+fn feature_gate_test_for_number_deserialization() {
+    // create files
+    let cargo_project_path = create_cargo_dir(number);
+    de_test(&cargo_project_path);
+}
+
+#[test]
+fn feature_gate_test_for_number_serialization() {
+    // create files
+    let cargo_project_path = create_cargo_dir(number);
+    ser_test(&cargo_project_path);
+}
+
+fn ser_test(cargo_project_path: &PathBuf) {
+    // runs cargo check --all-features to compile a code that requries serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --all-features"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check --features serde-serialize to compile a code that requries serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --features serde-serialize"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check --features serde-deserialize to compile a code that requries serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --features serde-deserialize"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check to compile a code that requries serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check --features serde-serialize to compile a code that requries serialization feature is needed.
+    // it is expected to fail to compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --features serde-serialize"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, false);
+
+    // runs cargo check --features serde-deserialize to compile a code that requries serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --features serde-deserialize"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+}
+fn de_test(cargo_project_path: &PathBuf) {
+    // runs cargo check --all-features to compile a code that requries de-serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --all-features"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check --features serde-serialize to compile a code that requries de-serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --features serde-serialize"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check --features serde-deserialize to compile a code that requries de-serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --features serde-deserialize"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check to compile a code that requries de-serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check --features serde-serialize to compile a code that requries de-serialization feature is needed.
+    // it is expected to successfully compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --features serde-serialize"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, true);
+
+    // runs cargo check --features serde-deserialize to compile a code that requries de-serialization feature is needed.
+    // it is expected to fail to compile.
+    let cmd = Command::new("bash")
+        .current_dir(&cargo_project_path)
+        .args(["-c", "cargo check --features serde-deserialize"])
+        .env("RUSTFLAGS", "--cfg aws_sdk_unstable");
+    let is_success = cmd.spawn().unwrap().wait().unwrap().success();
+    assert_eq!(is_success, false);
 }
