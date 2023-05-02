522c5afa6fbbbf269aea58ae0bf8b170abc42bde
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-05-02 09:03:39 +0000

    refactor

--- rust-runtime/aws-smithy-types/test_data/template/deser.rs
+++ rust-runtime/aws-smithy-types/test_data/template/deser.rs
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// this is a template for running feature-gate tests.
+// $PLACE_HOLDER$ is replaced with relevant data types
+
 fn main() {
     let _dt: Result<aws_smithy_types::$PLACE_HOLDER$, _> = serde_json::from_str("some_str");
 }
--- rust-runtime/aws-smithy-types/test_data/template/ser.rs
+++ rust-runtime/aws-smithy-types/test_data/template/ser.rs
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// this is a template for running feature-gate tests.
+// $PLACE_HOLDER$ is replaced with relevant data types
+
 fn main() {
     serde_json::to_string(&aws_smithy_types::$PLACE_HOLDER$::default());
 }
