9caa4fe5658b46e03fc42482bc1f3e2aa9535bbd
John DiSanti <jdisanti@amazon.com>
2023-05-11 10:50:58 -0700

    Fix CI

--- aws/rust-runtime/aws-config/src/connector.rs
+++ aws/rust-runtime/aws-config/src/connector.rs
@@ -13,4 +13,14 @@ pub(crate) fn expect_connector(connector: Option<DynConnector>) -> DynConnector 
     connector.expect("No HTTP connector was available. Enable the `rustls` or `native-tls` crate feature or set a connector to fix this.")
 }
 
+#[cfg(feature = "client-hyper")]
 pub use aws_smithy_client::conns::default_connector;
+
+/// Given `ConnectorSettings` and an `AsyncSleep`, create a `DynConnector` from defaults depending on what cargo features are activated.
+#[cfg(not(feature = "client-hyper"))]
+pub fn default_connector(
+    _settings: &aws_smithy_client::http_connector::ConnectorSettings,
+    _sleep: Option<std::sync::Arc<dyn aws_smithy_async::rt::sleep::AsyncSleep>>,
+) -> Option<DynConnector> {
+    None
+}
