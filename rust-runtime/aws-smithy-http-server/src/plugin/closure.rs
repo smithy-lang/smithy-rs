3ad704f2c4ae9b5cf43572b401fcb24f99e5fe14
82marbag <69267416+82marbag@users.noreply.github.com>
2023-05-12 10:39:18 +0200

    Revert "OperationShape::NAME as ShapeId (#2678)"
    This reverts commit 9b340fb89e0f0e303d0c4f3af5dfc8216d95ba8f.
    
    It is a breaking change and it will be included in the next release.

--- rust-runtime/aws-smithy-http-server/src/plugin/closure.rs
+++ rust-runtime/aws-smithy-http-server/src/plugin/closure.rs
@@ -6,59 +6,56 @@
 use tower::layer::util::Stack;
 
 use crate::operation::{Operation, OperationShape};
-use crate::shape_id::ShapeId;
 
 use super::Plugin;
 
-/// An adapter to convert a `Fn(ShapeId) -> Layer` closure into a [`Plugin`]. See [`plugin_from_operation_id_fn`] for more details.
-pub struct OperationIdFn<F> {
+/// An adapter to convert a `Fn(&'static str) -> Layer` closure into a [`Plugin`]. See [`plugin_from_operation_name_fn`] for more details.
+pub struct OperationNameFn<F> {
     f: F,
 }
 
-impl<P, Op, S, ExistingLayer, NewLayer, F> Plugin<P, Op, S, ExistingLayer> for OperationIdFn<F>
+impl<P, Op, S, ExistingLayer, NewLayer, F> Plugin<P, Op, S, ExistingLayer> for OperationNameFn<F>
 where
-    F: Fn(ShapeId) -> NewLayer,
+    F: Fn(&'static str) -> NewLayer,
     Op: OperationShape,
 {
     type Service = S;
     type Layer = Stack<ExistingLayer, NewLayer>;
 
     fn map(&self, input: Operation<S, ExistingLayer>) -> Operation<Self::Service, Self::Layer> {
-        let operation_id = Op::NAME;
-        input.layer((self.f)(operation_id))
+        input.layer((self.f)(Op::NAME))
     }
 }
 
-/// Constructs a [`Plugin`] using a closure over the operation name `F: Fn(ShapeId) -> L` where `L` is a HTTP
+/// Constructs a [`Plugin`] using a closure over the operation name `F: Fn(&'static str) -> L` where `L` is a HTTP
 /// [`Layer`](tower::Layer).
 ///
 /// # Example
 ///
 /// ```rust
-/// use aws_smithy_http_server::plugin::plugin_from_operation_id_fn;
-/// use aws_smithy_http_server::shape_id::ShapeId;
+/// use aws_smithy_http_server::plugin::plugin_from_operation_name_fn;
 /// use tower::layer::layer_fn;
 ///
 /// // A `Service` which prints the operation name before calling `S`.
 /// struct PrintService<S> {
-///     operation_name: ShapeId,
+///     operation_name: &'static str,
 ///     inner: S
 /// }
 ///
 /// // A `Layer` applying `PrintService`.
 /// struct PrintLayer {
-///     operation_name: ShapeId
+///     operation_name: &'static str
 /// }
 ///
 /// // Defines a closure taking the operation name to `PrintLayer`.
 /// let f = |operation_name| PrintLayer { operation_name };
 ///
 /// // This plugin applies the `PrintService` middleware around every operation.
-/// let plugin = plugin_from_operation_id_fn(f);
+/// let plugin = plugin_from_operation_name_fn(f);
 /// ```
-pub fn plugin_from_operation_id_fn<L, F>(f: F) -> OperationIdFn<F>
+pub fn plugin_from_operation_name_fn<L, F>(f: F) -> OperationNameFn<F>
 where
-    F: Fn(ShapeId) -> L,
+    F: Fn(&'static str) -> L,
 {
-    OperationIdFn { f }
+    OperationNameFn { f }
 }
