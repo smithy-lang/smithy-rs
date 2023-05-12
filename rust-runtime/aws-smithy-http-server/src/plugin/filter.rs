c2da4cf16d062f21f2e458f2b82b3c5ce5134c6c
82marbag <69267416+82marbag@users.noreply.github.com>
2023-05-12 10:07:52 +0100

    Revert "OperationShape::NAME as ShapeId (#2678)" (#2698)
    This reverts https://github.com/awslabs/smithy-rs/pull/2678.
    
    It is a breaking change and it will be included in the next release.
    
    ----
    
    _By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice._

--- rust-runtime/aws-smithy-http-server/src/plugin/filter.rs
+++ rust-runtime/aws-smithy-http-server/src/plugin/filter.rs
@@ -6,15 +6,14 @@
 use super::{either::Either, IdentityPlugin};
 
 use crate::operation::{Operation, OperationShape};
-use crate::shape_id::ShapeId;
 
 use super::Plugin;
 
 /// Filters the application of an inner [`Plugin`] using a predicate over the
 /// [`OperationShape::NAME`](crate::operation::OperationShape).
 ///
-/// See [`filter_by_operation_id`] for more details.
-pub struct FilterByOperationId<Inner, F> {
+/// See [`filter_by_operation_name`] for more details.
+pub struct FilterByOperationName<Inner, F> {
     inner: Inner,
     predicate: F,
 }
@@ -25,36 +24,35 @@ pub struct FilterByOperationId<Inner, F> {
 /// # Example
 ///
 /// ```rust
-/// use aws_smithy_http_server::plugin::filter_by_operation_id;
-/// use aws_smithy_http_server::shape_id::ShapeId;
+/// use aws_smithy_http_server::plugin::filter_by_operation_name;
 /// # use aws_smithy_http_server::{plugin::Plugin, operation::{Operation, OperationShape}};
 /// # struct Pl;
 /// # struct CheckHealth;
-/// # impl OperationShape for CheckHealth { const NAME: ShapeId = ShapeId::new("ns#CheckHealth", "ns", "CheckHealth"); type Input = (); type Output = (); type Error = (); }
+/// # impl OperationShape for CheckHealth { const NAME: &'static str = ""; type Input = (); type Output = (); type Error = (); }
 /// # impl Plugin<(), CheckHealth, (), ()> for Pl { type Service = (); type Layer = (); fn map(&self, input: Operation<(), ()>) -> Operation<(), ()> { input }}
 /// # let plugin = Pl;
 /// # let operation = Operation { inner: (), layer: () };
 /// // Prevents `plugin` from being applied to the `CheckHealth` operation.
-/// let filtered_plugin = filter_by_operation_id(plugin, |name| name != CheckHealth::NAME);
+/// let filtered_plugin = filter_by_operation_name(plugin, |name| name != CheckHealth::NAME);
 /// let new_operation = filtered_plugin.map(operation);
 /// ```
-pub fn filter_by_operation_id<Inner, F>(plugins: Inner, predicate: F) -> FilterByOperationId<Inner, F>
+pub fn filter_by_operation_name<Inner, F>(plugins: Inner, predicate: F) -> FilterByOperationName<Inner, F>
 where
-    F: Fn(ShapeId) -> bool,
+    F: Fn(&str) -> bool,
 {
-    FilterByOperationId::new(plugins, predicate)
+    FilterByOperationName::new(plugins, predicate)
 }
 
-impl<Inner, F> FilterByOperationId<Inner, F> {
-    /// Creates a new [`FilterByOperationId`].
+impl<Inner, F> FilterByOperationName<Inner, F> {
+    /// Creates a new [`FilterByOperationName`].
     fn new(inner: Inner, predicate: F) -> Self {
         Self { inner, predicate }
     }
 }
 
-impl<P, Op, S, L, Inner, F> Plugin<P, Op, S, L> for FilterByOperationId<Inner, F>
+impl<P, Op, S, L, Inner, F> Plugin<P, Op, S, L> for FilterByOperationName<Inner, F>
 where
-    F: Fn(ShapeId) -> bool,
+    F: Fn(&str) -> bool,
     Inner: Plugin<P, Op, S, L>,
     Op: OperationShape,
 {
