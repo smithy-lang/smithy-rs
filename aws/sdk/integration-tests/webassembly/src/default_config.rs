ee489fee9332cbf544f692cf4453e27059139c24
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-05-25 23:26:05 +0200

    chore: re-enable timeout and retry config

--- aws/sdk/integration-tests/webassembly/src/default_config.rs
+++ aws/sdk/integration-tests/webassembly/src/default_config.rs
@@ -3,15 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-use aws_smithy_types::{retry::RetryConfig, timeout::TimeoutConfig};
 use aws_types::region::Region;
 use std::future::Future;
 
 pub(crate) fn get_default_config() -> impl Future<Output = aws_config::SdkConfig> {
     aws_config::from_env()
         .region(Region::from_static("us-west-2"))
-        .timeout_config(TimeoutConfig::disabled())
-        .retry_config(RetryConfig::disabled())
         .load()
 }
 
