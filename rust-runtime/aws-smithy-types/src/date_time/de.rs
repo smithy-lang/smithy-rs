446d2efea424d8014a2beec7822cc7476fcf914b
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-06-01 10:23:47 +0000

    fix

--- rust-runtime/aws-smithy-types/src/date_time/de.rs
+++ rust-runtime/aws-smithy-types/src/date_time/de.rs
@@ -126,7 +126,6 @@ fn not_human_readable_datetime() {
     let datetime = DateTime::from_secs(1576540098);
 
     let mut buf1 = vec![];
-    let mut buf2 = vec![];
     let _ = ciborium::ser::into_writer(&datetime, &mut buf1);
     let res = ciborium::de::from_reader(std::io::Cursor::new(buf1));
     assert_eq!(res.ok(), Some(datetime));
