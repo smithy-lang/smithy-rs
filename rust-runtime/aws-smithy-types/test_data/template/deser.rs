98dcb335c644cd1e9bd54717838a47af9fb50e22
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-06-01 18:59:33 +0900

    Delete deser.rs

--- rust-runtime/aws-smithy-types/test_data/template/deser.rs
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-fn main() {
-    let _dt: Result<ReplaceDataType, _> = serde_json::from_str("some_str");
-}
