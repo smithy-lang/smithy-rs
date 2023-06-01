b7b9c9bf3770736d23578075d18af5cdc53d7e0e
John DiSanti <jdisanti@amazon.com>
2023-06-01 13:55:00 -0700

    Format as many files as possible on failure

--- tools/ci-build/please-fmt/src/main.rs
+++ tools/ci-build/please-fmt/src/main.rs
@@ -35,21 +35,37 @@ fn main() -> Result<(), BoxError> {
 }
 
 fn format_path(path: &Utf8Path) -> Result<(), BoxError> {
+    let mut errors = Vec::new();
     for entry in path.read_dir_utf8()? {
         let entry = entry?;
         let extension = entry.path().extension();
         let file_type = entry.file_type()?;
         if extension == Some("rs") && (file_type.is_file() || file_type.is_symlink()) {
-            let contents = fs::read_to_string(entry.path())
-                .map_err(|err| format!("failed to read file {}: {err}", entry.path()))?;
-            let ast = syn::parse_file(&contents)
-                .map_err(|err| format!("failed to parse file {}: {err}", entry.path()))?;
-            let formatted = prettyplease::unparse(&ast);
-            fs::write(entry.path(), formatted)
-                .map_err(|err| format!("failed to write file {}: {err}", entry.path()))?;
+            if let Err(err) = format_file(entry.path()) {
+                errors.push((entry.path().to_string(), err));
+            }
         } else if file_type.is_dir() || file_type.is_symlink() {
             format_path(entry.path())?;
         }
     }
+    if errors.is_empty() {
+        Ok(())
+    } else {
+        Err(errors
+            .into_iter()
+            .map(|entry| format!("error: {}: {}", entry.0, entry.1))
+            .collect::<Vec<String>>()
+            .join("\n")
+            .into())
+    }
+}
+
+fn format_file(path: &Utf8Path) -> Result<(), BoxError> {
+    let contents =
+        fs::read_to_string(path).map_err(|err| format!("failed to read file {path}: {err}"))?;
+    let ast =
+        syn::parse_file(&contents).map_err(|err| format!("failed to parse file {path}: {err}"))?;
+    let formatted = prettyplease::unparse(&ast);
+    fs::write(path, formatted).map_err(|err| format!("failed to write file {path}: {err}"))?;
     Ok(())
 }
