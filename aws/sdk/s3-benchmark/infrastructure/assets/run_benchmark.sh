2225b101c3fead5aee41bcf375c13d15669009ab
John DiSanti <jdisanti@amazon.com>
2023-06-06 16:39:39 -0700

    Run the benchmark in release mode

--- aws/sdk/s3-benchmark/infrastructure/assets/run_benchmark.sh
+++ aws/sdk/s3-benchmark/infrastructure/assets/run_benchmark.sh
@@ -10,7 +10,7 @@ set -eux
 BENCHMARK_BUCKET="${1}"
 echo "benchmark bucket: ${BENCHMARK_BUCKET}"
 
-COMMON_ARGS="--bucket ${BENCHMARK_BUCKET} --region us-east-1"
+COMMON_ARGS="--concurrency 18 --bucket ${BENCHMARK_BUCKET} --region us-east-1"
 
 source "${HOME}/.cargo/env"
 cd benchmark
@@ -18,11 +18,11 @@ cd benchmark
 # 1B
 for fs in "tmpfs" "disk"; do
     BENCH_RESULT_FILE="bench_results_put_object_${fs}_1B.txt"
-    cargo run -- --bench put-object --fs "${fs}" --size-bytes 1 ${COMMON_ARGS} &> "${BENCH_RESULT_FILE}"
+    cargo run --release -- --bench put-object --fs "${fs}" --size-bytes 1 ${COMMON_ARGS} &> "${BENCH_RESULT_FILE}"
     aws s3 cp "${BENCH_RESULT_FILE}" "s3://${BENCHMARK_BUCKET}/"
 
     BENCH_RESULT_FILE="bench_results_get_object_${fs}_1B.txt"
-    cargo run -- --bench get-object --fs "${fs}" --size-bytes 1 ${COMMON_ARGS} &> "${BENCH_RESULT_FILE}"
+    cargo run --release -- --bench get-object --fs "${fs}" --size-bytes 1 ${COMMON_ARGS} &> "${BENCH_RESULT_FILE}"
     aws s3 cp "${BENCH_RESULT_FILE}" "s3://${BENCHMARK_BUCKET}/"
 done
 
@@ -30,11 +30,11 @@ done
 for fs in "tmpfs" "disk"; do
     for size_bytes in "8388607" "8388609" "134217728" "4294967296" "32212254720"; do
         BENCH_RESULT_FILE="bench_results_put_object_multipart_${fs}_${size_bytes}B.txt"
-        cargo run -- --bench put-object-multipart --fs "${fs}" --size-bytes "${size_bytes}" --part-size-bytes "8388608" ${COMMON_ARGS} &> "${BENCH_RESULT_FILE}"
+        cargo run --release -- --bench put-object-multipart --fs "${fs}" --size-bytes "${size_bytes}" --part-size-bytes "8388608" ${COMMON_ARGS} &> "${BENCH_RESULT_FILE}"
         aws s3 cp "${BENCH_RESULT_FILE}" "s3://${BENCHMARK_BUCKET}/"
 
         BENCH_RESULT_FILE="bench_results_get_object_multipart_${fs}_${size_bytes}B.txt"
-        cargo run -- --bench get-object-multipart --fs "${fs}" --size-bytes "${size_bytes}" --part-size-bytes "8388608" ${COMMON_ARGS} &> "${BENCH_RESULT_FILE}"
+        cargo run --release -- --bench get-object-multipart --fs "${fs}" --size-bytes "${size_bytes}" --part-size-bytes "8388608" ${COMMON_ARGS} &> "${BENCH_RESULT_FILE}"
         aws s3 cp "${BENCH_RESULT_FILE}" "s3://${BENCHMARK_BUCKET}/"
     done
 done
