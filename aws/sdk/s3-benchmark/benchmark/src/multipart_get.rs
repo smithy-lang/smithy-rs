f7a8d0a1e3b28319d252689bf40edff3d676d5e5
John DiSanti <jdisanti@amazon.com>
2023-06-06 10:43:45 -0700

    Enforce order in multipart download

--- aws/sdk/s3-benchmark/benchmark/src/multipart_get.rs
+++ aws/sdk/s3-benchmark/benchmark/src/multipart_get.rs
@@ -5,10 +5,10 @@
 
 use crate::{Args, BoxError, BENCH_KEY};
 use aws_sdk_s3 as s3;
-use aws_smithy_http::byte_stream::ByteStream;
 use std::fmt;
 use std::path::Path;
 use std::sync::Arc;
+use tokio::sync::watch::channel;
 use tokio::sync::Semaphore;
 
 pub async fn get_object_multipart(
@@ -23,7 +23,7 @@ pub async fn get_object_multipart(
         part_count -= 1;
     }
 
-    let ranges = (0..part_count).map(|i| {
+    let mut ranges = (0..part_count).map(|i| {
         if i == part_count - 1 {
             let start = i * args.part_size_bytes as i64;
             ContentRange::new(start, start + size_of_last_part - 1, size_of_last_part)
@@ -35,16 +35,32 @@ pub async fn get_object_multipart(
             )
         }
     });
+    let (tx, rx) = channel(ranges.next().unwrap());
+    for range in ranges {
+        tx.send(range)?;
+    }
 
     let semaphore = Arc::new(Semaphore::new(args.concurrency));
     let mut tasks = Vec::new();
-    for range in ranges {
-        tasks.push(tokio::spawn(download_part(
-            semaphore.clone(),
-            client.clone(),
-            args.bucket.clone(),
-            range,
-        )));
+    for _ in 0..part_count {
+        let semaphore = semaphore.clone();
+        let client = client.clone();
+        let bucket = args.bucket.clone();
+        let mut rx = rx.clone();
+        tasks.push(tokio::spawn(async move {
+            let _permit = semaphore.acquire().await?;
+            let range = rx.borrow_and_update().to_string();
+
+            let part = client
+                .get_object()
+                .bucket(bucket)
+                .key(BENCH_KEY)
+                .range(range)
+                .send()
+                .await?;
+
+            Result::<_, BoxError>::Ok(part.body)
+        }));
     }
     let mut file = tokio::fs::File::create(path).await?;
     for task in tasks {
@@ -55,6 +71,7 @@ pub async fn get_object_multipart(
     Ok(())
 }
 
+#[derive(Debug)]
 struct ContentRange {
     start: i64,
     end: i64,
@@ -71,22 +88,4 @@ impl fmt::Display for ContentRange {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "bytes={}-{}/{}", self.start, self.end, self.length)
     }
-}
-
-async fn download_part(
-    semaphore: Arc<Semaphore>,
-    client: s3::Client,
-    bucket: String,
-    range: ContentRange,
-) -> Result<ByteStream, BoxError> {
-    let _permit = semaphore.acquire().await?;
-
-    let part = client
-        .get_object()
-        .bucket(bucket)
-        .key(BENCH_KEY)
-        .range(range.to_string())
-        .send()
-        .await?;
-    Ok(part.body)
 }
