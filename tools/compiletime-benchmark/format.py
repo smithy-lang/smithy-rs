7dd284ece38345920979737e7d9a8de3abaa2c9c
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-06-06 03:10:08 +0000

    update

--- tools/compiletime-benchmark/format.py
+++ tools/compiletime-benchmark/format.py
@@ -2,45 +2,36 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
+
+import itertools
+
+
 def main():
     file = open("/tmp/compiletime-benchmark.txt", "r").read()
-    stack = []
+    iter = map(lambda x: x.split("END"), file.split("START"))
+    iter = itertools.chain.from_iterable(iter)
+    markdown = """
+    | sdk name | dev | release | dev all features | release all features |
+    | -------- | --- | ------- | ---------------- | -------------------- |
+    """
     idx = 0
-    hashmap = {}
-    for i in file.split("\n"):
+    for i in iter:
         idx += 1
-        if idx > 5:
-            idx = 1
-
-        if idx == 1:
-            hashmap["sdk"] = i
+        print("============")
+        print(idx)
+        print(i)
+        print("============")
+        if i == "\n":
             continue
+        lines = i.splitlines()
+        sdk_name = lines[0]
+        row = "\n|" + sdk_name + \
+            "|".join(map(lambda x: float(x), lines[1:])) + "|"
 
-        i = float(i)
-        if idx == 2:
-            hashmap["dev"] = i
-        elif idx == 3:
-            hashmap["release"] = i
-        elif idx == 4:
-            hashmap["dev-w-all-features"] = i
-        elif idx == 5:
-            hashmap["release-w-all-features"] = i
-            stack.append(hashmap)
-            hashmap = {}
-
-    header = "|".join(stack[0].keys())
-    header = f"|{header}|"
-    table = [header]
-    for hashmap in stack:
-        row = []
-        for i in hashmap.keys():
-            row.append(str(hashmap[i]))
-        inner = "|".join(row)
-        inner = f"|{inner}|"
-        table.append(inner)
-
-    markdown = "\n".join(table)
+        markdown += row
+        
     print(markdown)
     # send markdown to somewhere
+
 
 main()
