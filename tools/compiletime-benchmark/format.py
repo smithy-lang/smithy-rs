f8862e196ced5eac67a505b3cbfe85df54de4d77
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-06-14 19:03:59 +0000

    fix

--- tools/compiletime-benchmark/format.py
+++ tools/compiletime-benchmark/format.py
@@ -7,34 +7,59 @@ import itertools
 
 
 def main():
+    markdown = parser(read_file())
+    print(markdown)
+    # write file
+    with open("/tmp/compiletime-benchmark.md", "w") as f:
+        f.write(markdown)
+        f.flush()
+        f.close()
+
+
+def read_file() -> itertools.chain[str]:
+    # read file
     f = open("/tmp/compiletime-benchmark.txt", "r").read()
     iter = map(lambda x: x.split("END"), f.split("START"))
     iter = itertools.chain.from_iterable(iter)
+    return iter
+
+
+def parser(iter: itertools.chain[str]) -> str:
+    # I could've used a dataframe like pandas but this works.
     markdown = """
     | sdk name | dev | release | dev all features | release all features |
     | -------- | --- | ------- | ---------------- | -------------------- |
     """
-    idx = 0
+
     for i in iter:
-        idx += 1
-        print("============")
-        print(idx)
+        outputs = []
         print(i)
-        print("============")
+        for l in i.splitlines():
+            if not "+" in l:
+                outputs.append(l.replace(" seconds", ""))
 
-        lines = i.splitlines()
-        if len(lines) > 1:
+        if len(outputs) != 6:
             continue
-
-        sdk_name = lines[0]
-        row = "\n|" + sdk_name + \
-            "|".join(map(lambda x: float(x), lines[1:])) + "|"
+        outputs = outputs[1:]
+        sdk_name = outputs[0]
+        row = f"|{sdk_name}|" + \
+            "|".join(outputs[1:]) + "|"
 
         markdown += row
 
-    print(markdown)
-    with open("/tmp/compiletime-benchmark.md", "w") as f:
-        f.write(markdown)
-        f.flush()
+    return markdown
 
-main()
+
+def test():
+    s = """
+    | sdk name | dev | release | dev all features | release all features |
+    | -------- | --- | ------- | ---------------- | -------------------- |
+    |iam|201.92|217.43|215.10|187.71|
+    """.strip()
+    s2 = parser(read_file()).strip()
+    assert s2 == s, f"{s2} \n== \n{s}"
+
+
+if __name__ == '__main__':
+    test()
+    main()
