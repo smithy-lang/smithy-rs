b2bdcba57ae04b1efca3398333f0151d2b4e2749
Harry Barber <106155934+hlbarber@users.noreply.github.com>
2023-06-15 22:37:24 +0100

    Parameterize `Plugin` by service rather than protocol (#2772)
    ## Motivation and Context
    
    Closes https://github.com/awslabs/smithy-rs/issues/1839
    
    Currently, `Plugin` is parameterized by protocol and operation. To
    improve symmetry, extensibility and uniformity we switch this to be
    parameterized by service instead. The protocol can still be recovered
    via the `type Protocol` associated type on `ServiceShape`.
    
    ## Description
    
    - Add `ServiceShape` trait, encoding the properties of a Smithy service.
    - Change `Plugin<Protocol, Operation, S>` to `Plugin<Service, Operation,
    S>`.
    - Add `FilterByOperation` and `filter_by_operation` `Plugin`s.

--- rust-runtime/aws-smithy-http-server/src/plugin/identity.rs
+++ rust-runtime/aws-smithy-http-server/src/plugin/identity.rs
@@ -8,8 +8,8 @@ use super::Plugin;
 /// A [`Plugin`] that maps a service to itself.
 pub struct IdentityPlugin;
 
-impl<P, Op, S> Plugin<P, Op, S> for IdentityPlugin {
-    type Service = S;
+impl<Ser, Op, S> Plugin<Ser, Op, S> for IdentityPlugin {
+    type Output = S;
 
     fn apply(&self, svc: S) -> S {
         svc
