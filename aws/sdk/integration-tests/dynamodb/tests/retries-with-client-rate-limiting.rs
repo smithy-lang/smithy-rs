05958f4c4fb019dcae560218d24f64f5d60adbdc
Zelda Hessler <zhessler@amazon.com>
2023-06-21 15:39:17 -0500

    fix usage of config in integration test

--- aws/sdk/integration-tests/dynamodb/tests/retries-with-client-rate-limiting.rs
+++ aws/sdk/integration-tests/dynamodb/tests/retries-with-client-rate-limiting.rs
@@ -6,6 +6,7 @@
 #[cfg(aws_sdk_orchestrator_mode)]
 mod test {
     use approx::assert_relative_eq;
+    use aws_sdk_dynamodb::config::Region;
     use aws_sdk_dynamodb::config::SharedAsyncSleep;
     use aws_sdk_dynamodb::{config::retry::RetryConfig, error::ProvideErrorMetadata};
     use aws_smithy_async::test_util::instant_time_and_sleep;
@@ -68,7 +69,8 @@ mod test {
         ];
 
         let conn = TestConnection::new(events);
-        let sdk_config = aws_config::from_env()
+        let config = aws_sdk_dynamodb::Config::builder()
+            .region(Region::new("us-east-1"))
             .retry_config(
                 RetryConfig::adaptive()
                     .with_max_attempts(4)
@@ -77,27 +79,25 @@ mod test {
             .time_source(SharedTimeSource::new(time_source))
             .sleep_impl(SharedAsyncSleep::new(sleep_impl.clone()))
             .http_connector(conn.clone())
-            .load()
-            .await;
-
+            .build();
         let expected_table_names = vec!["Test".to_owned()];
 
         // We create a new client each time to ensure that the cross-client retry state is working.
-        let client = aws_sdk_dynamodb::Client::new(&sdk_config);
+        let client = aws_sdk_dynamodb::Client::from_conf(config.clone());
         let res = client.list_tables().send().await.unwrap();
         assert_eq!(sleep_impl.total_duration(), Duration::from_secs(3));
         assert_eq!(res.table_names(), Some(expected_table_names.as_slice()));
         // Three requests should have been made, two failing & one success
         assert_eq!(conn.requests().len(), 3);
 
-        let client = aws_sdk_dynamodb::Client::new(&sdk_config);
+        let client = aws_sdk_dynamodb::Client::from_conf(config.clone());
         let res = client.list_tables().send().await.unwrap();
         assert_eq!(sleep_impl.total_duration(), Duration::from_secs(3 + 1));
         assert_eq!(res.table_names(), Some(expected_table_names.as_slice()));
         // Two requests should have been made, one failing & one success (plus previous requests)
         assert_eq!(conn.requests().len(), 5);
 
-        let client = aws_sdk_dynamodb::Client::new(&sdk_config);
+        let client = aws_sdk_dynamodb::Client::from_conf(config);
         let err = client.list_tables().send().await.unwrap_err();
         assert_eq!(sleep_impl.total_duration(), Duration::from_secs(3 + 1 + 7));
         assert_eq!(err.code(), Some("InternalServerError"));
@@ -125,7 +125,8 @@ mod test {
         ];
 
         let conn = TestConnection::new(events);
-        let sdk_config = aws_config::from_env()
+        let config = aws_sdk_dynamodb::Config::builder()
+            .region(Region::new("us-east-1"))
             .retry_config(
                 RetryConfig::adaptive()
                     .with_max_attempts(4)
@@ -142,25 +143,24 @@ mod test {
             .time_source(SharedTimeSource::new(time_source))
             .sleep_impl(SharedAsyncSleep::new(sleep_impl.clone()))
             .http_connector(conn.clone())
-            .load()
-            .await;
+            .build();
 
         let expected_table_names = vec!["Test".to_owned()];
 
         // We create a new client each time to ensure that the cross-client retry state is working.
-        let client = aws_sdk_dynamodb::Client::new(&sdk_config);
+        let client = aws_sdk_dynamodb::Client::from_conf(config.clone());
         let res = client.list_tables().send().await.unwrap();
         assert_eq!(res.table_names(), Some(expected_table_names.as_slice()));
         // Three requests should have been made, two failing & one success
         assert_eq!(conn.requests().len(), 2);
 
-        let client = aws_sdk_dynamodb::Client::new(&sdk_config);
+        let client = aws_sdk_dynamodb::Client::from_conf(config.clone());
         let res = client.list_tables().send().await.unwrap();
         assert_eq!(res.table_names(), Some(expected_table_names.as_slice()));
         // Two requests should have been made, one failing & one success (plus previous requests)
         assert_eq!(conn.requests().len(), 2 + 3);
 
-        let client = aws_sdk_dynamodb::Client::new(&sdk_config);
+        let client = aws_sdk_dynamodb::Client::from_conf(config);
         let err = client.list_tables().send().await.unwrap_err();
         assert_eq!(err.code(), Some("InternalServerError"));
         // four requests should have been made, all failing (plus previous requests)
