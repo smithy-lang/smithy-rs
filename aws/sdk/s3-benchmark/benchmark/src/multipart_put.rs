2a10b775d9d62ebfe27c3b3cb79fcbc96a21b050
Russell Cohen <rcoh@amazon.com>
2023-06-29 15:20:11 -0400

    Update the S3 benchmark for correctness and performance
    Updates the S3 benchmark to:
    1. Add verify step (untimed)
    2. Improve performance by reading and writing from the disk concurrently
    3. Add support for using multiple clients simultaeneously
    4. Fix correctness issues & simplify
    5. Add timeouts on the parts

--- aws/sdk/s3-benchmark/benchmark/src/multipart_put.rs
+++ aws/sdk/s3-benchmark/benchmark/src/multipart_put.rs
@@ -3,26 +3,60 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+use crate::put_test::PutBenchmark;
 use crate::{Args, BoxError, BENCH_KEY};
+use async_trait::async_trait;
+use aws_config::SdkConfig;
 use aws_sdk_s3 as s3;
+use aws_sdk_s3::Client;
 use aws_smithy_http::byte_stream::ByteStream;
-use aws_smithy_http::byte_stream::Length;
 use s3::types::CompletedMultipartUpload;
 use s3::types::CompletedPart;
+use std::io::SeekFrom;
 use std::path::Path;
 use std::path::PathBuf;
 use std::sync::Arc;
-use tokio::sync::Semaphore;
+use std::time::{Duration, SystemTime};
+use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncSeekExt};
+use tokio::sync::{OwnedSemaphorePermit, Semaphore};
+use tokio::time::timeout;
+
+pub(crate) struct PutObjectMultipart;
+
+#[async_trait]
+impl PutBenchmark for PutObjectMultipart {
+    type Setup = Vec<Client>;
+
+    async fn prepare(&self, conf: &SdkConfig) -> Self::Setup {
+        let clients = (0..32).map(|_| Client::new(&conf)).collect::<Vec<_>>();
+        for client in &clients {
+            let _ = client.list_buckets().send().await;
+        }
+        clients
+    }
+
+    async fn do_put(
+        &self,
+        state: Self::Setup,
+        target_key: &str,
+        local_file: &Path,
+        args: &Args,
+    ) -> Result<(), BoxError> {
+        put_object_multipart(&state, args, target_key, local_file).await
+    }
+}
 
 pub async fn put_object_multipart(
-    client: &s3::Client,
+    client: &[s3::Client],
     args: &Args,
+    target_key: &str,
     path: &Path,
 ) -> Result<(), BoxError> {
-    let upload_id = client
+    let upload_id = client[0]
         .create_multipart_upload()
         .bucket(&args.bucket)
-        .key(BENCH_KEY)
+        .key(target_key)
         .send()
         .await?
         .upload_id
@@ -44,15 +78,17 @@ pub async fn put_object_multipart(
         } else {
             args.part_size_bytes
         };
-        tasks.push(tokio::spawn(upload_part(
-            semaphore.clone(),
-            client.clone(),
+        let permit = semaphore.clone().acquire_owned().await?;
+        tasks.push(tokio::spawn(upload_part_retry_on_timeout(
+            permit,
+            client[part as usize % client.len()].clone(),
             args.bucket.clone(),
             upload_id.clone(),
             path.to_path_buf(),
             offset,
             length,
             part,
+            Duration::from_millis(args.part_upload_timeout_millis),
         )));
     }
     let mut parts = Vec::new();
@@ -60,7 +96,7 @@ pub async fn put_object_multipart(
         parts.push(task.await??);
     }
 
-    client
+    client[0]
         .complete_multipart_upload()
         .bucket(&args.bucket)
         .key(BENCH_KEY)
@@ -76,9 +112,8 @@ pub async fn put_object_multipart(
     Ok(())
 }
 
-#[allow(clippy::too_many_arguments)]
-async fn upload_part(
-    semaphore: Arc<Semaphore>,
+async fn upload_part_retry_on_timeout(
+    permit: OwnedSemaphorePermit,
     client: s3::Client,
     bucket: String,
     upload_id: String,
@@ -86,15 +121,40 @@ async fn upload_part(
     offset: u64,
     length: u64,
     part: u64,
+    timeout_dur: Duration,
 ) -> Result<CompletedPart, BoxError> {
-    let _permit = semaphore.acquire().await?;
+    loop {
+        match timeout(
+            timeout_dur,
+            upload_part(&client, &bucket, &upload_id, &path, offset, length, part),
+        )
+        .await
+        {
+            Ok(res) => {
+                drop(permit);
+                return res;
+            }
+            Err(_) => tracing::warn!(id = ?part, "timeout!"),
+        }
+    }
+}
 
-    let stream = ByteStream::read_from()
-        .path(path)
-        .offset(offset)
-        .length(Length::Exact(length))
-        .build()
-        .await?;
+#[allow(clippy::too_many_arguments)]
+async fn upload_part(
+    client: &s3::Client,
+    bucket: &str,
+    upload_id: &str,
+    path: &Path,
+    offset: u64,
+    length: u64,
+    part: u64,
+) -> Result<CompletedPart, BoxError> {
+    let start = SystemTime::now();
+    let mut file = File::open(path).await?;
+    file.seek(SeekFrom::Start(offset)).await?;
+    let mut buf = vec![0; length as usize];
+    file.read_exact(&mut buf).await?;
+    let stream = ByteStream::from(buf);
     let part_output = client
         .upload_part()
         .key(BENCH_KEY)
@@ -104,6 +164,7 @@ async fn upload_part(
         .part_number(part as i32 + 1) // S3 takes a 1-based index
         .send()
         .await?;
+    tracing::debug!(part = ?part, upload_duration = ?start.elapsed().unwrap(), "upload-part");
     Ok(CompletedPart::builder()
         .part_number(part as i32 + 1)
         .e_tag(part_output.e_tag.expect("must have an e-tag"))
