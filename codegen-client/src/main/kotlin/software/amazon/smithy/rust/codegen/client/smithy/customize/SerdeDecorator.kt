1c249918e1f8dedde4ccfc6cb34495dba86f7a1b
Thomas Cameron <thomas.k.cameron.1218@gmail.com>
2023-07-04 02:37:38 +0000

    update

--- codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customize/SerdeDecorator.kt
+++ codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customize/SerdeDecorator.kt
@@ -24,11 +24,11 @@ class SerdeDecorator : ClientCodegenDecorator {
     override val order: Byte = -1
 
     override fun extras(codegenContext: ClientCodegenContext, rustCrate: RustCrate) {
-        fun _feature(featureName: String, crateName: String): Feature {
-            return Feature(featureName, false, listOf("$crateName/$featureName"))
+        fun feature(featureName: String): Feature {
+            return Feature(featureName, false, listOf("aws-smithy-types/$featureName"))
         }
-        rustCrate.mergeFeature(_feature("serde-serialize", "aws-smithy-types"))
-        rustCrate.mergeFeature(_feature("serde-deserialize", "aws-smithy-types"))
+        rustCrate.mergeFeature(feature("serde-serialize"))
+        rustCrate.mergeFeature(feature("serde-deserialize"))
     }
 
     override fun libRsCustomizations(
@@ -39,26 +39,14 @@ class SerdeDecorator : ClientCodegenDecorator {
 
 class SerdeDocGenerator(private val codegenContext: ClientCodegenContext) : LibRsCustomization() {
     override fun section(section: LibRsSection): Writable {
-        if (section is LibRsSection.ModuleDoc && section.subsection is ModuleDocSection.ServiceDocs) {
+        if (section is LibRsSection.ModuleDoc) {
             return writable {
                 """
-                # How to enable `Serialize` and `Deserialize`
+                ### How to enable `Serialize` and `Deserialize`
                 This data type implements `Serialize` and `Deserialize` traits from the popular serde crate,
                 but those traits are behind feature gate.
 
                 As they increase it's compile time dramatically, you should not turn them on unless it's necessary.
-                Furthermore, implementation of serde is still unstable, and implementation may change anytime in future.
-
-                To enable traits, you must pass `aws_sdk_unstable` to RUSTFLAGS and enable `serde-serialize` or `serde-deserialize` feature.
-
-                e.g.
-                ```bash
-                export RUSTFLAGS="--cfg aws_sdk_unstable"
-                cargo build --features serde-serialize serde-deserialize
-                ```
-
-                If you enable `serde-serialize` and/or `serde-deserialize` without `RUSTFLAGS="--cfg aws_sdk_unstable"`,
-                compilation will fail with warning.
                 """.trimIndent()
             }
         } else {
