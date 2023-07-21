smithy-lang/smithy-rs

commit c187e3d5e063aa41828071761ffc2ecb69758197

Render full shape IDs in server error responses (#1982)
RestJson1 implementations can denote errors in responses in several
ways. New server-side protocol implementations MUST use a header field
named `X-Amzn-Errortype`.

Note that the spec says that implementations SHOULD strip the error
shape ID's namespace. However, our server implementation renders the
full shape ID (including namespace), since some existing clients rely on
it to deserialize the error shape and fail if only the shape name is
present. This is compliant with the spec, see
https://github.com/awslabs/smithy/pull/1493.

See https://github.com/awslabs/smithy/issues/1494 too.

codegen-core/common-test-models/rest-json-extras.smithy
@@ -81,12 +81,16 @@ service RestJsonExtras {
         appliesTo: "client",
     },
     {
-        documentation: "Upper case error modeled lower case",
+        documentation: """
+            Upper case error modeled lower case.
+            Servers render the full shape ID (including namespace), since some
+            existing clients rely on it to deserialize the error shape and fail
+            if only the shape name is present.""",
         id: "ServiceLevelErrorServer",
         protocol: "aws.protocols#restJson1",
         code: 500,
         body: "{}",
-        headers: { "X-Amzn-Errortype": "ExtraError" },
+        headers: { "X-Amzn-Errortype": "aws.protocoltests.restjson#ExtraError" },
         params: {},
         appliesTo: "server",
     }

codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/protocols/RestJson.kt
@@ -83,9 +83,15 @@ open class RestJson(val codegenContext: CodegenContext) : Protocol {
     /**
      * RestJson1 implementations can denote errors in responses in several ways.
      * New server-side protocol implementations MUST use a header field named `X-Amzn-Errortype`.
+     *
+     * Note that the spec says that implementations SHOULD strip the error shape ID's namespace.
+     * However, our server implementation renders the full shape ID (including namespace), since some
+     * existing clients rely on it to deserialize the error shape and fail if only the shape name is present.
+     * This is compliant with the spec, see https://github.com/awslabs/smithy/pull/1493.
+     * See https://github.com/awslabs/smithy/issues/1494 too.
      */
     override fun additionalErrorResponseHeaders(errorShape: StructureShape): List<Pair<String, String>> =
-        listOf("x-amzn-errortype" to errorShape.id.name)
+        listOf("x-amzn-errortype" to errorShape.id.toString())
 
     override fun structuredDataParser(operationShape: OperationShape): StructuredDataParserGenerator {
         fun builderSymbol(shape: StructureShape): Symbol =

codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/protocol/ServerProtocolTestGenerator.kt
@@ -1170,6 +1170,13 @@ class ServerProtocolTestGenerator(
         ): HttpRequestTestCase =
             testCase.toBuilder().putHeader("x-amz-target", "JsonProtocol.${operationShape.id.name}").build()
 
+        private fun fixRestJsonInvalidGreetingError(testCase: HttpResponseTestCase): HttpResponseTestCase =
+            testCase.toBuilder().putHeader("X-Amzn-Errortype", "aws.protocoltests.restjson#InvalidGreeting").build()
+        private fun fixRestJsonEmptyComplexErrorWithNoMessage(testCase: HttpResponseTestCase): HttpResponseTestCase =
+            testCase.toBuilder().putHeader("X-Amzn-Errortype", "aws.protocoltests.restjson#ComplexError").build()
+        private fun fixRestJsonComplexErrorWithNoMessage(testCase: HttpResponseTestCase): HttpResponseTestCase =
+            testCase.toBuilder().putHeader("X-Amzn-Errortype", "aws.protocoltests.restjson#ComplexError").build()
+
         // These are tests whose definitions in the `awslabs/smithy` repository are wrong.
         // This is because they have not been written from a server perspective, and as such the expected `params` field is incomplete.
         // TODO(https://github.com/awslabs/smithy-rs/issues/1288): Contribute a PR to fix them upstream.
@@ -1246,6 +1253,11 @@ class ServerProtocolTestGenerator(
         )
 
         private val BrokenResponseTests: Map<Pair<String, String>, KFunction1<HttpResponseTestCase, HttpResponseTestCase>> =
-            mapOf()
+            // TODO(https://github.com/awslabs/smithy/issues/1494)
+            mapOf(
+                Pair(RestJson, "RestJsonInvalidGreetingError") to ::fixRestJsonInvalidGreetingError,
+                Pair(RestJson, "RestJsonEmptyComplexErrorWithNoMessage") to ::fixRestJsonEmptyComplexErrorWithNoMessage,
+                Pair(RestJson, "RestJsonComplexErrorWithNoMessage") to ::fixRestJsonComplexErrorWithNoMessage,
+            )
     }
 }

codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/protocols/ServerHttpBoundProtocolGenerator.kt
@@ -33,6 +33,8 @@ import software.amazon.smithy.rust.codegen.core.rustlang.RustWriter
 import software.amazon.smithy.rust.codegen.core.rustlang.Writable
 import software.amazon.smithy.rust.codegen.core.rustlang.asType
 import software.amazon.smithy.rust.codegen.core.rustlang.conditionalBlock
+import software.amazon.smithy.rust.codegen.core.rustlang.escape
+import software.amazon.smithy.rust.codegen.core.rustlang.render
 import software.amazon.smithy.rust.codegen.core.rustlang.rust
 import software.amazon.smithy.rust.codegen.core.rustlang.rustBlock
 import software.amazon.smithy.rust.codegen.core.rustlang.rustBlockTemplate
@@ -636,7 +638,7 @@ private class ServerHttpBoundProtocolTraitImplGenerator(
                     builder = #{header_util}::set_response_header_if_absent(
                         builder,
                         http::header::HeaderName::from_static("$headerName"),
-                        "$headerValue"
+                        "${escape(headerValue)}"
                     );
                     """,
                     *codegenScope,

commit 5ed3f540be9ec9c7352dab9627e58b0d0db4eee8

Render only shape name in server error responses
This essentially reverts the behavior introduced in #1982.

The rationale for that change:

> [...] since some existing clients rely on it to deserialize the error shape
> and fail if only the shape name is present

no longer holds. Since serializing only the shape name is a SHOULD in
the spec, it's best that we honor it.

codegen-core/common-test-models/rest-json-extras.smithy
@@ -81,16 +81,12 @@ service RestJsonExtras {
         appliesTo: "client",
     },
     {
-        documentation: """
-            Upper case error modeled lower case.
-            Servers render the full shape ID (including namespace), since some
-            existing clients rely on it to deserialize the error shape and fail
-            if only the shape name is present.""",
+        documentation: "Upper case error modeled lower case.",
         id: "ServiceLevelErrorServer",
         protocol: "aws.protocols#restJson1",
         code: 500,
         body: "{}",
-        headers: { "X-Amzn-Errortype": "aws.protocoltests.restjson#ExtraError" },
+        headers: { "X-Amzn-Errortype": "ExtraError" },
         params: {},
         appliesTo: "server",
     }

codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/protocols/RestJson.kt
@@ -80,14 +80,19 @@ open class RestJson(val codegenContext: CodegenContext) : Protocol {
      * RestJson1 implementations can denote errors in responses in several ways.
      * New server-side protocol implementations MUST use a header field named `X-Amzn-Errortype`.
      *
-     * Note that the spec says that implementations SHOULD strip the error shape ID's namespace.
-     * However, our server implementation renders the full shape ID (including namespace), since some
-     * existing clients rely on it to deserialize the error shape and fail if only the shape name is present.
-     * This is compliant with the spec, see https://github.com/awslabs/smithy/pull/1493.
-     * See https://github.com/awslabs/smithy/issues/1494 too.
+     * Note that the spec says that implementations SHOULD strip the error shape ID's namespace
+     * (see https://smithy.io/2.0/aws/protocols/aws-restjson1-protocol.html#operation-error-serialization):
+     *
+     * > The value of this component SHOULD contain only the shape name of the error's Shape ID.
+     *
+     * But it's a SHOULD; we could strip the namespace if we wanted to. In fact, we did so in smithy-rs versions
+     * 0.52.0 to 0.55.4; see:
+     * - https://github.com/awslabs/smithy-rs/pull/1982
+     * - https://github.com/awslabs/smithy/pull/1493
+     * - https://github.com/awslabs/smithy/issues/1494
      */
     override fun additionalErrorResponseHeaders(errorShape: StructureShape): List<Pair<String, String>> =
-        listOf("x-amzn-errortype" to errorShape.id.toString())
+        listOf("x-amzn-errortype" to errorShape.id.name)
 
     override fun structuredDataParser(): StructuredDataParserGenerator =
         JsonParserGenerator(codegenContext, httpBindingResolver, ::restJsonFieldName)

codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/protocol/ServerProtocolTestGenerator.kt
@@ -921,15 +921,6 @@ class ServerProtocolTestGenerator(
                 ).asObjectNode().get(),
             ).build()
 
-        private fun fixRestJsonInvalidGreetingError(testCase: HttpResponseTestCase): HttpResponseTestCase =
-            testCase.toBuilder().putHeader("X-Amzn-Errortype", "aws.protocoltests.restjson#InvalidGreeting").build()
-
-        private fun fixRestJsonEmptyComplexErrorWithNoMessage(testCase: HttpResponseTestCase): HttpResponseTestCase =
-            testCase.toBuilder().putHeader("X-Amzn-Errortype", "aws.protocoltests.restjson#ComplexError").build()
-
-        private fun fixRestJsonComplexErrorWithNoMessage(testCase: HttpResponseTestCase): HttpResponseTestCase =
-            testCase.toBuilder().putHeader("X-Amzn-Errortype", "aws.protocoltests.restjson#ComplexError").build()
-
         // TODO(https://github.com/awslabs/smithy/issues/1506)
         private fun fixRestJsonMalformedPatternReDOSString(testCase: HttpMalformedRequestTestCase): HttpMalformedRequestTestCase {
             val brokenResponse = testCase.response
@@ -962,12 +953,7 @@ class ServerProtocolTestGenerator(
         )
 
         private val BrokenResponseTests: Map<Pair<String, String>, KFunction1<HttpResponseTestCase, HttpResponseTestCase>> =
-            // TODO(https://github.com/awslabs/smithy/issues/1494)
-            mapOf(
-                Pair(RestJson, "RestJsonInvalidGreetingError") to ::fixRestJsonInvalidGreetingError,
-                Pair(RestJson, "RestJsonEmptyComplexErrorWithNoMessage") to ::fixRestJsonEmptyComplexErrorWithNoMessage,
-                Pair(RestJson, "RestJsonComplexErrorWithNoMessage") to ::fixRestJsonComplexErrorWithNoMessage,
-            )
+            mapOf()
 
         private val BrokenMalformedRequestTests: Map<Pair<String, String>, KFunction1<HttpMalformedRequestTestCase, HttpMalformedRequestTestCase>> =
             // TODO(https://github.com/awslabs/smithy/issues/1506)

codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/protocols/ServerAwsJson.kt
@@ -68,6 +68,14 @@ class ServerAwsJsonFactory(
 /**
  * AwsJson requires errors to be serialized in server responses with an additional `__type` field. This
  * customization writes the right field depending on the version of the AwsJson protocol.
+ *
+ * From the specs:
+ * - https://smithy.io/2.0/aws/protocols/aws-json-1_0-protocol.html#operation-error-serialization
+ * - https://smithy.io/2.0/aws/protocols/aws-json-1_1-protocol.html#operation-error-serialization
+ *
+ * > Error responses in the <awsJson1_x> protocol are serialized identically to standard responses with one additional
+ * > component to distinguish which error is contained. New server-side protocol implementations SHOULD use a body
+ * > field named __type
  */
 class ServerAwsJsonError(private val awsJsonVersion: AwsJsonVersion) : JsonSerializerCustomization() {
     override fun section(section: JsonSerializerSection): Writable = when (section) {