4d65b276dd53bbffea906308fba7ee15f290e095
Thomas Cameron <68596478+thomas-k-cameron@users.noreply.github.com>
2023-08-06 11:15:44 +0900

    ok

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/CodegenDelegator.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/CodegenDelegator.kt
@@ -258,7 +258,7 @@ fun WriterDelegator<RustWriter>.finalize(
         LibRsGenerator(settings, model, libRsCustomizations, requireDocs).render(it)
     }
     val cargoDependencies =
-    
+
         this.dependencies.map { RustDependency.fromSymbolDependency(it) }
             .filterIsInstance<CargoDependency>().distinct()
             .mergeDependencyFeatures()
