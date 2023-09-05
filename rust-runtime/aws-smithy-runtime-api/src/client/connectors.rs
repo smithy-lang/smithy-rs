17a518c83cfeccf1705669b4dcc4e7f104caeeed
John DiSanti <jdisanti@amazon.com>
2023-09-05 16:21:36 -0700

    Improve docs around connectors

--- rust-runtime/aws-smithy-runtime-api/src/client/connectors.rs
+++ rust-runtime/aws-smithy-runtime-api/src/client/connectors.rs
@@ -3,6 +3,33 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+//! Smithy connectors and related code.
+//!
+//! # What is a connector?
+//!
+//! When we talk about connectors, we are referring to the [`HttpConnector`] trait, and implementations of
+//! that trait. This trait simply takes a HTTP request, and returns a future with the response for that
+//! request.
+//!
+//! This is slightly different from what a connector is in other libraries such as
+//! [`hyper`](https://crates.io/crates/hyper). In hyper 0.x, the connector is a
+//! [`tower`](https://crates.io/crates/tower) `Service` that takes a `Uri` and returns
+//! a future with something that implements `AsyncRead + AsyncWrite`.
+//!
+//! The [`HttpConnector`](crate::client::connectors::HttpConnector) is designed to be a layer on top of
+//! whole HTTP libraries, such as hyper, which allows Smithy clients to be agnostic to the underlying HTTP
+//! transport layer. This also makes it easy to write tests with a fake HTTP connector, and several
+//! such test connector implementations are availble in [`aws-smithy-runtime`](https://crates.io/crates/aws-smithy-runtime).
+//!
+//! # Responsibilities of a connector
+//!
+//! A connector primarily makes HTTP requests, but can also be used to implement connect and read
+//! timeouts. The `HyperConnector` in [`aws-smithy-runtime`](https://crates.io/crates/aws-smithy-runtime)
+//! is an example where timeouts are implemented as part of the connector.
+//!
+//! Connectors are also responsible for DNS lookup, TLS, connection reuse, pooling, and eviction.
+//! The Smithy clients have no knowledge of such concepts.
+
 use crate::client::orchestrator::{HttpRequest, HttpResponse};
 use aws_smithy_async::future::now_or_later::NowOrLater;
 use aws_smithy_http::result::ConnectorError;
