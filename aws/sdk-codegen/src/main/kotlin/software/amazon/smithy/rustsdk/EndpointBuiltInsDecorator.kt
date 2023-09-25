9f92425c9432a7d57f6ba3dd615578d12a3619c6
Zelda Hessler <zhessler@amazon.com>
2023-09-25 10:07:54 -0500

    sync some formatting with main

--- aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/EndpointBuiltInsDecorator.kt
+++ aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/EndpointBuiltInsDecorator.kt
@@ -57,7 +57,9 @@ fun ClientCodegenContext.getBuiltIn(builtIn: String): Parameter? {
 }
 
 private fun promotedBuiltins(parameter: Parameter) =
-    parameter == Builtins.FIPS || parameter == Builtins.DUALSTACK || parameter == Builtins.SDK_ENDPOINT
+    parameter.builtIn == Builtins.FIPS.builtIn ||
+        parameter.builtIn == Builtins.DUALSTACK.builtIn ||
+        parameter.builtIn == Builtins.SDK_ENDPOINT.builtIn
 
 private fun configParamNewtype(parameter: Parameter, name: String, runtimeConfig: RuntimeConfig): RuntimeType {
     val type = parameter.symbol().mapRustType { t -> t.stripOuter<RustType.Option>() }
@@ -197,6 +199,9 @@ val PromotedBuiltInsDecorators =
         decoratorForBuiltIn(Builtins.DUALSTACK),
         decoratorForBuiltIn(
             Builtins.SDK_ENDPOINT,
-            ConfigParam.Builder().name("endpoint_url").type(RuntimeType.String.toSymbol()).setterDocs(endpointUrlDocs),
+            ConfigParam.Builder()
+                .name("endpoint_url")
+                .type(RuntimeType.String.toSymbol())
+                .setterDocs(endpointUrlDocs),
         ),
     ).toTypedArray()
