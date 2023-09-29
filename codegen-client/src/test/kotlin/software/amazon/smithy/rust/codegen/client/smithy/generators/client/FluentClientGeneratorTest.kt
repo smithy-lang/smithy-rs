d4247b736c799ec10086dd44b5aab09505ae50eb
ysaito1001 <awsaito@amazon.com>
2023-09-29 10:39:37 -0500

    Add unit test to verify no dead code warnings

--- codegen-client/src/test/kotlin/software/amazon/smithy/rust/codegen/client/smithy/generators/client/FluentClientGeneratorTest.kt
+++ codegen-client/src/test/kotlin/software/amazon/smithy/rust/codegen/client/smithy/generators/client/FluentClientGeneratorTest.kt
@@ -162,4 +162,19 @@ class FluentClientGeneratorTest {
             test = test,
         )
     }
+
+    @Test
+    fun `dead-code warning should not be issued when a service has no operations`() {
+        val model = """
+            namespace com.example
+            use aws.protocols#awsJson1_0
+
+            @awsJson1_0
+            service HelloService {
+                version: "1"
+            }
+        """.asSmithyModel()
+
+        clientIntegrationTest(model)
+    }
 }
