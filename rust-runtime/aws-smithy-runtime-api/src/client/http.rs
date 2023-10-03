70e5cdafa51911f285674449f5f3a32a07f97bbd
John DiSanti <jdisanti@amazon.com>
2023-10-03 15:11:19 -0700

    Improve doc comment links

--- rust-runtime/aws-smithy-runtime-api/src/client/http.rs
+++ rust-runtime/aws-smithy-runtime-api/src/client/http.rs
@@ -12,21 +12,20 @@
 //! request.
 //!
 //! This is slightly different from what a connector is in other libraries such as
-//! [`hyper`](https://crates.io/crates/hyper). In hyper 0.x, the connector is a
-//! [`tower`](https://crates.io/crates/tower) `Service` that takes a `Uri` and returns
+//! [`hyper`]. In hyper 0.x, the connector is a [`tower`] `Service` that takes a `Uri` and returns
 //! a future with something that implements `AsyncRead + AsyncWrite`.
 //!
 //! The [`HttpConnector`] is designed to be a layer on top of
 //! whole HTTP libraries, such as hyper, which allows Smithy clients to be agnostic to the underlying HTTP
 //! transport layer. This also makes it easy to write tests with a fake HTTP connector, and several
-//! such test connector implementations are available in [`aws-smithy-runtime`](https://crates.io/crates/aws-smithy-runtime)
+//! such test connector implementations are available in [`aws-smithy-runtime`]
 //! with the `test-util` feature enabled.
 //!
 //! # Responsibilities of a connector
 //!
 //! A connector primarily makes HTTP requests, but is also the place where connect and read timeouts are
-//! implemented. The `HyperConnector` in [`aws-smithy-runtime`](https://crates.io/crates/aws-smithy-runtime)
-//! is an example where timeouts are implemented as part of the connector.
+//! implemented. The `HyperConnector` in [`aws-smithy-runtime`] is an example where timeouts are implemented
+//! as part of the connector.
 //!
 //! Connectors are also responsible for DNS lookup, TLS, connection reuse, pooling, and eviction.
 //! The Smithy clients have no knowledge of such concepts.
@@ -46,6 +45,10 @@
 //!
 //! Some of these aren't implemented yet, but they will appear in the [`HttpConnectorSettings`] struct
 //! once they are.
+//!
+//! [`hyper`]: https://crates.io/crates/hyper
+//! [`tower`]: https://crates.io/crates/tower
+//! [`aws-smithy-runtime`]: https://crates.io/crates/aws-smithy-runtime
 
 use crate::client::orchestrator::{HttpRequest, HttpResponse};
 use crate::client::runtime_components::RuntimeComponents;
@@ -114,7 +117,7 @@ impl StdFuture for HttpConnectorFuture {
 /// Ordinarily, a connector would use an underlying HTTP library such as [hyper](https://crates.io/crates/hyper),
 /// and any associated HTTPS implementation alongside it to service requests.
 ///
-/// However, it can also be useful to create fake connectors implementing this trait
+/// However, it can also be useful to create fake/mock connectors implementing this trait
 /// for testing.
 pub trait HttpConnector: Send + Sync + fmt::Debug {
     /// Asynchronously converts a request into a response.
