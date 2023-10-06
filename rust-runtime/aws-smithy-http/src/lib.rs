ef08eea53faadeba26226e0e790bb018490d078d
ysaito1001 <awsaito@amazon.com>
2023-10-06 11:59:03 -0500

    Reexport `body` and `byte_stream` in their entirety
    This commit addresses https://github.com/awslabs/smithy-rs/pull/3026#discussion_r1348764671

--- rust-runtime/aws-smithy-http/src/lib.rs
+++ rust-runtime/aws-smithy-http/src/lib.rs
@@ -25,7 +25,11 @@
 #![allow(clippy::derive_partial_eq_without_eq)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
-pub mod body;
+//TODO(runtimeCratesVersioningCleanup): Re-point those who use the following reexports to
+// directly depend on `aws_smithy_types` and remove the reexports below.
+pub use aws_smithy_types::body;
+pub use aws_smithy_types::byte_stream;
+
 pub mod endpoint;
 pub mod header;
 pub mod http;
@@ -42,8 +46,6 @@ pub mod retry;
 
 #[cfg(feature = "event-stream")]
 pub mod event_stream;
-
-pub mod byte_stream;
 
 pub mod connection;
 mod urlencode;
