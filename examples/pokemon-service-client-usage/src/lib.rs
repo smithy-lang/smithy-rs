f38d413b64577ba173cc0b18980ed1c9066e7684
Fahad Zubair <fahadzub@amazon.com>
2023-10-20 10:23:19 -0400

    Add copyright notice, use `aws_smithy_types::SdkBody` instead of `aws_smithy_http`

--- examples/pokemon-service-client-usage/src/lib.rs
+++ examples/pokemon-service-client-usage/src/lib.rs
@@ -1,3 +1,7 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 pub static POKEMON_SERVICE_URL: &str = "http://localhost:13734";
 
 /// Sets up the tracing subscriber to print `tracing::info!` and `tracing::error!` messages on the console.
