f940b7739d60c628a954e366193b1ae42974db6d
82marbag <69267416+82marbag@users.noreply.github.com>
2023-10-24 19:22:52 +0200

    Fix tests
    Signed-off-by: Daniele Ahmed <ahmeddan@amazon.de>

--- codegen-core/src/test/kotlin/software/amazon/smithy/rust/codegen/core/rustlang/RustTypeTest.kt
+++ codegen-core/src/test/kotlin/software/amazon/smithy/rust/codegen/core/rustlang/RustTypeTest.kt
@@ -61,7 +61,7 @@ internal class RustTypesTest {
     fun `RustType_Vec_writable produces a template-compatible RuntimeType`() {
         forInputExpectOutput(
             RustType.Vec(RustType.String).writable,
-            "'::std::vec::Vec<::std::string::String>'",
+            "'::std::vec::Vec::<::std::string::String>'",
         )
     }
 
@@ -146,8 +146,8 @@ internal class RustTypesTest {
     @Test
     fun `types render properly`() {
         val type = RustType.Box(RustType.Option(RustType.Reference("a", RustType.Vec(RustType.String))))
-        type.render(false) shouldBe "Box<Option<&'a Vec<String>>>"
-        type.render(true) shouldBe "::std::boxed::Box<::std::option::Option<&'a ::std::vec::Vec<::std::string::String>>>"
+        type.render(false) shouldBe "Box<Option<&'a Vec::<String>>>"
+        type.render(true) shouldBe "::std::boxed::Box<::std::option::Option<&'a ::std::vec::Vec::<::std::string::String>>>"
     }
 
     @Test
