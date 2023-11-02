8abeb04f5e3457a280ba022c7b9ee716aa677909
John DiSanti <jdisanti@amazon.com>
2023-11-02 10:48:31 -0700

    Move http types out of client and split headers out of request (#3138)
    This PR moves the HTTP types into the root of aws-smithy-runtime-api
    since they're not client-specific, and the serializers/deserializers
    will need to rely on them. It also refactors the headers and errors out
    of the request module.
    
    ----
    
    _By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice._

--- rust-runtime/aws-smithy-runtime-api/src/client/http.rs
+++ rust-runtime/aws-smithy-runtime-api/src/client/http.rs
@@ -50,9 +50,6 @@
 //! [`tower`]: https://crates.io/crates/tower
 //! [`aws-smithy-runtime`]: https://crates.io/crates/aws-smithy-runtime
 
-pub mod request;
-pub mod response;
-
 use crate::client::orchestrator::{HttpRequest, HttpResponse};
 use crate::client::result::ConnectorError;
 use crate::client::runtime_components::sealed::ValidateConfig;
@@ -61,6 +58,25 @@ use crate::impl_shared_conversions;
 use std::fmt;
 use std::sync::Arc;
 use std::time::Duration;
+
+/// Http Request Types
+pub mod request {
+    /// Deprecated: This type has moved to `aws_smithy_runtime_api::http::HttpError`.
+    #[deprecated(note = "This type has moved to `aws_smithy_runtime_api::http::HttpError`.")]
+    pub type HttpError = crate::http::HttpError;
+    /// Deprecated: This type has moved to `aws_smithy_runtime_api::http::HeaderValue`.
+    #[deprecated(note = "This type has moved to `aws_smithy_runtime_api::http::HeaderValue`.")]
+    pub type HeaderValue = crate::http::HeaderValue;
+    /// Deprecated: This type has moved to `aws_smithy_runtime_api::http::Headers`.
+    #[deprecated(note = "This type has moved to `aws_smithy_runtime_api::http::Headers`.")]
+    pub type Headers = crate::http::Headers;
+    /// Deprecated: This type has moved to `aws_smithy_runtime_api::http::HeadersIter`.
+    #[deprecated(note = "This type has moved to `aws_smithy_runtime_api::http::HeadersIter`.")]
+    pub type HeadersIter<'a> = crate::http::HeadersIter<'a>;
+    /// Deprecated: This type has moved to `aws_smithy_runtime_api::http::Request`.
+    #[deprecated(note = "This type has moved to `aws_smithy_runtime_api::http::Request`.")]
+    pub type Request = crate::http::Request;
+}
 
 new_type_future! {
     #[doc = "Future for [`HttpConnector::call`]."]
