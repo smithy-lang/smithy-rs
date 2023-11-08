159a01739d0a2fcf01b992e6007c8d76410d10d4
John DiSanti <jdisanti@amazon.com>
2023-11-08 15:23:31 -0800

    Fix the orchestrator vs. middleware benchmark

--- aws/sdk/benchmarks/orchestrator-vs-middleware/benches/middleware_vs_orchestrator.rs
+++ aws/sdk/benchmarks/orchestrator-vs-middleware/benches/middleware_vs_orchestrator.rs
@@ -8,36 +8,45 @@ extern crate criterion;
 use aws_sdk_s3 as s3;
 use criterion::{BenchmarkId, Criterion};
 
-macro_rules! test_connection {
-    ($package:ident) => {
-        $package::test_connection::infallible_connection_fn(|req| {
-            assert_eq!(
-                "https://test-bucket.s3.us-east-1.amazonaws.com/?list-type=2&prefix=prefix~",
-                req.uri().to_string()
-            );
-            assert!(req.headers().contains_key("authorization"));
-            http::Response::builder()
-                .status(200)
-                .body(
-                    r#"<?xml version="1.0" encoding="UTF-8"?>
-<ListBucketResult>
-    <Name>test-bucket</Name>
-    <Prefix>prefix~</Prefix>
-    <KeyCount>1</KeyCount>
-    <MaxKeys>1000</MaxKeys>
-    <IsTruncated>false</IsTruncated>
-    <Contents>
-        <Key>some-file.file</Key>
-        <LastModified>2009-10-12T17:50:30.000Z</LastModified>
-        <Size>434234</Size>
-        <StorageClass>STANDARD</StorageClass>
-    </Contents>
-</ListBucketResult>
-"#,
-                )
-                .unwrap()
-        })
+macro_rules! test_response {
+    (middleware) => {
+        test_response!(@internal middleware_s3::primitives::SdkBody)
     };
+    (orchestrator) => {
+        test_response!(@internal s3::primitives::SdkBody)
+    };
+    (@internal $sdk_body:ty) => {
+        http::Response::builder()
+            .status(200)
+            .body(
+                r#"<?xml version="1.0" encoding="UTF-8"?>
+                <ListBucketResult>
+                    <Name>test-bucket</Name>
+                    <Prefix>prefix~</Prefix>
+                    <KeyCount>1</KeyCount>
+                    <MaxKeys>1000</MaxKeys>
+                    <IsTruncated>false</IsTruncated>
+                    <Contents>
+                        <Key>some-file.file</Key>
+                        <LastModified>2009-10-12T17:50:30.000Z</LastModified>
+                        <Size>434234</Size>
+                        <StorageClass>STANDARD</StorageClass>
+                    </Contents>
+                </ListBucketResult>
+                "#,
+            )
+            .unwrap()
+    };
+}
+
+macro_rules! test_connection_body {
+    ($variant:tt, $req:ident) => {{
+        assert_eq!(
+            "https://test-bucket.s3.us-east-1.amazonaws.com/?list-type=2&prefix=prefix~",
+            $req.uri().to_string()
+        );
+        test_response!($variant)
+    }};
 }
 
 async fn orchestrator(client: &s3::Client) {
@@ -62,16 +71,21 @@ async fn middleware(client: &middleware_s3::Client) {
 
 fn bench(c: &mut Criterion) {
     let orchestrator_client = {
-        let conn = test_connection!(aws_smithy_client);
+        let http_client =
+            aws_smithy_runtime::client::http::test_util::infallible_client_fn(|req| {
+                test_connection_body!(orchestrator, req)
+            });
         let config = aws_sdk_s3::Config::builder()
             .credentials_provider(aws_sdk_s3::config::Credentials::for_tests())
             .region(aws_sdk_s3::config::Region::new("us-east-1"))
-            .http_connector(conn.clone())
+            .http_client(http_client)
             .build();
         aws_sdk_s3::Client::from_conf(config)
     };
     let middleware_client = {
-        let conn = test_connection!(middleware_smithy_client);
+        let conn = middleware_smithy_client::test_connection::infallible_connection_fn(|req| {
+            test_connection_body!(middleware, req)
+        });
         let config = middleware_s3::Config::builder()
             .credentials_provider(middleware_s3::config::Credentials::for_tests())
             .region(middleware_s3::config::Region::new("us-east-1"))
