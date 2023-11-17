ad520b080aa0a3e8239ab2857dafb519c3458088
Miles Ziemer <45497130+milesziemer@users.noreply.github.com>
2023-11-16 21:57:26 -0500

    Remove invalid defaults for some services (#3217)
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here -->
    
    Some services have generated types with properties that have a default
    value of zero. This can cause invalid requests if the service expects a
    value > 0.
    
    ## Description
    <!--- Describe your changes in detail -->
    
    Adds a customization that removes the default value for services with
    this issue.
    
    ## Testing
    <!--- Please describe in detail how you tested your changes -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    - Added a test for the customization to make sure defaults were removed.
    - Generated clients for the impacted services and verified the expected
    types had the default value removed.
    
    ## Checklist
    <!--- If a checkbox below is not applicable, then please DELETE it
    rather than leaving it unchecked -->
    - [x] I have updated `CHANGELOG.next.toml` if I made changes to the AWS
    SDK, generated SDK code, or SDK runtime crates
    
    ----
    
    _By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice._
    
    Co-authored-by: Russell Cohen <rcoh@amazon.com>

---
+++ aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/customize/RemoveDefaultsDecorator.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.rustsdk.customize
+
+import software.amazon.smithy.model.Model
+import software.amazon.smithy.model.shapes.ServiceShape
+import software.amazon.smithy.rust.codegen.client.smithy.ClientRustSettings
+import software.amazon.smithy.rust.codegen.client.smithy.customize.ClientCodegenDecorator
+import software.amazon.smithy.rust.codegen.core.util.shapeId
+import java.util.logging.Logger
+
+/**
+ * Removes default values from certain shapes, and any member that targets those shapes,
+ * for some services where the default value causes serialization issues, validation
+ * issues, or other unexpected behavior.
+ */
+class RemoveDefaultsDecorator : ClientCodegenDecorator {
+    override val name: String = "RemoveDefaults"
+    override val order: Byte = 0
+    private val logger: Logger = Logger.getLogger(javaClass.name)
+
+    // Service shape id -> Shape id of each root shape to remove the default from.
+    // TODO(https://github.com/smithy-lang/smithy-rs/issues/3220): Remove this customization after model updates.
+    private val removeDefaults = mapOf(
+        "com.amazonaws.emrserverless#AwsToledoWebService".shapeId() to setOf(
+            // Service expects this to have a min value > 0
+            "com.amazonaws.emrserverless#WorkerCounts".shapeId(),
+        ),
+    )
+
+    private fun applies(service: ServiceShape) =
+        removeDefaults.containsKey(service.id)
+
+    override fun transformModel(service: ServiceShape, model: Model, settings: ClientRustSettings): Model {
+        if (!applies(service)) {
+            return model
+        }
+        logger.info("Removing invalid defaults from ${service.id}")
+        return RemoveDefaults.processModel(model, removeDefaults[service.id]!!)
+    }
+}
