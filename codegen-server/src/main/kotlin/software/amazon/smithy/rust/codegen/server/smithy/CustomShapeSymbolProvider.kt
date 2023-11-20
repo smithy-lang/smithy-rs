7ec00a60badeb3d3117342732503e5d29a1eff90
82marbag <69267416+82marbag@users.noreply.github.com>
2023-11-20 09:11:37 +0100

    Add copyright
    Signed-off-by: Daniele Ahmed <ahmeddan@amazon.de>

--- codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/CustomShapeSymbolProvider.kt
+++ codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/CustomShapeSymbolProvider.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package software.amazon.smithy.rust.codegen.server.smithy
 
 import software.amazon.smithy.codegen.core.Symbol
