ea08cb2f49f8895dbe56ab4e1c95e3fe71f54e58
ysaito1001 <awsaito@amazon.com>
2023-11-30 21:57:36 -0600

    Return `None` from `Config::credentials_provider`
    This commit addresses https://github.com/smithy-lang/smithy-rs/pull/3276#discussion_r1411380969

--- aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/CredentialProviders.kt
+++ aws/sdk-codegen/src/main/kotlin/software/amazon/smithy/rustsdk/CredentialProviders.kt
@@ -84,7 +84,7 @@ class CredentialProviderConfig(private val codegenContext: ClientCodegenContext)
                     /// This function was intended to be removed, and has been broken since release-2023-11-15 as it always returns a `None`. Do not use.
                     ##[deprecated(note = "This function was intended to be removed, and has been broken since release-2023-11-15 as it always returns a `None`. Do not use.")]
                     pub fn credentials_provider(&self) -> Option<#{SharedCredentialsProvider}> {
-                        self.config.load::<#{SharedCredentialsProvider}>().cloned()
+                        #{None}
                     }
                     """,
                     *codegenScope,
