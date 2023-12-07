aa8059fccf9f457436db94c7995ddbf7fa151a24
John DiSanti <jdisanti@amazon.com>
2023-12-07 11:13:36 -0800

    Allow unknown lints

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/customizations/AllowLintsCustomization.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/customizations/AllowLintsCustomization.kt
@@ -15,6 +15,10 @@ import software.amazon.smithy.rust.codegen.core.smithy.generators.LibRsSection
 private val allowedRustcLints = listOf(
     // Deprecated items should be safe to compile, so don't block the compilation.
     "deprecated",
+
+    // Unknown lints need to be allowed since we use both nightly and our MSRV, and sometimes we need
+    // to disable lints that are in nightly but don't exist in the MSRV.
+    "unknown_lints",
 )
 
 private val allowedClippyLints = listOf(
