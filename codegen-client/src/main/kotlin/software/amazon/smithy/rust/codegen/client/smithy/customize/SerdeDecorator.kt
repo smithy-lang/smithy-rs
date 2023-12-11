077d868c46d453000603073a7c5c4076ec0f5074
Thomas K Cameron <thomas.k.cameron.1218@gmail.com>
2023-12-11 18:46:09 +0900

    asdf

--- codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customize/SerdeDecorator.kt
+++ codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customize/SerdeDecorator.kt
@@ -8,6 +8,7 @@ package software.amazon.smithy.rust.codegen.client.smithy.customize
 import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
 import software.amazon.smithy.rust.codegen.core.rustlang.containerDocs
 import software.amazon.smithy.rust.codegen.core.rustlang.Feature
+import software.amazon.smithy.rust.codegen.core.rustlang.RustWriter
 import software.amazon.smithy.rust.codegen.core.rustlang.Writable
 import software.amazon.smithy.rust.codegen.core.rustlang.writable
 import software.amazon.smithy.rust.codegen.core.smithy.RustCrate
@@ -42,39 +43,41 @@ class SerdeDecorator : ClientCodegenDecorator {
 class SerdeDocGenerator(private val codegenContext: ClientCodegenContext) : LibRsCustomization() {
     override fun section(section: LibRsSection): Writable {
         return when (section) {
-            is LibRsSection.ModuleDoc-> {
+            is LibRsSection.ModuleDoc->
                 if (section.subsection is ModuleDocSection.AWSSdkUnstable) {
-                    return writable {
-                        SerdeInfoText
-                    }
+                    serdeInfoText()
                 } else {
-                    return emptySection
+                    emptySection
                 }
-            }
+
             else -> emptySection
         }
     }
-    companion object {
 
-        val SerdeInfoText = """
-            ## How to enable `Serialize` and `Deserialize`
-            
-            This data type implements `Serialize` and `Deserialize` traits from the popular serde crate, but those traits are behind feature gate.
-
-            As they increase it's compile time dramatically, you should not turn them on unless it's necessary.
-            Implementation of `serde` traits in AWS SDK for Rust is still unstable, and implementation may change anytime in future.
-
-            To enable traits, you must pass `aws_sdk_unstable` to RUSTFLAGS and enable `serde-serialize` or `serde-deserialize` feature.
-
-            e.g.
-            ```bash,no_run
-            export RUSTFLAGS="--cfg aws_sdk_unstable"
-            cargo build --features serde-serialize serde-deserialize
-            ```
-
-            If you enable `serde-serialize` and/or `serde-deserialize` without `RUSTFLAGS="--cfg aws_sdk_unstable"`,
-            compilation will fail with warning.
-            
-        """.trimIndent()
+    private fun serdeInfoText(): Writable {
+        return writable {
+            containerDocs(
+                """
+                ## How to enable `Serialize` and `Deserialize`
+                
+                This data type implements `Serialize` and `Deserialize` traits from the popular serde crate, but those traits are behind feature gate.
+    
+                As they increase it's compile time dramatically, you should not turn them on unless it's necessary.
+                Implementation of `serde` traits in AWS SDK for Rust is still unstable, and implementation may change anytime in future.
+    
+                To enable traits, you must pass `aws_sdk_unstable` to RUSTFLAGS and enable `serde-serialize` or `serde-deserialize` feature.
+    
+                e.g.
+                ```bash,no_run
+                export RUSTFLAGS="--cfg aws_sdk_unstable"
+                cargo build --features serde-serialize serde-deserialize
+                ```
+    
+                If you enable `serde-serialize` and/or `serde-deserialize` without `RUSTFLAGS="--cfg aws_sdk_unstable"`,
+                compilation will fail with warning.
+                
+                """.trimIndent()
+            )
+        }
     }
 }
