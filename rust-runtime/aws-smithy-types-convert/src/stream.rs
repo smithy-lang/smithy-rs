11f1e645927d2bf3f9c2c8140e0b281c383f5586
Erlend Langseth <3rlendhl@gmail.com>
2023-12-14 16:27:15 +0100

    Use futures-core rather than tokio-stream, add better description,
    remove unnecessary trait impl bounds

--- rust-runtime/aws-smithy-types-convert/src/stream.rs
+++ rust-runtime/aws-smithy-types-convert/src/stream.rs
@@ -5,9 +5,9 @@
 
 //! Conversions from Stream-like structs to implementors of `futures::Stream`
 
+use futures_core::Stream;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use tokio_stream::Stream;
 
 use aws_smithy_async::future::pagination_stream::PaginationStream;
 
@@ -16,20 +16,14 @@ pub struct PaginationStreamImplStream<Item> {
     pagination_stream: PaginationStream<Item>,
 }
 
-impl<Item> PaginationStreamImplStream<Item>
-where
-    Item: Send + 'static,
-{
+impl<Item> PaginationStreamImplStream<Item> {
     /// Create a new Stream object wrapping a `PaginationStream`
     pub fn new(pagination_stream: PaginationStream<Item>) -> Self {
         PaginationStreamImplStream { pagination_stream }
     }
 }
 
-impl<Item> Stream for PaginationStreamImplStream<Item>
-where
-    Item: Send + 'static,
-{
+impl<Item> Stream for PaginationStreamImplStream<Item> {
     type Item = Item;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
@@ -40,11 +34,27 @@ where
 /// Trait to convert PaginationStream into implementor of `Stream`
 pub trait PaginationStreamExt<Item> {
     /// Convert PaginationStream into implementor of `Stream`
-    fn as_stream_01x(self) -> PaginationStreamImplStream<Item>;
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use aws_smithy_async::future::pagination_stream::PaginationStream;
+    /// use aws_smithy_types_convert::stream::PaginationStreamExt;
+    /// // Assuming you have obtained a pagination stream, by something like:
+    /// // ```
+    /// // let pagination_stream = s3_client
+    /// //     .list_objects_v2()
+    /// //     .bucket(bucket)
+    /// //     .into_paginator()
+    /// //     .send();
+    /// // ```
+    /// # let pagination_stream: PaginationStream<i32> = unimplemented!();
+    /// let futures_stream = pagination_stream.into_stream_03x();
+    /// ```
+    fn into_stream_03x(self) -> PaginationStreamImplStream<Item>;
 }
 
 impl<Item> PaginationStreamExt<Item> for PaginationStream<Item> {
-    fn as_stream_01x(self) -> PaginationStreamImplStream<Item> {
+    fn into_stream_03x(self) -> PaginationStreamImplStream<Item> {
         PaginationStreamImplStream {
             pagination_stream: self,
         }
