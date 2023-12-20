7b051a69add0fecf6e247ee8e771a8cba14cc18f
Russell Cohen <rcoh@amazon.com>
2023-12-20 11:40:42 -0500

    Update footer / add aws-smithy-types-convert to public crates

--- tools/ci-build/sdk-lints/src/readmes.rs
+++ tools/ci-build/sdk-lints/src/readmes.rs
@@ -8,8 +8,7 @@ use anyhow::{Context, Result};
 use std::fs;
 use std::path::{Path, PathBuf};
 
-const CRATES_TO_BE_USED_DIRECTLY: &[&str] =
-    ["aws-config", "aws-smithy-mocks-experimental"].as_slice();
+const CRATES_TO_BE_USED_DIRECTLY: &[&str] = ["aws-config", "aws-smithy-types-convert"].as_slice();
 
 pub(crate) struct ReadmesExist;
 impl Lint for ReadmesExist {
