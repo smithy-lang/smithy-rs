fe0abedc8112140a83c96c044c3af5ded0ed3821
Russell Cohen <rcoh@amazon.com>
2023-12-20 15:27:31 -0500

    wip

--- codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerBuilderGeneratorCommon.kt
+++ codegen-server/src/main/kotlin/software/amazon/smithy/rust/codegen/server/smithy/generators/ServerBuilderGeneratorCommon.kt
@@ -21,7 +21,6 @@ import software.amazon.smithy.model.shapes.DocumentShape
 import software.amazon.smithy.model.shapes.DoubleShape
 import software.amazon.smithy.model.shapes.EnumShape
 import software.amazon.smithy.model.shapes.FloatShape
-import software.amazon.smithy.model.shapes.IntEnumShape
 import software.amazon.smithy.model.shapes.IntegerShape
 import software.amazon.smithy.model.shapes.ListShape
 import software.amazon.smithy.model.shapes.LongShape
@@ -32,15 +31,13 @@ import software.amazon.smithy.model.shapes.ShortShape
 import software.amazon.smithy.model.shapes.StringShape
 import software.amazon.smithy.model.shapes.TimestampShape
 import software.amazon.smithy.model.traits.DefaultTrait
-import software.amazon.smithy.model.traits.EnumDefinition
 import software.amazon.smithy.rust.codegen.core.rustlang.RustWriter
 import software.amazon.smithy.rust.codegen.core.rustlang.rust
 import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
 import software.amazon.smithy.rust.codegen.core.rustlang.writable
 import software.amazon.smithy.rust.codegen.core.smithy.RuntimeConfig
 import software.amazon.smithy.rust.codegen.core.smithy.RustSymbolProvider
-import software.amazon.smithy.rust.codegen.core.smithy.generators.EnumMemberModel
-import software.amazon.smithy.rust.codegen.core.util.UNREACHABLE
+import software.amazon.smithy.rust.codegen.core.smithy.generators.PrimitiveInstantiator
 import software.amazon.smithy.rust.codegen.core.util.dq
 import software.amazon.smithy.rust.codegen.core.util.expectTrait
 import software.amazon.smithy.rust.codegen.core.util.isStreaming
@@ -127,32 +124,7 @@ fun defaultValue(
     val unsupportedDefaultValueException =
         CodegenException("Default value $node for member shape ${member.id} is unsupported or cannot exist; please file a bug report under https://github.com/smithy-lang/smithy-rs/issues")
     when (val target = model.expectShape(member.target)) {
-        is EnumShape, is IntEnumShape -> {
-            val value = when (target) {
-                is IntEnumShape -> node.expectNumberNode().value
-                is EnumShape -> node.expectStringNode().value
-                else -> throw CodegenException("Default value for shape ${target.id} must be of EnumShape or IntEnumShape")
-            }
-            val enumValues = when (target) {
-                is IntEnumShape -> target.enumValues
-                is EnumShape -> target.enumValues
-                else -> UNREACHABLE(
-                    "Target shape ${target.id} must be an `EnumShape` or an `IntEnumShape` at this point, otherwise it would have failed above",
-                )
-            }
-            val variant = enumValues
-                .entries
-                .filter { entry -> entry.value == value }
-                .map { entry ->
-                    EnumMemberModel.toEnumVariantName(
-                        symbolProvider,
-                        target,
-                        EnumDefinition.builder().name(entry.key).value(entry.value.toString()).build(),
-                    )!!
-                }
-                .first()
-            rust("#T::${variant.name}", symbolProvider.toSymbol(target))
-        }
+        is EnumShape -> PrimitiveInstantiator(runtimeConfig, symbolProvider).instantiate(target, node)(this)
 
         is ByteShape -> rust(node.expectNumberNode().value.toString() + "i8")
         is ShortShape -> rust(node.expectNumberNode().value.toString() + "i16")
@@ -162,28 +134,17 @@ fun defaultValue(
         is DoubleShape -> rust(node.expectNumberNode().value.toDouble().toString() + "f64")
         is BooleanShape -> rust(node.expectBooleanNode().value.toString())
         is StringShape -> rust("String::from(${node.expectStringNode().value.dq()})")
-        is TimestampShape -> when (node) {
-            is NumberNode -> rust(node.expectNumberNode().value.toString())
-            is StringNode -> {
-                val value = node.expectStringNode().value
-                rustTemplate(
-                    """
-                    #{SmithyTypes}::DateTime::from_str("$value", #{SmithyTypes}::date_time::Format::DateTime)
-                            .expect("default value `$value` cannot be parsed into a valid date time; please file a bug report under https://github.com/smithy-lang/smithy-rs/issues")
-                    """,
-                    "SmithyTypes" to types,
-                )
-            }
-            else -> throw unsupportedDefaultValueException
-        }
+        is TimestampShape -> PrimitiveInstantiator(runtimeConfig, symbolProvider).instantiate(target, node)(this)
         is ListShape -> {
             check(node is ArrayNode && node.isEmpty)
             rust("Vec::new()")
         }
+
         is MapShape -> {
             check(node is ObjectNode && node.isEmpty)
             rust("std::collections::HashMap::new()")
         }
+
         is DocumentShape -> {
             when (node) {
                 is NullNode -> rustTemplate(
@@ -191,8 +152,16 @@ fun defaultValue(
                     "SmithyTypes" to types,
                 )
 
-                is BooleanNode -> rustTemplate("""#{SmithyTypes}::Document::Bool(${node.value})""", "SmithyTypes" to types)
-                is StringNode -> rustTemplate("#{SmithyTypes}::Document::String(String::from(${node.value.dq()}))", "SmithyTypes" to types)
+                is BooleanNode -> rustTemplate(
+                    """#{SmithyTypes}::Document::Bool(${node.value})""",
+                    "SmithyTypes" to types,
+                )
+
+                is StringNode -> rustTemplate(
+                    "#{SmithyTypes}::Document::String(String::from(${node.value.dq()}))",
+                    "SmithyTypes" to types,
+                )
+
                 is NumberNode -> {
                     val value = node.value.toString()
                     val variant = when (node.value) {
@@ -212,14 +181,17 @@ fun defaultValue(
 
                 is ObjectNode -> {
                     check(node.isEmpty)
-                    rustTemplate("#{SmithyTypes}::Document::Object(std::collections::HashMap::new())", "SmithyTypes" to types)
+                    rustTemplate(
+                        "#{SmithyTypes}::Document::Object(std::collections::HashMap::new())",
+                        "SmithyTypes" to types,
+                    )
                 }
 
                 else -> throw unsupportedDefaultValueException
             }
         }
 
-        is BlobShape -> rust("Default::default()")
+        is BlobShape -> PrimitiveInstantiator(runtimeConfig, symbolProvider).instantiate(target, node)(this)
 
         else -> throw unsupportedDefaultValueException
     }
