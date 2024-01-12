5f714850d668e7cd109ca8e9a6667d0fc7c59d4e
John DiSanti <jdisanti@amazon.com>
2024-01-12 12:03:16 -0800

    Fix subtle break of endpoint prefixes due to semver (#3318)
    When we released smithy-rs release-2023-12-08, we introduced a silent
    failure for endpoint prefixes, and not in the newly released version,
    but in the previous releases. There is a subtle issue with semver that
    causes this. This PR addresses the endpoint prefix part of this problem.
    Other PRs will fix other parts that are broken by this semver issue.
    
    The issue is that unstable (0.x) runtime crates are declaring types that
    get placed into the `ConfigBag`, and these types are referenced in the
    `ConfigBag` across crate boundaries. This by itself isn't a problem, but
    because our stable 1.x crates depend on the unstable crates, it becomes
    a problem. By releasing 1.1.0 that depends on 0.61, consumers of 1.x
    pull in both 0.60 and 0.61. The generated code pulls in 0.60, and the
    1.1.x crates pull in 0.61. This is fine since two semver-incompatible
    crate versions can be in the dependency closure. Thus, the generated
    code which is using 0.60 is placing a 0.60 type in the `ConfigBag`, and
    the runtime crates that pull the type out of the `ConfigBag` are
    expecting a 0.61 version of it. This leads to the type not existing upon
    lookup, which then causes the silent break.
    
    This PR fixes this by moving the `EndpointPrefix` type and its
    associated methods into `aws-smithy-runtime-api`, a stable crate. The
    `aws-smithy-http` unstable crate is updated to point to the new stable
    version so that a patch release of that crate will solve the issue for
    old versions going forward as well.
    
    ----
    
    _By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice._

--- rust-runtime/aws-smithy-runtime/src/client/endpoint.rs
+++ rust-runtime/aws-smithy-runtime/src/client/endpoint.rs
@@ -3,37 +3,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-//! Code for resolving an endpoint (URI) that a request should be sent to
+//! Code for applying endpoints to a request.
 
-#![allow(deprecated)]
-
-use crate::endpoint::error::InvalidEndpointError;
+use aws_smithy_runtime_api::client::endpoint::{error::InvalidEndpointError, EndpointPrefix};
 use http::uri::{Authority, Uri};
 use std::borrow::Cow;
 use std::result::Result as StdResult;
 use std::str::FromStr;
 
-pub mod error;
-pub use error::ResolveEndpointError;
-
-/// An endpoint-resolution-specific Result. Contains either an [`Endpoint`](aws_smithy_types::endpoint::Endpoint) or a [`ResolveEndpointError`].
-#[deprecated(since = "0.60.1", note = "Was never used.")]
-pub type Result = std::result::Result<aws_smithy_types::endpoint::Endpoint, ResolveEndpointError>;
-
-/// A special type that adds support for services that have special URL-prefixing rules.
-#[deprecated(
-    since = "0.60.1",
-    note = "Use aws_smithy_runtime_api::client::endpoint::EndpointPrefix instead."
-)]
-pub type EndpointPrefix = aws_smithy_runtime_api::client::endpoint::EndpointPrefix;
-
 /// Apply `endpoint` to `uri`
 ///
 /// This method mutates `uri` by setting the `endpoint` on it
-#[deprecated(
-    since = "0.60.1",
-    note = "Use aws_smithy_runtime::client::endpoint::apply_endpoint instead."
-)]
 pub fn apply_endpoint(
     uri: &mut Uri,
     endpoint: &Uri,
