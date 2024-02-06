c6d9ad1daf26843b4edd068c02d5f52aba1561cd
ysaito1001 <awsaito@amazon.com>
2024-02-06 16:57:35 -0600

    Add integraton tests for S3 Express checksum default

--- aws/sdk/integration-tests/s3/tests/express.rs
+++ aws/sdk/integration-tests/s3/tests/express.rs
@@ -6,13 +6,28 @@
 use std::time::{Duration, SystemTime};
 
 use aws_config::Region;
+use aws_sdk_s3::config::Builder;
 use aws_sdk_s3::presigning::PresigningConfig;
 use aws_sdk_s3::primitives::SdkBody;
+use aws_sdk_s3::types::ChecksumAlgorithm;
 use aws_sdk_s3::{Client, Config};
 use aws_smithy_runtime::client::http::test_util::dvr::ReplayingClient;
 use aws_smithy_runtime::client::http::test_util::{ReplayEvent, StaticReplayClient};
 use aws_smithy_runtime::test_util::capture_test_logs::capture_test_logs;
 use http::Uri;
+
+async fn test_client<F>(update_builder: F) -> Client
+where
+    F: Fn(Builder) -> Builder,
+{
+    let sdk_config = aws_config::from_env()
+        .no_credentials()
+        .region("us-west-2")
+        .load()
+        .await;
+    let config = Config::from(&sdk_config).to_builder().with_test_defaults();
+    aws_sdk_s3::Client::from_conf(update_builder(config).build())
+}
 
 #[tokio::test]
 async fn list_objects_v2() {
@@ -20,17 +35,7 @@ async fn list_objects_v2() {
 
     let http_client =
         ReplayingClient::from_file("tests/data/express/list-objects-v2.json").unwrap();
-    let config = aws_config::from_env()
-        .http_client(http_client.clone())
-        .no_credentials()
-        .region("us-west-2")
-        .load()
-        .await;
-    let config = Config::from(&config)
-        .to_builder()
-        .with_test_defaults()
-        .build();
-    let client = aws_sdk_s3::Client::from_conf(config);
+    let client = test_client(|b| b.http_client(http_client.clone())).await;
 
     let result = client
         .list_objects_v2()
@@ -50,17 +55,7 @@ async fn mixed_auths() {
     let _logs = capture_test_logs();
 
     let http_client = ReplayingClient::from_file("tests/data/express/mixed-auths.json").unwrap();
-    let config = aws_config::from_env()
-        .http_client(http_client.clone())
-        .no_credentials()
-        .region("us-west-2")
-        .load()
-        .await;
-    let config = Config::from(&config)
-        .to_builder()
-        .with_test_defaults()
-        .build();
-    let client = aws_sdk_s3::Client::from_conf(config);
+    let client = test_client(|b| b.http_client(http_client.clone())).await;
 
     // A call to an S3 Express bucket where we should see two request/response pairs,
     // one for the `create_session` API and the other for `list_objects_v2` in S3 Express bucket.
@@ -186,4 +181,122 @@ async fn presigning() {
         &query_params
     );
     assert_eq!(presigned.headers().count(), 0);
+}
+
+fn operation_request_with_checksum(
+    query: &str,
+    kv: Option<(&str, &str)>,
+) -> http::Request<SdkBody> {
+    let mut b = http::Request::builder()
+        .uri(&format!("https://s3express-test-bucket--usw2-az1--x-s3.s3express-usw2-az1.us-west-2.amazonaws.com/{query}"))
+        .method("GET");
+    if let Some((key, value)) = kv {
+        b = b.header(key, value);
+    }
+    b.body(SdkBody::empty()).unwrap()
+}
+
+fn response_ok() -> http::Response<SdkBody> {
+    http::Response::builder()
+        .status(200)
+        .body(SdkBody::empty())
+        .unwrap()
+}
+
+#[tokio::test]
+async fn user_specified_checksum_should_be_respected() {
+    async fn runner(checksum: &str, value: &str) {
+        let http_client = StaticReplayClient::new(vec![
+            ReplayEvent::new(create_session_request(), create_session_response()),
+            ReplayEvent::new(
+                operation_request_with_checksum(
+                    "test?x-id=PutObject",
+                    Some((
+                        &format!("x-amz-checksum-{}", checksum.to_lowercase()),
+                        &format!("{value}"),
+                    )),
+                ),
+                response_ok(),
+            ),
+        ]);
+        let client = test_client(|b| b.http_client(http_client.clone())).await;
+
+        let _ = client
+            .put_object()
+            .bucket("s3express-test-bucket--usw2-az1--x-s3")
+            .key("test")
+            .body(SdkBody::empty().into())
+            .checksum_algorithm(ChecksumAlgorithm::from(checksum))
+            .send()
+            .await;
+
+        http_client.assert_requests_match(&[""]);
+    }
+
+    let crc32_checksum = "AAAAAA==";
+    let crc32c_checksum = "AAAAAA==";
+    let sha1_checksum = "2jmj7l5rSw0yVb/vlWAYkK/YBwk=";
+    let sha256_checksum = "47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=";
+    for (checksum, value) in ChecksumAlgorithm::values().iter().zip(&[
+        crc32_checksum,
+        crc32c_checksum,
+        sha1_checksum,
+        sha256_checksum,
+    ]) {
+        runner(*checksum, *value).await;
+    }
+}
+
+#[tokio::test]
+async fn default_checksum_should_be_crc32_for_operation_requiring_checksum() {
+    let http_client = StaticReplayClient::new(vec![
+        ReplayEvent::new(create_session_request(), create_session_response()),
+        ReplayEvent::new(
+            operation_request_with_checksum(
+                "?delete&x-id=DeleteObjects",
+                Some(("x-amz-checksum-crc32", "AAAAAA==")),
+            ),
+            response_ok(),
+        ),
+    ]);
+    let client = test_client(|b| b.http_client(http_client.clone())).await;
+
+    let _ = client
+        .delete_objects()
+        .bucket("s3express-test-bucket--usw2-az1--x-s3")
+        .send()
+        .await;
+
+    http_client.assert_requests_match(&[""]);
+}
+
+#[tokio::test]
+async fn default_checksum_should_be_none() {
+    let http_client = StaticReplayClient::new(vec![
+        ReplayEvent::new(create_session_request(), create_session_response()),
+        ReplayEvent::new(
+            operation_request_with_checksum("test?x-id=PutObject", None),
+            response_ok(),
+        ),
+    ]);
+    let client = test_client(|b| b.http_client(http_client.clone())).await;
+
+    let _ = client
+        .put_object()
+        .bucket("s3express-test-bucket--usw2-az1--x-s3")
+        .key("test")
+        .body(SdkBody::empty().into())
+        .send()
+        .await;
+
+    http_client.assert_requests_match(&[""]);
+
+    let mut all_checksums = ChecksumAlgorithm::values()
+        .iter()
+        .map(|checksum| format!("amz-checksum-{}", checksum.to_lowercase()))
+        .chain(std::iter::once("content-md5".to_string()));
+
+    assert!(!all_checksums.any(|checksum| http_client
+        .actual_requests()
+        .any(|req| req.headers().iter().any(|(key, _)| key == checksum))));
 }
