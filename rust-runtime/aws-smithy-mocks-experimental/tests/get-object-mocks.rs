bcc581e5a55c4493a9932afa8adbeb3934ad6545
Zelda Hessler <zhessler@amazon.com>
2024-02-23 11:38:25 -0600

    bump version

--- rust-runtime/aws-smithy-mocks-experimental/tests/get-object-mocks.rs
+++ rust-runtime/aws-smithy-mocks-experimental/tests/get-object-mocks.rs
@@ -16,8 +16,7 @@ use aws_smithy_types::error::ErrorMetadata;
 
 use aws_smithy_mocks_experimental::{mock, MockResponseInterceptor};
 
-#[allow(clippy::redundant_static_lifetimes)]
-const S3_NO_SUCH_KEY: &'static str = r#"<?xml version="1.0" encoding="UTF-8"?>
+const S3_NO_SUCH_KEY: &str = r#"<?xml version="1.0" encoding="UTF-8"?>
 <Error>
   <Code>NoSuchKey</Code>
   <Message>The resource you requested does not exist</Message>
