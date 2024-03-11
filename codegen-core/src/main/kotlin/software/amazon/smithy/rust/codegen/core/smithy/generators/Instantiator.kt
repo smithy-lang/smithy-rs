9777ac19ac1c079db4cc3cef6705ad44ca2918cc
Fahad Zubair <fahadzub@amazon.com>
2024-03-11 15:46:08 +0000

    Use `@defaultTrait` for setting the default value for a shape.

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/generators/Instantiator.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/generators/Instantiator.kt
@@ -66,6 +66,8 @@ import software.amazon.smithy.rust.codegen.core.util.hasTrait
 import software.amazon.smithy.rust.codegen.core.util.isTargetUnit
 import software.amazon.smithy.rust.codegen.core.util.letIf
 import java.math.BigDecimal
+import software.amazon.smithy.model.traits.DefaultTrait
+import software.amazon.smithy.rust.codegen.core.util.getTrait
 
 /**
  * Class describing an instantiator section that can be used in a customization.
@@ -448,12 +450,10 @@ open class Instantiator(
 
     /**
      * Returns a default value for a shape.
-     *
-     * Warning: this method does not take into account any constraint traits attached to the shape.
      */
     private fun fillDefaultValue(shape: Shape): Node =
         when (shape) {
-            is MemberShape -> fillDefaultValue(model.expectShape(shape.target))
+            is MemberShape -> shape.getTrait<DefaultTrait>()?.toNode() ?: fillDefaultValue(model.expectShape(shape.target))
 
             // Aggregate shapes.
             is StructureShape -> Node.objectNode()
