64206278d6823a5a1781efa38ba150c0833e353a
Russell Cohen <rcoh@amazon.com>
2024-03-29 15:35:08 -0400

    Add FIPS support to Hyper 1.0 Client
    This does several things:
    1. Upgrade to RusTLS 0.23 which enables FIPS support
    2. Add smoke test of the clients. This revealed a bug where https URLs were not supported.

--- rust-runtime/aws-smithy-experimental/examples/client-aws-lc.rs
+++ rust-runtime/aws-smithy-experimental/examples/client-aws-lc.rs
@@ -4,9 +4,15 @@
  */
 
 use aws_smithy_experimental::hyper_1_0::{CryptoMode, HyperClientBuilder};
+#[tokio::main]
 
-fn main() {
+async fn main() {
     let _client = HyperClientBuilder::new()
         .crypto_mode(CryptoMode::AwsLc)
+        .build_https();
+
+    // A FIPS client can also be created. Note that this has a more complex build environment required.
+    let _client = HyperClientBuilder::new()
+        .crypto_mode(CryptoMode::AwsLcFips)
         .build_https();
 }
