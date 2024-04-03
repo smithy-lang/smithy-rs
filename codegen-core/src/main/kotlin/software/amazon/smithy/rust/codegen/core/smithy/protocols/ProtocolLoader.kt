c5488227a9e919deefbf5faed948007b84ed7f24
Dj <djedward@users.noreply.github.com>
2024-04-03 14:36:10 -0700

    Improve error when service is missing protocol
    resolves #2452
    ----
    
    _By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice._

--- codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/protocols/ProtocolLoader.kt
+++ codegen-core/src/main/kotlin/software/amazon/smithy/rust/codegen/core/smithy/protocols/ProtocolLoader.kt
@@ -14,15 +14,27 @@ import software.amazon.smithy.model.traits.Trait
 import software.amazon.smithy.rust.codegen.core.smithy.CodegenContext
 
 open class ProtocolLoader<T, C : CodegenContext>(private val supportedProtocols: ProtocolMap<T, C>) {
+    private fun formatProtocols(): String {
+        return supportedProtocols.keys.joinToString(
+            prefix = "\t",
+            separator = "\n\t",
+        )
+    }
+
     fun protocolFor(
         model: Model,
         serviceShape: ServiceShape,
     ): Pair<ShapeId, ProtocolGeneratorFactory<T, C>> {
         val protocols: MutableMap<ShapeId, Trait> = ServiceIndex.of(model).getProtocols(serviceShape)
+        if (protocols.isEmpty()) {
+            throw CodegenException("Service must have a protocol trait. Available protocols:\n${formatProtocols()}")
+        }
+
         val matchingProtocols =
             protocols.keys.mapNotNull { protocolId -> supportedProtocols[protocolId]?.let { protocolId to it } }
         if (matchingProtocols.isEmpty()) {
-            throw CodegenException("No matching protocol — service offers: ${protocols.keys}. We offer: ${supportedProtocols.keys}")
+            val specified = protocols.keys.joinToString(", ")
+            throw CodegenException("Unable to find a matching protocol. Model specifies $specified, but must match an available protocol:\n${formatProtocols()}")
         }
         return matchingProtocols.first()
     }
