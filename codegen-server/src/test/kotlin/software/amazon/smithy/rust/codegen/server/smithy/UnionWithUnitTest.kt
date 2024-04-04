03059e475ad3fb01af76fa7ab7e84f4d47f04be2
Fahad Zubair <fahadzub@amazon.com>
2024-04-04 14:04:47 +0100

    Only compile as part of the test.

--- codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/UnionWithUnitTest.kt
+++ codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/UnionWithUnitTest.kt
@@ -6,10 +6,7 @@
 package software.amazon.smithy.rust.codegen.server.smithy
 
 import org.junit.jupiter.api.Test
-import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
 import software.amazon.smithy.rust.codegen.core.testutil.asSmithyModel
-import software.amazon.smithy.rust.codegen.core.testutil.testModule
-import software.amazon.smithy.rust.codegen.core.testutil.unitTest
 import software.amazon.smithy.rust.codegen.server.smithy.testutil.serverIntegrationTest
 
 class UnionWithUnitTest {
@@ -74,71 +71,7 @@ class UnionWithUnitTest {
             }            
             """.asSmithyModel()
 
-        serverIntegrationTest(model) { _, rustCrate ->
-            rustCrate.testModule {
-                rustTemplate(
-                    """
-                    use ::aws_smithy_eventstream::frame::MarshallMessage;
-                    use aws_smithy_types::event_stream::HeaderValue;
-                    
-                    ##[macro_export]
-                    macro_rules! assert_header {
-                        (${'$'}msg:expr, ${'$'}name:expr, ${'$'}expected_value:expr) => {
-                            let header = ${'$'}msg.headers().iter().find(|h| h.name().as_str() == ${'$'}name);
-                            match header {
-                                Some(h) => assert_eq!(*h.value(), HeaderValue::String(${'$'}expected_value.into())),
-                                None => panic!("Header with name {} not found", ${'$'}name),
-                            }
-                        };
-                    }
-                    """,
-                )
-
-                unitTest("message_with_keep_alive") {
-                    rustTemplate(
-                        """
-                        let event = crate::model::TestEvent::KeepAlive;
-                        let result = crate::event_stream_serde::TestEventMarshaller::new().marshall(event);
-                        assert!(result.is_ok(), "expected ok, got: {:?}", result);
-                        let message = result.unwrap();
-                        assert_header!(message, ":message-type", "event");
-                        assert_header!(message, ":event-type", "KeepAlive");
-                        assert_header!(message, ":content-type", "application/vnd.amazon.eventstream");
-                        assert_eq!(&b"{}"[..], message.payload());
-                        """,
-                    )
-                }
-
-                unitTest("message_with_keep_response") {
-                    rustTemplate(
-                        """
-                        let event = crate::model::TestEvent::Response(crate::model::TestResponseEvent::builder().data(Some("HelloWorld".to_string())).build());
-                        let result = crate::event_stream_serde::TestEventMarshaller::new().marshall(event);
-                        assert!(result.is_ok(), "expected ok, got: {:?}", result);
-                        let message = result.unwrap();
-                        assert_header!(message, ":message-type", "event");
-                        assert_header!(message, ":event-type", "Response");
-                        assert_header!(message, ":content-type", "application/vnd.amazon.eventstream");
-                        assert_eq!(&b"{\"data\":\"HelloWorld\"}"[..], message.payload());
-                        """,
-                    )
-                }
-
-                unitTest("unconstrained_to_constrained") {
-                    rustTemplate(
-                        """
-                        let unconstrained = crate::unconstrained::some_union_with_unit_unconstrained::SomeUnionWithUnitUnconstrained::Option1;
-                        let constrained = crate::model::SomeUnionWithUnit::try_from(unconstrained);
-                        assert!(constrained.is_ok(), "could not convert from unconstrained::SomeUnionWithUnconstrained, error: {:?}", constrained);
-                        let unconstrained = crate::unconstrained::some_union_with_unit_unconstrained::SomeUnionWithUnitUnconstrained::Option2("Hello, World!".to_owned());
-                        let constrained = crate::model::SomeUnionWithUnit::try_from(unconstrained);
-                        assert!(constrained.is_ok(), "could not convert from unconstrained::SomeUnionWithUnconstrained, error: {:?}", constrained);
-                        let constrained_value = constrained.as_ref().unwrap().as_option2().expect("Should have been converted to Option2");
-                        assert_eq!(constrained_value.as_str(), "Hello, World!");
-                        """,
-                    )
-                }
-            }
-        }
+        // Ensure the generated SDK compiles.
+        serverIntegrationTest(model) { _, _ -> }
     }
 }
