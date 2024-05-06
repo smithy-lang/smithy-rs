d5a6ddae497b4b1fed1c90b6f1eef823f47d668e
Zelda Hessler <zhessler@amazon.com>
2024-05-06 12:43:21 -0500

    add request compression runtime crate

---
+++ rust-runtime/aws-smithy-compression/src/gzip.rs
@@ -0,0 +1,128 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+use crate::{Compression, CompressionOptions};
+use aws_smithy_runtime_api::box_error::BoxError;
+use flate2::write::GzEncoder;
+use std::fmt;
+use std::io::prelude::*;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub(crate) struct Gzip {
+    compression: flate2::Compression,
+}
+
+impl Gzip {
+    fn compress_bytes(&self, bytes: &[u8], writer: impl Write) -> Result<(), Error> {
+        let mut encoder = GzEncoder::new(writer, self.compression);
+        encoder.write_all(bytes)?;
+        encoder.try_finish()?;
+
+        Ok(())
+    }
+}
+
+impl Compression for Gzip {
+    fn compress_bytes(&mut self, bytes: &[u8], writer: &mut dyn Write) -> Result<(), BoxError> {
+        Gzip::compress_bytes(self, bytes, writer).map_err(Into::into)
+    }
+}
+
+mod http_body_0_4_x {
+    impl crate::http::http_body_0_4_x::RequestCompressor for super::Gzip {
+        fn header_value(self: Box<Self>) -> http_0_2::HeaderValue {
+            http_0_2::HeaderValue::from_static("gzip")
+        }
+    }
+}
+
+mod http_body_1_x {
+    impl crate::http::http_body_1_x::RequestCompressor for super::Gzip {
+        fn header_value(self: Box<Self>) -> http_1_0::HeaderValue {
+            http_1_0::HeaderValue::from_static("gzip")
+        }
+    }
+}
+
+impl From<&CompressionOptions> for Gzip {
+    fn from(options: &CompressionOptions) -> Self {
+        Gzip {
+            compression: flate2::Compression::new(options.level),
+        }
+    }
+}
+
+impl From<CompressionOptions> for Gzip {
+    fn from(options: CompressionOptions) -> Self {
+        Gzip {
+            compression: flate2::Compression::new(options.level),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Error {
+    message: String,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "compression failed: {}", self.message)
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error {
+            message: err.to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Gzip;
+    use crate::CompressionOptions;
+    use flate2::read::GzDecoder;
+    use pretty_assertions::assert_eq;
+    use std::io::Read;
+
+    fn gettysburg_address() -> &'static [u8] {
+        include_bytes!("../test-data/gettysburg_address.txt")
+    }
+
+    fn gzip_compressed_gettysburg_address() -> &'static [u8] {
+        // This file was compressed using Apple gzip with the following command:
+        // `gzip -k gettysburg_address.txt -6`
+        include_bytes!("../test-data/gettysburg_address.txt.gz")
+    }
+
+    #[test]
+    fn test_gzip_compression() {
+        let gzip = Gzip::from(&CompressionOptions::default());
+        let mut compressed_output = Vec::new();
+        gzip.compress_bytes(gettysburg_address(), &mut compressed_output)
+            .expect("compression succeeds");
+
+        let uncompressed_expected = {
+            let mut s = String::new();
+            GzDecoder::new(gzip_compressed_gettysburg_address())
+                .read_to_string(&mut s)
+                .unwrap();
+            s
+        };
+        let uncompressed_actual = {
+            let mut s = String::new();
+            GzDecoder::new(&compressed_output[..])
+                .read_to_string(&mut s)
+                .unwrap();
+            s
+        };
+
+        assert_eq!(uncompressed_expected, uncompressed_actual);
+    }
+}
---
+++ rust-runtime/aws-smithy-compression/src/http.rs
@@ -0,0 +1,58 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+//! Checksum support for HTTP requests and responses.
+
+/// Support for the `http-body-0-4` and `http-0-2` crates.
+#[cfg(feature = "http-body-0-4-x")]
+pub mod http_body_0_4_x {
+    use crate::Compression;
+    use http_0_2::header::{HeaderMap, HeaderName, HeaderValue};
+
+    /// Implementors of this trait can be used to compress HTTP requests.
+    pub trait RequestCompressor: Compression + Send + Sync {
+        /// Return a map of headers that must be included when a request is compressed.
+        fn headers(self: Box<Self>) -> HeaderMap<HeaderValue> {
+            let mut header_map = HeaderMap::new();
+            header_map.insert(self.header_name(), self.header_value());
+
+            header_map
+        }
+
+        /// Return the header name for the content-encoding header.
+        fn header_name(&self) -> HeaderName {
+            HeaderName::from_static("content-encoding")
+        }
+
+        /// Return the header value for the content-encoding header.
+        fn header_value(self: Box<Self>) -> HeaderValue;
+    }
+}
+
+/// Support for the `http-body-1-0` and `http-1-0` crates.
+#[cfg(feature = "http-body-1-x")]
+pub mod http_body_1_x {
+    use crate::Compression;
+    use http_1_0::header::{HeaderMap, HeaderName, HeaderValue};
+
+    /// Implementors of this trait can be used to compress HTTP requests.
+    pub trait RequestCompressor: Compression + Send + Sync {
+        /// Return a map of headers that must be included when a request is compressed.
+        fn headers(self: Box<Self>) -> HeaderMap<HeaderValue> {
+            let mut header_map = HeaderMap::new();
+            header_map.insert(self.header_name(), self.header_value());
+
+            header_map
+        }
+
+        /// Return the header name for the content-encoding header.
+        fn header_name(&self) -> HeaderName {
+            HeaderName::from_static("content-encoding")
+        }
+
+        /// Return the header value for the content-encoding header.
+        fn header_value(self: Box<Self>) -> HeaderValue;
+    }
+}
