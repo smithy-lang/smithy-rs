ffecc014897c51215ac0d7dbf091cb2457dbf41e
ysaito1001 <awsaito@amazon.com>
2024-08-14 10:22:55 -0500

    Revert "Verify auth signature with deterministic headers"
    This reverts commit f513b924dc0e624d9810889b7177cb4eda6709d2.

--- aws/sdk-codegen/src/test/kotlin/software/amazon/smithy/rustsdk/SigV4AuthDecoratorTest.kt
+++ aws/sdk-codegen/src/test/kotlin/software/amazon/smithy/rustsdk/SigV4AuthDecoratorTest.kt
@@ -4,13 +4,8 @@
  */
 package software.amazon.smithy.rustsdk
 
-import SdkCodegenIntegrationTest
 import org.junit.jupiter.api.Test
-import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
-import software.amazon.smithy.rust.codegen.core.smithy.RuntimeType
 import software.amazon.smithy.rust.codegen.core.testutil.asSmithyModel
-import software.amazon.smithy.rust.codegen.core.testutil.integrationTest
-import software.amazon.smithy.rust.codegen.core.testutil.tokioTest
 
 class SigV4AuthDecoratorTest {
     private val modelWithSigV4AuthScheme =
@@ -66,94 +61,5 @@ class SigV4AuthDecoratorTest {
     @Test
     fun unsignedPayloadSetsCorrectHeader() {
         awsSdkIntegrationTest(modelWithSigV4AuthScheme) { _, _ -> }
-    }
-
-    @Test
-    fun authorizationHeader() {
-        awsSdkIntegrationTest(SdkCodegenIntegrationTest.model) { ctx, rustCrate ->
-            rustCrate.integrationTest("authorization_header") {
-                tokioTest("test_signature") {
-                    val rc = ctx.runtimeConfig
-                    val moduleName = ctx.moduleUseName()
-                    rustTemplate(
-                        """
-                        use $moduleName::config::{
-                            Credentials, FrozenLayer, Layer, Region, RuntimeComponentsBuilder, RuntimePlugin,
-                            SharedCredentialsProvider
-                        };
-                        use $moduleName::{Client, Config};
-
-                        ##[derive(Debug)]
-                        struct DeterministicAuthorizationHeaderPlugin(RuntimeComponentsBuilder);
-
-                        impl DeterministicAuthorizationHeaderPlugin {
-                            fn new() -> Self {
-                                Self(RuntimeComponentsBuilder::new("DeterministicAuthorizationHeaderPlugin"))
-                            }
-                        }
-
-                        // Exclude user agent headers, specifically `x-amz-user-agent`, to ensure the `authorization`
-                        // header is deterministic.
-                        // If we encounter signed headers that cause the `authorization` header to become
-                        // nondeterministic in the future, we will disable the corresponding interceptors here to ensure
-                        // the test remains valid. This plugin serves as a reference for managing the list of
-                        // interceptors that impact the determinism of the `authorization` header.
-                        impl RuntimePlugin for DeterministicAuthorizationHeaderPlugin {
-                            fn config(&self) -> Option<FrozenLayer> {
-                                let mut layer = Layer::new("DeterministicAuthorizationHeaderPlugin");
-                                layer.store_put(#{disable_interceptor}::<#{UserAgentInterceptor}>(
-                                    "DeterministicAuthorizationHeader",
-                                ));
-                                Some(layer.freeze())
-                            }
-
-                            fn runtime_components(
-                                &self,
-                                _: &RuntimeComponentsBuilder,
-                            ) -> std::borrow::Cow<'_, RuntimeComponentsBuilder> {
-                                std::borrow::Cow::Borrowed(&self.0)
-                            }
-                        }
-
-                        let (http_client, rcvr) = #{capture_request}(#{None});
-                        let config = Config::builder()
-                            .credentials_provider(SharedCredentialsProvider::new(Credentials::for_tests()))
-                            .region(Region::new("us-east-1"))
-                            .http_client(http_client.clone())
-                            .runtime_plugin(DeterministicAuthorizationHeaderPlugin::new())
-                            .with_test_defaults()
-                            .build();
-
-                        let client = Client::from_conf(config);
-                        let _ = client.some_operation().send().await;
-
-                        let expected_req = rcvr.expect_request();
-                        let auth_header = expected_req
-                            .headers()
-                            .get("authorization")
-                            .unwrap()
-                            .to_owned();
-
-                        let snapshot_signature =
-                            "Signature=54444af616c38775423dafed5497d19da7f5ef921d04bc89002c8f9e2dc8586c";
-                        assert!(
-                            auth_header.contains(snapshot_signature),
-                            "authorization header signature did not match expected signature: got {}, expected it to contain {}",
-                            auth_header,
-                            snapshot_signature
-                        );
-                        """,
-                        *RuntimeType.preludeScope,
-                        "UserAgentInterceptor" to
-                            AwsRuntimeType.awsRuntime(rc)
-                                .resolve("user_agent::UserAgentInterceptor"),
-                        "capture_request" to RuntimeType.captureRequest(rc),
-                        "disable_interceptor" to
-                            RuntimeType.smithyRuntimeApiClient(rc)
-                                .resolve("client::interceptors::disable_interceptor"),
-                    )
-                }
-            }
-        }
     }
 }
