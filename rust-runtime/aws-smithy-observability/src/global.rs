0b209b4e52c53d0e3a5b91190e19b242280cf76b
Landon James <lnj@amazon.com>
2024-11-06 11:00:46 -0800

    Cleaning up some tests and doc comments

--- rust-runtime/aws-smithy-observability/src/global.rs
+++ rust-runtime/aws-smithy-observability/src/global.rs
@@ -17,25 +17,61 @@ use crate::provider::{GlobalTelemetryProvider, TelemetryProvider};
 static GLOBAL_TELEMETRY_PROVIDER: Lazy<RwLock<GlobalTelemetryProvider>> =
     Lazy::new(|| RwLock::new(GlobalTelemetryProvider::new(TelemetryProvider::default())));
 
-/// Set the current global [TelemetryProvider], the previous [TelemetryProvider] is returned
-/// in an [Arc] so appropriate cleanup can be done if necessary.
-pub fn set_global_telemetry_provider(new_provider: TelemetryProvider) -> Arc<TelemetryProvider> {
+/// Set the current global [TelemetryProvider]. If [None] is supplied then a noop provider is set.
+/// The previous [TelemetryProvider] is returned in an [Arc] so appropriate cleanup can be done if necessary.
+pub fn set_global_telemetry_provider(
+    new_provider: Option<TelemetryProvider>,
+) -> Arc<TelemetryProvider> {
     // TODO(smithyObservability): would probably be nicer to return a Result here, but the Guard held by the error from
     // .try_write is not Send so I struggled to build an ObservabilityError from it
     let mut old_provider = GLOBAL_TELEMETRY_PROVIDER
         .try_write()
         .expect("GLOBAL_TELEMETRY_PROVIDER RwLock Poisoned");
 
-    let new_global_provider = GlobalTelemetryProvider::new(new_provider);
+    let new_global_provider = if let Some(tp) = new_provider {
+        GlobalTelemetryProvider::new(tp)
+    } else {
+        GlobalTelemetryProvider::new(TelemetryProvider::default())
+    };
 
     mem::replace(&mut *old_provider, new_global_provider).telemetry_provider
 }
 
 /// Get an [Arc] reference to the current global [TelemetryProvider].
-pub fn global_telemetry_provider() -> Arc<TelemetryProvider> {
+pub fn get_global_telemetry_provider() -> Arc<TelemetryProvider> {
+    // TODO(smithyObservability): would probably be nicer to return a Result here, but the Guard held by the error from
+    // .try_read is not Send so I struggled to build an ObservabilityError from it
     GLOBAL_TELEMETRY_PROVIDER
         .try_read()
         .expect("GLOBAL_TELEMETRY_PROVIDER RwLock Poisoned")
         .telemetry_provider()
         .clone()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::provider::TelemetryProvider;
+
+    #[test]
+    fn can_set_global_telemetry_provider() {
+        let my_provider = TelemetryProvider::default();
+
+        // Set the new counter and get a reference to the old one
+        let old_provider = set_global_telemetry_provider(Some(my_provider));
+
+        // Call shutdown on the old meter provider
+        let _old_meter = old_provider.meter_provider().shutdown().unwrap();
+    }
+
+    #[test]
+    fn can_get_global_telemetry_provider() {
+        let curr_provider = get_global_telemetry_provider();
+
+        // Use the global provider to create an instrument and record a value with it
+        let curr_mp = curr_provider.meter_provider();
+        let curr_meter = curr_mp.get_meter("TestMeter", None);
+        let instrument = curr_meter.create_monotonic_counter("TestMonoCounter".into(), None, None);
+        instrument.add(4, None, None);
+    }
 }
