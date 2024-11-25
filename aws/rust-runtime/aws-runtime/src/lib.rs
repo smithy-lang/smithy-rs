b9056f74b26ea4f97d4f2e81f96748dc15ff082f
ysaito1001 <awsaito@amazon.com>
2024-11-25 17:01:17 -0600

    Add `AwsSdkFeature` to track AWS specific features

--- aws/rust-runtime/aws-runtime/src/lib.rs
+++ aws/rust-runtime/aws-runtime/src/lib.rs
@@ -38,6 +38,9 @@ pub mod invocation_id;
 /// Supporting code for request metadata headers in the AWS SDK.
 pub mod request_info;
 
+#[doc(hidden)]
+pub mod sdk_feature;
+
 /// Interceptor that determines the clock skew between the client and service.
 pub mod service_clock_skew;
 
