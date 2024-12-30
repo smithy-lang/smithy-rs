1f3e82bdb4456e95ee1673dde2539a0bd257bc10
Landon James <lnj@amazon.com>
2024-12-30 11:18:22 -0700

    Add MVP of automated performance tests (#3953)
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here -->
    We have an EOY goal to to integrate with our internal automated
    performance monitoring solution.
    
    ## Description
    <!--- Describe your changes in detail -->
    Add a new crate at `aws/sdk/benchmarks/sdk-perf/` that runs some very
    basic performance tests (currently just serializing and deserializing
    DDB request/responses). Also add a new ci-script that generates the
    correct SDK crates, compiles the `sdk-perf` crate, and creates a shell
    script that the automated performance tests invoke to kick off our
    tests.
    
    ## Testing
    <!--- Please describe in detail how you tested your changes -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    I deployed this code to my personal Catapult instance (along with the
    changes to our internal CDK that will be CRed separately) and
    successfully ran an `e2e-test`.
    
    ## Checklist
    <!--- If a checkbox below is not applicable, then please DELETE it
    rather than leaving it unchecked -->
    No customer visible changes, this is strictly for our CI.
    
    ----
    
    _By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice._

---
+++ aws/sdk/benchmarks/sdk-perf/src/test_util.rs
@@ -0,0 +1,43 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+use crate::results::{Result, Results};
+use std::time::SystemTime;
+
+pub(crate) struct TestConfig {
+    pub(crate) name: String,
+    pub(crate) description: String,
+    pub(crate) unit: String,
+    pub(crate) runs: u8,
+}
+
+pub(crate) fn run_test<F: Fn()>(config: &TestConfig, results: &mut Results, func: F) {
+    let mut result = Result {
+        name: config.name.clone(),
+        description: config.description.clone(),
+        publish_to_cloudwatch: Some(true),
+        dimensions: None,
+        date: SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+        measurements: Vec::new(),
+        unit: config.unit.clone(),
+    };
+
+    // warmup the function
+    for _i in 0..5 {
+        func()
+    }
+
+    for _i in 0..config.runs {
+        let start = SystemTime::now();
+        func();
+        let time = start.elapsed().unwrap().as_micros() as f64;
+        result.measurements.push(time);
+    }
+
+    results.results.push(result);
+}
