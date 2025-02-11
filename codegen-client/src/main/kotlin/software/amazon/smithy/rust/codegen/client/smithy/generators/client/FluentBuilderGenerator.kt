fe93c5c84494640f27a6e16a5d8cf9c5bb93b62b
ysaito1001 <awsaito@amazon.com>
2025-02-11 13:52:40 -0600

    Turn inlined writable to a helper function
    This commit addresses https://github.com/smithy-lang/smithy-rs/pull/4004#discussion_r1950910468

--- codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/generators/client/FluentBuilderGenerator.kt
+++ codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/generators/client/FluentBuilderGenerator.kt
@@ -162,76 +162,87 @@ class FluentBuilderGenerator(
                 }
                 """,
                 *scope,
-                "epilogue" to
+                "epilogue" to (
+                    handleEventStreamInitialResponse() ?: writable {
+                        rustTemplate("#{Operation}::orchestrate(&runtime_plugins, input).await", *scope)
+                    }
+                ),
+            )
+        }
+
+    private fun handleEventStreamInitialResponse(): Writable? {
+        if (!codegenContext.protocol.isRpcBoundProtocol) {
+            return null
+        }
+
+        val eventStreamMember =
+            outputShape.members().find { member ->
+                member.isEventStream(codegenContext.model)
+            }
+        if (eventStreamMember == null) {
+            return null
+        }
+
+        return writable {
+            val eventStreamMemberName = symbolProvider.toMemberName(eventStreamMember)
+            rustTemplate(
+                """
+                let mut output =
+                    #{Operation}::orchestrate(
+                        &runtime_plugins,
+                        input,
+                    )
+                    .await?;
+
+                // Converts any error encountered beyond this point into an `SdkError` response error
+                // with an `HttpResponse`. However, since we have already exited the `orchestrate`
+                // function, the original `HttpResponse` is no longer available and cannot be restored.
+                // This means that header information from the original response has been lost.
+                //
+                // Note that the response body would have been consumed by the deserializer
+                // regardless, even if the initial message was hypothetically processed during
+                // the orchestrator's deserialization phase but later resulted in an error.
+                fn response_error(
+                    err: impl #{Into}<#{BoxError}>
+                ) -> #{SdkError}<#{OperationError}, #{HttpResponse}> {
+                    #{SdkError}::response_error(err, #{HttpResponse}::new(
+                        #{StatusCode}::try_from(200).expect("valid successful code"),
+                        #{SdkBody}::empty()))
+                }
+
+                let message = output.$eventStreamMemberName.try_recv_initial().await.map_err(response_error)?;
+
+                match message {
+                    #{Some}(_message) => {
+                        #{maybeRecreateOutputWithNonEventStreamMembers:W}
+                        #{Ok}(output)
+                    }
+                    #{None} => #{Ok}(output),
+                }
+                """,
+                *scope,
+                "maybeRecreateOutputWithNonEventStreamMembers" to
                     writable {
-                        val eventStreamMember =
-                            outputShape.members().find { member ->
-                                member.isEventStream(codegenContext.model)
-                            }
-                        if (eventStreamMember != null && codegenContext.protocol.isRpcBoundProtocol) {
-                            val eventStreamMemberName = symbolProvider.toMemberName(eventStreamMember)
+                        val structuredDataParser = codegenContext.protocolImpl?.structuredDataParser()
+                        structuredDataParser?.operationParser(operation)?.also { parser ->
                             rustTemplate(
                                 """
-                                let mut output =
-                                    #{Operation}::orchestrate(
-                                        &runtime_plugins,
-                                        input,
-                                    )
-                                    .await?;
-
-                                // Converts any error encountered beyond this point into an `SdkError` response error
-                                // with an `HttpResponse`. However, since we have already exited the `orchestrate`
-                                // function, the original `HttpResponse` is no longer available and cannot be restored.
-                                // This means that header information from the original response has been lost.
-                                //
-                                // Note that the response body would have been consumed by the deserializer
-                                // regardless, even if the initial message was hypothetically processed during
-                                // the orchestrator's deserialization phase but later resulted in an error.
-                                fn response_error(
-                                    err: impl #{Into}<#{BoxError}>
-                                ) -> #{SdkError}<#{OperationError}, #{HttpResponse}> {
-                                    #{SdkError}::response_error(err, #{HttpResponse}::new(
-                                        #{StatusCode}::try_from(200).expect("valid successful code"),
-                                        #{SdkBody}::empty()))
-                                }
-
-                                let message = output.$eventStreamMemberName.try_recv_initial().await.map_err(response_error)?;
-
-                                match message {
-                                    #{Some}(_message) => {
-                                        #{maybeRecreateOutputWithNonEventStreamMembers:W}
-                                        #{Ok}(output)
-                                    }
-                                    #{None} => #{Ok}(output),
-                                }
+                                let mut builder = output.into_builder();
+                                builder = #{parser}(
+                                    _message.payload(),
+                                    builder
+                                )
+                                .map_err(response_error)?;
+                                let output = builder.build().map_err(response_error)?;
                                 """,
+                                "parser" to parser,
                                 *scope,
-                                "maybeRecreateOutputWithNonEventStreamMembers" to
-                                    writable {
-                                        val structuredDataParser = codegenContext.protocolImpl?.structuredDataParser()
-                                        structuredDataParser?.operationParser(operation)?.also { parser ->
-                                            rustTemplate(
-                                                """
-                                                let mut builder = output.into_builder();
-                                                builder = #{parser}(
-                                                    _message.payload(),
-                                                    builder
-                                                )
-                                                .map_err(response_error)?;
-                                                let output = builder.build().map_err(response_error)?;
-                                                """,
-                                                "parser" to parser,
-                                                *scope,
-                                            )
-                                        }
-                                    },
                             )
-                        } else {
-                            rustTemplate("#{Operation}::orchestrate(&runtime_plugins, input).await", *scope)
                         }
                     },
             )
         }
+    }
 
     private fun RustWriter.renderStruct() {
         // Filter out any derive that isn't Clone. Then add a Debug derive input name
