368b4d7d7c46c631ad3f69b5bf72449c4400a234
ysaito1001 <awsaito@amazon.com>
2025-02-19 21:14:21 -0600

    test2

--- tools/ci-scripts/codegen-diff/semver-checks.py
+++ tools/ci-scripts/codegen-diff/semver-checks.py
@@ -27,7 +27,7 @@ def main(skip_generation=False):
         sys.exit(1)
 
     if not skip_generation:
-        checkout_commit_and_generate(head_commit_sha, CURRENT_BRANCH, targets=['aws:sdk'])
+        checkout_commit_and_generate(head_commit_sha, CURRENT_BRANCH, targets=['aws:sdk'], preserve_aws_sdk_build=True)
         checkout_commit_and_generate(base_commit_sha, BASE_BRANCH, targets=['aws:sdk'], preserve_aws_sdk_build=True)
     get_cmd_output(f'git checkout {CURRENT_BRANCH}')
     sdk_directory = os.path.join(repository_root, 'aws-sdk', 'sdk', 'build')
