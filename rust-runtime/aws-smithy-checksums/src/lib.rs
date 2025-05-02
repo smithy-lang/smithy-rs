412212c8af5c922662204d332eeb8c4b70a0ed8c
Don MacAskill <don@smugmug.com>
2025-05-02 10:06:13 -0700

    Use crc-fast for CRC32 and CRC32C
    Provides a massive performance improvement on modern hardware
    (>100GiB/s, depending on the platform).
    
    Also reduces the package dependencies, since crc-fast provides
    accelerated calculation for all 3 supported CRC checksums.

--- rust-runtime/aws-smithy-checksums/src/lib.rs
+++ rust-runtime/aws-smithy-checksums/src/lib.rs
@@ -129,9 +129,17 @@ pub trait Checksum: Send + Sync {
     fn size(&self) -> u64;
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Crc32 {
-    hasher: crc32fast::Hasher,
+    hasher: crc_fast::Digest,
+}
+
+impl Default for Crc32 {
+    fn default() -> Self {
+        Self {
+            hasher: crc_fast::Digest::new(crc_fast::CrcAlgorithm::Crc32IsoHdlc),
+        }
+    }
 }
 
 impl Crc32 {
@@ -140,7 +148,9 @@ impl Crc32 {
     }
 
     fn finalize(self) -> Bytes {
-        Bytes::copy_from_slice(self.hasher.finalize().to_be_bytes().as_slice())
+        let checksum = self.hasher.finalize() as u32;
+
+        Bytes::copy_from_slice(checksum.to_be_bytes().as_slice())
     }
 
     // Size of the checksum in bytes
@@ -161,21 +171,28 @@ impl Checksum for Crc32 {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Crc32c {
-    state: Option<u32>,
+    hasher: crc_fast::Digest,
+}
+
+impl Default for Crc32c {
+    fn default() -> Self {
+        Self {
+            hasher: crc_fast::Digest::new(crc_fast::CrcAlgorithm::Crc32Iscsi),
+        }
+    }
 }
 
 impl Crc32c {
     fn update(&mut self, bytes: &[u8]) {
-        self.state = match self.state {
-            Some(crc) => Some(crc32c::crc32c_append(crc, bytes)),
-            None => Some(crc32c::crc32c(bytes)),
-        };
+        self.hasher.update(bytes);
     }
 
     fn finalize(self) -> Bytes {
-        Bytes::copy_from_slice(self.state.unwrap_or_default().to_be_bytes().as_slice())
+        let checksum = self.hasher.finalize() as u32;
+
+        Bytes::copy_from_slice(checksum.to_be_bytes().as_slice())
     }
 
     // Size of the checksum in bytes
@@ -196,6 +213,7 @@ impl Checksum for Crc32c {
     }
 }
 
+#[derive(Debug)]
 struct Crc64Nvme {
     hasher: crc_fast::Digest,
 }
