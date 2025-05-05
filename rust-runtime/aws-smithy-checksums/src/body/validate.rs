efe45430823fd299be146bcf68e4f665def6291d
Landon James <lnj@amazon.com>
2025-05-05 13:32:32 -0700

    rustfmt in checksum crate

--- rust-runtime/aws-smithy-checksums/src/body/validate.rs
+++ rust-runtime/aws-smithy-checksums/src/body/validate.rs
@@ -162,10 +162,8 @@ mod tests {
     use std::io::Read;
 
     fn calculate_crc32_checksum(input: &str) -> Bytes {
-        let checksum = crc_fast::checksum(
-            crc_fast::CrcAlgorithm::Crc32IsoHdlc,
-            input.as_bytes()
-        ) as u32;
+        let checksum =
+            crc_fast::checksum(crc_fast::CrcAlgorithm::Crc32IsoHdlc, input.as_bytes()) as u32;
 
         Bytes::copy_from_slice(&checksum.to_be_bytes())
     }
