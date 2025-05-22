aa5c29a987f7f91566c4691a6049ccdb8d69ba9a
Fahad Zubair <fahadzub@amazon.com>
2025-05-22 16:29:30 +0100

    Add tests for output

--- codegen-server/python/src/test/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/generators/PythonServerRequiredPrecedeOptionalTest.kt
+++ codegen-server/python/src/test/kotlin/software/amazon/smithy/rust/codegen/server/python/smithy/generators/PythonServerRequiredPrecedeOptionalTest.kt
@@ -8,8 +8,11 @@ package software.amazon.smithy.rust.codegen.server.python.smithy.generators
 import org.junit.jupiter.api.Test
 import software.amazon.smithy.rust.codegen.core.rustlang.RustWriter
 import software.amazon.smithy.rust.codegen.core.rustlang.rust
+import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
+import software.amazon.smithy.rust.codegen.core.rustlang.writable
 import software.amazon.smithy.rust.codegen.core.testutil.asSmithyModel
 import software.amazon.smithy.rust.codegen.core.testutil.unitTest
+import software.amazon.smithy.rust.codegen.server.python.smithy.PythonServerCargoDependency
 import software.amazon.smithy.rust.codegen.server.python.smithy.testutil.cargoTest
 import software.amazon.smithy.rust.codegen.server.python.smithy.testutil.executePythonServerCodegenVisitor
 import software.amazon.smithy.rust.codegen.server.python.smithy.testutil.generatePythonServerPluginContext
@@ -77,8 +80,12 @@ internal class PythonServerRequiredPrecedeOptionalTest {
             @http(method: "POST", uri: "/opWithDefaults")
             operation OpWithDefaults {
                 input:= {
-                a: String,
-                b: String = "hi"
+                    a: String,
+                    b: String = "hi"
+                }
+                output:= {
+                    a: String,
+                    b: String = "hi"
                 }
             }
             """.asSmithyModel(smithyVersion = "2")
@@ -88,86 +95,125 @@ internal class PythonServerRequiredPrecedeOptionalTest {
 
         val writer = RustWriter.forModule("service")
         writer.unitTest("test_required_fields") {
-            rust(
+            fun createInstanceWithRequiredFieldsOnly(
+                module: String,
+                typeName: String,
+            ) = writable {
+                rustTemplate(
+                    """
+                    py.run(
+                        "data = $typeName(\"b\", \"d\")",
+                        Some(globals),
+                        Some(locals),
+                    ).unwrap();
+
+                    // Python should have been able to construct input.
+                    let data = locals
+                        .get_item("data")
+                        .expect("Python exception occurred during dictionary lookup")
+                        .unwrap()
+                        .extract::<$module::$typeName>()
+                        .unwrap();
+                    assert_eq!(data.b, "b");
+                    assert_eq!(data.d, "d");
+                    """,
+                )
+            }
+
+            fun createInstance(
+                module: String,
+                typeName: String,
+            ) = writable {
+                rustTemplate(
+                    """
+                    py.run(
+                        "data = $typeName(\"b\", \"d\", a = \"a\", c = \"c\")",
+                        Some(globals),
+                        Some(locals),
+                    ).unwrap();
+
+                    // Python should have been able to construct input.
+                    let data = locals
+                        .get_item("data")
+                        .expect("Python exception occurred during dictionary lookup")
+                        .unwrap()
+                        .extract::<$module::$typeName>()
+                        .unwrap();
+                    assert_eq!(data.b, "b");
+                    assert_eq!(data.d, "d");
+                    assert_eq!(data.a, Some("a".to_string()));
+                    assert_eq!(data.c, Some("c".to_string()));
+                    """,
+                )
+            }
+
+            fun createDefaultInstance(
+                module: String,
+                typeName: String,
+            ) = writable {
+                rustTemplate(
+                    """
+                    py.run(
+                        "data = $typeName(\"a\")",
+                        Some(globals),
+                        Some(locals),
+                    ).unwrap();
+
+                    // Python should have been able to construct input.
+                    let data = locals
+                        .get_item("data")
+                        .expect("Python exception occurred during dictionary lookup")
+                        .unwrap()
+                        .extract::<$module::$typeName>()
+                        .unwrap();
+                    assert_eq!(data.a, Some("a".to_string()));
+                    assert_eq!(data.b, "hi");
+                    """,
+                )
+            }
+
+            rustTemplate(
                 """
-                use crate::input;
-                use pyo3::{types::IntoPyDict, Python};
+                use crate::{input, output};
+                use #{pyo3}::{types::IntoPyDict, Python};
 
                 pyo3::prepare_freethreaded_python();
                 Python::with_gil(|py| {
                     let globals = [
-                        (
-                            "OpWithIncorrectOrderInput",
-                            py.get_type::<input::OpWithIncorrectOrderInput>(),
-                        ),
-                        (
-                            "OpWithCorrectOrderInput",
-                            py.get_type::<input::OpWithCorrectOrderInput>(),
-                        ),
-                        (
-                            "OpWithDefaultsInput",
-                            py.get_type::<input::OpWithDefaultsInput>(),
-                        )]
+                        ("OpWithIncorrectOrderInput", py.get_type::<input::OpWithIncorrectOrderInput>()),
+                        ("OpWithCorrectOrderInput", py.get_type::<input::OpWithCorrectOrderInput>()),
+                        ("OpWithDefaultsInput", py.get_type::<input::OpWithDefaultsInput>()),
+                        ("OpWithIncorrectOrderOutput", py.get_type::<output::OpWithIncorrectOrderOutput>()),
+                        ("OpWithCorrectOrderOutput", py.get_type::<output::OpWithCorrectOrderOutput>()),
+                        ("OpWithDefaultsOutput", py.get_type::<output::OpWithDefaultsOutput>())
+                        ]
                         .into_py_dict(py);
-                    let locals = [(
-                        "OpWithIncorrectOrderInput",
-                        py.get_type::<input::OpWithIncorrectOrderInput>(),
-                    )]
-                    .into_py_dict(py);
 
-                    py.run(
-                        "input = OpWithIncorrectOrderInput(\"b\", \"d\")",
-                        Some(globals),
-                        Some(locals),
-                    )
-                    .unwrap();
+                    let locals = [("OpWithIncorrectOrderInput", py.get_type::<input::OpWithIncorrectOrderInput>())].into_py_dict(py);
 
-                    // Python should have been able to construct input.
-                    let input = locals
-                        .get_item("input")
-                        .expect("Python exception occurred during dictionary lookup")
-                        .unwrap()
-                        .extract::<input::OpWithIncorrectOrderInput>()
-                        .unwrap();
-                    assert_eq!(input.b, "b");
-                    assert_eq!(input.d, "d");
-
-                    py.run(
-                        "input = OpWithCorrectOrderInput(\"b\", \"d\")",
-                        Some(globals),
-                        Some(locals),
-                    )
-                    .unwrap();
-
-                    // Python should have been able to construct input.
-                    let input = locals
-                        .get_item("input")
-                        .expect("Python exception occurred during dictionary lookup")
-                        .unwrap()
-                        .extract::<input::OpWithCorrectOrderInput>()
-                        .unwrap();
-                    assert_eq!(input.b, "b");
-                    assert_eq!(input.d, "d");
-
-                    py.run(
-                        "input = OpWithDefaultsInput(\"a\")",
-                        Some(globals),
-                        Some(locals),
-                    )
-                    .unwrap();
-
-                    // KanchaBilla
-                    // Python should have been able to construct input.
-                    let input = locals
-                        .get_item("input")
-                        .expect("Python exception occurred during dictionary lookup")
-                        .unwrap()
-                        .extract::<input::OpWithDefaultsInput>()
-                        .unwrap();
-                    assert_eq!(input.a, Some("a".to_string()));
-                    assert_eq!(input.b, "hi");
+                    #{IncorrectOrderInputRequiredOnly}
+                    #{CorrectOrderInputRequiredOnly}
+                    #{IncorrectOrderOutputRequiredOnly}
+                    #{CorrectOrderOutputRequiredOnly}
+                    #{IncorrectOrderInput}
+                    #{CorrectOrderInput}
+                    #{IncorrectOrderOutput}
+                    #{CorrectOrderOutput}
+                    #{DefaultsInput}
+                    #{DefaultsOutput}
                 });
                 """,
+                "pyo3" to PythonServerCargoDependency.PyO3.toDevDependency().toType(),
+                "IncorrectOrderInputRequiredOnly" to createInstanceWithRequiredFieldsOnly("input", "OpWithIncorrectOrderInput"),
+                "CorrectOrderInputRequiredOnly" to createInstanceWithRequiredFieldsOnly("input", "OpWithCorrectOrderInput"),
+                "IncorrectOrderOutputRequiredOnly" to createInstanceWithRequiredFieldsOnly("output", "OpWithIncorrectOrderOutput"),
+                "CorrectOrderOutputRequiredOnly" to createInstanceWithRequiredFieldsOnly("output", "OpWithCorrectOrderOutput"),
+                "IncorrectOrderInput" to createInstance("input", "OpWithIncorrectOrderInput"),
+                "CorrectOrderInput" to createInstance("input", "OpWithCorrectOrderInput"),
+                "IncorrectOrderOutput" to createInstance("output", "OpWithIncorrectOrderOutput"),
+                "CorrectOrderOutput" to createInstance("output", "OpWithCorrectOrderOutput"),
+                "DefaultsInput" to createDefaultInstance("input", "OpWithDefaultsInput"),
+                "DefaultsOutput" to createDefaultInstance("output", "OpWithDefaultsOutput"),
             )
         }
 
