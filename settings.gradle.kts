361d45b7de3b131b8924afaec7c815e9b7a0cf59
Aaron J Todd <todaaron@amazon.com>
2025-07-18 13:08:57 -0400

    fix GAV conflict and add runtime crates to publishing

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,10 @@ include(":codegen-server-test:python")
 include(":codegen-server-test:typescript")
 include(":rust-runtime")
 include(":aws:rust-runtime")
+// NOTE: we rename the aws/rust-runtime project so that it ends up with different GAV
+// coordinates otherwise it will conflict with the generic runtime which has coordinates
+// `software.amazon.smithy.rust:rust-runtime`.
+project(":aws:rust-runtime").name = "aws-rust-runtime"
 include(":aws:sdk")
 include(":aws:sdk-adhoc-test")
 include(":aws:sdk-codegen")
