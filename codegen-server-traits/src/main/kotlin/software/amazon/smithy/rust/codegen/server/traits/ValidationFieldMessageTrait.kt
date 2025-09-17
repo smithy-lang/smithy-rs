109e5fb8bce57952788894fb78ee99b866281b62
Jason Gin <jasongin88@gmail.com>
2025-09-17 16:08:06 -0400

    Add remaining custom validation exception traits
    Adds `ValidationFieldListTrait`, `ValidationFieldMessageTrait`, and
    `ValidationFieldNameTrait`

---
+++ codegen-server-traits/src/main/kotlin/software/amazon/smithy/rust/codegen/server/traits/ValidationFieldMessageTrait.kt
@@ -0,0 +1,26 @@
+package software.amazon.smithy.rust.codegen.server.traits
+
+import software.amazon.smithy.model.SourceLocation
+import software.amazon.smithy.model.node.Node
+import software.amazon.smithy.model.shapes.ShapeId
+import software.amazon.smithy.model.traits.AbstractTrait
+import software.amazon.smithy.model.traits.Trait
+
+class ValidationFieldMessageTrait(sourceLocation: SourceLocation) : AbstractTrait(ID, sourceLocation) {
+    companion object {
+        val ID: ShapeId = ShapeId.from("smithy.rust.codegen.server.traits#validationFieldMessage")
+    }
+
+    override fun createNode(): Node = Node.objectNode()
+
+    class Provider : AbstractTrait.Provider(ID) {
+        override fun createTrait(
+            target: ShapeId,
+            value: Node,
+        ): Trait {
+            val result = ValidationFieldMessageTrait(value.sourceLocation)
+            result.setNodeCache(value)
+            return result
+        }
+    }
+}
