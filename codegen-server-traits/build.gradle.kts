b2dac07ca6ebd7b2cf802b482a2c41582171784c
Jason Gin <jasongin88@gmail.com>
2025-09-29 13:56:42 -0400

    Add copyright label to top of files

--- codegen-server-traits/build.gradle.kts
+++ codegen-server-traits/build.gradle.kts
@@ -1,9 +1,9 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 /*
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     kotlin("jvm")
