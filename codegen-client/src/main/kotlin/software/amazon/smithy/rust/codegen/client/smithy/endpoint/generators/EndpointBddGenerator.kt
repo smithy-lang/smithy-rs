ce28867dd939151290c3b18fdd6ee7ce0de97b08
Landon James <lnj@amazon.com>
2025-10-18 22:35:55 -0700

    Basic ResolveEndpoint impl
    Still lots of missing pieces

--- codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/endpoint/generators/EndpointBddGenerator.kt
+++ codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/endpoint/generators/EndpointBddGenerator.kt
@@ -8,13 +8,14 @@ package software.amazon.smithy.rust.codegen.client.smithy.endpoint.generators
 import software.amazon.smithy.rulesengine.traits.EndpointBddTrait
 import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
 import software.amazon.smithy.rust.codegen.client.smithy.ClientRustModule
+import software.amazon.smithy.rust.codegen.client.smithy.endpoint.Types
 import software.amazon.smithy.rust.codegen.core.rustlang.InlineDependency
 import software.amazon.smithy.rust.codegen.core.rustlang.RustModule
 import software.amazon.smithy.rust.codegen.core.rustlang.RustWriter
 import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
 import software.amazon.smithy.rust.codegen.core.smithy.RuntimeType
 import software.amazon.smithy.rust.codegen.core.smithy.RuntimeType.Companion.preludeScope
-import software.amazon.smithy.rust.codegen.core.util.serviceNameOrDefault
+import software.amazon.smithy.rust.codegen.core.util.toPascalCase
 
 /**
  * Generator for BDD-based endpoint resolvers
@@ -24,7 +25,7 @@ class EndpointBddGenerator(
     private val bddTrait: EndpointBddTrait,
 ) {
     private val runtimeConfig = codegenContext.runtimeConfig
-    private val serviceName = codegenContext.serviceShape.serviceNameOrDefault("Unknown")
+    private val serviceName = codegenContext.serviceShape.id.name.toPascalCase()
     private val moduleUseName = codegenContext.moduleUseName()
 
     fun generateBddResolver(): RuntimeType {
@@ -34,8 +35,7 @@ class EndpointBddGenerator(
     }
 
     private fun generateBddResolverImpl(writer: RustWriter) {
-        val bdd = bddTrait.bdd
-        val nodes = generateNodeArray(bdd)
+        val nodes = generateNodeArray()
         val conditions = generateConditionArray()
         val results = generateResultArray()
 
@@ -47,36 +47,33 @@ class EndpointBddGenerator(
 
         writer.rustTemplate(
             """
-            use #{SmithyRuntimeApi}::client::endpoint::{EndpointFuture, ResolveEndpoint};
-            use #{SmithyRuntimeApi}::shared::IntoShared;
             use #{EndpointLib}::{evaluate_bdd, BddNode};
 
             $nodes
             $conditions
             $results
 
+            ##[derive(::std::fmt::Debug)]
             pub struct ${serviceName}BddResolver;
 
             impl ResolveEndpoint for ${serviceName}BddResolver {
-                fn resolve_endpoint<'a>(&'a self, params: &'a Params) -> EndpointFuture<'a> {
-                    #{Box}::pin(async move {
-                        let result = evaluate_bdd(
-                            &NODES,
-                            ${bdd.rootRef},
-                            params,
-                            &CONDITIONS,
-                            &RESULTS,
-                            |params, condition| {
-                                // TODO(bdd): Implement condition evaluation
-                                false
-                            }
-                        );
-
-                        match result {
-                            #{Some}(endpoint) => #{Ok}(endpoint.clone()),
-                            #{None} => #{Err}(#{Box}::new(#{EndpointError}::message("No endpoint rule matched"))),
+                fn resolve_endpoint<'a>(&'a self, params: &'a Params) -> #{EndpointFuture}<'a> {
+                    let result = evaluate_bdd(
+                        &NODES,
+                        ${bddTrait.bdd.rootRef},
+                        params,
+                        &CONDITIONS,
+                        &RESULTS,
+                        |_params, _condition| {
+                            // TODO(bdd): Implement condition evaluation
+                            false
                         }
-                    })
+                    );
+
+                    match result {
+                        #{Some}(endpoint) => #{EndpointFuture}::ready(#{Ok}(endpoint.clone())),
+                        #{None} => #{EndpointFuture}::ready(#{Err}(#{Box}::new(#{ResolveEndpointError}::message("No endpoint rule matched")))),
+                    }
                 }
             }
 
@@ -84,21 +81,23 @@ class EndpointBddGenerator(
                 pub fn new() -> Self {
                     Self
                 }
-
-                pub fn into_shared_resolver(self) -> #{SharedEndpointResolver} {
-                    #{SharedEndpointResolver}::new(self)
-                }
             }
             """,
             *preludeScope,
             "SmithyRuntimeApi" to RuntimeType.smithyRuntimeApiClient(runtimeConfig),
             "EndpointLib" to RuntimeType.forInlineDependency(endpointLib),
-            "SharedEndpointResolver" to RuntimeType.smithyRuntimeApiClient(runtimeConfig).resolve("client::endpoint::SharedEndpointResolver"),
-            "EndpointError" to RuntimeType.smithyRuntimeApiClient(runtimeConfig).resolve("client::endpoint::EndpointError"),
+            "SharedEndpointResolver" to
+                RuntimeType.smithyRuntimeApiClient(runtimeConfig)
+                    .resolve("client::endpoint::SharedEndpointResolver"),
+            "ResolveEndpointError" to
+                RuntimeType.smithyRuntimeApiClient(runtimeConfig)
+                    .resolve("client::endpoint::error::ResolveEndpointError"),
+            "EndpointFuture" to Types(runtimeConfig).endpointFuture,
         )
     }
 
-    private fun generateNodeArray(bdd: software.amazon.smithy.rulesengine.logic.bdd.Bdd): String {
+    private fun generateNodeArray(): String {
+        val bdd = bddTrait.bdd
         val nodes = mutableListOf<String>()
 
         bdd.getNodes { var_, high, low ->
