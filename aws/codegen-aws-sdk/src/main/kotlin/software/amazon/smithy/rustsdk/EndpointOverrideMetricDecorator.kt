286bfb8dcfe4d0e19698f35c702c1921a6c83537
vcjana <vcjana@amazon.com>
2025-11-24 15:44:57 -0800

    Generate endpoint override interceptor inline in config/endpoint.rs

--- aws/codegen-aws-sdk/src/main/kotlin/software/amazon/smithy/rustsdk/EndpointOverrideMetricDecorator.kt
+++ aws/codegen-aws-sdk/src/main/kotlin/software/amazon/smithy/rustsdk/EndpointOverrideMetricDecorator.kt
@@ -6,12 +6,15 @@
 package software.amazon.smithy.rustsdk
 
 import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
+import software.amazon.smithy.rust.codegen.client.smithy.ClientRustModule
 import software.amazon.smithy.rust.codegen.client.smithy.customize.ClientCodegenDecorator
 import software.amazon.smithy.rust.codegen.client.smithy.generators.ServiceRuntimePluginCustomization
 import software.amazon.smithy.rust.codegen.client.smithy.generators.ServiceRuntimePluginSection
 import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
 import software.amazon.smithy.rust.codegen.core.rustlang.writable
 import software.amazon.smithy.rust.codegen.core.smithy.RuntimeType
+import software.amazon.smithy.rust.codegen.core.smithy.RuntimeType.Companion.preludeScope
+import software.amazon.smithy.rust.codegen.core.smithy.RustCrate
 
 /**
  * Decorator that tracks endpoint override business metric when endpoint URL is configured.
@@ -20,28 +23,69 @@ class EndpointOverrideMetricDecorator : ClientCodegenDecorator {
     override val name: String = "EndpointOverrideMetric"
     override val order: Byte = 0
 
+    override fun extras(
+        codegenContext: ClientCodegenContext,
+        rustCrate: RustCrate,
+    ) {
+        // Generate the interceptor in the config::endpoint module
+        rustCrate.withModule(ClientRustModule.Config.endpoint) {
+            val runtimeConfig = codegenContext.runtimeConfig
+            val smithyRuntimeApi = RuntimeType.smithyRuntimeApiClient(runtimeConfig)
+            val smithyTypes = RuntimeType.smithyTypes(runtimeConfig)
+            val awsRuntime = AwsRuntimeType.awsRuntime(runtimeConfig)
+            val awsTypes = AwsRuntimeType.awsTypes(runtimeConfig)
+
+            rustTemplate(
+                """
+                /// Interceptor that tracks endpoint override business metric.
+                ##[derive(Debug, Default)]
+                pub(crate) struct EndpointOverrideFeatureTrackerInterceptor;
+
+                impl #{Intercept} for EndpointOverrideFeatureTrackerInterceptor {
+                    fn name(&self) -> &'static str {
+                        "EndpointOverrideFeatureTrackerInterceptor"
+                    }
+
+                    fn read_before_execution(
+                        &self,
+                        _context: &#{BeforeSerializationInterceptorContextRef}<'_>,
+                        cfg: &mut #{ConfigBag},
+                    ) -> #{Result}<(), #{BoxError}> {
+                        if cfg.load::<#{EndpointUrl}>().is_some() {
+                            cfg.interceptor_state()
+                                .store_append(#{AwsSdkFeature}::EndpointOverride);
+                        }
+                        #{Ok}(())
+                    }
+                }
+                """,
+                "Intercept" to smithyRuntimeApi.resolve("client::interceptors::Intercept"),
+                "BeforeSerializationInterceptorContextRef" to
+                    smithyRuntimeApi.resolve("client::interceptors::context::BeforeSerializationInterceptorContextRef"),
+                "ConfigBag" to smithyTypes.resolve("config_bag::ConfigBag"),
+                "BoxError" to smithyRuntimeApi.resolve("box_error::BoxError"),
+                "EndpointUrl" to awsTypes.resolve("endpoint_config::EndpointUrl"),
+                "AwsSdkFeature" to awsRuntime.resolve("sdk_feature::AwsSdkFeature"),
+                *preludeScope,
+            )
+        }
+    }
+
     override fun serviceRuntimePluginCustomizations(
         codegenContext: ClientCodegenContext,
         baseCustomizations: List<ServiceRuntimePluginCustomization>,
     ): List<ServiceRuntimePluginCustomization> =
-        baseCustomizations + listOf(EndpointOverrideFeatureTrackerInterceptor(codegenContext))
+        baseCustomizations + listOf(EndpointOverrideFeatureTrackerRegistration(codegenContext))
 }
 
-private class EndpointOverrideFeatureTrackerInterceptor(codegenContext: ClientCodegenContext) :
-    ServiceRuntimePluginCustomization() {
+private class EndpointOverrideFeatureTrackerRegistration(
+    private val codegenContext: ClientCodegenContext,
+) : ServiceRuntimePluginCustomization() {
     override fun section(section: ServiceRuntimePluginSection) =
         writable {
             if (section is ServiceRuntimePluginSection.RegisterRuntimeComponents) {
                 section.registerInterceptor(this) {
-                    rustTemplate(
-                        "#{Interceptor}",
-                        "Interceptor" to
-                            RuntimeType.forInlineDependency(
-                                InlineAwsDependency.forRustFile(
-                                    "endpoint_override",
-                                ),
-                            ).resolve("EndpointOverrideFeatureTrackerInterceptor"),
-                    )
+                    rustTemplate("crate::config::endpoint::EndpointOverrideFeatureTrackerInterceptor")
                 }
             }
         }
