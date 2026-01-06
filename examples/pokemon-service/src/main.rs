8a1d4181bec8f115657bd914b273a1c75baee111
Jason Gin <jasongin88@gmail.com>
2026-01-06 11:51:46 -0500

    initial commit

--- examples/pokemon-service/src/main.rs
+++ examples/pokemon-service/src/main.rs
@@ -8,19 +8,23 @@ mod plugin;
 
 use std::{net::SocketAddr, sync::Arc};
 
+use aws_smithy_http_server_metrics::layer::{DefaultMetrics, MetricsLayer, MetricsLayerBuilder};
 use clap::Parser;
+use metrique_writer::GlobalEntrySink;
 use pokemon_service_server_sdk::server::{
     extension::OperationExtensionExt,
     instrumentation::InstrumentExt,
     layer::alb_health_check::AlbHealthCheckLayer,
     plugin::{HttpPlugins, ModelPlugins, Scoped},
     request::request_id::ServerRequestIdProviderLayer,
+    routing::IntoMakeServiceWithConnectInfo,
     AddExtensionLayer,
 };
 
 use hyper::StatusCode;
 use plugin::PrintExt;
 
+use metrique::{emf::Emf, writer::{sink::AttachHandle, AttachGlobalEntrySinkExt, FormatExt}, ServiceMetrics};
 use pokemon_service::{
     do_nothing_but_log_request_ids, get_storage_with_local_approved, DEFAULT_ADDRESS, DEFAULT_PORT,
 };
@@ -29,6 +33,8 @@ use pokemon_service_common::{
     stream_pokemon_radio, State,
 };
 use pokemon_service_server_sdk::{scope, PokemonService, PokemonServiceConfig};
+use tower::Layer;
+use tracing_appender::rolling::{RollingFileAppender, Rotation};
 
 use crate::authz::AuthorizationPlugin;
 
@@ -47,6 +53,8 @@ struct Args {
 pub async fn main() {
     let args = Args::parse();
     setup_tracing();
+
+    let _metrics_handle = setup_metrics();
 
     scope! {
         /// A scope containing `GetPokemonSpecies` and `GetStorage`.
@@ -74,10 +82,6 @@ pub async fn main() {
     let config = PokemonServiceConfig::builder()
         // Set up shared state and middlewares.
         .layer(AddExtensionLayer::new(Arc::new(State::default())))
-        // Handle `/ping` health check requests.
-        .layer(AlbHealthCheckLayer::from_handler("/ping", |_req| async {
-            StatusCode::OK
-        }))
         // Add server request IDs.
         .layer(ServerRequestIdProviderLayer::new())
         .http_plugin(http_plugins)
@@ -92,15 +96,24 @@ pub async fn main() {
         .get_storage(get_storage_with_local_approved)
         .get_server_statistics(get_server_statistics)
         .capture_pokemon(capture_pokemon)
-        .do_nothing(do_nothing_but_log_request_ids)
         .check_health(check_health)
+        .do_nothing(do_nothing_but_log_request_ids)
         .stream_pokemon_radio(stream_pokemon_radio)
         .build()
         .expect("failed to build an instance of PokemonService");
 
-    // Using `into_make_service_with_connect_info`, rather than `into_make_service`, to adjoin the `SocketAddr`
+    // layer the health check outside the application, to avoid requiring a Smithy model for the health check route
+    let health_check_layer =
+        AlbHealthCheckLayer::from_handler("/ping", |_req| async { StatusCode::OK });
+    let service = health_check_layer.layer(app);
+
+    let metrics_layer = MetricsLayer::new();
+
+    let service = metrics_layer.layer(service);
+
+    // Using `IntoMakeServiceWithConnectInfo`, rather than `into_make_service`, to adjoin the `SocketAddr`
     // connection info.
-    let make_app = app.into_make_service_with_connect_info::<SocketAddr>();
+    let make_app = IntoMakeServiceWithConnectInfo::<_, SocketAddr>::new(service);
 
     // Bind the application to a socket.
     let bind: SocketAddr = format!("{}:{}", args.address, args.port)
@@ -108,8 +121,25 @@ pub async fn main() {
         .expect("unable to parse the server bind address and port");
     let server = hyper::Server::bind(&bind).serve(make_app);
 
+    println!("Listening on {}", bind);
+
     // Run forever-ish...
     if let Err(err) = server.await {
         eprintln!("server error: {err}");
     }
+}
+
+pub(crate) fn setup_metrics() -> AttachHandle {
+    let service_log_dir = "logs";
+    let service_log_name = "pokemon_service_metrics";
+
+    let emf = Emf::builder("Ns".to_string(), vec![vec![]])
+        .build()
+        .output_to_makewriter(RollingFileAppender::new(
+            Rotation::MINUTELY,
+            &service_log_dir,
+            &service_log_name,
+        ));
+
+    ServiceMetrics::attach_to_stream(emf)
 }
---
+++ rust-runtime/aws-smithy-http-server-metrics/src/lib.rs
@@ -0,0 +1,138 @@
+#![allow(missing_docs)]
+
+use crate::layer::MetricsLayer;
+
+pub mod layer;
+pub mod service;
+
+// struct MyMetrics {}
+
+// impl<Init, Req, Res, ReqBody, ResBody, CE, ES> Default
+//     for MetricsLayer<Init, Req, Res, ReqBody, ResBody, CE, ES>
+// where
+//     Init: Fn() -> AppendAndCloseOnDrop<CE, ES> + Clone,
+//     CE: CloseEntry,
+//     ES: EntrySink<RootEntry<CE::Closed>>,
+//     Req: for<'a, 'b> Fn(&'a Request<ReqBody>, &'b mut AppendAndCloseOnDrop<CE, ES>) + Clone,
+//     Res: for<'a, 'b> Fn(&'a Response<ResBody>, &'b mut AppendAndCloseOnDrop<CE, ES>) + Clone,
+// {
+//     fn default() -> Self {
+//         RequestMetricsLayerBuilder::default().build()
+//     }
+// }
+
+
+// #[derive(Debug)]
+// pub struct MetricsPlugin {
+//     pub(crate) with_default_request_metrics: bool,
+//     pub(crate) with_operation_name_metric: bool,
+//     pub(crate) with_service_name_metric: bool,
+//     pub(crate) with_service_version_metric: bool,
+// }
+
+// impl HttpMarker for MetricsPlugin {}
+
+// impl<Ser, Op, T> Plugin<Ser, Op, T> for MetricsPlugin
+// where
+//     Op: OperationShape,
+//     Ser: ServiceShape,
+// {
+//     type Output = MetricsOperationService<T>;
+
+//     fn apply(&self, inner: T) -> Self::Output {
+//         if !self.with_default_request_metrics {
+//             return MetricsOperationService {
+//                 inner,
+//                 operation_name: None,
+//                 service_name: None,
+//                 service_version: None,
+//             };
+//         }
+
+//         let service_name = self.with_service_name_metric.then(|| Ser::ID.name());
+//         let service_version = self
+//             .with_service_version_metric
+//             .then(|| Ser::VERSION)
+//             .flatten();
+//         let operation_name = self.with_operation_name_metric.then(|| Op::ID.name());
+
+//         MetricsOperationService {
+//             inner,
+//             operation_name: operation_name,
+//             service_name: service_name,
+//             service_version: service_version,
+//         }
+//     }
+// }
+
+// #[derive(Debug)]
+// pub struct MetricsOperationService<T> {
+//     inner: T,
+//     operation_name: Option<&'static str>,
+//     service_name: Option<&'static str>,
+//     service_version: Option<&'static str>,
+// }
+
+// impl<T> Clone for MetricsOperationService<T>
+// where
+//     T: Clone,
+// {
+//     fn clone(&self) -> Self {
+//         Self {
+//             inner: self.inner.clone(),
+//             operation_name: self.operation_name,
+//             service_name: self.service_name,
+//             service_version: self.service_version,
+//         }
+//     }
+// }
+
+// impl<T, ReqBody, ResBody> Service<Request<ReqBody>> for MetricsOperationService<T>
+// where
+//     T: Service<Request<ReqBody>, Response = Response<ResBody>>,
+//     T::Future: Send + 'static,
+// {
+//     type Response = T::Response;
+//     type Error = T::Error;
+//     type Future = std::pin::Pin<
+//         Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>,
+//     >;
+
+//     fn call(&mut self, mut request: Request<ReqBody>) -> Self::Future {
+//         let request_metrics = request
+//             .extensions_mut()
+//             .get_mut::<SlotGuard<DefaultRequestMetrics>>();
+
+//         if let Some(request_metrics) = request_metrics {
+//             request_metrics.operation_name = self.operation_name.map(|n| n.to_string());
+//             request_metrics.service_name = self.service_name.map(|n| n.to_string());
+//             request_metrics.service_version = self.service_version.map(|n| n.to_string());
+//         }
+
+//         self.inner.call(request).boxed()
+
+//         // TODO, have a fallback where if request metrics is not in the extensions, initialize here and do the same logic
+//         // That way, if users want to ignore the layer, they can only use the http plugin. We'll have some helper methods to
+//         // dedup / maintain parity, because we need it to be compatible both ways in case someone who only has the plugin
+//         // suddenly needs to switch over to using the outer layer middleware to fold their metrics.
+//     }
+
+//     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+//         self.inner.poll_ready(cx)
+//     }
+// }
+
+// fn try_extract_request_id<B>(request: &Request<B>) -> Option<String> {
+//     request
+//         .headers()
+//         .get("x-amzn-requestid")?
+//         .to_str()
+//         .inspect_err(|error| {
+//             tracing::warn!(
+//                 "x-amzn-requestid header was not representable as string: {:?}",
+//                 error
+//             );
+//         })
+//         .ok()
+//         .map(Into::into)
+// }
