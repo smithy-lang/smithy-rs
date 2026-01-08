42d0185c044831749ede5cfe51b4bbf321df92a6
Jason Gin <jasongin88@gmail.com>
2026-01-08 17:33:19 -0500

    Use generic implementation of closures

--- rust-runtime/aws-smithy-http-server-metrics/src/layer/inner.rs
+++ rust-runtime/aws-smithy-http-server-metrics/src/layer/inner.rs
@@ -19,8 +19,10 @@ where
     S: EntrySink<RootEntry<E::Closed>> + Send + Sync + 'static,
 {
     pub(crate) init_metrics: I,
-    pub(crate) set_default_request_metrics: Option<Rq>,
-    pub(crate) set_default_response_metrics: Option<Rs>,
+    pub(crate) set_default_request_metrics:
+        Option<fn(&mut Request<ReqBody>, &mut AppendAndCloseOnDrop<E, S>)>,
+    pub(crate) set_default_response_metrics:
+        Option<fn(&Response<ResBody>, &mut AppendAndCloseOnDrop<E, S>)>,
     pub(crate) set_request_metrics: Option<Rq>,
     pub(crate) set_response_metrics: Option<Rs>,
 }
@@ -37,8 +39,8 @@ where
 
     fn layer(&self, inner: Ser) -> Self::Service {
         MetricsLayerService::builder(inner, self.init_metrics.clone())
-            .set_default_request_metrics(self.set_default_request_metrics.clone())
-            .set_default_response_metrics(self.set_default_response_metrics.clone())
+            .set_default_request_metrics(self.set_default_request_metrics)
+            .set_default_response_metrics(self.set_default_response_metrics)
             .set_request_metrics(self.set_request_metrics.clone())
             .set_response_metrics(self.set_response_metrics.clone())
             .build()
