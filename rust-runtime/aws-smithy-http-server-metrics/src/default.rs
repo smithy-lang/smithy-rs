ea47996aab04f1c4738f6cc44e4226865e67d655
Jason Gin <jasongin88@gmail.com>
2026-01-13 18:43:57 -0500

    Add copyright headers, license file, docs.rs metadata

--- rust-runtime/aws-smithy-http-server-metrics/src/default.rs
+++ rust-runtime/aws-smithy-http-server-metrics/src/default.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 use std::fmt::Debug;
 
 use metrique::unit_of_work::metrics;
