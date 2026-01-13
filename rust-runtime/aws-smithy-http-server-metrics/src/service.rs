892a9b37db468461ec05567f792a23b842dfc956
Jason Gin <jasongin88@gmail.com>
2026-01-13 18:43:57 -0500

    Add copyright headers, license file, docs.rs metadata

--- rust-runtime/aws-smithy-http-server-metrics/src/service.rs
+++ rust-runtime/aws-smithy-http-server-metrics/src/service.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 use std::future::Future;
 use std::task::Context;
 use std::task::Poll;
--- rust-runtime/aws-smithy-http-server-metrics/src/types.rs
+++ rust-runtime/aws-smithy-http-server-metrics/src/types.rs
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 use aws_smithy_http_server::error::Error;
 use http::Request;
 use http::Response;
