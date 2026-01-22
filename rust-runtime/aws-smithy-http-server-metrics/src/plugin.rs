3e0d80dcbe1a8720a62b394b7c68cc49233f25d1
Jason Gin <jasongin88@gmail.com>
2026-01-22 14:58:03 -0500

    Use named futures

--- rust-runtime/aws-smithy-http-server-metrics/src/plugin.rs
+++ rust-runtime/aws-smithy-http-server-metrics/src/plugin.rs
@@ -4,6 +4,7 @@
  */
 
 use std::future::Future;
+use std::pin::Pin;
 use std::task::Context;
 use std::task::Poll;
 
@@ -11,13 +12,13 @@ use aws_smithy_http_server::operation::OperationShape;
 use aws_smithy_http_server::plugin::HttpMarker;
 use aws_smithy_http_server::plugin::Plugin;
 use aws_smithy_http_server::service::ServiceShape;
-use futures::FutureExt;
 use http::Request;
 use http::Response;
 use metrique::OnParentDrop;
 use metrique::ServiceMetrics;
 use metrique::Slot;
 use metrique_writer::AttachGlobalEntrySink;
+use pin_project_lite::pin_project;
 use tower::Service;
 
 use crate::default::DefaultMetrics;
@@ -29,6 +30,103 @@ use crate::default::DefaultResponseMetricsConfig;
 use crate::default::DefaultResponseMetricsExtension;
 use crate::types::ReqBody;
 use crate::types::ResBody;
+
+pin_project! {
+    /// Named future to avoid heap allocation
+    ///
+    /// `inner` types has `#[pin]` to prevent moving in memory once polled.
+    #[project = DefaultMetricsFutureProj]
+    pub enum DefaultMetricsFuture<F> {
+        /// Metrics extension from outer layer
+        WithExtension {
+            #[pin]
+            inner: F,
+        },
+        /// No outer metrics layer, but sink available
+        WithMetrics {
+            #[pin]
+            inner: F,
+            metrics: metrique::AppendAndCloseOnDrop<DefaultMetrics, metrique_writer::BoxEntrySink>,
+        },
+        /// No sink available
+        Passthrough {
+            #[pin]
+            inner: F,
+        },
+    }
+}
+
+impl<F, Err> Future for DefaultMetricsFuture<F>
+where
+    F: Future<Output = Result<Response<ResBody>, Err>>,
+{
+    type Output = Result<Response<ResBody>, Err>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.project() {
+            DefaultMetricsFutureProj::WithExtension { inner } => match inner.poll(cx) {
+                Poll::Ready(Ok(mut res)) => {
+                    let default_response_metrics = get_default_response_metrics(&res);
+
+                    let maybe_default_res_metrics_ext =
+                        res.extensions_mut()
+                            .get_mut::<DefaultResponseMetricsExtension>();
+
+                    if let Some(default_res_metrics_ext) = maybe_default_res_metrics_ext {
+                        *default_res_metrics_ext.metrics = apply_default_response_metrics_config(
+                            default_response_metrics,
+                            &default_res_metrics_ext.config,
+                        );
+                    }
+
+                    res.extensions_mut()
+                        .remove::<DefaultResponseMetricsExtension>();
+
+                    Poll::Ready(Ok(res))
+                }
+                Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
+                Poll::Pending => Poll::Pending,
+            },
+            DefaultMetricsFutureProj::WithMetrics { inner, metrics } => match inner.poll(cx) {
+                Poll::Ready(Ok(res)) => {
+                    metrics.default_response_metrics =
+                        Some(Slot::new(get_default_response_metrics(&res)));
+                    metrics.default_response_metrics
+                            .as_mut()
+                            .expect("unreachable: the option is set to some in this scope")
+                            .open(OnParentDrop::Discard)
+                            .expect("unreachable: the slot was created in this scope and is not opened before this point");
+
+                    Poll::Ready(Ok(res))
+                }
+                Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
+                Poll::Pending => Poll::Pending,
+            },
+            DefaultMetricsFutureProj::Passthrough { inner } => inner.poll(cx),
+        }
+    }
+}
+
+fn get_default_response_metrics(res: &Response<ResBody>) -> DefaultResponseMetrics {
+    DefaultResponseMetrics {
+        http_status_code: Some(res.status().to_string()),
+    }
+}
+
+fn apply_default_response_metrics_config(
+    metrics: DefaultResponseMetrics,
+    config: &DefaultResponseMetricsConfig,
+) -> DefaultResponseMetrics {
+    if config.disable_all {
+        return DefaultResponseMetrics::default();
+    }
+
+    DefaultResponseMetrics {
+        http_status_code: metrics
+            .http_status_code
+            .filter(|_| !config.disable_http_status_code),
+    }
+}
 
 #[derive(Default)]
 pub struct DefaultMetricsPlugin;
@@ -87,12 +185,6 @@ where
         }
     }
 
-    fn get_default_response_metrics(res: &Response<ResBody>) -> DefaultResponseMetrics {
-        DefaultResponseMetrics {
-            http_status_code: Some(res.status().to_string()),
-        }
-    }
-
     fn apply_default_request_metrics_config(
         metrics: DefaultRequestMetrics,
         config: &DefaultRequestMetricsConfig,
@@ -114,21 +206,6 @@ where
             request_id: metrics.request_id.filter(|_| !config.disable_request_id),
         }
     }
-
-    fn apply_default_response_metrics_config(
-        metrics: DefaultResponseMetrics,
-        config: &DefaultResponseMetricsConfig,
-    ) -> DefaultResponseMetrics {
-        if config.disable_all {
-            return DefaultResponseMetrics::default();
-        }
-
-        DefaultResponseMetrics {
-            http_status_code: metrics
-                .http_status_code
-                .filter(|_| !config.disable_http_status_code),
-        }
-    }
 }
 
 impl<Ser> Service<Request<ReqBody>> for DefaultMetricsPluginService<Ser>
@@ -138,9 +215,7 @@ where
 {
     type Response = Ser::Response;
     type Error = Ser::Error;
-    type Future = std::pin::Pin<
-        Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>,
-    >;
+    type Future = DefaultMetricsFuture<Ser::Future>;
 
     fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
         let default_request_metrics = self.get_default_request_metrics(&req);
@@ -159,38 +234,17 @@ where
                 req.extensions_mut()
                     .remove::<DefaultRequestMetricsExtension>();
 
-                let future = self.inner.call(req);
-                futures::FutureExt::boxed(async move {
-                    let mut res = match future.await {
-                        Ok(res) => res,
-                        Err(e) => return Err(e),
-                    };
-
-                    let default_response_metrics = Self::get_default_response_metrics(&res);
-
-                    let maybe_default_res_metrics_ext =
-                        res.extensions_mut()
-                            .get_mut::<DefaultResponseMetricsExtension>();
-
-                    if let Some(default_res_metrics_ext) = maybe_default_res_metrics_ext {
-                        *default_res_metrics_ext.metrics =
-                            Self::apply_default_response_metrics_config(
-                                default_response_metrics,
-                                &default_res_metrics_ext.config,
-                            );
-                    }
-
-                    res.extensions_mut()
-                        .remove::<DefaultResponseMetricsExtension>();
-
-                    Ok(res)
-                })
+                DefaultMetricsFuture::WithExtension {
+                    inner: self.inner.call(req),
+                }
             }
             None => {
                 // When no outer layer exists, provide the default metrics through metrique's
                 // application-wide global metric sink, if an underlying sink has been attached
                 let Some(sink) = ServiceMetrics::try_sink() else {
-                    return self.inner.call(req).boxed();
+                    return DefaultMetricsFuture::Passthrough {
+                        inner: self.inner.call(req),
+                    };
                 };
 
                 let mut metrics = DefaultMetrics::default().append_on_drop(sink);
@@ -203,23 +257,10 @@ where
                     .open(OnParentDrop::Discard)
                     .expect("unreachable: the slot was created in this scope and is not opened before this point");
 
-                let future = self.inner.call(req);
-                futures::FutureExt::boxed(async move {
-                    let res = match future.await {
-                        Ok(res) => res,
-                        Err(e) => return Err(e),
-                    };
-
-                    metrics.default_response_metrics =
-                        Some(Slot::new(Self::get_default_response_metrics(&res)));
-                    metrics.default_response_metrics
-                        .as_mut()
-                        .expect("unreachable: the option is set to some in this scope")
-                        .open(OnParentDrop::Discard)
-                        .expect("unreachable: the slot was created in this scope and is not opened before this point");
-
-                    Ok(res)
-                })
+                DefaultMetricsFuture::WithMetrics {
+                    inner: self.inner.call(req),
+                    metrics,
+                }
             }
         }
     }
--- rust-runtime/aws-smithy-http-server-metrics/src/service.rs
+++ rust-runtime/aws-smithy-http-server-metrics/src/service.rs
@@ -5,6 +5,7 @@
 
 use std::future::Future;
 use std::marker::PhantomData;
+use std::pin::Pin;
 use std::task::Context;
 use std::task::Poll;
 
@@ -14,6 +15,7 @@ use http::Response;
 use http_body::combinators::UnsyncBoxBody;
 use hyper::body::Body as ReqBody;
 use hyper::body::Bytes;
+use pin_project_lite::pin_project;
 use tower::Service;
 
 use crate::default::DefaultRequestMetricsConfig;
@@ -25,6 +27,59 @@ use crate::traits::RequestMetrics;
 use crate::traits::ResponseMetrics;
 
 type ResBody = UnsyncBoxBody<Bytes, Error>;
+
+pin_project! {
+    /// Named future to avoid heap allocation
+    ///
+    /// `inner` has `#[pin]` to prevent moving in memory once polled.
+    pub struct MetricsLayerServiceFuture<F, E, S, Rs>
+    where
+        F: Future,
+        E: MetriqueCloseEntry,
+        S: MetriqueEntrySink<E>,
+        Rs: ResponseMetrics<E>,
+    {
+        #[pin]
+        inner: F,
+        metrics: metrique::AppendAndCloseOnDrop<E, S>,
+        default_res_metrics_extension_fn: fn(&mut Response<ResBody>, &mut E, DefaultResponseMetricsConfig),
+        default_res_metrics_config: DefaultResponseMetricsConfig,
+        response_metrics: Option<Rs>,
+    }
+}
+
+impl<F, E, S, Rs, Err> Future for MetricsLayerServiceFuture<F, E, S, Rs>
+where
+    F: Future<Output = Result<Response<ResBody>, Err>>,
+    E: MetriqueCloseEntry,
+    S: MetriqueEntrySink<E>,
+    Rs: ResponseMetrics<E>,
+{
+    type Output = Result<Response<ResBody>, Err>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // for safely accessing the pinned inner future
+        let this = self.project();
+
+        match this.inner.poll(cx) {
+            Poll::Ready(Ok(mut res)) => {
+                (this.default_res_metrics_extension_fn)(
+                    &mut res,
+                    this.metrics,
+                    this.default_res_metrics_config.clone(),
+                );
+
+                if let Some(response_metrics) = this.response_metrics {
+                    (response_metrics)(&mut res, this.metrics);
+                }
+
+                Poll::Ready(Ok(res))
+            }
+            Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
 
 pub struct MetricsLayerService<Ser, E, S, I, Rq, Rs>
 where
@@ -83,9 +138,7 @@ where
 {
     type Response = Ser::Response;
     type Error = Ser::Error;
-    type Future = std::pin::Pin<
-        Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>,
-    >;
+    type Future = MetricsLayerServiceFuture<Ser::Future, E, S, Rs>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         self.inner.poll_ready(cx)
@@ -104,24 +157,13 @@ where
             (request_metrics)(&mut req, &mut metrics);
         }
 
-        let future = self.inner.call(req);
-        let default_res_metrics_extension_fn = self.default_res_metrics_extension_fn;
-        let default_res_metrics_config = self.default_res_metrics_config.clone();
-        let response_metrics = self.response_metrics.clone();
-
-        futures::FutureExt::boxed(async move {
-            let mut res = match future.await {
-                Ok(res) => res,
-                Err(e) => return Err(e),
-            };
-
-            (default_res_metrics_extension_fn)(&mut res, &mut metrics, default_res_metrics_config);
-
-            if let Some(response_metrics) = &response_metrics {
-                (response_metrics)(&mut res, &mut metrics);
-            }
-
-            Ok(res)
-        })
+        // Return named future type instead of boxing - zero heap allocation
+        MetricsLayerServiceFuture {
+            inner: self.inner.call(req),
+            metrics,
+            default_res_metrics_extension_fn: self.default_res_metrics_extension_fn,
+            default_res_metrics_config: self.default_res_metrics_config.clone(),
+            response_metrics: self.response_metrics.clone(),
+        }
     }
 }
