e55b535c8bd02ee496a2cecfefc1307dea2d4790
Avery Townsend <codeshaunted@gmail.com>
2026-03-24 17:51:24 -0400

    Add public traits for custom implementations of Env and Fs (#4570)
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here -->
    We are implementing a virtual machine with custom implementations of
    HTTP requests, environment variables, and filesystem I/O, and need
    `aws_config` to gather credentials using these custom implementations.
    For HTTP requests, custom clients are already supported. But, for
    environment variables and file I/O, we need a way to delegate to our VM
    and currently this isn't possible without patches to `aws-sdk-rust`.
    
    ## Description
    <!--- Describe your changes in detail -->
    This change creates public traits `Env` and `Fs` for environment
    variables and file I/O respectively, and adds them to `ProviderConfig`,
    set with `with_env` and `with_fs` (similar to how you'd use the
    `http_client` method). Shared wrappers for these traits, `SharedEnv` and
    `SharedFs` were also created (similar to `SharedHttpClient`, etc). This
    allows end users of `aws-sdk-rust` to pass in their own implementations
    of environment variables and file I/O.
    
    ## Open Questions
    - Should `os_shim_internal` be renamed? This change would make it
    largely public.
    - Is the approach taken with the async functions on the `Env` and `Fs`
    traits acceptable? (i.e. inlining the Box<dyn Future> type in the
    function signature. Traits like `HttpConnector` use the
    `new_type_future!` macro in `aws_smithy_runtime`, but this macro is not
    exported)
    
    ## Testing
    <!--- Please describe in detail how you tested your changes -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    Tested with existing Rust tests, as well as slightly reworked existing
    tests for `os_shim_internal`. Tested on Linux using JDK 17. Ran all
    `cargoTests` in gradle, but had trouble with getting doc-building
    specifically to run on my machine when running `sdkTests`.
    
    ## Checklist
    <!--- If a checkbox below is not applicable, then please DELETE it
    rather than leaving it unchecked -->
    - [x] For changes to the AWS SDK, generated SDK code, or SDK runtime
    crates, I have created a changelog entry Markdown file in the
    `.changelog` directory, specifying "aws-sdk-rust" in the `applies_to`
    key.
    
    ----
    
    _By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice._

--- aws/rust-runtime/aws-config/src/lib.rs
+++ aws/rust-runtime/aws-config/src/lib.rs
@@ -538,6 +538,24 @@ mod loader {
             self
         }
 
+        /// Override the environment variable abstraction used during config resolution.
+        ///
+        /// This can be used with [`Env::from_custom`] to provide a custom environment
+        /// variable backend (e.g., remote config stores, vaults).
+        pub fn env(mut self, env: Env) -> Self {
+            self.env = Some(env);
+            self
+        }
+
+        /// Override the filesystem abstraction used during config resolution.
+        ///
+        /// This can be used with [`Fs::from_custom`] to provide a custom filesystem
+        /// backend (e.g., in-memory stores, encrypted filesystems).
+        pub fn fs(mut self, fs: Fs) -> Self {
+            self.fs = Some(fs);
+            self
+        }
+
         /// Override the access token provider used to build [`SdkConfig`].
         ///
         /// # Examples
@@ -1019,19 +1037,6 @@ mod loader {
             builder.set_account_id_endpoint_mode(account_id_endpoint_mode);
             builder.set_auth_scheme_preference(auth_scheme_preference);
             builder.build()
-        }
-    }
-
-    #[cfg(test)]
-    impl ConfigLoader {
-        pub(crate) fn env(mut self, env: Env) -> Self {
-            self.env = Some(env);
-            self
-        }
-
-        pub(crate) fn fs(mut self, fs: Fs) -> Self {
-            self.fs = Some(fs);
-            self
         }
     }
 
