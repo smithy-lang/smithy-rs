79c78e50972256888c2ce2db0e41165295e7ecaf
Landon James <lnj@amazon.com>
2026-03-25 14:44:41 -0700

    Add SharedClientProtocol to operation.config() layer

--- codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customizations/SchemaDecorator.kt
+++ codegen-client/src/main/kotlin/software/amazon/smithy/rust/codegen/client/smithy/customizations/SchemaDecorator.kt
@@ -8,9 +8,12 @@ package software.amazon.smithy.rust.codegen.client.smithy.customizations
 import software.amazon.smithy.aws.traits.protocols.AwsJson1_0Trait
 import software.amazon.smithy.aws.traits.protocols.AwsJson1_1Trait
 import software.amazon.smithy.aws.traits.protocols.RestJson1Trait
+import software.amazon.smithy.model.shapes.OperationShape
 import software.amazon.smithy.model.shapes.ShapeId
 import software.amazon.smithy.rust.codegen.client.smithy.ClientCodegenContext
 import software.amazon.smithy.rust.codegen.client.smithy.customize.ClientCodegenDecorator
+import software.amazon.smithy.rust.codegen.client.smithy.generators.OperationCustomization
+import software.amazon.smithy.rust.codegen.client.smithy.generators.OperationSection
 import software.amazon.smithy.rust.codegen.client.smithy.generators.ServiceRuntimePluginCustomization
 import software.amazon.smithy.rust.codegen.client.smithy.generators.ServiceRuntimePluginSection
 import software.amazon.smithy.rust.codegen.client.smithy.generators.config.ConfigCustomization
@@ -82,6 +85,17 @@ class SchemaDecorator : ClientCodegenDecorator {
             baseCustomizations
         }
 
+    override fun operationCustomizations(
+        codegenContext: ClientCodegenContext,
+        operation: OperationShape,
+        baseCustomizations: List<OperationCustomization>,
+    ): List<OperationCustomization> =
+        if (SchemaSerdeAllowlist.usesSchemaSerdeExclusively(codegenContext)) {
+            baseCustomizations + SchemaOperationProtocolCustomization(codegenContext)
+        } else {
+            baseCustomizations
+        }
+
     override fun configCustomizations(
         codegenContext: ClientCodegenContext,
         baseCustomizations: List<ConfigCustomization>,
@@ -118,6 +132,49 @@ private class SchemaProtocolCustomization(
                             protocol == AwsJson1_1Trait.ID ->
                                 smithyJson.resolve("protocol::aws_json_rpc::AwsJsonRpcProtocol") to "aws_json_1_1(${serviceShapeName.dq()})"
                             else -> return@writable // Other protocols not yet implemented
+                        }
+
+                    rustTemplate(
+                        """
+                        ${section.newLayerName}.store_put(
+                            #{SharedClientProtocol}::new(#{ProtocolType}::$constructor)
+                        );
+                        """,
+                        "SharedClientProtocol" to smithySchema.resolve("protocol::SharedClientProtocol"),
+                        "ProtocolType" to protocolType,
+                    )
+                }
+                else -> {}
+            }
+        }
+}
+
+/**
+ * Stores the [SharedClientProtocol] in each operation's runtime plugin config layer,
+ * so that the serializer and deserializer can access it without requiring the full
+ * service config stack (e.g. in protocol tests and benchmarks).
+ */
+private class SchemaOperationProtocolCustomization(
+    private val codegenContext: ClientCodegenContext,
+) : OperationCustomization() {
+    override fun section(section: OperationSection) =
+        writable {
+            when (section) {
+                is OperationSection.AdditionalRuntimePluginConfig -> {
+                    val smithyJson = CargoDependency.smithyJson(codegenContext.runtimeConfig).toType()
+                    val smithySchema = RuntimeType.smithySchema(codegenContext.runtimeConfig)
+                    val protocol = codegenContext.protocol
+                    val serviceShapeName = codegenContext.serviceShape.id.name
+
+                    val (protocolType, constructor) =
+                        when {
+                            protocol == RestJson1Trait.ID ->
+                                smithyJson.resolve("protocol::aws_rest_json_1::AwsRestJsonProtocol") to "new()"
+                            protocol == AwsJson1_0Trait.ID ->
+                                smithyJson.resolve("protocol::aws_json_rpc::AwsJsonRpcProtocol") to "aws_json_1_0(${serviceShapeName.dq()})"
+                            protocol == AwsJson1_1Trait.ID ->
+                                smithyJson.resolve("protocol::aws_json_rpc::AwsJsonRpcProtocol") to "aws_json_1_1(${serviceShapeName.dq()})"
+                            else -> return@writable
                         }
 
                     rustTemplate(
