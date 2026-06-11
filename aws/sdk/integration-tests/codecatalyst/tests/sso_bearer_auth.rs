20a7e6a9837c8e689aab95ea2b2f647fb6ae65fb
ysaito1001 <awsaito@amazon.com>
2026-06-11 11:01:03 -0500

    Revise fix for `sso_bearer_auth`

--- aws/sdk/integration-tests/codecatalyst/tests/sso_bearer_auth.rs
+++ aws/sdk/integration-tests/codecatalyst/tests/sso_bearer_auth.rs
@@ -29,11 +29,5 @@ async fn sso_bearer_auth() {
     let item = &response.items.unwrap()[0];
     assert_eq!("somespacename", item.name);
 
-    replay
-        .validate_body_and_headers_except(
-            &["x-amz-user-agent", "user-agent", "authorization"],
-            "application/json",
-        )
-        .await
-        .unwrap();
+    replay.relaxed_validate("application/json").await.unwrap();
 }
