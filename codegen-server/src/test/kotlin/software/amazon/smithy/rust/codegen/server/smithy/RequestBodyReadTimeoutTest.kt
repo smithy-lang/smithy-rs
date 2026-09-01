2bb4d6594335a88f707a6aad24052206d47671c3
Fahad Zubair <fahadzub@amazon.com>
2026-09-01 14:56:15 +0000

    Add actual server timeout test

--- codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/RequestBodyReadTimeoutTest.kt
+++ codegen-server/src/test/kotlin/software/amazon/smithy/rust/codegen/server/smithy/RequestBodyReadTimeoutTest.kt
@@ -11,8 +11,14 @@ import software.amazon.smithy.codegen.core.CodegenException
 import software.amazon.smithy.model.node.Node
 import software.amazon.smithy.model.node.ObjectNode
 import software.amazon.smithy.model.shapes.ShapeId
+import software.amazon.smithy.rust.codegen.core.rustlang.rustTemplate
+import software.amazon.smithy.rust.codegen.core.rustlang.writable
+import software.amazon.smithy.rust.codegen.core.smithy.HttpVersion
+import software.amazon.smithy.rust.codegen.core.smithy.RuntimeType
 import software.amazon.smithy.rust.codegen.core.testutil.IntegrationTestParams
 import software.amazon.smithy.rust.codegen.core.testutil.asSmithyModel
+import software.amazon.smithy.rust.codegen.core.testutil.testModule
+import software.amazon.smithy.rust.codegen.core.testutil.tokioTest
 import software.amazon.smithy.rust.codegen.server.smithy.testutil.serverIntegrationTest
 
 internal class RequestBodyReadTimeoutTest {
@@ -63,6 +69,118 @@ internal class RequestBodyReadTimeoutTest {
             model,
             IntegrationTestParams(additionalSettings = disabledReadTimeoutSettings()),
         )
+    }
+
+    @Test
+    fun `slow request body returns request timeout over http`() {
+        serverIntegrationTest(
+            model,
+            IntegrationTestParams(additionalSettings = wireReadTimeoutSettings()),
+        ) { codegenContext, rustCrate ->
+            val startServer =
+                writable {
+                    when (codegenContext.runtimeConfig.httpVersion) {
+                        HttpVersion.Http0x ->
+                            rustTemplate(
+                                """
+                                let std_listener = listener.into_std().expect("failed to convert listener");
+                                let server = #{Tokio}::spawn(async move {
+                                    #{Hyper}::Server::from_tcp(std_listener)
+                                        .expect("failed to create server")
+                                        .serve(app.into_make_service())
+                                        .await
+                                        .expect("server failed");
+                                });
+                                """,
+                                "Hyper" to ServerCargoDependency.hyperDev(codegenContext.runtimeConfig).toType(),
+                                "Tokio" to RuntimeType.Tokio,
+                            )
+
+                        HttpVersion.Http1x ->
+                            rustTemplate(
+                                """
+                                let server = #{Tokio}::spawn(async move {
+                                    crate::serve(listener, app.into_make_service())
+                                        .configure_hyper(|builder| builder.http1_only())
+                                        .await
+                                        .expect("server failed");
+                                });
+                                """,
+                                "Tokio" to RuntimeType.Tokio,
+                            )
+                    }
+                }
+
+            rustCrate.testModule {
+                rustTemplate(
+                    """
+                    async fn echo(
+                        input: crate::input::EchoInput,
+                    ) -> Result<crate::output::EchoOutput, crate::error::EchoError> {
+                        Ok(crate::output::EchoOutput {
+                            message: input.message,
+                        })
+                    }
+                    """,
+                )
+
+                tokioTest("slow_request_body_returns_request_timeout_over_http") {
+                    rustTemplate(
+                        """
+                        use #{Tokio}::io::{AsyncReadExt, AsyncWriteExt};
+
+                        let config = crate::TestServiceConfig::builder().build();
+                        let app = crate::TestService::builder(config)
+                            .echo(echo)
+                            .build_unchecked();
+
+                        let listener = #{Tokio}::net::TcpListener::bind("127.0.0.1:0")
+                            .await
+                            .expect("failed to bind listener");
+                        let addr = listener.local_addr().expect("failed to get local address");
+                        #{StartServer:W}
+
+                        let mut stream = #{Tokio}::net::TcpStream::connect(addr)
+                            .await
+                            .expect("failed to connect to server");
+                        stream
+                            .write_all(
+                                b"POST /echo HTTP/1.1\r\n\
+                                  Host: localhost\r\n\
+                                  Content-Type: application/json\r\n\
+                                  Content-Length: 100\r\n\
+                                  \r\n\
+                                  {\"message\"",
+                            )
+                            .await
+                            .expect("failed to write partial request");
+
+                        let mut response = Vec::new();
+                        #{Tokio}::time::timeout(
+                            std::time::Duration::from_secs(2),
+                            stream.read_to_end(&mut response),
+                        )
+                        .await
+                        .expect("timed out waiting for response")
+                        .expect("failed to read response");
+                        server.abort();
+
+                        let response = String::from_utf8_lossy(&response);
+                        assert!(
+                            response.starts_with("HTTP/1.1 408 Request Timeout"),
+                            "unexpected response: {response:?}",
+                        );
+                        assert!(
+                            response.to_ascii_lowercase().contains("\r\nconnection: close\r\n"),
+                            "response missing connection close: {response:?}",
+                        );
+                        """,
+                        "StartServer" to startServer,
+                        "Tokio" to RuntimeType.Tokio,
+                    )
+                }
+            }
+        }
     }
 
     @Test
@@ -243,6 +361,19 @@ internal class RequestBodyReadTimeoutTest {
                 "customizationConfig": {
                     "readTimeouts": {
                         "defaultMillis": 0
+                    }
+                }
+            }
+            """,
+        )
+
+    private fun wireReadTimeoutSettings(): ObjectNode =
+        objectNode(
+            """
+            {
+                "customizationConfig": {
+                    "readTimeouts": {
+                        "defaultMillis": 100
                     }
                 }
             }
